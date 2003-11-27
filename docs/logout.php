fc472e7fbccf1afeb5490a40abcf95ace25d4cdb
joel kronenberg <joel.kronenberg@utoronto.ca>
2003-11-27 16:23:58 +0000

    bug fix: http://atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000210

--- docs/logout.php
+++ docs/logout.php
@@ -17,15 +17,15 @@ $_include_path = 'include/';
 require ('include/vitals.inc.php');
 
 $sql = "DELETE FROM ".TABLE_PREFIX."users_online WHERE member_id=$_SESSION[member_id]";
-$result = @mysql_query($sql, $db);
+@mysql_query($sql, $db);
 
 session_destroy(); 
 session_unset();
 
-header('Location:login.php?f='.AT_FEEDBACK_LOGOUT);
 
-//require($_include_path.'basic_html/header.php');
-//echo _AT('logged_out');
-//require($_include_path.'basic_html/footer.php'); 
+require($_include_path.'basic_html/header.php');
+print_feedback(AT_FEEDBACK_LOGOUT);
+echo _AT('logged_out');
+require($_include_path.'basic_html/footer.php'); 
 
 ?>
