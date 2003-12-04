01bcf1585a5d9d1771aa62f30ccfe30938421e90
joel kronenberg <joel.kronenberg@utoronto.ca>
2003-12-04 19:18:16 +0000

    no message

--- docs/admin/course_categories.php
+++ docs/admin/course_categories.php
@@ -17,65 +17,103 @@ require(AT_INCLUDE_PATH.'vitals.inc.php');
 
 if (!$_SESSION['s_is_super_admin']) { exit; }
 
-if($_POST['cancel']){
-	Header('Location: '.$_SERVER['PHP_SELF'].'?current_cat='.$_POST['category_parent'].SEP.'f='.urlencode_feedback(AT_FEEDBACK_CANCELLED));
-	exit;
+function print_categories($categories, $cat_id) {
+	if ($cat_id == 0) {
+		echo '<ul>';
+		foreach($categories[0] as $child_cat_id) {
+			print_categories($categories, $child_cat_id);
+		}
+		echo '</ul>';
+	} else {
+		echo '<li>';
+		if ($cat_id == $_REQUEST['cat_id']) {
+			echo '<strong>'.$categories[$cat_id]['cat_name'].'</strong>';
+		} else {
+			echo '<a href="'.$_SERVER['PHP_SELF'].'?cat_id='.$cat_id.'">'.$categories[$cat_id]['cat_name'].'</a>';
+		}
+		echo ' <small class="spacer">('.$categories[$cat_id]['num_courses'].' ';
+		echo ($categories[$cat_id]['num_courses']==1) ? _AT('course') : _AT('courses').')</small>';
+		if (is_array($categories[$cat_id]['children'])) {
+			echo '<ul>';
+			foreach($categories[$cat_id]['children'] as $child_cat_id) {
+				print_categories($categories, $child_cat_id);
+			}
+			echo '</ul>';
+		}
+		echo '</li>';
+	}
 }
 
-$sql = "SELECT * FROM ".TABLE_PREFIX."course_cats";
-$result = mysql_query($sql);
-if(mysql_num_rows($result) != 0){
-	while($row = mysql_fetch_assoc($result)){
-		$categories[$row['cat_id']]['cat_name']   = $row['cat_name'];
-		$categories[$row['cat_id']]['cat_parent'] = $row['cat_parent'];
+function select_categories($categories, $cat_id, $current_cat_id, $depth=0) {
+	if ($cat_id == 0) {
+		foreach($categories[0] as $child_cat_id) {
+			select_categories($categories, $child_cat_id, $current_cat_id);
+		}
+	} else {
+		echo '<option value="'.$cat_id.'"';
+		if ($cat_id == $current_cat_id) {
+			echo ' selected="selected"';
+		}
+		echo '>';
+		echo str_repeat("&nbsp", $depth*3);
+		echo $categories[$cat_id]['cat_name'].'</option>';
 
-		if ($row['cat_parent'] >0) {
-			$categories[$row['cat_parent']]['children'][] = $row['cat_id'];
+		if (is_array($categories[$cat_id]['children'])) {
+			foreach($categories[$cat_id]['children'] as $child_cat_id) {
+				select_categories($categories, $child_cat_id, $current_cat_id, $depth+1);
+			}
 		}
 	}
 }
 
+/* get all the categories: */
+/* $categories[category_id] = array(cat_name, cat_parent, num_courses, [array(children)]) */
+$sql = "SELECT * FROM ".TABLE_PREFIX."course_cats ORDER BY cat_parent, cat_name";
+$result = mysql_query($sql, $db);
+while ($row = mysql_fetch_assoc($result)) {
+	$categories[$row['cat_id']]['cat_name']    = $row['cat_name'];
+	$categories[$row['cat_id']]['cat_parent']  = $row['cat_parent'];
+	$categories[$row['cat_id']]['num_courses'] = 0;
+
+	if ($row['cat_parent'] >0) {
+		$categories[$row['cat_parent']]['children'][] = $row['cat_id'];
+	} else {
+		$categories[0][] = $row['cat_id'];
+	}
+}
+
+/* get the number of courses in each category */
+/* special case: uncategorized courses get stored in $num_uncategorized */
+$sql = "SELECT cat_id, COUNT(*) AS cnt FROM ".TABLE_PREFIX."courses GROUP BY cat_id";
+$result = mysql_query($sql, $db);
+while ($row = mysql_fetch_assoc($result)) {
+	if ($row['cat_id'] == 0) {
+		$num_uncategorized = $row['cnt'];
+	} else {
+		$categories[$row['cat_id']]['num_courses'] = $row['cnt'];
+	}
+}
+
+if (isset($_GET['cat_id'])) {
+	$cat_id = intval($_GET['cat_id']);
+}
+
 require(AT_INCLUDE_PATH.'admin_html/header.inc.php');
-echo '<h2>'._AT('cats_course_categories').'</h2><br />';
+echo '<h2>'._AT('cats_course_categories').'</h2>';
 ?>
-<table cellspacing="0" cellpadding="0" border="0" class="bodyline" summary="" align="center" width="100%">
-	<tr><th style="border-right:1pt solid gray;"><?php echo _AT('cats_category_name').' '.$current_cats[$_GET['current_cat']]; ?></th>
-		<th><?php echo _AT('cats_edit_categories').' '.$current_cats[$_GET['current_cat']]; ?></th>
-	</tr>
-	<tr><td style="border-right:1pt solid gray;" width="40%">
-
-	<!---replace with function -->
-	<ul><li><a href="/browse.php?current_cat=0;show_courses=0"><strong>Uncategorized</strong></a> <small>( <a href="/browse.php?current_cat=0;this_category=0;show_courses=0#browse_top">11</a> )</small></li>
-	<li><a href="/browse.php?current_cat=110;show_courses=110">Anthropology</a> <small>( 0 )</small><ul><li><a href="/browse.php?current_cat=127;show_courses=127">Science</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=126;show_courses=126">Social Science</a> <small>( 0 )</small></li>
-	</ul></li>
-	<li><a href="/browse.php?current_cat=111;show_courses=111">Botany</a> <small>( 0 )</small><ul><li><a href="/browse.php?current_cat=128;show_courses=128">Molecular Plant Biology</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=129;show_courses=129">Plant Physiology</a> <small>( 0 )</small></li>
-	</ul></li>
-	<li><a href="/browse.php?current_cat=112;show_courses=112">Chemistry</a> <small>( 0 )</small><ul><li><a href="/browse.php?current_cat=130;show_courses=130">Biological Chemistry</a> <small>( <a href="/browse.php?current_cat=130;this_category=130;show_courses=130#browse_top">1</a> )</small></li>
-	<li><a href="/browse.php?current_cat=131;show_courses=131">Chemical Physics</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=132;show_courses=132">Environmental Chemistry</a> <small>( 0 )</small></li>
-	</ul></li>
-	<li><a href="/browse.php?current_cat=121;show_courses=121">Computer Science</a> <small>( <a href="/browse.php?current_cat=121;this_category=121;show_courses=121#browse_top">1</a> )</small><ul><li><a href="/browse.php?current_cat=133;show_courses=133">Information Systems</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=134;show_courses=134">Software Engineering</a> <small>( 0 )</small></li>
-	</ul></li>
-	<li><a href="/browse.php?current_cat=114;show_courses=114">Economics</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=115;show_courses=115">Geology</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=116;show_courses=116">History</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=117;show_courses=117">Linguistics</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=118;show_courses=118">Mathematics</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=119;show_courses=119">Philosophy</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=120;show_courses=120">Physics</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=122;show_courses=122">Psychology</a> <small>( <a href="/browse.php?current_cat=122;this_category=122;show_courses=122#browse_top">2</a> )</small></li>
-	<li><a href="/browse.php?current_cat=123;show_courses=123">Sociology</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=136;show_courses=136">www</a> <small>( 0 )</small></li>
-	<li><a href="/browse.php?current_cat=125;show_courses=125">Zoology</a> <small>( 0 )</small></li>
-	</ul>
-	<!---end replace function -->
-		</td>
-		<td colspan="2"><?php include(AT_INCLUDE_PATH.'html/cat_editor.inc.php'); ?></td>
-	</tr>
-
+<p><?php echo $num_uncategorized; ?> uncategorized course(s) not listed.</p>
+<table cellspacing="0" cellpadding="0" border="0" summary="" align="center" width="100%">
+<tr>
+	<td style="border-right:1pt solid gray;" width="40%"><?php
+			print_categories($categories, 0);
+	?></td>
+	<td><?php 
+		if (isset($cat_id)) {
+			require(AT_INCLUDE_PATH.'html/cat_editor.inc.php');
+		} else {
+			echo _AT('select_category_to_edit');	
+		}?></td>
+</tr>
 </table>
 
 <?php
--- docs/include/html/cat_editor.inc.php
+++ docs/include/html/cat_editor.inc.php
@@ -14,35 +14,34 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 
 ?>
 <form action ="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" name="form">
-	<input type="hidden" name="cat_id" value="<?php echo $_GET['current_cat'] ?>" />
-	<input type="hidden" name="parent_cat" value="<?php echo $_GET['parent_cat']; ?>" />
-	<table cellspacing="1" cellpadding="0" border="0" class="bodyline" summary="" align="center">
-	<tr><th colspan="2"><?php echo _AT('cats_edit_categories').' '.$current_cats[$_GET['current_cat']]; ?></th></tr>
-	<tr><td height="1" class="row2" colspan="2"></td></tr>
-	<tr><td class="row1"><label for="category_name"><?php echo _AT('cats_category_name'); ?></label>:</td><td class="row1"><input type="text" id="category_name" name="category_name" value="<?php echo $current_cats[$_GET['current_cat']]; ?>"  class="formfield"/></td></tr>
-	<tr><td height="1" class="row2" colspan="2"></td></tr>
-	<tr><td class="row1"><label for="category_parent"><?php echo _AT('cats_parent_category'); ?></label>:</td><td class="row1"><select name="category_parent"  id="category_parent" class="formfield">
-	<?php
-		$sql = "SELECT * FROM ".TABLE_PREFIX."course_cats";
-		$result = mysql_query($sql);
+<table cellspacing="1" cellpadding="0" border="0" class="bodyline" summary="" align="center">
+<tr>
+	<th colspan="2"><?php echo _AT('cats_edit_categories').' '.$current_cats[$_GET['current_cat']]; ?></th>
+</tr>
+<tr>
+	<td height="1" class="row2" colspan="2"></td>
+</tr>
+<tr>
+	<td class="row1"><label for="category_name"><?php echo _AT('cats_category_name'); ?></label>:</td>
+	<td class="row1"><input type="text" id="category_name" name="category_name" value="<?php echo $categories[$cat_id]['cat_name']; ?>" class="formfield" /></td>
+</tr>
+<tr>
+	<td height="1" class="row2" colspan="2"></td>
+</tr>
+<tr>
+	<td class="row1"><label for="category_parent"><?php echo _AT('cats_parent_category'); ?></label>:</td>
+	<td class="row1"><select name="category_parent"  id="category_parent"><?php
 
-		$cats = array();
-
-		while($row = mysql_fetch_assoc($result)){
-			$cats[$row['cat_parent']][] = $row;
-		}
-
-		$cat_row = $parent_cats[$_GET['current_cat']];
-		echo '<option value="0"> - '._AT('cats_none').' - </option>';
-		unset($cats[$_GET['current_cat']]);
-		print_course_cats(0, $cats, $cat_row);
-	?>
-	</select>
-	</td></tr>
-	<tr><td height="1" class="row2" colspan="2"></td></tr>
-	<tr><td colspan="2" class="row1" align="right">
-	<input type="submit" name="submit" value="<?php echo _AT('cats_edit_categories'); ?>" class="button" accesskey="s" />
-	<input type="submit" name="cancel" value="<?php echo _AT('cancel'); ?>" class="button" />
-	</td></tr>
-	</table>
-	</form>
+				echo '<option value="0"> - '._AT('cats_none').' - </option>';
+				select_categories($categories, 0, $categories[$cat_id]['cat_parent']);
+			?></select></td>
+</tr>
+<tr>
+	<td height="1" class="row2" colspan="2"></td>
+</tr>
+<tr>
+	<td colspan="2" class="row1" align="right"><input type="submit" name="submit" value=" <?php echo _AT('edit'); ?> " class="button" accesskey="s" />
+	<input type="submit" name="cancel" value="<?php echo _AT('cancel'); ?>" class="button" /></td>
+</tr>
+</table>
+</form>
