d0e04b09d1518da1214fdca30b6784350ff515e2
heidi valles <hvalles@ocad.ca>
2003-12-08 17:52:19 +0000

    added new warning

--- docs/include/lib/lang_constants.inc.php
+++ docs/include/lib/lang_constants.inc.php
@@ -294,6 +294,7 @@ define('AT_WARNING_CHAT_TRAN_EXISTS', $i); $i++;
 define('AT_WARNING_DELETE_LANG', $i); $i++;
 define('AT_WARNING_LANG_EXISTS', $i); $i++;
 define('AT_WARNING_DELETE_CAT_CATEGORY', $i); $i++;
+define('AT_WARNING_LOGIN_INSTRUCTOR', $i); $i++;
 //////////////////////////////////////
 // FEEDBACK DEFINITIONS
 define('AT_FEEDBACK_SUCCESS', $i); $i++;
