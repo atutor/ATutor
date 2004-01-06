fd1c8dae31f2240eb8126fea6486a0dd8df523c8
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-01-06 21:48:28 +0000

    preview when editing content

--- docs/include/html/tabs/preview.inc.php
+++ docs/include/html/tabs/preview.inc.php
@@ -1,8 +1,14 @@
 	<tr>
-		<td colspan="2" valign="top" align="left" class="row1"><?php if ($_POST['text']) {
+		<td colspan="2" valign="top" align="left" class="row1"><?php 
+		
+		if ($_POST['text']) {
 			echo format_content($_POST['text'], $_POST['formatting']);
-		} else {
+		} else if ($row['text']) {
 			echo format_content($row['text'], $row['formatting']);
+		} else { 
+			$infos[] = AT_INFOS_NO_PAGE_CONTENT;
+			print_infos($infos);
+	
 		} ?>
 		</td>
 	</tr>
