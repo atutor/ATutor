ead83decacb736d2b77ed072d571447abf977fc0
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-01-06 21:33:59 +0000

    <select>'s for language when more than 5 are available.

--- docs/include/html/languages.inc.php
+++ docs/include/html/languages.inc.php
@@ -24,7 +24,6 @@ if(count($available_languages) < 2){
 		echo '<form method="get" action="'.$_my_uri.'">';
 		echo '<label for="lang">'._AT('translate_to').'</label> <select name="lang" id="lang">';
 		foreach ($available_languages as $temp_key => $val) {
-
 			echo '<option value="'.$temp_key.'"';
 			if ($temp_key == $_SESSION['lang']) {
 				echo ' selected="selected"';
@@ -34,6 +33,7 @@ if(count($available_languages) < 2){
 			echo $val[3].'</option>';
 		}
 		echo '</select>';
+		echo '<input type="submit" name="submit" class="button" value="'._AT('submit').'" />';
 		echo '</form>';
 	} else {
 		echo _AT('translate_to');
