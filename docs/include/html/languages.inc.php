92f7e67f79fcd58af2c6c2c110328453ffe636c8
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-01-06 20:48:50 +0000

    if # langs > 5, use a <select> instead

--- docs/include/html/languages.inc.php
+++ docs/include/html/languages.inc.php
@@ -20,21 +20,31 @@ if(count($available_languages) < 2){
 
 <div align="center" class="hide"><small><?php
 
-	echo _AT('translate_to');
-	echo ' ';
+	if (count($available_languages) > 5) {
+		echo '<form method="get" action="'.$_my_uri.'">';
+		echo '<label for="lang">'._AT('translate_to').'</label> <select name="lang" id="lang">';
+		foreach ($available_languages as $temp_key => $val) {
 
-	foreach ($available_languages as $temp_key => $val) {
-		$border = 0;
-		if ($temp_key == $_SESSION['lang']) {
-			$border = 2;
+			echo '<option value="'.$temp_key.'"';
+			if ($temp_key == $_SESSION['lang']) {
+				echo ' selected="selected"';
+			}
+			echo '>';
+			
+			echo $val[3].'</option>';
 		}
-		
-		//echo '<a href="'.$_my_uri.'lang='.$temp_key.'"><img src="images/flags/'.$temp_key.'.gif" alt="'.$val[3].'" height="16" width="24" border="'.$border.'" class="menuimage" /></a>';
-		if(!$l){
-			echo '<a href="'.$_my_uri.'lang='.$temp_key.'">'.$val[3].'</a> ';
-		}else{
-			echo '| <a href="'.$_my_uri.'lang='.$temp_key.'">'.$val[3].'</a> ';
+		echo '</select>';
+		echo '</form>';
+	} else {
+		echo _AT('translate_to');
+		echo ' ';
+		foreach ($available_languages as $temp_key => $val) {
+			if(!$l){
+				echo '<a href="'.$_my_uri.'lang='.$temp_key.'">'.$val[3].'</a> ';
+			}else{
+				echo '| <a href="'.$_my_uri.'lang='.$temp_key.'">'.$val[3].'</a> ';
+			}
+			$l++;
 		}
-		$l++;
 	}
 ?></small></div>
