fe2b7f04eb213e60b916670144b25e2598e8ce54
heidi valles <hvalles@ocad.ca>
2004-02-03 21:07:08 +0000

    no message

--- docs/users/view_profile.php
+++ docs/users/view_profile.php
@@ -16,11 +16,12 @@ define('AT_INCLUDE_PATH', '../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
 //require (AT_INCLUDE_PATH.'lib/atutor_mail.inc.php');
 
-$sql = "SELECT * from ".TABLE_PREFIX."members where member_id='$_GET[mid]'";
-$result = mysql_query($sql,$db);
 
 $title = _AT('course_enrolment'); 
 require(AT_INCLUDE_PATH.'cc_html/header.inc.php');
+
+$sql = "SELECT * from ".TABLE_PREFIX."members where member_id='".$_GET['mid']."'";
+$result = mysql_query($sql,$db);
 
 ?>
 	<table cellspacing="1" cellpadding="0" border="0" class="bodyline" summary="">
@@ -28,15 +29,15 @@ require(AT_INCLUDE_PATH.'cc_html/header.inc.php');
 		<th colspan="2" align="left" class="cyan"><?php echo _AT('profile_student_information');  ?></th>
 	</tr>
 <?php 
-if ($row=mysql_fetch_array($result)){
 
-	echo '<tr><td class="row1" align="right"><strong>'._AT('login').':</strong></td><td class="row1">'.$row['login'].'</td></tr>';
+if ($row=mysql_fetch_array($result)){
+	echo '<tr><td class="row1" align="right"><strong>'._AT('login').':</strong></td><td class="row1">'.AT_print($row['login'],'members.login').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-	echo '<tr><td class="row1" align="right"><strong>'._AT('first_name').':</strong></td><td class="row1">&nbsp;'.$row['first_name'].'</td></tr>';
+	echo '<tr><td class="row1" align="right"><strong>'._AT('first_name').':</strong></td><td class="row1">&nbsp;'.AT_print($row['first_name'],'members.first_name').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-	echo '<tr><td class="row1" align="right"><strong>'._AT('last_name').':</strong></td><td class="row1">&nbsp;'.$row['last_name'].'</td></tr>';
+	echo '<tr><td class="row1" align="right"><strong>'._AT('last_name').':</strong></td><td class="row1">&nbsp;'.AT_print($row['last_name'],'members.last_name').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-	echo '<tr><td class="row1" align="right"><strong>'._AT('email').':</strong></td><td class="row1">'.$row['email'].'</td></tr>';
+	echo '<tr><td class="row1" align="right"><strong>'._AT('email').':</strong></td><td class="row1"><a href="mailto:'.AT_print($row['email'],'members.email').'">'.$row['email'].'</a></td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
 	echo '<tr><td class="row1" align="right"><strong>'._AT('status').':</strong></td><td class="row1">';
 	if ($row['status'] == 0) {
@@ -46,25 +47,25 @@ if ($row=mysql_fetch_array($result)){
 	}
 	echo '</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('age').':</strong></td><td class="row1">'.$row['age'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('age').':</strong></td><td class="row1">'.AT_print($row['age'],'members.age').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('gender').':</strong></td><td class="row1">'.$row['gender'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('gender').':</strong></td><td class="row1">'.AT_print($row['gender'],'members.gender').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('street_address').':</strong></td><td class="row1">'.$row['address'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('street_address').':</strong></td><td class="row1">'.AT_print($row['address'],'members.address').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('city').':</strong></td><td class="row1">'.$row['city'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('city').':</strong></td><td class="row1">'.AT_print($row['city'],'members.city').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('province').':</strong></td><td class="row1">'.$row['province'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('province').':</strong></td><td class="row1">'.AT_print($row['province'],'members.province').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('postal_code').':</strong></td><td class="row1">'.$row['postal'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('postal_code').':</strong></td><td class="row1">'.AT_print($row['postal'],'members.postal').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('country').':</strong></td><td class="row1">'.$row['country'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('country').':</strong></td><td class="row1">'.AT_print($row['country'],'members.country').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('web_site').':</strong></td><td class="row1">'.$row['website'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('web_site').':</strong></td><td class="row1">'.AT_print($row['website'],'members.website').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('phone').':</strong></td><td class="row1">'.$row['phone'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('phone').':</strong></td><td class="row1">'.AT_print($row['phone'],'members.phone').'</td></tr>';
 	echo '<tr><td height="1" class="row2" colspan="2"></td></tr>';
-		echo '<tr><td class="row1" align="right"><strong>'._AT('date_created').':</strong></td><td class="row1">'.$row['creation_date'].'</td></tr>';
+		echo '<tr><td class="row1" align="right"><strong>'._AT('date_created').':</strong></td><td class="row1">'.AT_print($row['creation_date'],'members.creation_date').'</td></tr>';
 ?>
 	</table>
 
