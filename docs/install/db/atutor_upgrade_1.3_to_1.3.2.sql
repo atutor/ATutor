e8643797d096d228721a6784725c70154ac0947b
heidi valles <hvalles@ocad.ca>
2004-02-12 19:33:42 +0000

    no message

--- docs/install/db/atutor_upgrade_1.3_to_1.3.2.sql
+++ docs/install/db/atutor_upgrade_1.3_to_1.3.2.sql
@@ -5,3 +5,8 @@
 # add new field to course_enrollment table
 
 ALTER TABLE `course_enrollment` ADD `last_cid` MEDIUMINT UNSIGNED DEFAULT '0' NOT NULL;
+
+
+# add new field to content table
+
+ALTER TABLE `content` ADD ` inherit_release_date` TINYINT UNSIGNED DEFAULT '1' NOT NULL;
