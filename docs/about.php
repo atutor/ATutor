6705855dce7984e0a5785e20b33392670b45ca83
heidi valles <hvalles@ocad.ca>
2004-02-13 19:10:53 +0000

    no message

--- docs/about.php
+++ docs/about.php
@@ -1,15 +1,15 @@
 <?php
-/****************************************************************/
-/* ATutor														*/
-/****************************************************************/
-/* Copyright (c) 2002-2004 by Greg Gay & Joel Kronenberg        */
-/* Adaptive Technology Resource Centre / University of Toronto  */
-/* http://atutor.ca												*/
-/*                                                              */
-/* This program is free software. You can redistribute it and/or*/
-/* modify it under the terms of the GNU General Public License  */
-/* as published by the Free Software Foundation.				*/
-/****************************************************************/
+/************************************************************************/
+/* ATutor																*/
+/************************************************************************/
+/* Copyright (c) 2002-2004 by Greg Gay, Joel Kronenberg & Heidi Hazelton*/
+/* Adaptive Technology Resource Centre / University of Toronto			*/
+/* http://atutor.ca														*/
+/*																		*/
+/* This program is free software. You can redistribute it and/or		*/
+/* modify it under the terms of the GNU General Public License			*/
+/* as published by the Free Software Foundation.						*/
+/************************************************************************/
 
 $section = 'users';
 $page    = 'about';
@@ -27,14 +27,29 @@ unset($_SESSION['is_guest']);
 
 ?>
 <h3><?php echo _AT('about_atutor'); ?></h3><br />
-<h5><?php echo _AT('welcome_to_atutor'); ?></h5>
-<p><?php echo _AT('atutor_is');  ?></p>
 
-<h5><?php echo _AT('acquiring_atutor'); ?></h5>
-<p><?php echo _AT('atutor_available'); ?></p>
+	<table border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td valign="top"><img src="images/ss.gif" height="198" width="254" alt="ATutor screen shot"/></td>
+		<td><img src="images/clr.gif" height="1" width="1" alt="" />			
+		<p><strong><em>ATutor</em></strong> is an Open Source Web-based <strong>Learning Content Management System (LCMS)</strong> designed with accessibility and adaptability in mind. Administrators can install or update ATutor in minutes. Educators can quickly assemble, package, and redistribute Web-based instructional content, and conduct their courses online. Students learn in an adaptive learning environment.</p>
+		<p>Learn more about <a href="http://atutor.ca/index.php">ATutor</a> by browsing the following links:
 
-<h5><?php echo _AT('more_information'); ?></h5>
-<p><?php echo _AT('find_latest'); ?></p><br />
+		<ul>
+		<li><a href="http://atutor.ca/atutor/docs/howto.php">ATutor HowTo Course</a> - Learn how to use ATutor in this instructional course</li>
+		<li><a href="http://atutor.ca/atutor/docs/index.php">FAQs</a> - Frequently asked questions and answers</li>
+		<li><a href="http://atutor.ca/forums/index.php">Support Forums</a> - Post to the user forums</li>
+		<li><a href="http://atutor.ca/services/index.php">Support Services</a> - If you need our help, support is available</li>		
+		<li><a href="http://atutor.ca/atutor/translate/index.php">Translation</a> - Download language packs, become a translator</li>
+
+
+		<li><a href="http://atutor.ca/services/licensing.php">Licensing</a> - ATutor software is available for free under certain terms</li>
+		<li><a href="http://atutor.ca/atutor/download.php">Download ATutor</a> - All of the system software required to run ATutor can be downloaded here</li>
+
+		</ul></p><br /><br />
+		</td>
+	</tr>
+	</table>
 
 <?php
 	require (AT_INCLUDE_PATH.'basic_html/footer.php'); 
