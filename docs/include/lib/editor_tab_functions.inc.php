8c3a4f11685edb8300bc141462698c43355df3a4
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-02-26 17:18:09 +0000

    http://www.atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000466

--- docs/include/lib/editor_tab_functions.inc.php
+++ docs/include/lib/editor_tab_functions.inc.php
@@ -241,7 +241,7 @@ function check_for_changes($row) {
 
 			/* check if added terms have changed */
 			foreach ($_POST['glossary_defs'] as $w => $d) {
-				if ($d != $glossary[$w]) {
+				if (stripslashes($d) != $glossary[$w]) {
 					/* an existing term has been changed */
 					$changes[2] = true;
 					break;
