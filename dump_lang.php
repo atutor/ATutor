72f1fc037fce397fe42f4c269833eb246684c5dd
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-03-10 16:04:54 +0000

    no message

---
+++ dump_lang.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+define('AT_INCLUDE_PATH', true);
+
+require('cvs_development.inc.php');
+
+$sql	= 'SELECT `variable`,`key`,`text`,`context` FROM lang_base L WHERE project=`atutor` ORDER BY `variable`, `key`';
+$result = mysql_query($sql, $lang_db);
+
+echo mysql_error();
+ob_start();
+while($row = mysql_fetch_assoc($result)) {
+	$row['text'] = mysql_real_escape_string($row['text']);
+	$row['context'] = mysql_real_escape_string($row['context']);
+	echo "INSERT INTO `lang_base` VALUES ('$row[variable]','$row[key]','$row[text]',NOW(),'$row[context]');\n";
+}
+ob_flush();
+
+?>
