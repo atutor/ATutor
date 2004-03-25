30d4db9ebb0690992273363f2e2cb61f882b6740
heidi valles <hvalles@ocad.ca>
2004-03-25 19:29:47 +0000

    no message

--- docs/resources/index.php
+++ docs/resources/index.php
@@ -46,11 +46,11 @@ echo '</h2>';
 <tr>
 	<?php 
 			if ($_SESSION['prefs'][PREF_CONTENT_ICONS] != 2) {
-				echo '<td rowspan="2" valign="top"><a href="resources/tile_search.php?g=29"><img src="images/icons/default/links-small.gif" class="menuimage"width="28" height="25" border="0" alt="*" /></a></td>';
+				echo '<td rowspan="2" valign="top"><a href="resources/tile/tile_search.php?g=29"><img src="images/icons/default/links-small.gif" class="menuimage"width="28" height="25" border="0" alt="*" /></a></td>';
 			}
 			echo '<td>';
 			if ($_SESSION['prefs'][PREF_CONTENT_ICONS] != 1) {
-				echo ' <a href="resources/tile_search.php?g=29">'._AT('tile_search').'</a>';
+				echo ' <a href="resources/tile/tile_search.php?g=29">'._AT('tile_search').'</a>';
 			}
 			echo '</td></tr><tr><td>';
 			echo _AT('tile_search_text');
