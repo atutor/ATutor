f6884347e65594389660fea1a3b7255aafc74f42
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-04-22 19:07:09 +0000

    no message

--- docs/templates/include/course_footer.tmpl.php
+++ docs/templates/include/course_footer.tmpl.php
@@ -10,7 +10,8 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
-// $Id: course_footer.tmpl.php,v 1.5 2004/04/22 18:45:19 heidi Exp $
+// $Id: course_footer.tmpl.php,v 1.6 2004/04/22 19:07:09 joel Exp $
+
 
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 	echo $tmpl_next_prev_links; ?>
@@ -20,7 +21,7 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 		<?php echo $tmpl_help_link; ?>
 
 		<?php if ($tmpl_show_imgs): ?>
-			<a href="<?php echo $tmpl_my_uri; ?>g=6#content" title="<?php _AT('back_to_top'); ?> ALT-c"><img src="<?php $tmpl_base_path; ?>images/top.gif" alt="<?php _AT('back_to_top'); ?>" border="0" class="menuimage4" height="25" width="28"  /></a><br />
+			<a href="<?php echo $tmpl_my_uri; ?>g=6#content" title="<?php _AT('back_to_top'); ?> ALT-c"><img src="<?php echo $tmpl_base_path; ?>images/top.gif" alt="<?php _AT('back_to_top'); ?>" border="0" class="menuimage4" height="25" width="28"  /></a><br />
 		<?php endif; ?>
 		<?php if ($tmpl_show_seq_icons): ?>
 			<a href="<?php echo $tmpl_my_uri; ?>g=6#content" title="<?php _AT('back_to_top'); ?> ALT-c"><?php echo _AT('top'); ?></a>
