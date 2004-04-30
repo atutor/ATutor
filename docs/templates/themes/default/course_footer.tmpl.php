6f0fbef24d35fb21a6e9a28d36603a0204d58441
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-04-30 14:03:55 +0000

    http://www.atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000135

--- docs/templates/themes/default/course_footer.tmpl.php
+++ docs/templates/themes/default/course_footer.tmpl.php
@@ -10,7 +10,7 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
-// $Id: course_footer.tmpl.php,v 1.12 2004/04/29 19:54:23 heidi Exp $
+// $Id: course_footer.tmpl.php,v 1.13 2004/04/30 14:03:55 joel Exp $
 
 
 if (!defined('AT_INCLUDE_PATH')) { exit; }
@@ -21,10 +21,10 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 		<?php echo $tmpl_help_link; ?>
 
 		<?php if ($tmpl_show_imgs): ?>
-			<a href="<?php echo $tmpl_my_uri; ?>g=6#content" title="<?php _AT('back_to_top'); ?> ALT-c"><img src="<?php echo $tmpl_base_path; ?>images/top.gif" alt="<?php _AT('back_to_top'); ?>" border="0" class="menuimage4" height="25" width="28"  /></a><br />
+			<a href="<?php echo $_SERVER['REQUEST_URI']; ?>#course-content" title="<?php _AT('back_to_top'); ?> ALT-c"><img src="<?php echo $tmpl_base_path; ?>images/top.gif" alt="<?php _AT('back_to_top'); ?>" border="0" class="menuimage4" height="25" width="28"  /></a><br />
 		<?php endif; ?>
 		<?php if ($tmpl_show_seq_icons): ?>
-			<a href="<?php echo $tmpl_my_uri; ?>g=6#content" title="<?php _AT('back_to_top'); ?> ALT-c"><?php echo _AT('top'); ?></a>
+			<a href="<?php echo $_SERVER['REQUEST_URI']; ?>#course-content" title="<?php _AT('back_to_top'); ?> ALT-c"><?php echo _AT('top'); ?></a>
 		<?php endif; ?>
 		&nbsp;&nbsp;</small>
 	</div>
