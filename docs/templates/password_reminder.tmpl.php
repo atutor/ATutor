0d6498da1413bc4fde5a093e7b6a5c539ef91742
heidi valles <hvalles@ocad.ca>
2004-05-05 14:19:56 +0000

    no message

--- docs/templates/password_reminder.tmpl.php
+++ docs/templates/password_reminder.tmpl.php
@@ -9,17 +9,17 @@
 	<br />
 	<table cellspacing="1" cellpadding="0" border="0" align="center" width="60%" summary="">
 	<tr>
-		<td class="row1" align="left" colspan="2"><?php echo _AT('password_blurb'); ?><br /></td>
+		<td class="public-row" align="left" colspan="2"><?php echo _AT('password_blurb'); ?><br /></td>
 	</tr>
-	<tr><td height="1" class="row1" colspan="2"></td></tr>
+	<tr><td height="1" class="public-row" colspan="2"></td></tr>
 	<tr>
-		<td valign="top" align="right" class="row1"><label for="email"><strong><?php echo _AT('email_address'); ?>: </strong></label></td>
-		<td valign="top" align="left" class="row1"><input type="text" class="formfield" name="form_email" id="email" /><br /><br /></td>
+		<td valign="top" align="right" class="public-row"><label for="email"><strong><?php echo _AT('email_address'); ?>: </strong></label></td>
+		<td valign="top" align="left" class="public-row"><input type="text" class="formfield" name="form_email" id="email" /><br /><br /></td>
 	</tr>
-	<tr><td height="1" class="row1" colspan="2"></td></tr>
-	<tr><td height="1" class="row1" colspan="2"></td></tr>
+	<tr><td height="1" class="public-row" colspan="2"></td></tr>
+	<tr><td height="1" class="public-row" colspan="2"></td></tr>
 	<tr>
-		<td align="center" colspan="2" class="row1"><input type="submit" name="submit" class="button" value="<?php echo _AT('submit'); ?>" /> - <input type="submit" name="cancel" class="button" value=" <?php echo _AT('cancel'); ?> " /></td>
+		<td align="center" colspan="2" class="public-row"><input type="submit" name="submit" class="button" value="<?php echo _AT('submit'); ?>" /> - <input type="submit" name="cancel" class="button" value=" <?php echo _AT('cancel'); ?> " /></td>
 	</tr>
 	</table>
 </form>
