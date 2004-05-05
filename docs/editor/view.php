428f6295649de9eae2587019c07a8b0affc97db5
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-05-05 18:15:31 +0000

    *** empty log message ***

--- docs/editor/view.php
+++ docs/editor/view.php
@@ -10,12 +10,12 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
+// $Id: view.php,v 1.2 2004/05/05 18:15:31 joel Exp $
 
 	define('AT_INCLUDE_PATH', '../include/');
 	require(AT_INCLUDE_PATH.'vitals.inc.php');
 
 	$url = urldecode($_GET['url']);
-	debug($url);
 
 	@readfile($url);
 
