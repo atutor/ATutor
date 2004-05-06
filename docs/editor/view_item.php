4f31f9c3dc3b9b5b00f8c5fe0cde284aa42dcbf9
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-05-06 18:44:40 +0000

    renamed view.php to view_item.php

--- docs/editor/view_item.php
+++ docs/editor/view_item.php
@@ -10,7 +10,7 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
-// $Id: view.php,v 1.2 2004/05/05 18:15:31 joel Exp $
+// $Id: view_item.php,v 1.1 2004/05/06 18:44:21 joel Exp $
 
 	define('AT_INCLUDE_PATH', '../include/');
 	require(AT_INCLUDE_PATH.'vitals.inc.php');
