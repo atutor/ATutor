b63a9a75987682f58bfb9910cd0f734da014853d
boon hau <boonhau@gmail.com>
2004-05-14 21:14:42 +0000

    no message

--- docs/forum/delete_thread.php
+++ docs/forum/delete_thread.php
@@ -84,17 +84,10 @@ echo'<h3>';
 if ($_SESSION['prefs'][PREF_CONTENT_ICONS] != 2) {
 	echo '<img src="images/icons/default/forum-large.gif" width="42" height="38" border="0" alt="" class="menuimageh3" />';
 }
-echo '<a href="forum/list.php">'._AT('forums').'</a>';
+echo '<a href="forum/list.php">'._AT('forums').'</a> - <a href="forum/index.php?fid='.$fid.SEP.'g=11">'.AT_print(get_forum($fid), 'forums.title').'</a>';
 
 echo '</h3>';
 
-echo'<h3>&nbsp;';
-if ($_SESSION['prefs'][PREF_CONTENT_ICONS] != 2) {
-		echo '<img src="images/icons/default/forum-large.gif" width="42" height="38" border="0" alt="" class="menuimageh3" />';
-}
-echo '<a href="forum/index.php?fid='.$fid.SEP.'g=11">'.AT_print(get_forum($fid), 'forums.title').'</a>';
-
-echo '</h3>';
 
 if($ppid=='' || $ppid =='0'){
 	$warnings[]=AT_WARNING_DELETE_THREAD;
