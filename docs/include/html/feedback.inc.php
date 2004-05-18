953b990958e134176ccac3121f1631b19c1e2355
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-05-18 18:56:43 +0000

    http://www.atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000664

--- docs/include/html/feedback.inc.php
+++ docs/include/html/feedback.inc.php
@@ -4,21 +4,26 @@ if ($_GET['f']) {
 	$f = intval($_GET['f']);
 	if ($f > 0) {
 		print_feedback($f);
+		unset($_GET['f']);
 	} else {
 		/* it's probably an array */
 		$f = unserialize(urldecode(stripslashes($_GET['f'])));
 		print_feedback($f);
+		unset($_GET['f']);
 	}
 } else if ($feedback) {
 	print_feedback($feedback);
+	unset($feedback);
 }
 
 if (isset($_info)) {
 	print_infos($_info);
+	unset($_info);
 }
 
 if (isset($warnings)) {
 	print_warnings($warnings);
+	unset($warnings);
 }
 
 
