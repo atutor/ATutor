386a18893e6766e0216561f03096f3122149e076
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-05-18 19:12:57 +0000

    no message

--- docs/include/html/feedback.inc.php
+++ docs/include/html/feedback.inc.php
@@ -1,4 +1,16 @@
 <?php
+/****************************************************************/
+/* ATutor														*/
+/****************************************************************/
+/* Copyright (c) 2002-2003 by Greg Gay & Joel Kronenberg        */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca												*/
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.				*/
+/****************************************************************/
+// $Id: feedback.inc.php,v 1.3 2004/05/18 19:12:57 joel Exp $
 
 if ($_GET['f']) {
 	$f = intval($_GET['f']);
