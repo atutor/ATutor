325cb17774cbaa154deba4899f577f35ecd3820f
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-07-05 14:51:12 +0000

    http://www.atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000857

--- docs/include/lib/constants.inc.php
+++ docs/include/lib/constants.inc.php
@@ -10,7 +10,7 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
-// $Id: constants.inc.php,v 1.42 2004/05/31 18:29:18 joel Exp $
+// $Id$
 
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 
@@ -79,13 +79,13 @@ if (strpos(@ini_get('arg_separator.input'), ';') !== false) {
 define('AT_ACHECKER_URL', 'http://checker.atrc.utoronto.ca/servlet/');
 
 /* the URL to the WSDL of the TILE repository of choice. */
-define('AT_TILE_WSDL', 'http://tile-daily.atrc.utoronto.ca/tile/services/search?wsdl');
+define('AT_TILE_WSDL', 'http://tile.atutor.ca/tile/services/search?wsdl');
 
 /* the URL to the content package export servlet of the TILE repository of choice. */
-define('AT_TILE_EXPORT', 'http://tile-daily.atrc.utoronto.ca/tile/servlet/export');
+define('AT_TILE_EXPORT', 'http://tile.atutor.ca/tile/servlet/export');
 
 /* the URL to the content importing servlet of the TILE repository. */
-define('AT_TILE_IMPORT', 'http://tile-daily.atrc.utoronto.ca/tile/servlet/put');
+define('AT_TILE_IMPORT', 'http://tile.atutor.ca/tile/servlet/put');
 
 
 if (!isset($_SERVER['REQUEST_URI'])) {
