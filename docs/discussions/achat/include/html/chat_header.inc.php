769e88e84e7e6ff41c8e9bd62cb79f4a87750903
boon hau <boonhau@gmail.com>
2004-08-18 14:49:33 +0000

    javascript function for refreshing window appeared in this file when it shouldn't have (this include file is used for both the chat messages and the message poster frame).  This function already appears in display.php and is not needed in this file.

--- docs/discussions/achat/include/html/chat_header.inc.php
+++ docs/discussions/achat/include/html/chat_header.inc.php
@@ -1,27 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
 <head>
-	<title><?php echo _AT('upload_progress'); ?></title>
+	<title>ATutor AChat</title>
 	<?php if ($_GET['frame']) { ?>
 		<META HTTP-EQUIV="refresh" content="3;URL=prog.php?frame=1"> 
 	<?php } ?>
 	<link rel="stylesheet" href="<?php echo $_base_href.'/themes/'.$_SESSION['prefs']['PREF_THEME']; ?>/styles.css" type="text/css" />
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 </head>
-<?php
-	if ($myPrefs['refresh'] != 'manual') {
-?>
-	<script language="javascript" type="text/javascript">
-	<!--
-		setTimeout("reDisplay()", <?php echo $myPrefs['refresh'] * 1000; ?>);
-		function reDisplay() {
-			window.location.href = "<?php echo $_SERVER[PHP_SELF]; ?>";
-		}
-	//-->
-	</script>
-<?php
-	} /* end if */
-?>
 
 <body <?php
 	if ($_SESSION['done']) {
