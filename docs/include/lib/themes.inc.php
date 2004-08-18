d65643cd34870ef602454fcfa57de4f3161fd038
boon hau <boonhau@gmail.com>
2004-08-18 20:20:06 +0000

    theme related functions
    get_available_themes - return array of information on available themes

---
+++ docs/include/lib/themes.inc.php
@@ -0,0 +1,27 @@
+<?php
+/****************************************************************/
+/* ATutor														*/
+/****************************************************************/
+/* Copyright (c) 2002-2004 by Greg Gay & Joel Kronenberg        */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca												*/
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.				*/
+/****************************************************************/
+
+if (!defined('AT_INCLUDE_PATH')) { exit; }
+
+
+// Returns an array of information on all available themes found from config.inc.php
+function get_available_themes () {
+	$theme_list = explode(',' , AVAILABLE_THEMES);
+	foreach ($theme_list as $theme) {
+		$theme_info [$theme] = get_theme_info($theme);
+	}
+	return $theme_info;
+}
+
+
+?>
