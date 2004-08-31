6242844228254b368e56971ce1393eef7855f028
joel kronenberg <joel.kronenberg@utoronto.ca>
2004-08-31 13:35:05 +0000

    http://www.atutor.ca/atutor/mantis/bug_view_page.php?bug_id=0000926

--- docs/tools/take_test.php
+++ docs/tools/take_test.php
@@ -217,9 +217,9 @@ if ($row['random']) {
 }
 
 $result	= mysql_query($sql, $db);
-echo '<table class="bodyline" width="90%"><tr><td>';
 $count = 1;
 if ($row = mysql_fetch_assoc($result)){
+	echo '<table class="bodyline" width="90%"><tr><td>';
 	echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">';
 	echo '<input type="hidden" name="tid" value="'.$tid.'" />';
 	echo '<input type="hidden" name="automark" value="'.$automark.'" />';
