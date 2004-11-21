884a3537e2a2fb1af68594fd07c795c777a44c35
greg gay <ggay@ocad.ca>
2004-11-21 00:03:56 +0000

    added forum title, thread subject, and author to the title text for messages displayed

--- docs/include/html/dropdowns/posts.inc.php
+++ docs/include/html/dropdowns/posts.inc.php
@@ -25,18 +25,32 @@ if ($_GET['menu_jump']) {
 	$savant->assign('tmpl_menu_url', '');	
 }
 
+//Number of posts to display
+$post_limit = 8;
+//-------
+
+
+$course = intval($_SESSION['course_id']);
+
+//Get the forum titles
+$sql	= "SELECT forum_id, title FROM ".TABLE_PREFIX."forums";
+$result = mysql_query($sql, $db);
+while($row = mysql_fetch_array($result)){
+	$forum_info[$row['forum_id']] = $row['title'];
+}
+
 if ($_SESSION['prefs'][PREF_POSTS] == 1){
 	ob_start(); 
 
 	echo '<tr>';
 	echo '<td class="dropdown" align="left">';
-
-	$sql = "SELECT T.login, T.subject, T.post_id, T.forum_id, F.course_id, F.forum_id FROM ".TABLE_PREFIX."forums_threads T, ".TABLE_PREFIX."forums_courses F WHERE F.course_id=". $_SESSION['course_id']." AND T.forum_id=F.forum_id ORDER  BY date DESC LIMIT 5";
+	
+	$sql = "SELECT T.login, T.subject, T.post_id, T.forum_id, F.course_id, F.forum_id FROM ".TABLE_PREFIX."forums_threads T, ".TABLE_PREFIX."forums_courses F WHERE F.course_id=". $_SESSION['course_id']." AND T.forum_id=F.forum_id ORDER  BY date DESC LIMIT $post_limit";
 
 	$result = mysql_query($sql, $db);
 	if ($row = mysql_fetch_assoc($result)) {
 		do {
-			echo '&#176; <a href="'.$_base_href.'forum/view.php?fid='.$row['forum_id'].SEP.'pid='.$row['post_id'].'" title="'.$row['login'].'">'.$row['subject'].'</a><br />';
+			echo '&#176; <a href="'.$_base_href.'forum/view.php?fid='.$row['forum_id'].SEP.'pid='.$row['post_id'].'" title="'.$forum_info[$row['forum_id']].': '.$row['subject'].': '.$row['login'].'">'.$row['subject'].'</a><br />';
 
 		} while ($row = mysql_fetch_assoc($result));
 	} else {
