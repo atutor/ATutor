47ffe052f8a85e810f08a5d79f33dd382c98f369
greg gay <ggay@ocad.ca>
2004-11-28 17:01:23 +0000

    added help box

--- docs/search.php
+++ docs/search.php
@@ -17,13 +17,20 @@ $_user_location = 'public';
 
 define('AT_INCLUDE_PATH', 'include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
+require_once(AT_INCLUDE_PATH.'classes/Message/Message.class.php');
+
+global $savant;
+$msg =& new Message($savant);
 
 require(AT_INCLUDE_PATH . 'lib/search.inc.php');
 
 require(AT_INCLUDE_PATH.'header.inc.php');
 
+
 echo '<h2>'._AT('search').'</h2>';
 
+$msg->addHelp('SEARCH_ALL_PUBLIC');
+$msg->printAll();
 
 require(AT_INCLUDE_PATH.'html/search.inc.php');
 
