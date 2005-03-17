7ae2751b758b27bf84b03a0fed47f52d0bb6e043
shozubq <shozub@gmail.com>
2005-03-17 17:57:13 +0000

    0001456: private_enroll.php - styles missing/logic causing bugs

--- docs/users/private_enroll.php
+++ docs/users/private_enroll.php
@@ -16,112 +16,96 @@ $_user_location	= 'users';
 define('AT_INCLUDE_PATH', '../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 
-$course = intval($_GET['course']);
-if ($course == 0) {
-	$course = intval($_POST['form_course_id']);
+if (!$_SESSION['valid_user']) {
+	$msg->printErrors('LOGIN_ENROL');
+	echo '<br /><a href="login.php">'._AT('account_login').'</a><br />';
+	echo '<br /><a href="registration.php">'._AT('register_an_account').'</a><br />';
 }
+
+$course = intval($_REQUEST['course']);
 if ($course == 0) {
 	exit;
 }
 
 $sql	= "SELECT access, member_id FROM ".TABLE_PREFIX."courses WHERE course_id=$course";
 $result = mysql_query($sql, $db);
-$course_info = mysql_fetch_array($result);
+$course_info = mysql_fetch_assoc($result);
 
 if ($_POST['submit']) {
 	$_SESSION['enroll'] = AT_ENROLL_YES;
-	$_POST['form_course_id'] = intval($_POST['form_course_id']);
 
-	if ($course_info[0] == 'private') {
-		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $_POST[form_course_id], 'n', 0, '"._AT('student')."', 0)";
+	if ($course_info['access'] == 'private') {
+		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $course, 'n', 0, '"._AT('student')."', 0)";
 		$result = mysql_query($sql, $db);
 
-		// send the email thing. if needed
+		// send the email - if needed
+		if ($system_courses[$course]['notify'] == 1) {
 
-		$sql	= "SELECT notify, member_id FROM ".TABLE_PREFIX."courses WHERE course_id=$_POST[form_course_id] AND notify=1";
-		$result = mysql_query($sql, $db);
-		if ($row = mysql_fetch_array($result)) {
-			// notify is enabled. get the email
-			$sql	= "SELECT email, first_name, last_name FROM ".TABLE_PREFIX."members WHERE member_id=$row[member_id]";
+			$ins_id = $system_courses[$course]['member_id'];
+			$sql	= "SELECT email, first_name, last_name FROM ".TABLE_PREFIX."members WHERE member_id=$ins_id";
 			$result = mysql_query($sql, $db);
-			$row	= mysql_fetch_array($result);
+			$row	= mysql_fetch_assoc($result);
 
 			$to_email = $row['email'];
-
-			$message  = $row['first_name'].' '.$row['last_name']."\n\n";
-			$message .= _AT('enrol_messagenew', $system_courses[$_POST['form_course_id']]['title'], $_base_href );
+			$message  = $row['first_name']  .' ' . $row['last_name']."\n\n";
+			$message .= _AT('enrol_messagenew', $system_courses[$course]['title'], $_base_href );
 
 			if ($to_email != '') {
 				require(AT_INCLUDE_PATH . 'classes/phpmailer/atutormailer.class.php');
 
 				$mail = new ATutorMailer;
-
 				$mail->From     = EMAIL;
+				$mail->FromName = SITE_NAME;
 				$mail->AddAddress($to_email);
 				$mail->Subject = _AT('enrol_message3');
 				$mail->Body    = $message;
 
-				if(!$mail->Send()) {
-				   //echo 'There was an error sending the message';
+				if (!$mail->Send()) {
+				   require(AT_INCLUDE_PATH.'header.inc.php');
 				   $msg->printErrors('SENDING_ERROR');
+				   require(AT_INCLUDE_PATH.'footer.inc.php');
 				   exit;
 				}
-
 				unset($mail);
 			}
 		}
-	} else {
-		// public or protected
-		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $_POST[form_course_id], 'y', 0, '"._AT('student')."', 0)";
+		require(AT_INCLUDE_PATH.'header.inc.php');
+		$msg->printFeedbacks('APPROVAL_PENDING');
+		require(AT_INCLUDE_PATH.'footer.inc.php');
+		exit;
+	} 
+	// public or protected
+	else {
+		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $course, 'y', 0, '"._AT('student')."', 0)";
 		$result = mysql_query($sql, $db);
 	}
 }
 
-$title = _AT('course_enrolment');
+$sql	= "SELECT * FROM ".TABLE_PREFIX."course_enrollment WHERE member_id=$_SESSION[member_id] AND course_id=$course";
+$result = mysql_query($sql, $db);
+$row_in = mysql_fetch_assoc($result);
+
+// request has already been made
+if ($row_in['member_id'] == $_SESSION['member_id'] ) {
+ 	$msg->addFeedback('ALREADY_REQUESTED');
+	header('Location: ./index.php');
+	exit;
+}
+
 require(AT_INCLUDE_PATH.'header.inc.php');
 ?>
 
-<?php
-if ($_SESSION['valid_user']) {
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<input type="hidden" name="course" value="<?php echo $course; ?>">
+<div class="input-form">
+	<div class="row">
+		<?php echo _AT('private_enroll'); ?>
+	</div>
 
-	$sql	= "SELECT * FROM ".TABLE_PREFIX."course_enrollment WHERE member_id=$_SESSION[member_id] AND course_id=$course";
-	$result = mysql_query($sql, $db);
-	$row	= mysql_fetch_array($result);
+	<div class="row buttons">
+		<input type="submit" name="submit" value="<?php echo _AT('request_enrollment'); ?>" />
+	</div>
+</div>
+</form>
 
-	if ((!$_POST['submit']) && ($row == '')) {
-?>
-		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		<input type="hidden" name="form_course_id" value="<?php echo $course; ?>">
-		<div class="input-form">
-			
-			<div class="row">
-				<?php echo _AT('private_enroll'); ?>
-			</div>
-
-			<div class="row buttons">
-				<input type="submit" name="submit" value="<?php echo _AT('request_enrollment'); ?>" />
-			</div>
-
-		</div>
-		</form>
-<?php
-
-		} else if ($_POST['submit']) {
-			$msg->addInfo('APPROVAL_PENDING');
-			$msg->printInfos();
-		} else if ($course_info[1] != $_SESSION['member_id'] ){
-		 // request has already been made
-		 	$msg->printInfos('ALREADY_ENROLLED');
-
-		} else {
-			$msg->printErrors('ALREADY_OWNED');
-		}
-
-} else {
-	$msg->printErrors('LOGIN_ENROL');
-	echo '<br /><a href="login.php">'._AT('account_login').'</a><br />
-	<a href="registration.php">'._AT('register_an_account').'</a><br /></td></tr>';
-}
-
-	require(AT_INCLUDE_PATH.'footer.inc.php');
-?>
+<?php	require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
