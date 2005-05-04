437ffc180831bfec18a161103dc5d84a64571ff8
jonathan hung <jhung@ocad>
2005-05-04 20:16:11 +0000

    Edits.

--- docs/documentation/admin/2.3.2.managing_existing_languages.php
+++ docs/documentation/admin/2.3.2.managing_existing_languages.php
@@ -1,7 +1,7 @@
 <?php require('../common/body_header.inc.php'); ?>
 
 <h2>2.3.2 Managing Existing Languages</h2>
-	<p>Installed languages can be deleted or exported as an ATutor language pack for redistribution. You cannot delete the default language as specified by the <a href="2.2.system_preferences.php">System Preferences</a> <em>Default Language</em> option.</p>
+	<p>Installed languages can be deleted or exported as an ATutor language pack for redistribution. You cannot delete the default language that is specified in the <a href="2.2.system_preferences.php">System Preferences</a> <em>Default Language</em> option.</p>
 
 	<p>See the section on <a href="2.3.3.translating_atutor.php">Translating ATutor</a> for details on how to edit, or create a new language that isn't already available as a language pack.</p>
 
