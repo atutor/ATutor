e240332eec1e6c2173121546c54eb5be574eb45f
jonathan hung <jhung@ocad>
2005-05-04 16:00:14 +0000

    Fixed some <pre></pre> formatting.

--- docs/documentation/admin/1.1.requirements_recommendations.php
+++ docs/documentation/admin/1.1.requirements_recommendations.php
@@ -5,9 +5,12 @@
 		<p>We strongly recommend Apache 1.3 as the required web server and discourage the use of Apache 2 in production environments. We believe that unless your server is running Windows, Apache 2 does not provide a significant performance improvement over Apache 1 to necessitate migration. ATutor has also been successfully installed on Microsoft IIS, but it is not part of our development testing, so we cannot make any guarantees.</p>
 
 	<h3>PHP</h3>
-		<p><acronym title="Recursive acronym for PHP: Hypertext Preprocessor, the language ATutor is written in">PHP</acronym> 4.2.0 or higher with Zlib, MySQL, and session support enabled is also required. PHP version 4.3.0 or higher is highly recommended as it provides greater performance than previous versions. PHP version 5.0.4 or higher should be fine for non-production environments. Additionally, the following <kbd>php.ini</kbd> configuration settings are recommended, and in the case of <kbd>safe_mode = off</kbd>, required:</p>
-		<pre>safe_mode               = Off
-display_errors          = Off
+		<p><acronym title="Recursive acronym for PHP: Hypertext Preprocessor, the language ATutor is written in">PHP</acronym> 4.2.0 or higher with Zlib, MySQL, and session support enabled is also required. PHP version 4.3.0 or higher is highly recommended as it provides greater performance than previous versions. PHP version 5.0.4 or higher should be fine for non-production environments. Additionally, the following <kbd>php.ini</kbd> configuration setting is required:</p>
+
+		<pre>safe_mode               = Off</pre>
+
+		<p>The following are recommended settings:</p>
+<pre>display_errors          = Off
 arg_separator.input     = ";&"
 register_globals        = Off
 magic_quotes_gpc        = Off
