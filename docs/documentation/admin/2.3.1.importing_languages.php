7045cc16431edcbfead87739e9966ccd8956d120
jonathan hung <jhung@ocad>
2005-05-04 20:06:54 +0000

    Edits.

--- docs/documentation/admin/2.3.1.importing_languages.php
+++ docs/documentation/admin/2.3.1.importing_languages.php
@@ -10,11 +10,13 @@
 		<li>Use the <code>Import</code> button to import the language.</li>
 	</ol>
 
-	<p>If your ATutor installation is connected to the Internet and can contact the atutor.ca website then it will try to retrieve the list remotely. If it cannot retrieve the list a message indicating so will be presented rather than a drop down list, in that case you will have to use the manual method described above. To automatically import a new language pack from within ATutor:</p>
+	<p>If your ATutor installation is connected to the Internet and can contact the atutor.ca website then it will try to retrieve the list remotely. To automatically import a new language pack from within ATutor:</p>
 
 	<ol>
 		<li>Select the language you want to import from the drop down.</li>
 		<li>Use the <code>Import</code> button to import the selected language.</li>
 	</ol>
+
+	<p>If your installation cannot retrieve the language list from atutor.ca, a message indicating so will be presented rather than a drop down list. In this case you will have to use the manual method described above.</p>
 
 <?php require('../common/body_footer.inc.php'); ?>
