06e344ae7869da4d9e08d8c548b79e0232916ef2
jonathan hung <jhung@ocad>
2005-05-05 16:32:42 +0000

    edit.

--- docs/documentation/admin/3.1.instructor_requests.php
+++ docs/documentation/admin/3.1.instructor_requests.php
@@ -1,7 +1,7 @@
 <?php require('../common/body_header.inc.php'); ?>
 
 <h2>3.1 Instructor Requests</h2>
-	<p>If the <a href="2.2.system_preferences.php">System Preferences</a> <em>Allow Instructor Requests</em> option is enabled and the <em>Auto Approve Instructor Requests</em> option is disabled then pending instructor account requests will be listed on this page.</p>
+	<p>If the <a href="2.2.system_preferences.php">System Preferences</a> <em>Allow Instructor Requests</em> option is enabled and the <em>Auto Approve Instructor Requests</em> option is disabled, then pending instructor account requests will be listed on this page.</p>
 
 	<p>Using the <code>Deny</code> or <code>Approve</code> buttons after selecting an entry will remove it from the list and take the appropriate action. An email message will be sent to the account holder notifying them of the change.</p>
 
