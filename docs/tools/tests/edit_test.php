37e3aa8395e3afc45ffd2637281bbc4723c180df
jonathan hung <jhung@ocad>
2005-06-01 13:43:53 +0000

    Fix for bug 1765.

--- docs/tools/tests/edit_test.php
+++ docs/tools/tests/edit_test.php
@@ -71,6 +71,11 @@ if (isset($_POST['cancel'])) {
 		$msg->addError('END_DATE_INVALID');
 	}
 
+	if (mktime($hour_end,   $min_end,   0, $month_end,   $day_end,   $year_end) < 
+		mktime($hour_start, $min_start, 0, $month_start, $day_start, $year_start)) {
+			$msg->addError('END_DATE_INVALID');
+	}
+
 	if (strlen($month_start) == 1){
 		$month_start = "0$month_start";
 	}
