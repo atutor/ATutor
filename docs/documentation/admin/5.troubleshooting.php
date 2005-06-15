2b5d36ee3b97901756c1361852809a7571d39707
greg gay <ggay@ocad.ca>
2005-06-15 15:28:57 +0000

    tweak to wording

--- docs/documentation/admin/5.troubleshooting.php
+++ docs/documentation/admin/5.troubleshooting.php
@@ -15,7 +15,7 @@
 
 		</dd>
 		<dt>Error Logging</dt>
-		<dd>View the error log recorded in ATutor through the Configuration section.</dd>
+		<dd>View the error log recorded in ATutor through the Configuration section. There may be information in the error reports that can help you identify where or how an error occured. The output from the error log can be sent to the ATutor team to help them track donw problem you might be experiencing on your system.</dd>
 
 		<dt>phpinfo()</dt>
 		<dd>It is often possible to determine the cause of an error by reviewing the phpinfo page for your system. It will list all the configuration options for your system. Review the <a href="1.1.requirements_recommendations.php">Requirement & Recommendations</a> for different values that should be set and displayed in the phpinfo output. Below is the contents of a phpinfo file:<br /><br />
