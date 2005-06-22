348f64d38bbb78332ee2e65a3dcc28ede7f51299
jonathan hung <jhung@ocad>
2005-06-22 19:29:20 +0000

    Added error message when user does not have privs. http://www.atutor.ca/atutor/mantis/view.php?id=1849

--- docs/tools/tests/results_all_quest.php
+++ docs/tools/tests/results_all_quest.php
@@ -14,7 +14,12 @@ $page = 'tests';
 define('AT_INCLUDE_PATH', '../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 
-authenticate(AT_PRIV_TEST_MARK);
+if (!authenticate(AT_PRIV_TEST_MARK, true)) {
+	$msg->addError('ACCESS_DENIED');
+	header('Location: index.php');
+	exit;
+}
+
 
 $tid = intval($_REQUEST['tid']);
 
