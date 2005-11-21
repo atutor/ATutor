b7e3e99cf8327b04d8b1eb0d9cb6d4e4bdcc4b23
joel kronenberg <joel.kronenberg@utoronto.ca>
2005-11-21 21:37:39 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2160

--- docs/documentation/general/pages.inc.php
+++ docs/documentation/general/pages.inc.php
@@ -12,9 +12,20 @@
 /************************************************************************/
 // $Id$
 
-
-    $_pages['1.0.voices.php']					= 'Voices';
-    $_pages['2.0.atalker.php']					= 'ATalker';
-
+    $_pages['0.0.introduction.php']				= 'Introduction';
+    $_pages['1.0.login.php']					= 'Login';
+    $_pages['2.0.register.php']					= 'Register';
+    $_pages['3.0.browse_courses.php']			= 'Browse Courses';
+    $_pages['4.0.password_reminder.php']		= 'Password Reminder';
+    $_pages['5.0.my_start_page.php']			= 'My Start Page';
+    $_pages['5.1.my_courses.php']				= 'My Courses';
+    $_pages['5.1.1.create_course.php']			= 'Create Courses';
+    $_pages['5.2.profile.php']					= 'Profile';
+    $_pages['5.3.preferences.php']				= 'Preferences';
+    $_pages['5.4.inbox.php']					= 'Inbox';
+	$_pages['6.0.inside_course.php']			= 'Inside a Course';
+	$_pages['6.1.export_content.php']			= 'Export Content';
+	$_pages['6.2.packages.php']					= 'Packages';
+	$_pages['6.3.tile.php']						= 'TILE Repository Search';
 
 ?>
