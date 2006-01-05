59100243086a0ec1e565a7c52c84168dae24b02f
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-01-05 19:02:31 +0000

    initial version

---
+++ mods/phpmyadmin/module.sql
@@ -0,0 +1,14 @@
+# sql file for phpMyAdmin module
+
+
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin','phpMyAdmin',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_add_url','Type in the location/URL of your phpMyAdmin installation.',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_text','Use phpMyAdmin to access the ATutor database. Care should taken when modifying database entries. In most cases ATutor tools should be used instead of modifying the database directly. Use the button below to open phpMyAdmin into a new window.',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_open','Open phpMyAdmin',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_save','Save phpMyAdmin Location',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_edit','Edit phpMyAdmin Location',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','phpmyadmin_location','The following is the location of phpMyAdmin:',NOW(),'');
+
+INSERT INTO `language_text` VALUES ('en', '_msgs','AT_ERROR_PHPMYADMINURL_ADD_FAILED','Failed to add phpMyAdmin URL to database.',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_msgs','AT_FEEDBACK_PHPMYADMINURL_ADD_SAVED','Location of phpMyAdmin was successfully saved..',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_msgs','AT_ERROR_PHPMYADMINURL_ADD_EMPTY','You must enter a URL to the location of phpMyAdmin',NOW(),'');
