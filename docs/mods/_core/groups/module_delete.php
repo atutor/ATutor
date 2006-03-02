567c8c9dbc27948881aa8ecdafbf6aa98ee2496d
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-03-02 18:13:06 +0000

    group -> delete course

--- docs/mods/_core/groups/module_delete.php
+++ docs/mods/_core/groups/module_delete.php
@@ -3,7 +3,7 @@
 function groups_delete($course) {
 	global $db;
 
-	$sql	= "SELECT group_id FROM ".TABLE_PREFIX."groups WHERE course_id=$course";
+	$sql	= "SELECT G.group_id FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types USING (type_id) WHERE T.course_id=$course";
 	$result = mysql_query($sql, $db);
 	while ($row = mysql_fetch_assoc($result)) {
 		$sql	= "DELETE FROM ".TABLE_PREFIX."groups_members WHERE group_id=$row[group_id]";
@@ -11,8 +11,12 @@ function groups_delete($course) {
 
 		$sql	= "DELETE FROM ".TABLE_PREFIX."tests_groups WHERE group_id=$row[group_id]";
 		$result2 = mysql_query($sql, $db);
+
+		$sql	= "DELETE FROM ".TABLE_PREFIX."groups WHERE group_id=$row[group_id]";
+		$result = mysql_query($sql, $db);
 	}
-	$sql	= "DELETE FROM ".TABLE_PREFIX."groups WHERE course_id=$course";
+
+	$sql	= "DELETE FROM ".TABLE_PREFIX."groups_types WHERE course_id=$course";
 	$result = mysql_query($sql, $db);
 
 	// -- remove assoc between tests and groups:
