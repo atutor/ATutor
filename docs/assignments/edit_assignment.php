5f0695715b1fda3439145b3c5ed3259a2a73af65
chris ridpath <chris.ridpath@utoronto.ca>
2006-03-14 19:51:35 +0000

    Initial version of file. Just calls the "add_assignment.php" page.

---
+++ docs/assignments/edit_assignment.php
@@ -0,0 +1,14 @@
+<?php
+/****************************************************************/
+/* ATutor														*/
+/****************************************************************/
+/* Copyright (c) 2002-2006                                      */
+/* Written by Greg Gay, Joel Kronenberg & Chris Ridpath         */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca												*/
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.				*/
+/****************************************************************/
+require ('add_assignment.php');
