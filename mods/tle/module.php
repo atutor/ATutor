3a76c2f1e9badde3f4659688a5e9d0a4f5e4e4c3
greg gay <ggay@ocad.ca>
2006-03-29 15:33:00 +0000

    little fix

--- mods/tle/module.php
+++ mods/tle/module.php
@@ -1,13 +1,13 @@
 <?php
 if (!defined('AT_INCLUDE_PATH')) { exit; }
+
 if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__
 FILE__ . ' is not a Module'); }
 
 $_student_tool = 'mods/tle/index.php';
 
 if (admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-        $this->_pages['admin/config_edit.php']['children'] = array('mods/tle/index_admin.php
-');
+        $this->_pages['admin/config_edit.php']['children'] = array('mods/tle/index_admin.php');
         $this->_pages['mods/tle/index_admin.php']['title_var'] = 'tle';
         $this->_pages['mods/tle/index_admin.php']['parent']    = 'admin/config_edit.php';
 }
@@ -19,6 +19,5 @@ $this->_pages['mods/tle/index.php']['title_var'] = 'tle';
 $this->_pages['mods/tle/index.php']['parent']    = 'tools/index.php';
 
 $this->_pages['mods/tle/import.php']['title_var'] = '';
-
 $this->_pages['mods/tle/index.php']['img']       = 'images/home-tile_search.gif';
 ?>
