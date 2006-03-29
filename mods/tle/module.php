8ed85b71b898189ce0f454e47cb10cef473d5558
greg gay <ggay@ocad.ca>
2006-03-29 17:15:57 +0000

    minor fix

--- mods/tle/module.php
+++ mods/tle/module.php
@@ -1,23 +1,24 @@
 <?php
 if (!defined('AT_INCLUDE_PATH')) { exit; }
-
-if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__
-FILE__ . ' is not a Module'); }
+if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__FILE__ . ' is not a Module'); }
 
 $_student_tool = 'mods/tle/index.php';
 
 if (admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-        $this->_pages['admin/config_edit.php']['children'] = array('mods/tle/index_admin.php');
-        $this->_pages['mods/tle/index_admin.php']['title_var'] = 'tle';
-        $this->_pages['mods/tle/index_admin.php']['parent']    = 'admin/config_edit.php';
+	$this->_pages['admin/config_edit.php']['children'] = array('mods/tle/index_admin.php');
+	$this->_pages['mods/tle/index_admin.php']['title_var'] = 'tle';
+	$this->_pages['mods/tle/index_admin.php']['parent']    = 'admin/config_edit.php';
 }
-
-$this->_pages['tools/content/index.php']['title_var'] = 'tle';
-$this->_pages['tools/content/index.php']['children'] = array('mods/tle/index.php');
 
 $this->_pages['mods/tle/index.php']['title_var'] = 'tle';
 $this->_pages['mods/tle/index.php']['parent']    = 'tools/index.php';
 
 $this->_pages['mods/tle/import.php']['title_var'] = '';
+
+$this->_pages['tools/content/index.php']['title_var'] = 'tle';
+$this->_pages['tools/content/index.php']['children'] = array('mods/tle/index.php');
+
+$this->_pages['mods/tle/import.php']['title_var'] = '';
 $this->_pages['mods/tle/index.php']['img']       = 'images/home-tile_search.gif';
+
 ?>
