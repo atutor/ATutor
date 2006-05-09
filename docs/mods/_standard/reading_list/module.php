7e8f3e44c15314a109866ffea38553279cd31d17
greg gay <ggay@ocad.ca>
2006-05-09 18:38:19 +0000

    cleaned up reading list module.php

--- docs/mods/_standard/reading_list/module.php
+++ docs/mods/_standard/reading_list/module.php
@@ -13,32 +13,14 @@ if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module')
  * assign the instructor and admin privileges to the constants.
  */
 define('AT_PRIV_READING_LIST',       $this->getPrivilege());
-//define('AT_ADMIN_PRIV_HELLO_WORLD', $this->getAdminPrivilege());
 
-/*******
- * create a side menu box/stack.
- */
-//$this->_stacks['hello_world'] = array('title_var'=>'hello_world', 'file'=>'hello_world/side_menu.inc.php');
-// ** possible alternative: **
-// $this->addStack('hello_world', array('title_var' => 'hello_world', 'file' => './side_menu.inc.php');
 
 /*******
  * if this module is to be made available to students on the Home or Main Navigation.
  */
 $_student_tool = 'reading_list/index.php';
-// ** possible alternative: **
-// $this->addTool('./index.php');
 
-/*******
- * add the admin pages when needed.
- */
-/*
-if (admin_authenticate(AT_ADMIN_PRIV_HELLO_WORLD, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-	$this->_pages[AT_NAV_ADMIN] = array('hello_world/index_admin.php');
-	$this->_pages['hello_world/index_admin.php']['parent']    = AT_NAV_ADMIN;
-	$this->_pages['hello_world/index_admin.php']['title_var'] = 'hello_world';
-}
-*/
+
 /*******
  * instructor Manage section:
  */
@@ -105,9 +87,6 @@ $this->_pages['reading_list/index_instructor.php']['guide'] = 'instructor/?p=rea
 	$this->_pages['reading_list/delete_resource.php']['title_var'] = 'rl_delete_resource';
 	$this->_pages['reading_list/delete_resource.php']['parent']    = 'reading_list/index_instructor.php';
 
-// ** possible alternative: **
-// $this->pages['./index_instructor.php']['title_var'] = 'hello_world';
-// $this->pages['./index_instructor.php']['parent']    = 'tools/index.php';
 
 /*******
  * student page.
