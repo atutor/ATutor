1c294aa3ab8d0268891e29bef35b238d0d5ac9eb
greg gay <ggay@ocad.ca>
2006-05-24 13:11:22 +0000

    minor edit

--- docs/documentation/instructor/groups.php
+++ docs/documentation/instructor/groups.php
@@ -12,14 +12,14 @@
 
 
 	<h3>Create a single group manually</h3>
-		<p>It is also possible to create groups manually, one by one. This is a good way of creating groups if you have particular needs in terms of which students work together, and a relatively small class. This method is also good for adding groups to existing projects or types of groups. For example, if groups were created and populated automatically, and then several new students enroll, it's possible to manually add the new students to a new group within an existing project or type.</p>
+		<p>It is also possible to create groups manually, one by one. This is a good way of creating groups if you have particular needs in terms of which students work together, and a relatively small class. This method is also good for adding groups to existing projects or types of groups. For example, if groups were created and populated automatically, and then several new students enroll, it's possible to manually add the new students to a new group within an existing project or type. A single group might be created to which students who missed a quiz could be assigned, so they could take a makeup test. A single group might be created with the blog tool enabled, so all students in the class can post to the same blog.</p>
 
-		<p>Enter the type of project, title, and description. Also choose which tools be made avialable to this groups (File Storage, Forums) and use the <code>Create</code> button.</p>
+		<p>Enter the type of project, title, and description. Also choose which tools be made avialable to this groups (File Storage, Forums, Blog, etc.) and use the <code>Create</code> button.</p>
 
 	<h3>Managing Groups</h3>
 
 	<p>From the groups page, it is possible to <code>Edit</code> a group's title, description and tools or <code>Delete</code> a group entirely.</p>
 
-	<p>To manage the members of a group, select the group and use the <code>Members</code> button. The group members screen displays the course list. All unassigned students can automatically be added to the group by using the <code>Assign Unassigned</code> button, or picked manually and saved using the dropdowns, and then the <code>Save</code> button.</p>
+	<p>To manage the members of a group, select the group and use the <code>Members</code> button. The group members screen displays the course list. All unassigned students can automatically be added to the group by using the <code>Assign Unassigned</code> button, or picked manually and saved using the drop down menus, and then the using <code>Save</code> button.</p>
 
 <?php require('../common/body_footer.inc.php'); ?>
