4c11fd1b15f094d1a66f5038b520a262552b3e03
heidi valles <hvalles@ocad.ca>
2006-05-26 17:45:06 +0000

    0002450

--- docs/mods/_standard/links/module_delete.php
+++ docs/mods/_standard/links/module_delete.php
@@ -4,14 +4,14 @@ function links_delete($course) {
 	global $db;
 
 	// get groups
-	$groups = array();
+	$groups = '';
 	$sql	= "SELECT G.group_id FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types USING (type_id) WHERE T.course_id=$course";
 	$result = mysql_query($sql, $db);
 	while ($group_row = mysql_fetch_assoc($result)) {
-		$groups[] = $group_row['group_id'];
+		$groups .= $group_row['group_id'].', ';
 		mysql_query($sql, $db);
 	}
-
+	$groups = substr($groups, 0, -2);
 	$sql	= "SELECT cat_id FROM ".TABLE_PREFIX."links_categories WHERE (owner_id=$course AND owner_type=".LINK_CAT_COURSE.") OR (owner_id IN ($groups) AND owner_type=".LINK_CAT_GROUP.")";
 	$result = mysql_query($sql, $db);
 
