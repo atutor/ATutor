3223552ba561a56589eeb06552e91ee54222a8b0
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-05-30 13:58:44 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2461

--- docs/mods/_standard/file_storage/module_delete.php
+++ docs/mods/_standard/file_storage/module_delete.php
@@ -16,7 +16,7 @@ function file_storage_delete($course) {
 	}
 
 	// delete group files
-	$sql	= "SELECT G.group_id FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types USING (type_id) WHERE T.course_id=$course";
+	$sql	= "SELECT G.group_id FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types T USING (type_id) WHERE T.course_id=$course";
 	$result = mysql_query($sql, $db);
 	while ($group_row = mysql_fetch_assoc($result)) {
 		fs_delete_workspace(WORKSPACE_GROUP, $group_row['group_id']);
