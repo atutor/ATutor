915ab843422ed5e1842912a721a8225e3c03bbaf
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-05-30 13:52:15 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2382

--- docs/mods/_standard/links/module_delete.php
+++ docs/mods/_standard/links/module_delete.php
@@ -9,19 +9,16 @@ function links_delete($course) {
 	$result = mysql_query($sql, $db);
 	while ($group_row = mysql_fetch_assoc($result)) {
 		$groups .= ', '.$group_row['group_id'];
-		mysql_query($sql, $db);
 	}
 
 	$sql	= "SELECT cat_id FROM ".TABLE_PREFIX."links_categories WHERE (owner_id=$course AND owner_type=".LINK_CAT_COURSE.") OR (owner_id IN ($groups) AND owner_type=".LINK_CAT_GROUP.")";
 	$result = mysql_query($sql, $db);
-
 	while ($row = mysql_fetch_assoc($result)) {
 		$sql = "DELETE FROM ".TABLE_PREFIX."links WHERE cat_id=$row[cat_id]";
-		$result2 = mysql_query($sql, $db);
+		mysql_query($sql, $db);
 	}
-	$sql	= "DELETE FROM ".TABLE_PREFIX."links_categories WHERE (owner_id=$course AND owner_type=".LINK_CAT_COURSE.") OR (owner_id IN ($groups) AND owner_type=".LINK_CAT_GROUP.")";
-	$result = mysql_query($sql, $db);
-
+	$sql = "DELETE FROM ".TABLE_PREFIX."links_categories WHERE (owner_id=$course AND owner_type=".LINK_CAT_COURSE.") OR (owner_id IN ($groups) AND owner_type=".LINK_CAT_GROUP.")";
+	mysql_query($sql, $db);
 }
 
 ?>
