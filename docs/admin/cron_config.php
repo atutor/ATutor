a1901d926663438ef0aad76b34919e5dfe5bb69b
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-06-14 16:49:38 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2392

--- docs/admin/cron_config.php
+++ docs/admin/cron_config.php
@@ -3,15 +3,9 @@ define('AT_INCLUDE_PATH', '../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
 admin_authenticate(AT_ADMIN_PRIV_ADMIN);
 
-//$key = $_config['gsearch'];
-
-if (isset($_POST['submit'])) {
-	$_POST['key'] = trim($_POST['key']);
-}
-
-if (!isset($_config['cron_key'])) {
+if (!isset($_config['cron_key']) || !$_config['cron_key']) {
 	$_config['cron_key'] = strtoupper(substr(str_replace(array('l','o','0','i'), array(), md5(time())), 0, 6));
-	$sql = "INSERT INTO ".TABLE_PREFIX."config VALUES ('cron_key', '{$_config['cron_key']}')";
+	$sql = "REPLACE INTO ".TABLE_PREFIX."config VALUES ('cron_key', '{$_config['cron_key']}')";
 	mysql_query($sql, $db);
 }
 
@@ -20,11 +14,11 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 
 <div class="input-form">
 	<div class="row">
-		<p><?php echo _AT('cron_secret_key_usage'); ?></p>
+		<p><?php echo _AT('cron_url_usage'); ?></p>
 	</div>
 	<div class="row">
-		<label for="key"><?php echo _AT('cron_secret_key'); ?></label><br />
-		<input type="text" name="secret_key" size="7" value="<?php echo $_config['cron_key']; ?>" id="key" />
+		<?php echo _AT('cron_url'); ?><br />
+		<code><?php echo $_base_href; ?>admin/cron.php?k=<?php echo $_config['cron_key']; ?></code>
 	</div>
 </div>
 
