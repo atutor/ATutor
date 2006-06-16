1fe5cb8c2f2c057eccf291234552a04e281cc705
greg gay <ggay@ocad.ca>
2006-06-16 19:39:38 +0000

    added tinymce to course banner

--- docs/tools/course_properties.php
+++ docs/tools/course_properties.php
@@ -14,7 +14,7 @@
 
 define('AT_INCLUDE_PATH', '../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
-
+require(AT_INCLUDE_PATH.'lib/tinymce.inc.php');
 require(AT_INCLUDE_PATH.'classes/Backup/Backup.class.php');
 
 authenticate(AT_PRIV_ADMIN);
@@ -27,6 +27,24 @@ if (isset($_POST['cancel'])) {
 	$msg->addFeedback('CANCELLED');
 	header('Location: index.php');
 	exit;
+
+}else if($_POST['submit']){
+
+	require(AT_INCLUDE_PATH.'lib/course.inc.php');
+	$_POST['instructor'] = $_SESSION['member_id'];
+
+	$errors = add_update_course($_POST);
+
+	if (is_numeric($errors)) {
+		$msg->addFeedback('COURSE_PROPERTIES');
+		header('Location: '.$_base_href.'tools/index.php');	
+		exit;
+	}
+		
+//}else if(($_POST['setvisual'] && !$_POST['settext']) || $_GET['setvisual'])){
+} else if (($_POST['setvisual'] || $_POST['settext'])){
+		//header('Location: '.$_SESSION['PHP_SELF'].'');	
+		//exit;
 } else if (isset($_POST['course'])) {
 	require(AT_INCLUDE_PATH.'lib/course.inc.php');
 	$_POST['instructor'] = $_SESSION['member_id'];
