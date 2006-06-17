7840d336d8f78d3024af19e92680b3c4006ec9e7
greg gay <ggay@ocad.ca>
2006-06-17 18:47:41 +0000

    typo

--- mods/userplane/module.sql
+++ mods/userplane/module.sql
@@ -4,7 +4,7 @@ INSERT INTO `language_text` VALUES ('en', '_module','userplane','UserPlane',NOW(
 INSERT INTO `language_text` VALUES ('en', '_module','userplane_id','UserPlane ID',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_module','userplane_text','Your Userplane ID is displayed in the box below. ',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_module','userplane_missing_id','Create a Userplane account at http://www.userplane.com/buy/index.cfm. Enter the ID you are assigned into the box below. ',NOW(),'');
-INSERT INTO `language_text` VALUES ('en', '_module','userplane_howto','Use Userplane Web Chat communicate with other ATutor users using voice and video. <strong>Requires Javascript and Flash</strong>. Enter a screen name then press Login, or choose Sign Up to create a user profile.',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','userplane_howto','Use Userplane Web Chat to communicate with other ATutor users using voice and video. <strong>Requires Javascript and Flash</strong>. Enter a screen name then press Login, or choose Sign Up to create a user profile.',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_msgs','AT_FEEDBACK_USERPLANE_ID_SAVED','UserPlane ID was successfully saved.',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_msgs','AT_ERROR_USERPLANE_ID_ADD_EMPTY','UserPlane ID cannot be empty.',NOW(),'');
 
