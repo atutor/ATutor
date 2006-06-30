a571bda45659d9dca8e7f040fd7b420a400e8a39
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-06-30 17:31:05 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2593

--- docs/documentation/admin/administrator_activity_log.php
+++
@@ -1,6 +0,0 @@
-<?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate$';?>
-
-<h2>Administrator Activity Log</h2>
-	<p>The Administrator Activity Log lists all actions made to the ATutor database tables. Viewing a log entry will give detailed information about the selected activity. The log can be reset by using the <em>Reset Log</em> feature.</p>
-
-<?php require('../common/body_footer.inc.php'); ?>
