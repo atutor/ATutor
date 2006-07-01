febcde36b94accfddca059c46ef2424ebaf6cab9
greg gay <ggay@ocad.ca>
2006-07-01 20:36:00 +0000

    tweek

--- docs/documentation/instructor/accessibility.php
+++ docs/documentation/instructor/accessibility.php
@@ -8,7 +8,7 @@
 	<p>Correct the known problems by reviewing the report, then returning to the Content tab to modify the HTML. Return to the Accessibility tab again when the known problems are corrected to see a <em>Conditional Pass</em>. Select from the choices available in the potential problems listed, then press <code>Make Decisions</code> to update the report. When all known problems are corrected, and decisions have been made on all potential problems, a <em>Full Pass</em> will be displayed, after which you can be sure the content will be accessible to all your students.</p>
 
 <h2>Content Editor Accessiblity</h2>
-<p>The Content Editor includes a number of added features to help make it accessible to assistive technology users, The accesskeys listed below allow navigation through the editor by keyboard. In addition to the Accessibility Tab content accessibility, the visual editor also includes an accessibility checker.</p>
+<p>The Content Editor includes a number of added features to help make it accessible to assistive technology users, The accesskeys listed below allow navigation through the editor by keyboard. In addition to the Accessibility tab, the visual editor includes its own accessibility checker, as well as prompts while authoring, that prevent making accessibility mistakes.</p>
 <h3>Content Editor AccessKeys</h3>
 <ul>
 <li>[Alt-n] Content Tab</li>
