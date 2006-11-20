ad4014ae61acae2d8d8c683baf2375860a7a877c
joel kronenberg <joel.kronenberg@utoronto.ca>
2006-11-20 17:43:42 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2816

--- docs/documentation/instructor/creating_questions.php
+++ docs/documentation/instructor/creating_questions.php
@@ -1,6 +1,20 @@
 <?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate$'; ?>
 
 <h2>Creating Test Questions</h2>
-	<p>Test questions are created through the <a href="question_database.php">Question Database</a> by selecting a type of question from the dropdown and using the <code>Create</code> button. There are a variety of different types of questions: multiple choice, true or false, open ended, and Likert. Open ended questions can be single words, a sentence, paragraph, or a single page answer. Options will differ depending on the type of question being created. All new questions, when saved, will be added to the Question Database where they can then be used in Tests or Surveys.</p>
+	<p>Test questions are created in the <a href="question_database.php">Question Database</a>. Options differ depending on the type of question being created. All questions are saved to the Question Database where they can then be added to Tests or Surveys. The following questions are supported:</p>
+	
+	<dl>
+		<dt>Multiple Choice</dt>
+		<dd>Multiple choice questions require the test or survey taker to answer a question by selecting one or more correct answers.</dd>
+		
+		<dt>True or False</dt>
+		<dd>True/False questions require the test or survey taker to specify whether or not the given statement is true or false.</dd>
+		
+		<dt>Open Ended</dt>
+		<dd>Open ended questions require the test or survey taker to enter text in the specified area.</dd>
+		
+		<dt>Likert</dt>
+		<dd>Likert questions require the test or survey taker to specify their choice based on the scale provided.</dd>
+	</dl>
 
 <?php require('../common/body_footer.inc.php'); ?>
