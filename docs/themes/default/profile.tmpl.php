7c26c2f71bd55214ea6363357eb35f43a65bc925
greg gay <ggay@ocad.ca>
2006-11-23 14:00:43 +0000

    added vegards changes

--- docs/themes/default/profile.tmpl.php
+++ docs/themes/default/profile.tmpl.php
@@ -17,54 +17,53 @@
 <div class="input-form">
 
 	<div class="row">
-		<h3><?php echo AT_print($this->row['login'], 'members.login'); ?></h3>
-	</div>
-
-	<div class="row">
-		<a href="inbox/send_message.php?id=<?php echo $this->row['member_id']; ?>"><?php echo _AT('send_message'); ?></a>
-	</div>
-
-	<div class="row">
-		<?php echo _AT('name'); ?><br />
+		<h3>
 		<?php if($this->row['first_name'] || $this->row['last_name']) {
 			echo AT_print($this->row['first_name'] .' '. $this->row['second_name'] .' '. $this->row['last_name'], 'members.first_name');
 		} else {
 			echo "--";
 		}
 		?>
+			
+    (<?php echo AT_print($this->row['login'], 'members.login'); ?>)
+		</h3>
 	</div>
 
 	<div class="row">
-		<?php echo _AT('email'); ?><br />
+		<p><a href="inbox/send_message.php?id=<?php echo $this->row['member_id']; ?>"><?php echo _AT('send_message'); ?></a></p>
+	</div>
+
+	<div class="row">
+		<p><strong><?php echo _AT('email'); ?>:</strong>
 		<?php if($this->row['private_email']): ?>
 			<?php echo _AT('private'); ?>
 		<?php else: ?>
 			<a href="mailto:<?php echo $this->row['email']; ?>"><?php echo $this->row['email']; ?></a>
-		<?php endif; ?>
+		<?php endif; ?></p>
 	</div>
 
 	<div class="row">
-		<?php echo _AT('web_site'); ?><br />
+		<p><strong><?php echo _AT('web_site'); ?>:</strong>
 		<?php if ($this->row['website']) { 
 			echo '<a href="'.$this->row['website'].'">'.AT_print($this->row['website'], 'members.website').'</a>'; 
 		} else {
 			echo "--";
 		}
-		?>
+		?></p>
 	</div>
 
 	<div class="row">
-		<?php echo _AT('phone'); ?><br />
+		<p><strong><?php echo _AT('phone'); ?>:</strong>
 		<?php if ($this->row['phone']) { 
 			echo $this->row['phone'];
 		} else {
 			echo "--";
 		}
-		?>
+		?></p>
 	</div>
 
 	<div class="row">
-		<?php echo _AT('status'); ?><br />
+		<p><strong><?php echo _AT('status'); ?>:</strong>
 		<?php echo $this->status; ?>
 		<?php 
 		if ($this->privs) { 
@@ -75,7 +74,7 @@
 			$priv_string = substr($priv_string, 0, -2);
 			echo $priv_string .')';
 		}
-		?>
+		?></p>
 	</div>
 </div>
 </form>
