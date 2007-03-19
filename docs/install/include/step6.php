41c908df9319707ed2f29b978ada2971f2972a12
joel kronenberg <joel.kronenberg@utoronto.ca>
2007-03-19 15:13:12 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=2976

--- docs/install/include/step6.php
+++ docs/install/include/step6.php
@@ -54,7 +54,7 @@ if (isset($errors)) {
 	unset($errors);
 	print_hidden($step);
 
-	echo '<p><strong>Note:</strong> To change permissions on Unix use <kbd>chmod a+rwx</kbd> then the file or directory name.</p>';
+	echo '<p><strong>Note:</strong> To change permissions on Unix use <kbd>chmod a+rw</kbd> then the file name.</p>';
 
 	echo '<p align="center"><input type="submit" class="button" value=" Try Again " name="retry" />';
 
@@ -69,10 +69,10 @@ if (isset($errors)) {
 
 		print_feedback($progress);
 
-		$errors[] = 'include/config.inc.php cannot be written! Please verify that the file exists and is writeable. On Unix issue the command <kbd>chmod a+rwx include/config.inc.php</kbd> to make the file writeable. On Windows edit the file\'s properties ensuring that the <kbd>Read-only</kbd> attribute is <em>not</em> checked and that <kbd>Everyone</kbd> access permissions are given to that file.';
+		$errors[] = 'include/config.inc.php cannot be written! Please verify that the file exists and is writeable. On Unix issue the command <kbd>chmod a+rw include/config.inc.php</kbd> to make the file writeable. On Windows edit the file\'s properties ensuring that the <kbd>Read-only</kbd> attribute is <em>not</em> checked and that <kbd>Everyone</kbd> access permissions are given to that file.';
 		print_errors($errors);
 
-		echo '<p><strong>Note:</strong> To change permissions on Unix use <kbd>chmod a+rwx</kbd> then the file or directory name.</p>';
+		echo '<p><strong>Note:</strong> To change permissions on Unix use <kbd>chmod a+rw</kbd> then the file name.</p>';
 
 		echo '<p align="center"><input type="submit" class="button" value=" Try Again " name="retry" />';
 
--- docs/install/include/ustep5.php
+++ docs/install/include/ustep5.php
@@ -65,7 +65,7 @@ if (isset($errors)) {
 
 		print_feedback($progress);
 
-		$errors[] = 'include/config.inc.php cannot be written! Please verify that the file exists and is writeable. On Unix issue the command <kbd>chmod a+rwx include/config.inc.php</kbd> to make the file writeable. On Windows edit the file\'s properties ensuring that the <kbd>Read-only</kbd> attribute is <em>not</em> checked and that <kbd>Everyone</kbd> access permissions are given to that file.';
+		$errors[] = 'include/config.inc.php cannot be written! Please verify that the file exists and is writeable. On Unix issue the command <kbd>chmod a+rw include/config.inc.php</kbd> to make the file writeable. On Windows edit the file\'s properties ensuring that the <kbd>Read-only</kbd> attribute is <em>not</em> checked and that <kbd>Everyone</kbd> access permissions are given to that file.';
 		print_errors($errors);
 
 		echo '<p><strong>Note:</strong> To change permissions on Unix use <kbd>chmod a+rw</kbd> then the file name.</p>';
