f8291bd2207c0271548a8ee14d8b0c3787ffe125
joel kronenberg <joel.kronenberg@utoronto.ca>
2007-04-20 14:29:58 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3040

--- docs/documentation/admin/default_preferences.php
+++ docs/documentation/admin/default_preferences.php
@@ -1,7 +1,7 @@
 <?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate$'; ?>
 
-	<h2>Default Preferences</h2>
+<h2>Default Preferences</h2>
 
-<p>Administrators are able to set the preferences for newly created members by specifying default preferences. Note that the default theme is set in the <a href="themes.php">Themes</a> area, and default language is set in the <a href="system_preferences.php">System Preferences</a> area.  Also, Auto-Login is not decided by administrators. Members can alter their settings once they have logged in by going to the Preferences area in the main navigation. For more information, see <a href="../general/preferences.php">Preferences</a> in the general help area.</p>
+<p>The default preferences are only applied to newly created accounts at the time of their creation and to guests when accessing public courses. Note that the default theme is set in the <a href="themes.php">Themes</a> area, and default language is set in the <a href="system_preferences.php">System Preferences</a> area.  Also, Auto-Login is not decided by administrators. Members may alter their settings once they have logged in by going to the Preferences area in the main navigation. For more information, see <a href="../general/preferences.php">Preferences</a> in the general help area.</p>
 
 <?php require('../common/body_footer.inc.php'); ?>
