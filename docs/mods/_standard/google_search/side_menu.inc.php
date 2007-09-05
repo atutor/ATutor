d27f9b62e02f7a2aa717470b844f39c95c411c20
harris wong <hwong@ocad.ca>
2007-09-05 15:44:09 +0000

    AJAX Google Search

--- docs/mods/_standard/google_search/side_menu.inc.php
+++ docs/mods/_standard/google_search/side_menu.inc.php
@@ -4,7 +4,7 @@ ob_start();
 ?>
 
 <?php if ($_config['gsearch']): ?>
-	<form action="<?php echo $_base_path; ?>mods/_standard/google_search/index.php" method="get" name="gsearchform">
+	<form action="<?php echo $_base_path; ?>google_search/index.php" method="get" name="gsearchform">
 <?php else: ?>
 	<form action="http://www.google.com/search" method="get" target="_new">
 	<input type="hidden" name="l" value="<?php echo $_SESSION['lang']; ?>" />
@@ -16,8 +16,9 @@ ob_start();
 
 <input type="hidden" name="search" value="1" />
 
-<input type="text" name="q" class="formfield" size="20" value="<?php echo $stripslashes(htmlspecialchars($_GET['search_query'])); ?>" /><br /><br />
-<input type="submit" name="submit" value="<?php echo _AT('search'); ?>" class="button" />
+<input type="text" name="q" class="formfield" size="20" value="<?php echo $stripslashes(htmlspecialchars($_GET['q'])); ?>" /><br /><br />
+<input type="hidden" name="submit" value="<?php echo _AT('search'); ?>" />
+<input type="submit" class="button" />
 </form>
 
 <?php
