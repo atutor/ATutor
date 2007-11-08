fbe6f586bb330ce86fd2d36c4a234f1fa10facb1
greg gay <ggay@ocad.ca>
2007-11-08 17:19:18 +0000

    bundle script adjustments, defining path to svn executable

--- bundle.sh
+++ bundle.sh
@@ -9,6 +9,7 @@ set now = `date +"%Y_%m_%d"`
 set atutor_dir = "ATutor_$now"
 set bundle = "ATutor"
 set svndir = "http://atutorsvn.atrc.utoronto.ca/repos/atutor/trunk/docs/"
+set svnexec = "/usr/bin/svn"
 
 echo "\033[1mATutor Bundle Script [for CVS 1.3.1+] \033[0m"
 echo "--------------------"
@@ -47,7 +48,7 @@ sleep 1
 echo "\nExporting from SVN/ to $atutor_dir"
 mkdir $atutor_dir
 #cp -R docs $atutor_dir/ATutor
-/usr/bin/svn --force export $svndir
+$svnexec --force export $svndir
 mv 'docs' $atutor_dir/ATutor
 sleep 1
 
@@ -88,7 +89,7 @@ sed "s/define('AT_DEVEL_TRANSLATE', 1);/define('AT_DEVEL_TRANSLATE', 0);/" $atut
 sleep 1
 
 echo -n "<?php "'$svn_data = '"'" >> $atutor_dir/ATutor/svn.php
-/usr/local/bin/svn log  -q -r HEAD http://atutorsvn.atrc.utoronto.ca/repos/atutor/trunk/  >> $atutor_dir/ATutor/svn.php
+$svnexec log  -q -r HEAD http://atutorsvn.atrc.utoronto.ca/repos/atutor/trunk/  >> $atutor_dir/ATutor/svn.php
 echo -n "';?>" >> $atutor_dir/ATutor/svn.php
 
 echo "\nTargz'ing $bundle${extension}.tar.gz $atutor_dir/ATutor/"
