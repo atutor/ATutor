1ba3df00f9290f57bccaf6586a8aa88490f4111d
harris wong <hwong@ocad.ca>
2007-12-03 18:49:49 +0000

    renaming 1.5.6 to 1.6

--- docs/install/db/atutor_upgrade_1.5.5_to_1.6.sql
+++ docs/install/db/atutor_upgrade_1.5.5_to_1.6.sql
@@ -3,9 +3,8 @@
 ###############################################################
 
 # one question per page - #3090
-ALTER TABLE `tests_results` ADD `max_pos` TINYINT UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `tests` ADD `display` TINYINT NOT NULL DEFAULT '0';
+#ALTER TABLE `tests_results` ADD `max_pos` TINYINT UNSIGNED NOT NULL DEFAULT '0';
+#ALTER TABLE `tests` ADD `display` TINYINT NOT NULL DEFAULT '0';
 
 INSERT INTO `themes` VALUES ('Greenmin', '1.5.6', 'greenmin', NOW(), 'This is the plone look-alike theme in green.', 1);
-
 
