1d8aac4bc45b1e02acde3d0ee8ecbe7c90e64190
greg gay <ggay@ocad.ca>
2007-12-06 16:23:40 +0000

    added reference to 1.5 series default theme

--- docs/install/db/atutor_upgrade_1.5.5_to_1.6.sql
+++ docs/install/db/atutor_upgrade_1.5.5_to_1.6.sql
@@ -6,5 +6,6 @@
 #ALTER TABLE `tests_results` ADD `max_pos` TINYINT UNSIGNED NOT NULL DEFAULT '0';
 #ALTER TABLE `tests` ADD `display` TINYINT NOT NULL DEFAULT '0';
 
-INSERT INTO `themes` VALUES ('Greenmin', '1.5.6', 'greenmin', NOW(), 'This is the plone look-alike theme in green.', 1);
+INSERT INTO `themes` VALUES ('Greenmin', '1.6', 'greenmin', NOW(), 'This is the plone look-alike theme in green.', 1);
+INSERT INTO `themes` VALUES ('ATutor 1.5', '1.6', 'default15', NOW(), 'This is the 1.5 series default theme.', 1);
 
