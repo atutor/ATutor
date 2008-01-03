08626d389a758a9ce53a79e2678e16f7be39e352
harris wong <hwong@ocad.ca>
2008-01-03 19:42:04 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3273

--- docs/include/lib/course.inc.php
+++ docs/include/lib/course.inc.php
@@ -13,6 +13,39 @@
 // $Id$
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 
+/**
+ * To resize course_icon images
+ * @param	uploaded image source path 
+ * @param	uploaded image path to be saved as
+ * @param	uploaded image's height
+ * @param	uploaded image width
+ * @param	save file with this height
+ * @param	save file with this width
+ * @param	file extension type
+ * @return	true if successful, false otherwise
+ */
+function resize_image($src, $dest, $src_h, $src_w, $dest_h, $dest_w, $type) {
+	$thumbnail_img = imagecreatetruecolor($dest_w, $dest_h);
+	if ($type == 'gif') {
+		$source = imagecreatefromgif($src);
+	} else if ($type == 'jpg') {
+		$source = imagecreatefromjpeg($src);
+	} else {
+		$source = imagecreatefrompng($src);
+	}
+	
+	$result = imagecopyresampled($thumbnail_img, $source, 0, 0, 0, 0, $dest_w, $dest_h, $src_w, $src_h);
+
+	if ($type == 'gif') {
+		$result &= imagegif($thumbnail_img, $dest);
+	} else if ($type == 'jpg') {
+		$result &= imagejpeg($thumbnail_img, $dest, 75);
+	} else {
+		$result &= imagepng($thumbnail_img, $dest, 7);
+	}
+	return $result;
+}
+
 function add_update_course($_POST, $isadmin = FALSE) {
 	require(AT_INCLUDE_PATH.'lib/filemanager.inc.php');
 
@@ -23,6 +56,7 @@ function add_update_course($_POST, $isadmin = FALSE) {
 	global $msg;
 	global $_config;
 	global $_config_defaults;
+	global $stripslashes;
 
 	$Backup =& new Backup($db);
 	$missing_fields = array();
@@ -56,6 +90,15 @@ function add_update_course($_POST, $isadmin = FALSE) {
 	$_POST['category_parent']	= intval($_POST['category_parent']);
 	$_POST['rss']       = intval($_POST['rss']);
 
+	// Custom icon
+	if ($_FILES['customicon']['name'] != ''){
+		// Use custom icon instead if it exists
+		$_POST['icon']	  = $addslashes($_FILES['customicon']['name']);
+	} 
+	if ($_FILES['customicon']['error'] == UPLOAD_ERR_FORM_SIZE){
+		// Check if filesize is too large for a POST
+		$msg->addError(array('FILE_MAX_SIZE', $_config['prof_pic_max_file_size'] . ' ' . _AT('bytes')));
+	}
 	if ($_POST['release_date']) {
 		$day_release	= intval($_POST['day_release']);
 		$month_release	= intval($_POST['month_release']);
@@ -264,6 +307,94 @@ function add_update_course($_POST, $isadmin = FALSE) {
 		$Backup->restore($material = TRUE, 'append', $initial_content_info[0], $initial_content_info[1]);
 	}
  
+ 	// custom icon, have to be after directory is created
+//	$_FILES['customicon'] = $_POST['customicon'];	//copy to $_FILES.
+	if($_FILES['customicon']['tmp_name'] != ''){
+        $_POST['comments'] = trim($_POST['comments']);
+
+        $owner_id = $_SESSION['course_id'];
+        $owner_type = "1";
+        if ($_FILES['customicon']['error'] == UPLOAD_ERR_INI_SIZE) {
+            $msg->addError(array('FILE_TOO_BIG', get_human_size(megabytes_to_bytes(substr(ini_get('upload_max_filesize'), 0, -1)))));
+        } else if (!isset($_FILES['customicon']['name']) || ($_FILES['customicon']['error'] == UPLOAD_ERR_NO_FILE) || ($_FILES['customicon']['size'] == 0)) {
+            $msg->addError('FILE_NOT_SELECTED');
+
+        } else if ($_FILES['customicon']['error'] || !is_uploaded_file($_FILES['customicon']['tmp_name'])) {
+            $msg->addError('FILE_NOT_SAVED');
+        }
+        
+        if (!$msg->containsErrors()) {
+            $_POST['description'] = $addslashes(trim($_POST['description']));
+            $_FILES['customicon']['name'] = addslashes($_FILES['customicon']['name']);
+
+            if ($_POST['comments']) {
+                $num_comments = 1;
+            } else {
+                $num_comments = 0;
+            }
+            
+            $path = AT_CONTENT_DIR.$owner_id."/custom_icons/";
+		
+            if (!is_dir($path)) {
+                @mkdir($path);
+            }
+			
+			// if we can upload custom course icon, it means GD is enabled, no need to check extension again.
+			$gd_info = gd_info();
+			$supported_images = array();
+			if ($gd_info['GIF Create Support']) {
+				$supported_images[] = 'gif';
+			}
+			if ($gd_info['JPG Support']) {
+				$supported_images[] = 'jpg';
+			}
+			if ($gd_info['PNG Support']) {
+				$supported_images[] = 'png';
+			}
+
+			// check if this is a supported file type
+			$filename   = $stripslashes($_FILES['customicon']['name']);
+			$path_parts = pathinfo($filename);
+			$extension  = strtolower($path_parts['extension']);
+			$image_attributes = getimagesize($_FILES['customicon']['tmp_name']);
+
+			if ($extension == 'jpeg') {
+				$extension = 'jpg';
+			}
+
+			// resize the original but don't backup a copy.
+			$width  = $image_attributes[0];
+			$height = $image_attributes[1];
+			$original_img	= $_FILES['customicon']['tmp_name'];
+			$thumbnail_img	= $path . $_FILES['customicon']['name'];
+
+			if ($width > $height && $width>79) {
+				$thumbnail_height = intval(79 * $height / $width);
+				$thumbnail_width  = 79;
+				if (!resize_image($original_img, $thumbnail_img, $height, $width, $thumbnail_height, $thumbnail_width, $extension)){
+					$msg->addError('FILE_NOT_SAVED');
+				}
+			} else if ($width <= $height && $height > 79) {
+				$thumbnail_height= 100;
+				$thumbnail_width = intval(100 * $width / $height);
+				if (!resize_image($original_img, $thumbnail_img, $height, $width, $thumbnail_height, $thumbnail_width, $extension)){
+					$msg->addError('FILE_NOT_SAVED');
+				}
+			} else {
+				// no resizing, just copy the image.
+				// it's too small to resize.
+				copy($original_img, $thumbnail_img);
+			}
+
+        } else {
+            $msg->addError('FILE_NOT_SAVED');
+            
+        }
+        //header('Location: index.php'.$owner_arg_prefix.'folder='.$parent_folder_id);
+        //exit;
+    }
+    //----------------------------------------
+
 	/* delete the RSS feeds just in case: */
 	if (file_exists(AT_CONTENT_DIR . 'feeds/' . $new_course_id . '/RSS1.0.xml')) {
 		@unlink(AT_CONTENT_DIR . 'feeds/' . $_POST['course'] . '/RSS1.0.xml');
@@ -276,7 +407,7 @@ function add_update_course($_POST, $isadmin = FALSE) {
 		$_SESSION['course_id'] = -1;
 	}
 
-	$_SESSION['course_title'] = stripslashes($_POST['title']);
+	$_SESSION['course_title'] = $stripslashes($_POST['title']);
 	return $new_course_id;
 }
 
--- docs/tools/course_properties.php
+++ docs/tools/course_properties.php
@@ -20,39 +20,6 @@ require(AT_INCLUDE_PATH.'lib/file_storage.inc.php');
 
 authenticate(AT_PRIV_ADMIN);
 
-/**
- * To resize course_icon images
- * @param	uploaded image source path 
- * @param	uploaded image path to be saved as
- * @param	uploaded image's height
- * @param	uploaded image width
- * @param	save file with this height
- * @param	save file with this width
- * @param	file extension type
- * @return	true if successful, false otherwise
- */
-function resize_image($src, $dest, $src_h, $src_w, $dest_h, $dest_w, $type) {
-	$thumbnail_img = imagecreatetruecolor($dest_w, $dest_h);
-	if ($type == 'gif') {
-		$source = imagecreatefromgif($src);
-	} else if ($type == 'jpg') {
-		$source = imagecreatefromjpeg($src);
-	} else {
-		$source = imagecreatefrompng($src);
-	}
-	
-	$result = imagecopyresampled($thumbnail_img, $source, 0, 0, 0, 0, $dest_w, $dest_h, $src_w, $src_h);
-
-	if ($type == 'gif') {
-		$result &= imagegif($thumbnail_img, $dest);
-	} else if ($type == 'jpg') {
-		$result &= imagejpeg($thumbnail_img, $dest, 75);
-	} else {
-		$result &= imagepng($thumbnail_img, $dest, 7);
-	}
-	return $result;
-}
-
 $course = $_SESSION['course_id'];
 $isadmin   = FALSE;
 
@@ -63,99 +30,6 @@ if (isset($_POST['cancel'])) {
 
 
 }else if($_POST['submit']){
-
-    // added by Martin - for custom course icons
-
-    if($_FILES['customicon']['tmp_name'] != ''){
-        $_POST['comments'] = trim($_POST['comments']);
-
-        $owner_id = $_SESSION['course_id'];
-        $owner_type = "1";
-            
-        if ($_FILES['customicon']['error'] == UPLOAD_ERR_INI_SIZE) {
-            $msg->addError(array('FILE_TOO_BIG', get_human_size(megabytes_to_bytes(substr(ini_get('upload_max_filesize'), 0, -1)))));
-
-        } else if (!isset($_FILES['customicon']['name']) || ($_FILES['customicon']['error'] == UPLOAD_ERR_NO_FILE) || ($_FILES['customicon']['size'] == 0)) {
-            $msg->addError('FILE_NOT_SELECTED');
-
-        } else if ($_FILES['customicon']['error'] || !is_uploaded_file($_FILES['customicon']['tmp_name'])) {
-            $msg->addError('FILE_NOT_SAVED');
-        }
-        
-        if (!$msg->containsErrors()) {
-            $_POST['description'] = $addslashes(trim($_POST['description']));
-            $_FILES['customicon']['name'] = addslashes($_FILES['customicon']['name']);
-
-            if ($_POST['comments']) {
-                $num_comments = 1;
-            } else {
-                $num_comments = 0;
-            }
-            
-            $path = AT_CONTENT_DIR.$owner_id."/custom_icons/";
-		
-            if (!is_dir($path)) {
-                @mkdir($path);
-            }
-			
-			// if we can upload custom course icon, it means GD is enabled, no need to check extension again.
-			$gd_info = gd_info();
-			$supported_images = array();
-			if ($gd_info['GIF Create Support']) {
-				$supported_images[] = 'gif';
-			}
-			if ($gd_info['JPG Support']) {
-				$supported_images[] = 'jpg';
-			}
-			if ($gd_info['PNG Support']) {
-				$supported_images[] = 'png';
-			}
-
-			// check if this is a supported file type
-			$filename   = $stripslashes($_FILES['customicon']['name']);
-			$path_parts = pathinfo($filename);
-			$extension  = strtolower($path_parts['extension']);
-			$image_attributes = getimagesize($_FILES['customicon']['tmp_name']);
-
-			if ($extension == 'jpeg') {
-				$extension = 'jpg';
-			}
-
-			// resize the original but don't backup a copy.
-			$width  = $image_attributes[0];
-			$height = $image_attributes[1];
-			$original_img	= $_FILES['customicon']['tmp_name'];
-			$thumbnail_img	= $path . $_FILES['customicon']['name'];
-
-			if ($width > $height && $width>79) {
-				$thumbnail_height = intval(79 * $height / $width);
-				$thumbnail_width  = 79;
-				if (!resize_image($original_img, $thumbnail_img, $height, $width, $thumbnail_height, $thumbnail_width, $extension)){
-					$msg->addError('FILE_NOT_SAVED');
-				}
-			} else if ($width <= $height && $height > 79) {
-				$thumbnail_height= 100;
-				$thumbnail_width = intval(100 * $width / $height);
-				if (!resize_image($original_img, $thumbnail_img, $height, $width, $thumbnail_height, $thumbnail_width, $extension)){
-					$msg->addError('FILE_NOT_SAVED');
-				}
-			} else {
-				// no resizing, just copy the image.
-				// it's too small to resize.
-				copy($original_img, $thumbnail_img);
-			}
-
-        } else {
-            $msg->addError('FILE_NOT_SAVED');
-            
-        }
-        $_POST['icon'] = $_FILES['customicon']['name'];
-        //header('Location: index.php'.$owner_arg_prefix.'folder='.$parent_folder_id);
-        //exit;
-    }
-
-    //----------------------------------------
-
 	require(AT_INCLUDE_PATH.'lib/course.inc.php');
 	$_POST['instructor'] = $_SESSION['member_id'];
 
