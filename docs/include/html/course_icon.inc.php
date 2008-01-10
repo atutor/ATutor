d7baace1277e0f61faf367731f2c84446b1ef88c
greg gay <ggay@ocad.ca>
2008-01-10 17:04:55 +0000

    fixed GD requirment, to display not supported message if absent

--- docs/include/html/course_icon.inc.php
+++ docs/include/html/course_icon.inc.php
@@ -10,46 +10,43 @@
 /* modify it under the terms of the GNU General Public License			*/
 /* as published by the Free Software Foundation.						*/
 /************************************************************************/
-// $Id$
+// $Id
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 
-?>
 
-<?php
 // check if GD is installed
 if (!extension_loaded('gd')) {
-	require(AT_INCLUDE_PATH.'header.inc.php');
-	$msg->printInfos('FEATURE_NOT_AVAILABLE');
-	require(AT_INCLUDE_PATH.'footer.inc.php');
-	exit;
-}
+	$msg->addInfo('FEATURE_NOT_AVAILABLE');
+}else{
 
-$gd_info = gd_info();
-$supported_images = array();
-if ($gd_info['GIF Create Support']) {
-	$supported_images[] = 'gif';
-}
-if ($gd_info['JPG Support']) {
-	$supported_images[] = 'jpg';
-}
-if ($gd_info['PNG Support']) {
-	$supported_images[] = 'png';
-}
+	$gd_info = gd_info();
+	$supported_images = array();
 
-if (!$supported_images) {
-	require(AT_INCLUDE_PATH.'header.inc.php');
-	$msg->printInfos('FEATURE_NOT_AVAILABLE');
-	require(AT_INCLUDE_PATH.'footer.inc.php');
-	exit;
+	if ($gd_info['GIF Create Support']) {
+		$supported_images[] = 'gif';
+	}
+	if ($gd_info['JPG Support']) {
+		$supported_images[] = 'jpg';
+	}
+	if ($gd_info['PNG Support']) {
+		$supported_images[] = 'png';
+	}
+	
+	if (!$supported_images) {
+		$msg->addInfo('FEATURE_NOT_AVAILABLE');
+	}
 }
-
 ?>
 <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $_config['prof_pic_max_file_size']; ?>" />
 
     <div style="float:right;width:40%;">
 		<h3 style="padding-top:0;"><?php echo _AT('upload_icon'); ?></h3>
+		<?php
+			if(!extension_loaded('gd')){
+				$msg->printInfos();
+			}else{
+
+		?>
 		<input type="file"  name="customicon" id="customicon" /> (<?php echo implode(', ', $supported_images); ?>)
+		<?php } ?>
 	</div>
-
-
-<?php  //require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
