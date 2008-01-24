87f6504edc76b2696b7e4ab471b89d596e475573
cindy li <cli@ocad.ca>
2008-01-24 19:47:25 +0000

    1. Menu change: instead of displaying "UTF8 convert" in navigation, make it a submenu in "content" for instructors and a submenu in "course" for admins.
    2. More functionalities to handle the scenarios that 0 or more than 1 character sets is/are found in html & xml files. In these cases, display a drop-down list box to let user to select.

--- mods/utf8conv/module.php
+++ mods/utf8conv/module.php
@@ -16,52 +16,20 @@ define('AT_PRIV_UTF8CONV',       $this->getPrivilege());
 define('AT_ADMIN_PRIV_UTF8CONV', $this->getAdminPrivilege());
 
 /*******
- * create a side menu box/stack.
- */
-//$this->_stacks['utf8conv'] = array('title_var'=>'utf8conv', 'file'=>'mods/utf8conv/side_menu.inc.php');
-// ** possible alternative: **
-// $this->addStack('utf8conv', array('title_var' => 'utf8conv', 'file' => './side_menu.inc.php');
-
-/*******
- * if this module is to be made available to students on the Home or Main Navigation.
- */
-//$_group_tool = $_student_tool = 'mods/utf8conv/index.php';
-//$_student_tool = 'mods/utf8conv/index.php';
-
-/*******
  * add the admin pages when needed.
  */
 if (admin_authenticate(AT_ADMIN_PRIV_UTF8CONV, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-	$this->_pages[AT_NAV_ADMIN] = array('mods/utf8conv/index_admin.php');
+	$this->_pages['admin/courses.php']['children'] = array('mods/utf8conv/index_admin.php');
 	$this->_pages['mods/utf8conv/index_admin.php']['title_var'] = 'utf8conv';
-	$this->_pages['mods/utf8conv/index_admin.php']['parent']    = AT_NAV_ADMIN;
+	$this->_pages['mods/utf8conv/index_admin.php']['parent']   = 'admin/courses.php';
 }
 
 /*******
  * instructor Manage section:
  */
+$this->_pages['tools/content/index.php']['children'] = array('mods/utf8conv/index_instructor.php');
 $this->_pages['mods/utf8conv/index_instructor.php']['title_var'] = 'utf8conv';
-$this->_pages['mods/utf8conv/index_instructor.php']['parent']   = 'tools/index.php';
-// ** possible alternative: **
-// $this->pages['./index_instructor.php']['title_var'] = 'utf8conv';
-// $this->pages['./index_instructor.php']['parent']    = 'tools/index.php';
-
-/*******
- * student page.
- */
-//$this->_pages['mods/utf8conv/index.php']['title_var'] = 'utf8conv';
-//$this->_pages['mods/utf8conv/index.php']['img']       = 'mods/utf8conv/utf8conv.jpg';
-
-
-/* public pages */
-//$this->_pages[AT_NAV_PUBLIC] = array('mods/utf8conv/index_public.php');
-//$this->_pages['mods/utf8conv/index_public.php']['title_var'] = 'utf8conv';
-//$this->_pages['mods/utf8conv/index_public.php']['parent'] = AT_NAV_PUBLIC;
-
-/* my start page pages */
-//$this->_pages[AT_NAV_START]  = array('mods/utf8conv/index_mystart.php');
-//$this->_pages['mods/utf8conv/index_mystart.php']['title_var'] = 'utf8conv';
-//$this->_pages['mods/utf8conv/index_mystart.php']['parent'] = AT_NAV_START;
+$this->_pages['mods/utf8conv/index_instructor.php']['parent']   = 'tools/content/index.php';
 
 function utf8conv_get_group_url($group_id) {
 	return 'mods/utf8conv/index.php';
---
+++ mods/utf8conv/utf8conv.php
@@ -0,0 +1,386 @@
+<?php
+/****************************************************************/
+/* ATutor														                            */
+/****************************************************************/
+/* Copyright (c) 2002-2008 by Greg Gay & Cindy Qi Li            */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca												                      */
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.				        */
+/****************************************************************/
+// $Id: utf8conv.php 2008-01-23 14:49:24Z cindy $
+/*
+ * This script only works when being included in the scripts
+ * where vitals.inc.php is included as:
+ * 
+ * define('AT_INCLUDE_PATH', '../../include/');
+ * require (AT_INCLUDE_PATH.'vitals.inc.php');
+ * 
+ * structure of this document (in order):
+ * 
+ * 1. Unzip uploaded file to module's content directory
+ * 2. Read content folder recursively to convert
+ *    Read thru all html and xml file to find all character sets
+ *    definitions. If number of character sets are:
+ *    1) 1: perfect scenario, convert all given file types to it
+ *    2) 0: display default charsets for user to choose one of them
+ *          and convert all given file types, except html, xml, to it
+ *    3) More than 1: display them for user to choose one and 
+ *          convert all given file types, except html, xml, to it
+ * 3. Zip converted files
+ * 4. force zipped converted file to download
+ * 5. clear all temp files
+ *
+ ***/
+
+// Define character set to convert to
+$charset_to = "UTF-8";
+
+// Define all file types to be converted
+$filetypes = ARRAY('html', 'xml', 'csv', 'txt', 'sql');
+
+$filetypes = array_change_key_case($filetypes);   // use lower case
+
+$default_charsets = ARRAY(
+'BIG5','EUC-JP','GB2312','ISO-8859-1','ISO-8859-2','ISO-8859-3','ISO-8859-4','ISO-8859-5',
+'ISO-8859-6','ISO-8859-7','ISO-8859-8','ISO-8859-9','ISO-8859-10','ISO-8859-11','ISO-8859-13',
+'ISO-8859-14','ISO-8859-15','ISO-8859-16','US-ASCII','WINDOWS-874','WINDOWS-936','WINDOWS-1250',
+'WINDOWS-1251','WINDOWS-1252','WINDOWS-1253','WINDOWS-1254','WINDOWS-1255','WINDOWS-1256',
+'WINDOWS-1257','WINDOWS-1258');
+
+$charsets = array();
+
+// This function finds charset definition from html & xml files
+function find_charset($filename)
+{
+	if (preg_match("/\.html$/i", $filename)) 
+	{ 
+		$pattern = '/<meta.*charset=(.*) /i';
+		preg_match($pattern, file_get_contents($filename), $matches);
+		
+		// remove quote signs in the match
+		$charset = strtoupper(trim(preg_replace('/(\'|\")/', '', $matches[1])));
+	}
+	
+	if (preg_match("/\.xml$/i", $filename)) 
+	{ 
+		if (preg_match("#<charset>(.*)</charset>#i", file_get_contents($filename), $matches)) 
+		{
+			$charset  = strtoupper(trim($matches[1]));
+		}
+	}
+	
+	return $charset;
+}
+
+// This function finds all charsets defined in all html & xml files in the given zip
+function find_all_charsets($path, $filename)
+{
+	global $charsets;
+	
+	$charset = find_charset($path);
+
+	if (strlen($charset) > 0 && !in_array($charset, $charsets))
+	{
+			array_push($charsets, $charset);
+	}
+}
+
+// This function does:
+// 1. Find all "charset=XXX" in html <meta> tag and replace the tag 
+//    value to "UTF-8"
+// 2. Convert the file from old charset "XXX" to new charset "UTF-8"
+// 3. Only process html and xml files
+function convert($path, $filename)
+{
+	global $charset_from, $charset_to;
+	global $filetypes;
+	global $charsets;
+	
+	// 1. html & xml files:
+	//    if charset is defined, convert from defined charset,
+	//    otherwise, convert from $charset_from
+	// 2. Other files with defined file type
+	//    convert from $charset_from
+	if ((in_array('html', $filetypes) && preg_match("/\.html$/i", $path)) ||
+	    (in_array('xml', $filetypes) && preg_match("/\.xml$/i", $path))) 
+	{ 
+		$charset_in = find_charset($path);
+	
+		$content = file_get_contents($path);
+		// convert file
+		if (strlen($charset_in) > 0)
+		{
+			// replace old charset in <meta> tag to new charset
+			$content = str_ireplace($charset_in,$charset_to,$content);
+	
+			// convert file from old charset to new charset
+			$content = iconv($charset_in, $charset_to. '//IGNORE', $content);
+		}
+		else
+		{
+			$content = iconv($charset_from, $charset_to. '//IGNORE', $content);
+		} // end inner if
+	
+		$fp = fopen($path,'w');
+		fwrite($fp,$content);
+		fclose($fp);
+	}
+	elseif (in_array(strtolower(substr($path, (strripos($path, '.')+1))),$filetypes))
+	{
+		$content = file_get_contents($path);
+		$content = iconv($charset_from, $charset_to. '//IGNORE', $content);
+
+		$fp = fopen($path,'w');
+		fwrite($fp,$content);
+		fclose($fp);
+	}
+}
+
+// This function display a drop-down box with all options as values in $charsets_array
+function display_options($charsets_array)
+{
+?>
+	<select name="charfrom" id="charfrom" class="input">
+<?php
+	foreach($charsets_array as $charset)
+	{
+?>
+    <option><?php echo $charset; ?></option>
+<?php
+	}
+?>
+  </select>
+<?php
+}
+
+// This function deletes $dir recrusively without deleting given dir itself.
+function clear_dir($dir) {
+	require(AT_INCLUDE_PATH . '/lib/filemanager.inc.php');
+	
+	if(!$opendir = @opendir($dir)) {
+		return false;
+	}
+	
+	while(($readdir=readdir($opendir)) !== false) {
+		if (($readdir !== '..') && ($readdir !== '.')) {
+			$readdir = trim($readdir);
+
+			clearstatcache(); /* especially needed for Windows machines: */
+
+			if (is_file($dir.'/'.$readdir)) {
+				if(!@unlink($dir.'/'.$readdir)) {
+					return false;
+				}
+			} else if (is_dir($dir.'/'.$readdir)) {
+				/* calls lib function to clear subdirectories recrusively */
+				if(!clr_dir($dir.'/'.$readdir)) {
+					return false;
+				}
+			}
+		}
+	} /* end while */
+
+	@closedir($opendir);
+	
+	return true;
+}
+
+// Main Convert process
+if (isset($_POST['Convert']) || isset($_POST['Go']))
+{
+	$module_content_folder = AT_CONTENT_DIR . "utf8conv";
+	
+	// clean up module content folder
+	clear_dir($module_content_folder);
+	
+	require(AT_INCLUDE_PATH . '/classes/pclzip.lib.php');
+	
+	// 1. unzip uploaded file to module's content directory
+	$archive = new PclZip($_FILES['userfile']['tmp_name']);
+
+	if ($archive->extract(PCLZIP_OPT_PATH, $module_content_folder) == 0)
+	{
+    clear_dir($module_content_folder);
+    die("Cannot unzip file " . $_FILES['userfile']['tmp_name'] . "<br>Error : ".$archive->errorInfo(true));
+  }
+  
+  // 2. Read content folder recursively to convert.
+  require("readDir.php");
+  
+	$dir = new readDir();
+	// set the directory to read
+	if (!$dir->setPath( $module_content_folder )) 
+	{ 
+		clear_dir($module_content_folder);
+		die($dir->error());
+	} 
+
+	// set recursive reading of sub folders
+	$dir->readRecursive(true); 
+	
+	// set a function to call when a new file is read
+	if (!$dir->setEvent( 'readDir_file', 'find_all_charsets' ))
+	{ 
+		clear_dir($module_content_folder);
+		die($dir->error());
+	} 
+	
+	// read the dir
+	if ( !$dir->read() ) 
+	{ 
+		clear_dir($module_content_folder);
+		die($dir->error());
+	}
+	
+	// If only one character set is found in all html & xml files
+	if ((count($charsets) == 1 && $_POST['Convert']) ||
+	    (count($charsets) != 1 && $_POST['Go']))
+	{
+		if (count($charsets) == 1 && $_POST['Convert'])
+			$charset_from = $charsets[0];
+		elseif (count($charsets) != 1 && $_POST['Go'])
+			$charset_from = $_POST['charfrom'];
+		
+
+		$dir = new readDir();
+		$dir->setPath( $module_content_folder );
+		$dir->readRecursive(true); 
+		$dir->setEvent( 'readDir_file', 'convert' );
+		$dir->read();
+	
+	  // 3. ZIP converted files
+	  $zip_filename = AT_CONTENT_DIR . "/" . str_replace('.zip','_'.$charset_to . '.zip', $_FILES['userfile']['name']);
+	
+	  $archive = new PclZip($zip_filename);
+	
+	  if ($archive->create($module_content_folder, PCLZIP_OPT_REMOVE_PATH, $module_content_folder) == 0) {
+	    clear_dir($module_content_folder);
+	    die("Cannot zip converted files. <br>Error : ".$archive->errorInfo(true));
+	  }
+	
+		// 4. force zipped converted file to download
+		ob_end_clean();
+	
+		header('Content-Type: application/x-zip');
+		header('Content-transfer-encoding: binary'); 
+		header('Content-Disposition: attachment; filename="'.htmlspecialchars(basename($zip_filename)) . '"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Length: '.filesize($zip_filename));
+	
+		readfile($zip_filename);
+	
+		// 5. clear all temp files
+		unlink($zip_filename);
+		clear_dir($module_content_folder);
+	}
+}
+// End of main convert process
+
+require (AT_INCLUDE_PATH.'header.inc.php');
+
+// Check ICONV library is installed and enabled
+if (!extension_loaded('iconv') || !function_exists('iconv'))
+{
+	die ('Warning: This utility is not available as PHP ICONV module is not installed or not enabled.');
+}
+
+?>
+
+<HTML>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+
+String.prototype.trim = function() {
+	return this.replace(/^\s+|\s+$/g,"");
+}
+
+// This function validates if and only if a zip file is given
+function SubmitFile() {
+  // check file type
+  var file = document.frm_upload.userfile.value;
+  if (!file || file.trim()=='') {
+    alert('Please give a zip file!');
+    return;
+  }
+  
+  while (file.indexOf("\\") != -1)
+      file = file.slice(file.indexOf("\\") + 1);
+  
+  var ext = file.slice(file.lastIndexOf(".")).toLowerCase();
+  if(ext != '.zip') {
+    alert('Please upload ZIP file only!');
+    return false;
+  }
+}
+
+//  End -->
+//-->
+</script>
+
+<BODY>
+  <FORM NAME="frm_upload" ENCTYPE="multipart/form-data" METHOD=POST ACTION="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return SubmitFile()">
+
+  <TABLE>	
+		<TR>
+      <TD colspan="3"><strong>Upload a zip file to convert the character set to UTF-8</strong><br /></TD>
+		</TR>
+
+		<TR>
+			<INPUT TYPE="hidden" name="MAX_FILE_SIZE" VALUE="52428800">
+			<TD><INPUT TYPE="file" NAME="userfile" SIZE=50></td>
+			<TD><INPUT TYPE="submit" name="Convert" value="Convert" class="submit" /></TD>
+		</TR>
+<?php
+if ($_POST["Convert"] && count($charsets) != 1)
+{
+?>
+		<TR>
+      <TD colspan="3"><strong>Multiple character sets are defined in html & xml files, please select one to convert from:</strong><br /></TD>
+		</TR>
+		<TR>
+			<TD>
+<?php
+	if (count($charsets) > 1)
+	{
+?>
+		<TR>
+      <TD colspan="3"><strong>Multiple character sets are defined in html & xml files, please select one to convert from:</strong><br /></TD>
+		</TR>
+		<TR>
+			<TD>
+<?php
+		display_options($charsets);
+	}
+	else
+	{
+?>
+		<TR>
+      <TD colspan="3"><strong>No character set is found, please select one to convert from:</strong><br /></TD>
+		</TR>
+		<TR>
+			<TD>
+<?php
+		display_options($default_charsets);
+	}
+?>
+		</TD>
+			<TD><INPUT TYPE="submit" name="Go" value="Go" class="submit" /></TD>
+		</TR>
+
+<?php
+}
+?>
+  </TABLE>
+	</FORM>
+  <hr/>  
+</BODY>	
+</HTML>
+
+
+<?php require (AT_INCLUDE_PATH.'footer.inc.php'); ?>
