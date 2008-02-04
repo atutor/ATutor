4b0cea5154649bce23ea96d03ab9806e05a633d2
harris wong <hwong@ocad.ca>
2008-02-04 16:59:00 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3268

--- docs/documentation/instructor/tests_surveys.php
+++ docs/documentation/instructor/tests_surveys.php
@@ -3,4 +3,6 @@
 <h2>Tests and Surveys</h2>
 	<p>The instructor, and assistants with test privileges, can create tests and surveys to be administered to enrolled students. There are a variety of options for defining tests like setting the release date, and using randomized questions or group-specific tests. Once a test or survey has been created, add questions to the Question Database, and then add these questions to the new test. </p>
 
+	<p>Please be aware that the instructor could no long modify the anonymous option when submission has been made on a test.</p>
+
 <?php require('../common/body_footer.inc.php'); ?>
