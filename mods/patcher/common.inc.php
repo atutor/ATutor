55f27678f58bcc9edf4caa9f6b89792cd5171250
cindy li <cli@ocad.ca>
2008-03-04 16:05:40 +0000

    change wording

--- mods/patcher/common.inc.php
+++ mods/patcher/common.inc.php
@@ -30,7 +30,7 @@ function print_feedback( $feedback, $notes='' ) {
 	<div class="input-form">
 	<table border="0" class="fbkbox" cellpadding="3" cellspacing="2" width="100%" summary="" align="center">
 	<tr class="fbkbox">
-	<td><h3 class="feedback2"><img src="images/feedback.gif" align="top" alt="" class="img" /> The patch is installed successfully!</h3>
+	<td><h3 class="feedback2"><img src="images/feedback.gif" align="top" alt="" class="img" /> The patch has been installed successfully!</h3>
 		<?php
 			echo '<ul>';
 			foreach ($feedback as $p) {
