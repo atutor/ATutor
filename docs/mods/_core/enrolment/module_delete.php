6156137401b3a5ce350d0cf1f33496befa15a573
cindy li <cli@ocad.ca>
2008-03-10 17:49:14 +0000

    delete associated records from auto_enroll_courses when a course is deleted

--- docs/mods/_core/enrolment/module_delete.php
+++ docs/mods/_core/enrolment/module_delete.php
@@ -5,6 +5,9 @@ function enrolment_delete($course) {
 
 	$sql	= "DELETE FROM ".TABLE_PREFIX."course_enrollment WHERE course_id=$course";
 	$result = mysql_query($sql, $db);
+
+	$sql	= "DELETE FROM ".TABLE_PREFIX."auto_enroll_courses WHERE course_id=$course";
+	$result = mysql_query($sql, $db);
 }
 
 ?>
