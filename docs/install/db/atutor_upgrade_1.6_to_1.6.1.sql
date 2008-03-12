16e03c895692d9bdf1776c48711d1cdc11a929fc
cindy li <cli@ocad.ca>
2008-03-12 15:15:55 +0000

    add db change for enable auto enrollment at registration

--- docs/install/db/atutor_upgrade_1.6_to_1.6.1.sql
+++ docs/install/db/atutor_upgrade_1.6_to_1.6.1.sql
@@ -3,22 +3,25 @@
 ###############################################################
 
 # support new changes for Test/Survey
-INSERT INTO `language_text` VALUES 
-('en', '_template', 'pass_score', 'Pass Score', now(), ''),
-('en', '_template', 'pass_feedback', 'Pass Feedback', now(), ''),
-('en', '_template', 'fail_feedback', 'Fail Feedback', now(), ''),
-('en', '_template', 'test_description', 'Test Description', now(), ''),
-('en', '_template', 'no_pass_score', 'No pass score', now(), ''),
-('en', '_template', 'percentage_score', 'percentage score', now(), ''),
-('en', '_template', 'points_score', 'points score', now(), ''),
-('en', '_template', 'all_passed_students', 'All Passed Students', now(), ''),
-('en', '_template', 'all_failed_students', 'All Failed Students', now(), '')
-;
-
 ALTER TABLE `tests`
 ADD `description` TEXT NOT NULL AFTER `title` , 
 ADD `passscore` MEDIUMINT NOT NULL AFTER `content_id` , 
 ADD `passpercent` MEDIUMINT NOT NULL AFTER `passscore` ,
 ADD `passfeedback` TEXT NOT NULL AFTER `passpercent` , 
 ADD `failfeedback` TEXT NOT NULL AFTER `passfeedback` ;
+
+# support auto enrollment at registration
+CREATE TABLE `auto_enroll` (
+   `auto_enroll_id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+   `associate_string` VARCHAR(10) NOT NULL,
+   `name` VARCHAR( 50 ) NOT NULL default '',
+   PRIMARY KEY ( `auto_enroll_id` )
+);
+
+CREATE TABLE `auto_enroll_courses` (
+   `auto_enroll_courses_id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+   `auto_enroll_id` MEDIUMINT UNSIGNED NOT NULL default 0,
+   `course_id` MEDIUMINT UNSIGNED NOT NULL default 0,
+   PRIMARY KEY ( `auto_enroll_courses_id` )
+);
 
