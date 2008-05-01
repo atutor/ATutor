35aa30a055afb08ba90157542d5a8a698b6b4aa8
cindy li <cli@ocad.ca>
2008-05-01 13:50:04 +0000

    When student unenroll from the course, unsubscribe the student from forums and forum threads of this course

--- docs/users/remove_course.php
+++ docs/users/remove_course.php
@@ -24,8 +24,20 @@ if (isset($_POST['submit_no'])) {
 	$course = intval($_POST['course']);
 	if ($system_courses[$course]['member_id'] != $_SESSION['member_id']) {
 		$sql	= "DELETE FROM ".TABLE_PREFIX."course_enrollment WHERE member_id=$_SESSION[member_id] AND course_id=$course";
+		$result = mysql_query($sql, $db) or die(mysql_error());
 
-		$result = mysql_query($sql, $db);
+		// Unsubscribe from forums and threads of the course
+		$sql	= "DELETE FROM ".TABLE_PREFIX."forums_subscriptions 
+		         WHERE forum_id IN (SELECT forum_id FROM ".TABLE_PREFIX."forums_courses WHERE course_id=$course)
+		           AND member_id=".$_SESSION[member_id];
+		$result = mysql_query($sql, $db) or die(mysql_error());
+
+		$sql	= "UPDATE ".TABLE_PREFIX."forums_accessed 
+		           SET subscribe = 0
+		         WHERE post_id IN (SELECT distinct t.post_id FROM ".TABLE_PREFIX."forums_courses c, ".TABLE_PREFIX."forums_threads t WHERE c.course_id=$course)
+		           AND member_id=".$_SESSION[member_id];
+		$result = mysql_query($sql, $db) or die(mysql_error());
+
 		$msg->addFeedback('COURSE_REMOVED');
 	}
 	header("Location: ".AT_BASE_HREF."users/index.php");
