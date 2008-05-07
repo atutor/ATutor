c22e3ea9647bce13e47a07219002e28450f09ab3
cindy li <cli@ocad.ca>
2008-05-07 16:39:41 +0000

    add new fields content.head, content.use_customized_head to the end of the table to solve the problem of restoring backups created from old ATutor versions.

--- docs/mods/_core/content/module_backup.php
+++ docs/mods/_core/content/module_backup.php
@@ -2,7 +2,7 @@
 
 $sql = array();
 
-$sql['content'] = 'SELECT content_id, content_parent_id, ordering, last_modified, revision, formatting, release_date, head, use_customized_head, keywords, content_path, title, text FROM '.TABLE_PREFIX.'content WHERE course_id=? ORDER BY content_parent_id, ordering';
+$sql['content'] = 'SELECT content_id, content_parent_id, ordering, last_modified, revision, formatting, release_date, keywords, content_path, title, text, head, use_customized_head FROM '.TABLE_PREFIX.'content WHERE course_id=? ORDER BY content_parent_id, ordering';
 
 $sql['related_content'] = 'SELECT R.content_id, R.related_content_id FROM '.TABLE_PREFIX.'related_content R, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND R.content_id=C.content_id ORDER BY R.content_id ASC';
 
