7182fd79ad88c1c53eb758b4f7ada72815ed93b8
cindy li <cli@ocad.ca>
2008-05-16 17:56:55 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3399
    Removed some tinymce buttons that are lack of accessibility for the disabled

--- docs/include/lib/tinymce.inc.php
+++ docs/include/lib/tinymce.inc.php
@@ -22,6 +22,28 @@ function load_editor($name = FALSE, $mode="textareas") {
 		$course_base_href = 'content/' . $_SESSION['course_id'] . '/'; 
 	}
 
+// Note: Some tinymce buttons are removed due to lack of accessibility for disabled.
+// They are:
+//New Document: newdocument
+//Insert Time: inserttime
+//Insert Date: insertdate
+//Preview: preview
+//toggle guidelines: visualaid
+//spellcheck: iespell
+//embed media: media
+//print: print
+//Insert Layer: insertlayer
+//move forward: moveforward
+//move backward: movebackward
+//toggle absolute positioning: absolute
+//citation: cite
+//deletion: del
+//insertion: ins
+//visual control characters on/off: visualchars
+//insert non-breaking space character: nonbreaking
+//insert predefined template: template
+//insert page break: pagebreak
+
 	echo '
 <script language="javascript" type="text/javascript" src="'.AT_BASE_HREF.'jscripts/tiny_mce/tiny_mce.js"></script>
 <script language="javascript" type="text/javascript">
@@ -43,10 +65,10 @@ tinyMCE.init({
 	plugins : "acheck, table,safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
 	// Theme options
-	theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,|,acheck",
+	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,forecolor,backcolor",
+	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,|,sub,sup,|,charmap,emotions,advhr,|,ltr,rtl,|,fullscreen",
+	theme_advanced_buttons4 : "styleprops,|,abbr,acronym,attribs,|,acheck",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_path_location : "bottom",
