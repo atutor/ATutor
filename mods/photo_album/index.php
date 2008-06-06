45759742d67a3f33964c17e6ad134c2a2b5631a0
greg gay <ggay@ocad.ca>
2008-06-06 13:57:04 +0000

    fix the bug that the fileRoot is not set. Set it to "./mods/photo_album/Template"

--- mods/photo_album/index.php
+++ mods/photo_album/index.php
@@ -94,6 +94,7 @@ if ($index->isError()!=true){	//if there is no error in index object, display th
 	
 	/* display index page from here */
 	$template=new HTML_Template_ITX("./Template");
+	$template->setRoot("./mods/photo_album/Template");
 	$template->loadTemplatefile("index.tpl.php", true, true);
 	
 	/* display images */
