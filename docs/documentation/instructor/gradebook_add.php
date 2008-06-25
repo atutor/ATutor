2a4260bdfc38a8c5d03bfc0415863691afad4a04
greg gay <ggay@ocad.ca>
2008-06-25 15:15:24 +0000

    added note about surveys missing from available test to add to the gradebook.

--- docs/documentation/instructor/gradebook_add.php
+++ docs/documentation/instructor/gradebook_add.php
@@ -21,7 +21,7 @@ require('../common/body_header.inc.php'); ?>
 	<dd>Assignments that have been created using the ATutor Assignment Manager can be added to the gradebook. Select the <strong>Title</strong> of the assignment from those available, then select the scale to be used. Once an assignment has been added to the gradebook, marks are entered as <strong>External Marks</strong>. Marks may be entered either as a percentage mark or a scale mark. If percentage is used, the gradebook will attempt to convert those percentage marks to a scale mark</dd>
 
 	<dt>Add ATutor Test</dt>
-	<dd>Tests that are created using the ATutor Test & Survey Manager can be added to the gradebook if the test's <strong>Attempts Allowed</strong> property has been set to 1 attempt.   Select the test <strong>Title</strong> from those available, then select the Grade Scale to be used for the test. Additional scales can be created by using the Grade Scales tool.</dd>
+	<dd>Tests that are created using the ATutor Test & Survey Manager can be added to the gradebook if the test's <strong>Attempts Allowed</strong> property has been set to 1 attempt.   Select the test <strong>Title</strong> from those available, then select the Grade Scale to be used for the test. Additional scales can be created by using the Grade Scales tool. Note that Surveys will not be available to add to the Gradebook. Surveys are technically  tests in the Test & Survey Manager that do not have any "weight" or mark assigned to questions.</dd>
 </dl>
 
 
