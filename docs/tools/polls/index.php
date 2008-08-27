601bee6abe7f3cbd82f818b8756b1f20e23eaa80
greg gay <ggay@ocad.ca>
2008-08-27 14:46:02 +0000

    added at_timezone()

--- docs/tools/polls/index.php
+++ docs/tools/polls/index.php
@@ -87,7 +87,7 @@ $result = mysql_query($sql, $db);
 		<tr onmousedown="document.form['p_<?php echo $row['poll_id']; ?>'].checked = true; rowselect(this);" id="r_<?php echo $row['poll_id']; ?>">
 			<td><input type="radio" id="p_<?php echo $row['poll_id']; ?>" name="poll" value="<?php echo $row['poll_id']; ?>" /></td>
 			<td><label for="p_<?php echo $row['poll_id']; ?>"><?php echo AT_print($row['question'], 'polls.question'); ?></label></td>
-			<td><?php echo $row['created_date']; ?></td>
+			<td><?php echo at_timezone($row['created_date']); ?></td>
 			<td><?php echo $row['total']; ?></td>
 		</tr>
 	<?php } while($row = mysql_fetch_assoc($result)); ?>
