3282034e0a57113d6dcced43c19ad62dc428ba56
greg gay <ggay@ocad.ca>
2008-09-08 15:28:42 +0000

    iAdded access4all SQL to installer

--- docs/install/db/atutor_schema.sql
+++ docs/install/db/atutor_schema.sql
@@ -1036,3 +1036,45 @@ CREATE TABLE `auto_enroll_courses` (
 );
 
 
+#Setup Table for Access4All
+CREATE TABLE `primary_resources` (
+  `primary_resource_id` mediumint(8) unsigned NOT NULL auto_increment,
+  `content_id` mediumint(8) unsigned NOT NULL default '0',
+  `resource` text NOT NULL,
+  `language_code` varchar(20) default NULL,
+  PRIMARY KEY  (`primary_resource_id`)
+) TYPE = MYISAM;
+
+CREATE TABLE `primary_resources_types` (
+  `primary_resource_id` mediumint(8) unsigned NOT NULL,
+  `type_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY  (`primary_resource_id`,`type_id`)
+) TYPE = MYISAM;
+
+CREATE TABLE `resource_types` (
+  `type_id` mediumint(8) unsigned NOT NULL auto_increment,
+  `type` text NOT NULL,
+  PRIMARY KEY  (`type_id`)
+) TYPE = MYISAM;
+
+CREATE TABLE `secondary_resources` (
+  `secondary_resource_id` mediumint(8) unsigned NOT NULL auto_increment,
+  `primary_resource_id` mediumint(8) unsigned NOT NULL,
+  `secondary_resource` text NOT NULL,
+  `language_code` varchar(20) default NULL,
+  PRIMARY KEY  (`secondary_resource_id`)
+) TYPE = MYISAM;
+
+CREATE TABLE `secondary_resources_types` (
+  `secondary_resource_id` mediumint(8) unsigned NOT NULL,
+  `type_id` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY  (`secondary_resource_id`,`type_id`)
+) TYPE = MYISAM;
+
+INSERT INTO `AT_resource_types` VALUES
+(1, 'auditory'),
+(2, 'sign_language'),
+(3, 'textual'),
+(4, 'visual');
+
+#End Access4All setup 
