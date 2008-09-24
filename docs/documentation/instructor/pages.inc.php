efa027d8aaf24807239ad25cf0c454ddf209abc1
greg gay <ggay@ocad.ca>
2008-09-24 17:18:24 +0000

    Updated preferecnes handbook page with new a4a options

--- docs/documentation/instructor/pages.inc.php
+++ docs/documentation/instructor/pages.inc.php
@@ -15,8 +15,8 @@
     $_pages['content_properties.php']            = 'Content Properties';
     $_pages['glossary_terms.php']                = 'Glossary Terms';
     $_pages['content_preview.php']               = 'Preview';
-    $_pages['accessibility.php']		 = 'Accessibility';
     $_pages['content_alternatives.php']          = 'Alternate Content';
+    $_pages['accessibility.php']		 = 'Accessibility';
     $_pages['content_tests.php']		 = 'Associating Tests';
     $_pages['content_packages.php']              = 'Import/Export Content';
     $_pages['content_usage.php']                 = 'Content Usage';
