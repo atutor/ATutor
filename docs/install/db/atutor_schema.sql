8976ac386c8fca338add8906e74d5bde628a40b9
harris wong <hwong@ocad.ca>
2008-09-26 15:07:23 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3578

--- docs/install/db/atutor_schema.sql
+++ docs/install/db/atutor_schema.sql
@@ -123,6 +123,7 @@ CREATE TABLE `content` (
   `head` TEXT NOT NULL,
   `use_customized_head` TINYINT(4) NOT NULL,
   `test_message` TEXT NOT NULL,
+  `allow_test_export` TINYINT(1) UNSIGNED NOT NULL,
   PRIMARY KEY  (`content_id`),
   KEY `course_id` (`course_id`)
 ) TYPE=MyISAM ;
