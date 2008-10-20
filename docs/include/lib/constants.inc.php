a9e275217ee1c36e239812721415b6f378969278
greg gay <ggay@ocad.ca>
2008-10-20 13:54:34 +0000

    update version number to 1.6.2

--- docs/include/lib/constants.inc.php
+++ docs/include/lib/constants.inc.php
@@ -204,7 +204,7 @@ define('AT_GUIDES_PATH', $_base_path . 'documentation/');
 
 define('AT_BACKUP_DIR', AT_CONTENT_DIR . 'backups/'); // where the backups get stored
 
-define('VERSION',		'1.6.1');
+define('VERSION',		'1.6.2');
 define('ONLINE_UPDATE', 3); /* update the user expiry every 3 min */
 
 /* valid date format_types:						*/
