fc641d1a3c4029c7f3b0fe3333436420978a89f7
greg gay <ggay@ocad.ca>
2008-10-28 13:44:35 +0000

    module docs updated

--- docs/documentation/admin/modules.php
+++ docs/documentation/admin/modules.php
@@ -2,11 +2,16 @@
 
 <h2>Modules</h2>
 	<h3>Import Modules from atutor.ca</h3>
-	<p>As of version 1.6.2 the preferred method of installing modules is through the <strong> Import Modules</strong> screen of the ATutor Module Manager. This screen will list all the extra modules available on the atutor.ca Web site. <strong>Read the descriptions</strong> for modules you want to install to be sure all prerequisites are met, and note the version numbers the module has been tested with. Though in most cases older modules will function without any trouble in newer versions of ATutor, occassionaly they need to be updated to work with a current version. Please report any problems you experience with modules to the <a href="http://www.atutor.ca/forum/17/1.html">Modules Forum on atutor.ca</a></p>
+	<p>As of version 1.6.2 the preferred method of installing modules is through the <strong> Import Modules</strong> screen of the ATutor Module Manager. This screen will list all the extra modules available on the atutor.ca Web site. <strong>Read the descriptions</strong> for modules you want to install to be sure all prerequisites are met, and note the version numbers the module has been tested with. Though in most cases older modules will function without any trouble in newer versions of ATutor, occassionally they need to be updated to work with a current version. Please report any problems you experience with modules to the <a href="http://www.atutor.ca/forum/17/1.html">Modules Forum on atutor.ca</a></p>
 
-	<h4>Unistalling Imported Modules</h4>
-	<p>Modules that have been installed using the Import method above, can be uninstalled using the Module Manager's <strong>Uninstall</strong> button. After pressing the Uninstall button you will be given the option to remove the data, and the directories associated with the module. Select from the checkboxes, and press continue.  If you are removing the module completely, you will probably want to delete the data and directories. If you are upgrading or reinstalling a module, you will want to save the data and directories so they can be reused with the upgraded or reinstalled version of the module.</p>
+	<h4>Uninstalling Imported Modules</h4>
+	<p>Modules that have been installed using the Import method above, can be uninstalled using the Module Manager's <strong>Uninstall</strong> button. After pressing the Uninstall button you will be given the option to remove the data and the directories associated with the module. Select from the checkboxes, and press continue.  If you are removing the module completely, you will probably want to delete the data and directories. If you are upgrading or reinstalling a module, you will want to save the data and directories so they can be reused with the upgraded or reinstalled version of the module.</p>
 
+	<h4>Missing Modules</h4>	
+	<p>When an ATutor system is upgraded modules need to be reinstalled. Thos modules will be listed as <strong>missing</strong> in the Module Manager.
+
+	<h4>Partially Installed Modules</h4>
+	<p>When a module is uninstalled, but its data and/or directories are saved, it will be listed as <strong>Partially Uninstalled</strong>. The same status will appear for modules for which the uninstall process was not completed for one reason or another. To remove these modules completely, they must either be reinstalled then uninstalled once more, or they must be removed by deleting the database tables and/or directories manually.</p>
 
 	<h3>Manual Module Install</h3>
 	<p>Installing modules manually, requires that they be uninstalled manually if they need to be removed. In most cases you should use the Import method described above. Manually installing a module is useful if you are installing a third party module not available at atutor.ca, or if you are a module developer and need control over the module files for editing etc. you will want to use the manual method. </p>
