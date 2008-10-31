b6899d8a11844aa57f18802f9acfcb184da26e9c
cindy li <cli@ocad.ca>
2008-10-31 14:30:27 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3605
    backup tables for AccessForAll: 
    primary_resources
    primary_resources_types
    secondary_resources
    secondary_resources_types

--- docs/mods/_core/content/module_backup.php
+++ docs/mods/_core/content/module_backup.php
@@ -4,12 +4,56 @@ $sql = array();
 
 $sql['content'] = 'SELECT content_id, content_parent_id, ordering, last_modified, revision, formatting, release_date, keywords, content_path, title, text, head, use_customized_head, test_message, allow_test_export FROM '.TABLE_PREFIX.'content WHERE course_id=? ORDER BY content_parent_id, ordering';
 
-$sql['related_content'] = 'SELECT R.content_id, R.related_content_id FROM '.TABLE_PREFIX.'related_content R, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND R.content_id=C.content_id ORDER BY R.content_id ASC';
+$sql['primary_resources'] = 'SELECT P.primary_resource_id, P.content_id, P.resource, P.language_code FROM '.TABLE_PREFIX.'primary_resources P, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND P.content_id=C.content_id ORDER BY P.content_id ASC';
+
+$sql['primary_resources'] = 'SELECT P.primary_resource_id, P.content_id, P.resource, P.language_code FROM '.TABLE_PREFIX.'primary_resources P, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND P.content_id=C.content_id ORDER BY P.content_id ASC';
+
+$sql['primary_resources_types'] = 'SELECT PRT.primary_resource_id, PRT.type_id FROM '.TABLE_PREFIX.'primary_resources_types PRT, '.TABLE_PREFIX.'primary_resources P, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND C.content_id=P.content_id AND P.primary_resource_id = PRT.primary_resource_id ORDER BY PRT.primary_resource_id ASC';
+
+$sql['secondary_resources'] = 'SELECT S.secondary_resource_id, S.primary_resource_id, S.secondary_resource, S.language_code FROM '.TABLE_PREFIX.'secondary_resources S, '.TABLE_PREFIX.'primary_resources P, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND C.content_id=P.content_id AND P.primary_resource_id = S.primary_resource_id ORDER BY S.primary_resource_id ASC';
+
+$sql['secondary_resources_types'] = 'SELECT SRT.secondary_resource_id, SRT.type_id FROM '.TABLE_PREFIX.'secondary_resources_types SRT, '.TABLE_PREFIX.'secondary_resources S, '.TABLE_PREFIX.'primary_resources P, '.TABLE_PREFIX.'content C WHERE C.course_id=? AND C.content_id=P.content_id AND P.primary_resource_id = S.primary_resource_id AND S.secondary_resource_id = SRT.secondary_resource_id ORDER BY SRT.secondary_resource_id ASC';
 
 function related_content_convert($row, $course_id, $table_id_map, $version) {
 	$new_row = array();
 	$new_row[0] = $table_id_map['content'][$row[0]];
 	$new_row[1] = $table_id_map['content'][$row[1]];
+
+	return $new_row;
+}
+
+function primary_resources_convert($row, $course_id, $table_id_map, $version) {
+	$new_row = array();
+	$new_row = $row;
+	
+	$new_row[1] = $table_id_map['content'][$row[1]];
+
+	return $new_row;
+}
+
+function primary_resources_types_convert($row, $course_id, $table_id_map, $version) {
+	$new_row = array();
+	$new_row = $row;
+	
+	$new_row[0] = $table_id_map['primary_resources'][$row[0]];
+
+	return $new_row;
+}
+
+function secondary_resources_convert($row, $course_id, $table_id_map, $version) {
+	$new_row = array();
+	$new_row = $row;
+	
+	$new_row[1] = $table_id_map['primary_resources'][$row[1]];
+
+	return $new_row;
+}
+
+function secondary_resources_types_convert($row, $course_id, $table_id_map, $version) {
+	$new_row = array();
+	$new_row = $row;
+	
+	$new_row[0] = $table_id_map['secondary_resources'][$row[0]];
 
 	return $new_row;
 }
