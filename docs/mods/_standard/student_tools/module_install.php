e07ff49f2437db78ffbe0e981c4aa0690f0aead5
greg gay <ggay@ocad.ca>
2008-11-07 14:42:52 +0000

    added fha student tools

---
+++ docs/mods/_standard/student_tools/module_install.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('AT_INCLUDE_PATH')) { exit; }
+
+$_course_privilege = AT_PRIV_ADMIN; // possible values: FALSE | AT_PRIV_ADMIN | TRUE
+$_admin_privilege  = FALSE; // possible values: FALSE | TRUE
+
+
+if (!$msg->containsErrors() && file_exists(dirname(__FILE__) . '/module.sql')) {
+	// deal with the SQL file:
+	require(AT_INCLUDE_PATH . 'classes/sqlutility.class.php');
+	$sqlUtility =& new SqlUtility();
+
+	$sqlUtility->queryFromFile(dirname(__FILE__) . '/module.sql', TABLE_PREFIX);
+}
+
+?>
