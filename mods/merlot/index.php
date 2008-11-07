cc382ab0e19f4bfd504e1d855534cd2bb90ef349
cindy li <cli@ocad.ca>
2008-11-07 20:53:14 +0000

    change for new merlot web service

--- mods/merlot/index.php
+++ mods/merlot/index.php
@@ -15,38 +15,31 @@
 define('AT_INCLUDE_PATH', '../../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
 $_custom_css = $_base_path . 'mods/merlot/module.css'; // use a custom stylesheet
+$default_num_of_results = 25;
 
-$merlot_key = $_config['merlot_key'];
-//$merlot_location =$_config['merlot_location'];
-
-$merlot_location = "http://www.merlot.org/merlot/materials.rest";
 //global $search;
 $advanced = intval($advanced);
 $browse =  intval($browse);
 
-// See if merlot is configured
-if(!$_config['merlot_key'] || !$_config['merlot_location']){
+if (!isset($_POST["search_type"])) $_POST["search_type"] = 0;
+if (!isset($_POST["num_of_results"])) $_POST["num_of_results"] = $default_num_of_results;
+
+// check if merlot is configured
+if(!isset($_config['merlot_key']) || !isset($_config['merlot_location']))
+{
 	$msg->addError('MERLOT_NOT_CONFIG');
-}
 
-
-if($msg->containsErrors()){
 	require (AT_INCLUDE_PATH.'header.inc.php');
+	$msg->printAll();
 	require(AT_INCLUDE_PATH.'footer.inc.php');
 	exit;
-}else{
-
+}
+else
+{
 // If Merlot is configured, display the simple search form, and results
-	require (AT_INCLUDE_PATH.'header.inc.php');
-
-
-
-
-
+require (AT_INCLUDE_PATH.'header.inc.php');
 ?>
 <script type="text/javascript" language="JavaScript" src="<?php echo $_base_path; ?>mods/merlot/merlot.js"></script>
-
-
 
 <?php
 
@@ -54,31 +47,56 @@ if($_REQUEST['advanced']){
 		require (AT_INCLUDE_PATH.'../mods/merlot/merlot_adv.php');
 
 }else{?>
-	<!-- form action="<?php echo $_SERVER['PHP_SELF']; ?>#search_results" method="get" name="form" -->
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>#search_results" method="get" name="form">
-		<div class="input-form" style="width: 40%;padding:5px;">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>#search_results" method="post" name="form">
+		<div class="input-form">
 
 			<div>
 			<img src="<?php echo $_base_path; ?>mods/merlot/merlot.gif" height="50" width="50" style="margin-right:3px;float:left;text-align:right;" alt="<?php  echo _AT('merlot'); ?>" />
 			<?php  echo _AT('merlot_howto'); ?>
 			</div>
-			<div class="row">
+			<table>
+				<tr>
+					<td colspan="2">
+						<input type="radio" name="search_type" value="0" id="allKeyWords" <?php if ($_POST["search_type"] == 0) echo 'checked="checked"'; ?> /><label for="allKeyWords">All Words</label>
+						<input type="radio" name="search_type" value="1" id="anyKeyWords" <?php if ($_POST["search_type"] == 1) echo 'checked="checked"'; ?> /><label for="anyKeyWords">Any Word</label>
+						<input type="radio" name="search_type" value="2"  id="exactPhraseKeyWords" <?php if ($_POST["search_type"] == 2) echo 'checked="checked"'; ?> /><label for="exactPhraseKeyWords">Exact Phrase</label>
+					</td>
+				</tr>
+					
+				<tr>
+					<td><label for="words2"><?php echo _AT('keywords'); ?></label></td>
+					<td><input type="text" name="keywords" size="100" id="words2" value="<?php echo $_POST['keywords']; ?>" /></td>
+				</tr>
 
-					<label for="words2"><?php echo _AT('search_words'); ?></label><br />
-					<input type="hidden" name="licenseKey" value="PZ8SRMDOFKA3I2DQF7ODJ9BUBJX7BPWGRQP" />
-					<input type="text" name="keywords" size="40" id="words2" value="<?php echo stripslashes(htmlspecialchars($_GET['keywords'])); ?>" /><br />
-<input type="hidden" name="totalCountOnly" value="20"/>
-					<fieldset><legend>Filters</legend>
+				<tr>
+					<td><label for="title"><?php echo _AT('title'); ?></label></td>
+					<td><input type="text" name="title" size="100" id="title" value="<?php echo $_POST['title']; ?>" /></td>
+				</tr>
 
-<input type="radio" name="allKeyWords" value="" id="allKeyWords"/><label for="allKeyWords">All Words</label><br />
-<input type="radio" name="anyKeyWords" value="" id="anyKeyWords" /><label for="anyKeyWords">Any Word</label><br />
-<input type="radio" name="exactPhraseKeyWords" value=""  id="exactPhraseKeyWords" /><label for="exactPhraseKeyWords">Exact Phrase</label><br />
-<input type="radio" name="title" value=""  id="title" /><label for="title">Title</label><br />
-<input type="radio" name="description" value=""  id="description" /><label for="description">Description</label><br />
-<input type="radio" name="creativeCommons" value=""  id="creativeCommons" /><label for="creativeCommons">Creative Common</label><br />
-<input type="radio" name="" value="author"  id="author" /><label for="author">Author</label><br />
-					</fieldset>
-			</div>
+				<tr>
+					<td><label for="description"><?php echo _AT('description'); ?></label></td>
+					<td><input type="text" name="description" size="100" id="description" value="<?php echo $_POST['description']; ?>" /></td>
+				</tr>
+
+				<tr>
+					<td><label for="author"><?php echo _AT('author'); ?></label></td>
+					<td><input type="text" name="author" size="100" id="author" value="<?php echo $_POST['author']; ?>" /></td>
+				</tr>
+
+				<tr>
+					<td colspan="2">
+						<input type="checkbox" name="creativeCommons" value="true" id="creativeCommons" <?php if ($_POST["creativeCommons"] == "true") echo 'checked="checked"'; ?> /><label for="creativeCommons"><?php echo _AT("merlot_creative_commons"); ?></label>
+					</td>
+				</tr>
+
+				<tr>
+					<td colspan="2">
+						<label for="num_of_results"><?php echo _AT('merlot_num_of_results'); ?></label>
+						<input type="text" name="num_of_results" size="20" id="num_of_results" value="<?php echo $_POST['num_of_results']; ?>" />
+					</td>
+				</tr>
+			</table>
+			
 			<div class="row buttons">
 					<input type="submit" name="submit" value="<?php echo _AT('merlot_search'); ?>" />
 			</div>
@@ -99,9 +117,11 @@ if($_REQUEST['advanced']){
 
 }
 ?>
-<iframe src="http://localhost/atutorsvn/mods/merlot/test.xml" width="90%">
+<!--
+<iframe src="http://localhost/docs/content/tmp_merlot_results.xml" width="90%">
 
 </iframe>
+//-->
 
 <?php
 require(AT_INCLUDE_PATH.'footer.inc.php');
--- mods/merlot/merlot_rest.php
+++ mods/merlot/merlot_rest.php
@@ -10,115 +10,83 @@
 /* modify it under the terms of the GNU General Public License  */
 /* as published by the Free Software Foundation.				*/
 /****************************************************************/
-// $Id: merlot.php 6614 2006-09-27 19:32:29Z greg $
+// $Id: merlot_rest.php 6614 2006-09-27 19:32:29Z greg $
 
-// Check if links is turned on for this course.
+require("./classes/MerlotResultParser.class.php");
 
+$keywords = trim($_POST['keywords']);
+$title = trim($_POST['title']);
+$description = trim($_POST['description']);
+$author = trim($_POST['author']);
+$num_of_results = intval(trim($_POST['num_of_results']));
 
+if($keywords <> "" || $title <> "" || $description <> "" || $author <> "" || $_POST["creativeCommons"] == "true")
+{
+	if ($keywords <> "") $url_search = "&keywords=".urlencode($keywords);
+	if ($title <> "") $url_search .= "&title=".urlencode($title);
+	if ($description <> "") $url_search .= "&description=".urlencode($description);
+	if ($author <> "") $url_search .= "&author=".urlencode($author);
+	if ($_POST["search_type"] == 0) $url_search .= "&allKeyWords=true";
+	if ($_POST["search_type"] == 1) $url_search .= "&anyKeyWords=true";
+	if ($_POST["search_type"] == 2) $url_search .= "&exactPhraseKeyWords=true";
+	if ($_POST["creativeCommons"] == "true") $url_search .= "&creativeCommons=true";
+	if ($num_of_results > 25 || $num_of_results == 0) $url_search .= "&size=".$default_num_of_results;
+	else $url_search .= "&size=".$num_of_results;
 
+	$url = $_config['merlot_location']."?licenseKey=".$_config['merlot_key'].$url_search;
 
-$url = $merlot_location."?licenseKey=".$_GET['licenseKey'];
-if($_GET['keywords'] != ""){
-$keyword = "&keywords=".$_GET['keywords'];
-$url .=$keyword;
-}
-
-
-
-$xml_results = file_get_contents($url);
-
-debug($xml_results);
-
-//$p = xml_parser_create();
-//xml_parse_into_struct($p, $xml_results, &$vals, &$index);
-//xml_parse($xml_results);
-
-
-
-//xml_parser_free($p);
-
+	$xml_results = file_get_contents($url);
+	
+	if (!$xml_results)
+	{
+		$infos = array('CANNOT_CONNECT_SERVER', $_config['merlot_location']);
+		$msg->addInfo($infos);
+	}
+	else
+	{
+		$MerlotResultParser =& new MerlotResultParser();
+		$MerlotResultParser->parse($xml_results);
+		$result_list = $MerlotResultParser->getParsedArray();
+//		debug($result_list);
+		if ($result_list['status'] == 'failed')  // failed, display error
+			echo "<span style='color:red'>"._AT('error').": ".$result_list['error']."</span>";
+		else  // success, display results
+		{
+//			debug($result_list);
+			if (is_array($result_list))
+			{
+				echo '	<div id="search_results">';
+				echo "		<h2>". _AT('results')." <small>(".$result_list["summary"]["resultCount"]." out of ".$result_list["summary"]["totalCount"].")</small></h2>";
+				foreach ($result_list as $key=>$result)
+				{
+					if (is_int($key))
+					{
 ?>
 
-<h2>Results <?php echo $index['TOTALCOUNT']['0']; ?></h2> 
+		<dl class="browse-course">
 
+			<dt></dt>
+			<dd><h3><a href="<?php echo $result['detailURL']; ?>"><?php echo $result['title']; ?></a></h3></dd>
+						
+			<dt><?php echo _AT("author"); ?></dt>
+			<dd><?php echo $result['authorName']; ?></dd>
+
+			<dt><?php echo _AT("merlot_creation_date"); ?></dt>
+			<dd><?php echo $result['creationDate']; ?></dd>
+
+			<dt><?php echo _AT("description"); ?></dt>
+			<dd><?php if (strlen($result['description']) > 120) echo substr($result['description'], 0, 120). "..."; ?></dd>
+
+			<dt><?php echo _AT("merlot_creative_commons"); ?></dt>
+			<dd><?php if ($result['creativeCommons'] == "true") echo _AT('yes'); else echo _AT('no'); ?></dd>
+		</dl>
+		<br />
 <?php
-/*
-foreach($index['RESULTS'] as $result){
-
-echo $result;
-
-}
-
-foreach($vals as $records => $record){
-
-echo $record['title']."";
-}
-
-echo "<pre>";
-
-echo "Index array\n";
-debug($index);
-echo "\nVals array\n";
-debug($vals);
-echo "</pre>";
-
-*/
-$xml_parser = xml_parser_create( 'UTF-8' ); // UTF-8 or ISO-8859-1
-xml_parser_set_option( $xml_parser, XML_OPTION_CASE_FOLDING, 0 );
-xml_parser_set_option( $xml_parser, XML_OPTION_SKIP_WHITE, 1 );
-xml_parse_into_struct( $xml_parser, $xml_results, $vals );
-xml_parser_free($xml_parser);
-//debug($aryXML);
-//now use aryXML array to xml string:
-        $o='';
-foreach( $vals as $tag ){
-    //tab space:
-    for($i=1; $i < $tag['level'];$i++)
-        $o.="\t";
-    if($tag['type']!='close'){
-        if($tag['type']!='cdata')
-            $o.='<'.$tag['tag'];
-        if(isset($tag['attributes'])){
-            foreach($tag['attributes'] as $attr=>$aval){
-                $o.=' '.$attr.'="'.$aval.'"';
-            }
-        }
-        if($tag['type']!='cdata'){
-            $o.=($tag['type']=='complete' && (!isset($tag['value']) || $tag['value']==''))?'/>':'>';
-        }
-        $o.=(isset($tag['value']))?$tag['value']:'';
-        if($tag['type']!='cdata'){
-            $o.=($tag['type']=='complete' && (isset($tag['value']) && $tag['value']<>''))?'</'.$tag['tag'].'>':'';
-        }
-        $o.="\n";
-    }else{
-        $o.='</'.$tag['tag'].'>'."\n";
-    }
+					}
+				}
+				echo '</div>';
+			}
+		}
+	}
 }
 ?>
-
-<?php
-//debug($o);
-$file=$xml_results;
-
-//debug($file);
-$file = str_replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><?xml-stylesheet type=\"text/xsl\" href=\"".$_base_href."mods/merlot/result.xsl\"?>", $file);
-//debug($file);
-//$file = preg_replace("\<?", "\<?", $file);
-
-
-
-if($handle = fopen(AT_CONTENT_DIR."tmp_merlot_results.xml", "w+")){
-
-fwrite($handle, $file);
-fclose($handle);
-
-}else{
-echo "failed";
-
-}
-$merlot_results = $_base_href."content/tmp_merlot_results.xml";
-//echo file_get_contents("http://localhost/atutorsvn/mods/merlot/test.xml");
-?>
-
-
