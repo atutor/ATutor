27ff96952cb14610a229dd395cfda571088590e9
harris wong <hwong@ocad.ca>
2008-12-09 20:52:36 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3666

--- docs/include/lib/editor_tab_functions.inc.php
+++ docs/include/lib/editor_tab_functions.inc.php
@@ -150,6 +150,7 @@ function save_changes($redir, $current_tab) {
 			$old_w = $w;
 			$key = in_array_cin($w, $glossary_ids);
 			$w = urldecode($w);
+			$d = $addslashes($d);
 
 			if (($key !== false) && (($glossary[$old_w] != $d) || isset($_POST['related_term'][$old_w])) ) {
 				$w = addslashes($w);
