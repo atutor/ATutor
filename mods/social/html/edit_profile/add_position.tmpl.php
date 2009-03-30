48d5c068d97e770ac19abd31f81b7c7108ce2c80
greg gay <ggay@ocad.ca>
2009-03-30 18:47:16 +0000

    setup edit forms for profile

--- mods/social/html/edit_profile/add_position.tmpl.php
+++ mods/social/html/edit_profile/add_position.tmpl.php
@@ -1,31 +1,29 @@
+<div class="headingbox"><h3><?php echo _AT('add_new_position'); ?></h3></a></div>
+<div class="contentbox">
 <form method="POST" action="<?php echo url_rewrite('mods/social/edit_profile.php'); ?>">
-<div>
-	<div>
-		<label for="company"><?php echo _AT('company'); ?></label>
-		<input type="text" id="company" name="company" value="<?php echo $company; ?>" />
-	</div>
+<div style="width:60%;">
 
-	<div>
-		<label for="title"><?php echo _AT('title'); ?></label>
-		<input type="text" name="title" value="<?php echo $title; ?>" />
-	</div>
+	<dl id="public-profile">
+		<dt><label for="company"><?php echo _AT('company'); ?>:</label></dt>
+		<dd><input type="text" id="company" name="company" value="<?php echo $company; ?>" /></dd>
+	      
+		<dt><label for="title"><?php echo _AT('title'); ?>:</label></dt>
+		<dd><input type="text" id="title" name="title" value="<?php echo $title; ?>" /></dd>
 
-	<div>
-		<label for="from"><?php echo _AT('from'); ?></label>
-		<input type="text" name="from" value="<?php echo $from; ?>" />
-	</div>
+		<dt><label for="from"><?php echo _AT('from'); ?>:</label></dt>
+		<dd><input type="text" id="from" name="from" value="<?php echo $from; ?>" /></dd>
 
-	<div>
-		<label for="to"><?php echo _AT('to'); ?></label>	
-		<input type="text" name="to" value="<?php echo $to; ?>" />
-	</div>
+		<dt><label for="to"><?php echo _AT('to'); ?></label>	</dt>
+		<dd><input type="text" id="to" name="to" value="<?php echo $to; ?>" /></dd>
 
-	<div>
-		<label for="description"><?php echo _AT('description'); ?></label>	
-		<textarea name="description"><?php echo $description; ?></textarea>
-	</div>
+
+		<dt><label for="description"><?php echo _AT('description'); ?></label>	</dt>
+		<dd><textarea name="description" id="description" cols="40" rows="4"><?php echo $description; ?></textarea></dd>
+	</dl>
+
 	<input type="hidden" name="add" value="position" />
-	<input type="submit" name="submit" value="<?php echo _AT('save'); ?>" />
-	<input type="submit" name="cancel" value="<?php echo _AT('cancel'); ?>" />
+	<input type="submit" name="submit" class="button" value="<?php echo _AT('save'); ?>" />
+	<input type="submit" name="cancel"  class="button" value="<?php echo _AT('cancel'); ?>" />
 </div>
 </form>
+</div>
--- mods/social/html/edit_profile/edit_position.tmpl.php
+++ mods/social/html/edit_profile/edit_position.tmpl.php
@@ -8,35 +8,24 @@
 	$from = intval($this->from);
 	$to = intval($this->to);
 ?>
+<div class="headingbox"><h3><?php echo _AT('edit_position'); ?></h3></a></div>
+<div class="contentbox">
 <form method="POST" action="<?php echo url_rewrite('mods/social/edit_profile.php'); ?>">
-<div>
-	<div>
-		<label for="company"><?php echo _AT('company'); ?></label>
-		<input type="text" id="company" name="company" value="<?php echo $company; ?>" />
-	</div>
+	<dl id="public-profile">
+	<dt><label for="company"><?php echo _AT('company'); ?></label></dt>
+	<dd><input type="text" id="company" name="company" value="<?php echo $company; ?>" /></dd>
+	<dt><label for="title"><?php echo _AT('title'); ?></label></dt>
+	<dd><input type="text" id="title" name="title" value="<?php echo $title; ?>" /></dd>
+	<dt><label for="from"><?php echo _AT('from'); ?></label></dt>
+	<dd><input type="text" id="from" name="from" value="<?php echo $from; ?>" /></dd>
+	<dt><label for="to"><?php echo _AT('to'); ?></label></dt>
+	<dd><input type="text" id="to" name="to" value="<?php echo $to; ?>" /></dd>
+	<dt><label for="description"><?php echo _AT('description'); ?></label>	</dt>
+	<dd><textarea name="description" id="description" cols="40" rows="5"><?php echo $description; ?></textarea></dd>
 
-	<div>
-		<label for="title"><?php echo _AT('title'); ?></label>
-		<input type="text" name="title" value="<?php echo $title; ?>" />
-	</div>
-
-	<div>
-		<label for="from"><?php echo _AT('from'); ?></label>
-		<input type="text" name="from" value="<?php echo $from; ?>" />
-	</div>
-
-	<div>
-		<label for="to"><?php echo _AT('to'); ?></label>	
-		<input type="text" name="to" value="<?php echo $to; ?>" />
-	</div>
-
-	<div>
-		<label for="description"><?php echo _AT('description'); ?></label>	
-		<textarea name="description"><?php echo $description; ?></textarea>
-	</div>
 	<input type="hidden" name="id" value="<?php echo $this->id; ?>" />
 	<input type="hidden" name="edit" value="position" />
-	<input type="submit" name="submit" value="<?php echo _AT('save'); ?>" />
-	<input type="submit" name="cancel" value="<?php echo _AT('cancel'); ?>" />
+	<input type="submit" class="button" name="submit" value="<?php echo _AT('save'); ?>" />
+	<input type="submit" class="button" name="cancel" value="<?php echo _AT('cancel'); ?>" />
 </div>
 </form>
