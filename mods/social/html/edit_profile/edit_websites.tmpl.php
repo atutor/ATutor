ebe7013842b15078b4ef1b1a9ae091a5724d65e4
greg gay <ggay@ocad.ca>
2009-03-30 20:39:18 +0000

    replace add website with modified edit website

--- mods/social/html/edit_profile/edit_websites.tmpl.php
+++ mods/social/html/edit_profile/edit_websites.tmpl.php
@@ -5,7 +5,7 @@
 	$url		= $addslashes($this->url);
 	$site_name	= $addslashes($this->site_name);
 ?>
-<div class="headingbox"><h3><?php echo _AT('edit_website'); ?></h3></a></div>
+<div class="headingbox"><h3><?php if($_GET['id']){echo _AT('edit_websites');}else{echo  _AT('add_new_website');}?></h3></div>
 <div class="contentbox">
 <form method="POST" action="<?php echo url_rewrite('mods/social/edit_profile.php'); ?>">
 	<dl id="public-profile">
@@ -16,7 +16,12 @@
 	<dd><input type="text" id="site_name" name="site_name" value="<?php echo $site_name; ?>" /></dd>
 
 	<input type="hidden" name="id" value="<?php echo $this->id; ?>" />
-	<input type="hidden" name="edit" value="websites" />
+		<input type="hidden" name="id" value="<?php echo $this->id; ?>" />
+		<?php if($_GET['id']){ ?>
+		<input type="hidden" name="edit" value="websites" />
+		<?php }else { ?>
+		<input type="hidden" name="add" value="websites" />
+		<?php } ?>
 	<input type="submit" name="submit" class="button" value="<?php echo _AT('save'); ?>" />
 	<input type="submit" name="cancel" class="button" value="<?php echo _AT('cancel'); ?>" />
 	</dl>
