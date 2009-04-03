de5f6652ece62b179b47a28eab657749cceccc76
greg gay <ggay@ocad.ca>
2009-04-03 19:24:10 +0000

    added profile picture editor

--- mods/social/module.php
+++ mods/social/module.php
@@ -81,6 +81,9 @@ $this->_pages['mods/social/sprofile.php']['parent'] = 'mods/social/index.php';
 $this->_pages['mods/social/edit_profile.php']['title_var'] = 'edit_profile';
 $this->_pages['mods/social/edit_profile.php']['parent'] = 'mods/social/sprofile.php';
 
+$this->_pages['mods/social/profile_picture.php']['title_var'] = 'picture';
+$this->_pages['mods/social/profile_picture.php']['parent'] = 'mods/social/edit_profile.php';
+
 $this->_pages['mods/social/applications.php']['title_var'] = 'gadgets';
 $this->_pages['mods/social/applications.php']['parent'] = 'mods/social/index.php';
 
