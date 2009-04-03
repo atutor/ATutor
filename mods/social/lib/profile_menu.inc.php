0ac974949d1c4521d8e94a62dc2db743589fd747
greg gay <ggay@ocad.ca>
2009-04-03 20:01:06 +0000

    added profile menu

---
+++ mods/social/lib/profile_menu.inc.php
@@ -0,0 +1,6 @@
+
+<ul class="social_inline_menu">
+	<li class="inlinelist"><a href="<?php echo 'mods/social/profile_picture.php'; ?>"><?php echo _AT('Picture'); ?></a></li>
+	<li class="inlinelist"><a href="<?php echo 'mods/social/basic_profile.php'; ?>"><?php echo _AT('basic_profile'); ?></a></li>
+	<li class="inlinelist"><a href="<?php echo 'mods/social/edit_profile.php'; ?>"><?php echo _AT('social_profile'); ?></a></li>
+</ul>
