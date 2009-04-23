d4a897f87beb129a07888185f12ed2356d2a22e7
harris wong <hwong@ocad.ca>
2009-04-23 22:15:14 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3735 Temp fix to avoid backup error.

--- mods/social/module_backup.php
+++ mods/social/module_backup.php
@@ -13,7 +13,9 @@
 // $Id$
 
 /* each table to be backed up. includes the sql entry and fields */
-
+/*
+ * TODO: decide how to back up all the social data
+ *
 $dirs = array();
 $dirs['hello_world/'] = AT_CONTENT_DIR . 'hello_world' . DIRECTORY_SEPARATOR;
 
@@ -27,5 +29,5 @@ function hello_world_convert($row, $course_id, $table_id_map, $version) {
 
 	return $new_row;
 }
-
+*/
 ?>
