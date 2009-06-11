545f20c08f0ab5cdca6824cdda56b2a5e9bf8494
greg gay <ggay@ocad.ca>
2009-06-11 20:18:19 +0000

    initial atutor social handbook pages

---
+++ docs/documentation/general/my_gadgets.php
@@ -0,0 +1,33 @@
+<?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate: 2006-07-01 18:04:33 -0400 (Sat, 01 Jul 2006) $'; ?>
+
+<h2>Gadgets</h2>
+
+<p>Gadgets are applications you can add to your Social Networking environment that provide a whole range of potential networking functionality. They are much link Apps on an IPhone. You can find many gadgets around the Intenet that can be added by copying the URL to the Gadget into the Add Gadget field on the Gadgets screen of ATutor Social.</p>
+
+<p>Here's a list of sites that offer Open Social Gadgets, most of which can be added to ATutor Social. Note that some gadgets that require special authentication to use, may not function with ATutor social currently. Use a search engine to search for variations of the terms "open social gadgets" to find many more. Once a gadget has been added to ATutor, it becomes available for everyone on that ATutor server to select by following <strong>Show Available Gadgets</strong></p>
+
+<h3>Gadget Sites</h3>
+<ul>
+<li><a href="http://www.google.com/ig/directory?hl=en&type=gadgets">http://www.google.com/ig/directory</a></li>
+<li><a href="http://www.hyves.nl/gadgetgallery/#searchhyver=&tabFilterType=featured&__state__=1">http://www.hyves.nl/gadgetgallery/</a></li>
+<li><a href="http://developer.ning.com/opensocial/application/list">http://developer.ning.com/opensocial/application/list</a></li>
+<li><a href="http://opensocial-examples.googlemashups.com/">http://opensocial-examples.googlemashups.com/</a></li>
+<li><a href="http://www.slideshare.net/category/gadgets-reviews">http://www.slideshare.net/</a></li>
+<li><a href="http://www.box.net/">http://www.box.net/</a></li>
+<li><a href="http://www.huddle.net/">http://www.huddle.net/</a></li>
+
+<li><a href="http://www.labpixies.com">http://www.labpixies.com</a></li>
+<li><a href="http://code.google.com/apis/gadgets/docs/legacy/gs.html#GGE">http://code.google.com/apis/gadgets/</a></li>
+
+</ul>
+
+<h3>Sample Gadgets</h3>
+<p>To install a gadget, copy the path to the gadget's XML file into the Add Gadget by URL field:</p>
+
+<ul>
+<li>OpenSocialDev App: - http://osda.appspot.com/gadget/osda-igoogle.xml </li>
+<li>Babylon Dictionary: - http://www.labpixies.com/campaigns/babylon/babylon.xml </li>
+<li>Todo List: - http://www.labpixies.com/campaigns/todo/todo.xml </li>
+</ul>
+
+<?php require('../common/body_footer.inc.php'); ?>
---
+++ docs/documentation/general/my_profile.php
@@ -0,0 +1,11 @@
+<?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate: 2006-07-01 18:04:33 -0400 (Sat, 01 Jul 2006) $'; ?>
+
+<h2>Network Profile</h2>
+
+<p>Your Network Profile contains information about you that others might like to know, such as your work experience, education, perhaps your personal intersts, or maybe your personal Web site. Click on the edit icon (<img src="../../mods/_standard/social/images/edit_profile.gif" alt="ex">)  while viewing your <strong>Network Profile</strong> to add and make changes to your personal information. Also see <a href="my_settings.php?en"><strong>Settings</strong></a> for details about controlling what parts of your profile others can see. </p>
+
+<p>Also view <strong>Activities</strong> for a list of things you have recently done in your social network. </p>
+
+<p>View your <strong>Contacts'</strong> profiles while viewing your own.</p>
+
+<?php require('../common/body_footer.inc.php'); ?>
