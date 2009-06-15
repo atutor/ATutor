93fdcf22405a703ce2a285a213372e1614cb9075
harris wong <hwong@ocad.ca>
2009-06-15 16:19:04 +0000

    - took out a \n\r

--- docs/themes/default/test_questions/long_stats.tmpl.php
+++ docs/themes/default/test_questions/long_stats.tmpl.php
@@ -11,8 +11,7 @@
 	<td><?php echo $this->row['question']; ?></td>
 	<td align="center" width="70" valign="top"><?php echo $this->num_blanks; ?> / <?php echo $this->num_results; ?></td>
 	<td align="center" valign="top">
-		<?php 
-		if ((count($this->answers) - (isset($this->answers[''])?1:0)) > 0): ?>
+		<?php if ((count($this->answers) - (isset($this->answers[''])?1:0)) > 0): ?>
 			<a href="tools/tests/results_quest_long.php?tid=<?php echo intval($_GET['tid']).SEP; ?>qid=<?php echo $this->row['question_id'].SEP.'q='.urlencode($this->row['question']); ?>"><?php echo _AT('view_responses'); ?> (<?php echo (count($this->answers) - (isset($this->answers[''])?1:0)); ?>)</a>
 		<?php else: ?>
 			<?php echo _AT('none'); ?>		
