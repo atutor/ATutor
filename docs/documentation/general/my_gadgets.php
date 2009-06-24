5987c242f1bd7de3cc169963ccbece963b4882da
greg gay <ggay@ocad.ca>
2009-06-24 17:18:03 +0000

    fix typo

--- docs/documentation/general/my_gadgets.php
+++ docs/documentation/general/my_gadgets.php
@@ -2,9 +2,9 @@
 
 <h2>Gadgets</h2>
 
-<p>Gadgets are applications you can add to your Social Networking environment that provide a whole range of potential networking functionality. They are much link Apps on an IPhone. You can find many gadgets around the Intenet that can be added by copying the URL to the Gadget into the Add Gadget field on the Gadgets screen of ATutor Social.</p>
+<p>Gadgets are applications you can add to your Social Networking environment that provide a whole range of potential networking functionality. They are much link Apps on an IPhone. You can find many gadgets around the Internet that can be added by copying the URL to the Gadget's XML file into the Add Gadget field on the Gadgets screen of ATutor Social.</p>
 
-<p>Here's a list of sites that offer Open Social Gadgets, most of which can be added to ATutor Social. Note that some gadgets that require special authentication to use, may not function with ATutor social currently. Use a search engine to search for variations of the terms "open social gadgets" to find many more. Once a gadget has been added to ATutor, it becomes available for everyone on that ATutor server to select by following <strong>Show Available Gadgets</strong></p>
+<p>Here's a list of some sites that offer Open Social Gadgets, most of which can be added to ATutor Social. Note that some gadgets that require special authentication to use, may not function with ATutor social currently. Use a search engine to search for variations of the terms "open social gadgets" to find many more. Once a gadget has been added to ATutor, it becomes available for everyone on that ATutor server to select by following <strong>Show Available Gadgets</strong></p>
 
 <h3>Gadget Sites</h3>
 <ul>
