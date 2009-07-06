ca8c244d6af23bf2eee1e863d8da7576022849a8
cindy li <cli@ocad.ca>
2009-07-06 15:27:45 +0000

    fix html validation errors

--- docs/themes/default/index.tmpl.php
+++ docs/themes/default/index.tmpl.php
@@ -32,7 +32,7 @@ if(authenticate(AT_PRIV_ADMIN,AT_PRIV_RETURN) && count($this->home_links) > 0){
 if($this->view_mode==0){ ?>
 	<div style="width: 100%; margin-top: -5px; float:left;">
 		<ul id="home-links">
-		<br>
+		<br/>
 		<?php if (is_array($this->home_links)): ?>
 		<?php foreach ($this->home_links as $link): ?>
 			<li><a href="<?php echo $link['url']; ?>"><img src="<?php echo $link['img']; ?>" alt="" class="img-size-home" border="0" /><?php echo $link['title']; ?></a></li>
@@ -43,7 +43,7 @@ if($this->view_mode==0){ ?>
 } else {		// Detail View, $this->view_mode=1.?>
 	
 	<div style="width: 100%; margin-top: -5px; float: left; ">
-		<ul id="home-links">
+		<ul>
 		<?php 				// create table container divided into two columns for the placement of modules
 		if (is_array($this->home_links))
 			foreach ($this->home_links as $link)				// counting the number of modules present in the home for the student. need for controls on the positioning of the arrows of the various modules.
@@ -51,43 +51,45 @@ if($this->view_mode==0){ ?>
 			
 		if(authenticate(AT_PRIV_ADMIN,AT_PRIV_RETURN) && is_array($this->home_links)){		// display enabled course tool
 			foreach ($this->home_links as $link){ ?>
-			<div id="home_box"> 
-				<div id="home_toolbar"><?php
+			<li>
+			<div class="home_box"> 
+				<div class="home_toolbar"><?php
 				if($num_modules!=0){													//si controlla se sono presenti moduli visibili nella home.
 					if($num_modules != 1 && $link['check'] == 'visible'){ 				//se check � impostato 'visible' significa che il modulo sar� presente nella home e potrebbe necessitare delle frecce direzionali
 						if($count_modules <= 2 ){ 
 							if($num_modules >2 && ($num_modules-$count_modules)>1) 								//significa che ci sono possibili moduli sottostanti quindi sar� da stampare la freccia "down"
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=down"><img src="'.AT_BASE_HREF.'images/arrow-mini-down.png" alt="move down" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=down"><img src="'.AT_BASE_HREF.'images/arrow-mini-down.png" alt="move down" border="0"/></a>&nbsp;';
 							if($column=="left")
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp;';
 							else 
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp;';
 						} else if($count_modules>2 && ($num_modules-$count_modules)>1){	//moduli intermedi, dovranno essere stampate obbligatoriamente le frecce 'up', 'down' e a seconda della colonna anche 'sx' o 'dx'
-							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=down"><img src="'.AT_BASE_HREF.'images/arrow-mini-down.png" alt="move down" border="0"/></a>&nbsp';
-							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp';
+							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=down"><img src="'.AT_BASE_HREF.'images/arrow-mini-down.png" alt="move down" border="0"/></a>&nbsp;';
+							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp;';
 							if($column=="left")
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp;';
 							else
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp';	
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp;';	
 						}else if($count_modules>2 && ($num_modules-$count_modules)==1){
-							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp';
+							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp;';
 							if($column=="left")
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=right"><img src="'.AT_BASE_HREF.'images/arrow-mini-right.png" alt="move right" border="0"/></a>&nbsp;';
 							else 
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp;';
 						} else {														//caso in cui la differenza sia pari a zero. se l 'ultimo modulo � nella posizione di sx sar� stampata solo la freccia 'up' altrim se nella posizione di destra: freccia 'up' e 'sx'
-							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp';
+							echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=up"><img src="'.AT_BASE_HREF.'images/arrow-mini-up.png" alt="move up" border="0"/></a>&nbsp;';
 							if($column=="right")
-								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp';
+								echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'&amp;move=left"><img src="'.AT_BASE_HREF.'images/arrow-mini-left.png" alt="move left" border="0"/></a>&nbsp;';
 						}
-						echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'"><img src="'.AT_BASE_HREF.'images/eye-mini-close.png" alt="set invisible" border="0"/></a>&nbsp';
+						echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'"><img src="'.AT_BASE_HREF.'images/eye-mini-close.png" alt="set invisible" border="0"/></a>&nbsp;';
 					} else if($num_modules == 1 && $link['check'] == 'visible'){ //
-						echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'"><img src="'.AT_BASE_HREF.'images/eye-mini-close.png" alt="set invisible" border="0"/></a>&nbsp';
+						echo '<a href ="'.AT_BASE_HREF.'move_module.php?mid='.$count_modules.'"><img src="'.AT_BASE_HREF.'images/eye-mini-close.png" alt="set invisible" border="0"/></a>&nbsp;';
 					}
 				} ?>
 				</div> <?php
 				print_sublinks($link); 						//chiamata alla funzione di stampa dei moduli?>
-			</div> <?php
+			</div>
+			</li> <?php
 				if($column=='left'){									//caso in cui sia appena stata definita la prima cella della riga (posizione left)
 					$column='right';									//$column impostato a right per definire l'eventuale seconda cella
 				} 
@@ -99,13 +101,15 @@ if($this->view_mode==0){ ?>
 		} else {														//caso in cui debbano essere visualizzati i moduli per lo studente nella detail view
 			if (is_array($this->home_links)) {
 				foreach ($this->home_links as $link){?>
-					<div id="home_box">
-						<div id="home_toolbar">
-							<br>
+					<li>
+					<div class="home_box">
+						<div class="home_toolbar">
+							<br/>
 						</div><?php
 						print_sublinks($link); 							//chiamata alla funzione di stampa dei moduli (si ricorda che nel ciclo sono trattati solo quelli visibili nella home)?>
 						
 					</div>
+					</li>
 					<?php
 					if($column=='left'){									//caso in cui sia appena stata definita la prima cella della riga (posizione left)
 						$column='right';									//$column impostato a right per definire l'eventuale seconda cella
@@ -148,11 +152,11 @@ if ($this->announcements): ?>
 * i dati necessari (preventivamente caricati) per la visualizzazione. in questo modo possono essere gestite le due distinte visualizzazioni: istruttore e studente
 */
 function print_sublinks($link){ ?>
-	<div id="home_icon_title">
-		<div id="home_icon">
+	<div class="home_icon_title">
+		<div class="home_icon">
 				<img src="<?php echo $link['img']; ?>" alt="" border="0"/>					
 		</div>
-		<div id="home_title">
+		<div class="home_title">
 				<font size="+1"> 
 					<a href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a>	<!-- inserimento link associato -->
 				</font>
@@ -165,21 +169,21 @@ function print_sublinks($link){ ?>
 				<i><?php echo _AT('none_found'); ?></i>
 			</div><?php
 		} else { ?>								<!-- stampa dei sottocontenuti, per ognuno verr� stampata la sub-icon relativa e il collegamento al sottocontenuto stesso -->
-			<div id="home_content"><?php
+			<div class="home_content"><?php
 				for($i=0; $array[$i]['sub_url']!=""; $i++){ 			//si esegue il ciclo di stampa fin quando saranno presenti sottocontenuti per il modulo corrente. il limite � impostato per default a 3?>
-					<img src="<?php echo $link['icon']; ?>" border="0"/> <?php
+					<img src="<?php echo $link['icon']; ?>" border="0" alt="" /> <?php
 					$text = validate_length($array[$i]['sub_text'], 38, VALIDATE_LENGTH_FOR_DISPLAY); //controllo della lunghezza del testo dei sub content
 					if($text != $array[$i]['sub_text'])					//nel caso in cui la lunghezza sia superiore a quella prefissata viene visualizzato l' "alt" in modo da rapprsentare l'intera stringa
 						$title = $array[$i]['sub_text'];
 					else
 						$title=''; 										//$title impostato '' nel caso in cui non sia necessario visualizzare l' "atl" ?>
 					<a href="<?php echo $array[$i]['sub_url']; ?>" title="<?php echo $title; ?>"> <?php echo $text; ?> </a> 
-					<br> <?php
+					<br/> <?php
 				} ?>
 			</div> <?php
 		}						
 	} else { 									//se non sono presenti sottocontenuti viene adattata la tabella di conseguenza e stampata una breve descrizione?>
-		<div id="home_text">
+		<div class="home_text">
 		<?php echo $link['text']; ?> 
 		</div><?php
 	}
