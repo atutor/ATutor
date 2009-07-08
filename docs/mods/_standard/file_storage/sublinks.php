c7a03e208238e080d13f4f82b5dda5758bde9cf1
greg gay <ggay@ocad.ca>
2009-07-08 19:28:20 +0000

    use description of file as link text if available, download the file

--- docs/mods/_standard/file_storage/sublinks.php
+++ docs/mods/_standard/file_storage/sublinks.php
@@ -6,12 +6,17 @@ global $db;
 
 $file_limit = 3;		//Numero massimo dei possibili sottocontenuti visualizzabili nella home-page
 
-$sql = "SELECT file_id, file_name FROM ".TABLE_PREFIX."files WHERE owner_id=$_SESSION[course_id] ORDER BY date DESC LIMIT $file_limit";
+$sql = "SELECT file_id, file_name, description FROM ".TABLE_PREFIX."files WHERE owner_id=$_SESSION[course_id] ORDER BY date DESC LIMIT $file_limit";
 $result = mysql_query($sql, $db);
 
 if (mysql_num_rows($result) > 0) {
 	while ($row = mysql_fetch_assoc($result)) {
-		$file_list[] = array('sub_url' => $_base_path.url_rewrite('file_storage/home_view.php?fid='.$row['file_id']), 'sub_text' => $row['file_name']); 
+		if($row['description'] !=""){
+			$filetext = $row['description'];
+		}else{
+			$filetext = $row['file_name'];
+		}
+		$file_list[] = array('sub_url' => $_base_path.url_rewrite('file_storage/index.php?download='.$row['file_id']).SEP.'files='. $row['file_id'], 'sub_text' => $filetext); 
 	}
 	return $file_list;	
 } else {
