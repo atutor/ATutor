cd11be7511651ed29cbdc13aee349d3d68cc1877
greg gay <ggay@ocad.ca>
2009-07-13 19:59:56 +0000

    adjust search button to be compatible with fluid.

--- docs/mods/_standard/social/side_menu.inc.php
+++ docs/mods/_standard/social/side_menu.inc.php
@@ -63,7 +63,7 @@ $myApplications = $applications_obj->listMyApplications();
 <form action="<?php echo url_rewrite(AT_SOCIAL_BASENAME.'connections.php', AT_PRETTY_URL_HEADER);?>" method="post">
 	<input type="text" name="search_friends_123" value="<?php echo urldecode($_POST['searchFriends']); ?>" title="<?php echo _AT('search_for_friends'); ?>" />
 	<input type="hidden" name="rand_key" value="123"/><br />
-	<input type="submit" name="search" value="<?php echo _AT('search_for_friends'); ?>" class="button" />
+	<input type="submit" name="search" value="<?php echo _AT('search_for_friends'); ?>" class="button" style="width:10em; margin:auto;float:none;" />
 </form>
 
 <?php
