ae00f55c4608a4348b4b835b5029c134374b5cf3
greg gay <ggay@ocad.ca>
2009-07-13 18:38:45 +0000

    update docs for Find Gadgets

--- docs/documentation/general/my_gadgets.php
+++ docs/documentation/general/my_gadgets.php
@@ -2,27 +2,14 @@
 
 <h2>Gadgets</h2>
 
-<p>Gadgets are applications you can add to your Social Networking environment that provide a whole range of potential networking functionality. They are much link Apps on an IPhone. You can find many gadgets around the Internet that can be added by copying the URL to the Gadget's XML file into the Add Gadget field on the Gadgets screen of ATutor Social.</p>
+<p>Gadgets are applications you can add to your Social Networking environment that provide a whole range of potential networking functionality. They are much like Apps on an IPhone. You can find many gadgets by searching around the Internet, They can be <strong>added by copying the URL to the Gadget's XML file</strong> into the "Add gadget by URL" field on the Add Gadgets screen of ATutor Social.</p>
 
-<p>Here's a list of some sites that offer Open Social Gadgets, most of which can be added to ATutor Social. Note that some gadgets that require special authentication to use, may not function with ATutor social currently. Use a search engine to search for variations of the terms "open social gadgets" to find many more. Once a gadget has been added to ATutor, it becomes available for everyone on that ATutor server to select by following <strong>Show Available Gadgets</strong></p>
+<p>You can use a search engine to search for variations of the terms "open social gadgets" to find many more. Once a gadget has been added to ATutor, it becomes available for everyone on that ATutor server to add to their own networking environement by following <strong>Show Available Gadgets</strong></p>
 
-<h3>Gadget Sites</h3>
-<ul>
-<li><a href="http://www.google.com/ig/directory?hl=en&type=gadgets">http://www.google.com/ig/directory</a></li>
-<li><a href="http://www.hyves.nl/gadgetgallery/#searchhyver=&tabFilterType=featured&__state__=1">http://www.hyves.nl/gadgetgallery/</a></li>
-<li><a href="http://developer.ning.com/opensocial/application/list">http://developer.ning.com/opensocial/application/list</a></li>
-<li><a href="http://opensocial-examples.googlemashups.com/">http://opensocial-examples.googlemashups.com/</a></li>
-<li><a href="http://www.slideshare.net/category/gadgets-reviews">http://www.slideshare.net/</a></li>
-<li><a href="http://www.box.net/">http://www.box.net/</a></li>
-<li><a href="http://www.huddle.net/">http://www.huddle.net/</a></li>
-
-<li><a href="http://www.labpixies.com">http://www.labpixies.com</a></li>
-<li><a href="http://code.google.com/apis/gadgets/docs/legacy/gs.html#GGE">http://code.google.com/apis/gadgets/</a></li>
-
-</ul>
+<p>Use <strong>Find Gadgets</strong> to search through the Google gadget repository. When you find a gaadget you want to add, click on "Add to your webpage" then "Get the Code." In the code that gets generated, look for the URL of an XML file, then copy that URL into the "Add gadget by URL" field, then click the "Add Gadget" button.</p>
 
 <h3>Sample Gadgets</h3>
-<p>To install a gadget, copy the path to the gadget's XML file into the Add Gadget by URL field:</p>
+<p>To install a gadget, copy the path to the gadget's XML file into the "Add gadget by URL" field:</p>
 
 <ul>
 <li>OpenSocialDev App: - http://osda.appspot.com/gadget/osda-igoogle.xml </li>
