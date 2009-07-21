986fdaddd769db46492b4d53405b16ef337e14b7
greg gay <ggay@ocad.ca>
2009-07-21 17:40:47 +0000

    Tweek default theme to give it a slightly different look than 1.6.2

--- docs/themes/default/include/footer.tmpl.php
+++ docs/themes/default/include/footer.tmpl.php
@@ -18,6 +18,6 @@
 	<?php require(AT_INCLUDE_PATH.'html/languages.inc.php'); ?>
 	<?php require(AT_INCLUDE_PATH.'html/copyright.inc.php'); ?>
 </div>
-
+</div> <!-- end page wrapper -->
 </body>
 </html>
--- docs/themes/default/include/header.tmpl.php
+++ docs/themes/default/include/header.tmpl.php
@@ -180,7 +180,7 @@ function toggleToc(objId) {
 }
 //-->
 </script>
-
+<div class="page_wrapper">
 <div id="header">
 	<a href="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES); ?>#content" accesskey="c">
 	<img src="<?php echo $this->base_path; ?>images/clr.gif" height="1" width="1" border="0" alt="<?php echo _AT('goto_content'); ?> ALT+c" /></a>		
