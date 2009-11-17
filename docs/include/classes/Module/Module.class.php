441a0c0cbe79fd0210a19725b6db482ef7484565
cindy li <cli@ocad.ca>
2009-11-17 21:05:07 +0000

    move the conversion of content structure before 1.6.4 to new structure from 1.6.4

--- docs/include/classes/Module/Module.class.php
+++ docs/include/classes/Module/Module.class.php
@@ -665,9 +665,7 @@ class Module {
 		global $db;
 		
 		/* convert all content nodes to the IMS standard. (adds null nodes for all top pages) */
-		include(AT_INCLUDE_PATH.'../install/include/ustep_content_conversion.php');
-
-		/* Convert db to a tree */
+		/* 1. Convert db to a tree */
 		$sql = 'SELECT * FROM '.TABLE_PREFIX.'content where course_id='.$course_id;
 		
 		$result = mysql_query($sql, $db);
@@ -676,14 +674,129 @@ class Module {
 		while ($row = mysql_fetch_assoc($result)){
 			$content_array[$row['content_parent_id']][$row['ordering']] = $row['content_id'];
 		}
-		$tree = buildTree($content_array[0], $content_array);
+		$tree = $this->buildTree($content_array[0], $content_array);
 
-		/* Restructure the tree */
-		$tree = rebuild($tree);
+		/* 2. Restructure the tree */
+		$tree = $this->rebuild($tree);
 
-		/* Update the Db based on this new tree */
-		reconstruct($tree, '', 0, TABLE_PREFIX);
+		/* 3. Update the Db based on this new tree */
+		$this->reconstruct($tree, '', 0, TABLE_PREFIX);
+	}
+
+	/** 
+	 * Construct a tree based on table entries
+	 * @param	array	current node, (current parent)
+	 * @param	mixed	a set of parents, where each parents is in the format of [parent]=>children
+	 *					should remain the same throughout the recursion.
+	 * @return	A tree structure representation of the content entries.
+	 * @author	Harris Wong
+	 */
+	private function buildTree($current, $content_array){
+		$folder = array();
+		foreach($current as $order=>$content_id){
+			//if has children
+			if (isset($content_array[$content_id])){
+				$wrapper[$content_id] = $this->buildTree($content_array[$content_id], $content_array);
+			}
+	
+			//no children.
+			if ($wrapper){
+				$folder['order_'.$order] = $wrapper;
+				unset($wrapper);
+			} else {
+				$folder['order_'.$order] = $content_id;
+			}
+		}	
+		return $folder;
+	}
+	
+	
+	/**
+	 * Transverse the content tree structure, and reconstruct it with the IMS spec.  
+	 * This tree has the structure of [order=>array(id)], so the first layer is its order, second is the id
+	 * if param merge is true, if node!=null, merge it to top layer, and + offset to all others
+	 * @param	mixed	Tree from the buildTree() function, or sub-tree
+	 * @param	mixed	the current tree.
+	 * @return	A new content tree that meets the IMS specification.
+	 * @author	Harris Wong 
+	 */
+	private function rebuild($tree, $node=''){
+	    $order_offset = 0;
+	    $folder = array();
+	    if (!is_array($tree)){
+	        return $tree;
+	    }
+	    if ($node!=''){
+	        $tree['order_0'] = $node;
+	        $order_offset += 1;
+	    }
+	    //go through the tree
+	    foreach($tree as $k=>$v){
+	        if (preg_match('/order\_([\d]+)/', $k, $match)==1){
+	            //if this is the order layer
+	            $folder['order_'.($match[1]+$order_offset)] = $this->rebuild($v);
+	        } else {
+	            //if this is the content layer
+	            if(is_array($v)){
+	                $folder[$k] = $this->rebuild($v, $k);
+	            }
+	        }
+	    }
+	    return $folder;
+	}
+	
+	/**
+	 * Transverse the tree and update/insert entries based on the updated structure.
+	 * @param	array	The tree from rebuild(), and the subtree from the recursion.
+	 * @param	int		the ordering of this subtree respect to its parent.
+	 * @param	int		parent content id
+	 * @return	null (nothing to return, it updates the db only)
+	 */
+	private function reconstruct($tree, $order, $content_parent_id, $table_prefix){
+		global $db;
+	
+		//a content page.
+		if (!is_array($tree)){
+			$sql = 'UPDATE '.$table_prefix."content SET ordering=$order, content_parent_id=$content_parent_id WHERE content_id=$tree";
+			if (!mysql_query($sql, $db)){
+				//throw error
+				echo mysql_error();
+			}
+			return;
+		}
+		foreach ($tree as $k=>$v){
+	        if (preg_match('/order\_([\d]+)/', $k, $match)==1){
+				//order layer
+				$this->reconstruct($v, $match[1], $content_parent_id, $table_prefix);	//inherit the previous layer id
+			} else {
+				//content folder layer
+				$sql = 'SELECT * FROM '.$table_prefix."content WHERE content_id=$k";
+				$result = mysql_query($sql, $db);
+				$old_content_row = mysql_fetch_assoc($result);
+				$sql = 'INSERT INTO '.$table_prefix.'content (course_id, content_parent_id, ordering, last_modified, revision, formatting, release_date, keywords, content_path, title, use_customized_head, allow_test_export, content_type) VALUES ('
+					.$old_content_row['course_id'] . ', '
+					.$content_parent_id . ', '
+					.$order . ', '
+					.'\''. $old_content_row['last_modified'] . '\', '
+					.$old_content_row['revision'] . ', '
+					.$old_content_row['formatting'] . ', '
+					.'\''. $old_content_row['release_date'] . '\', '
+					.'\''. $old_content_row['keywords'] . '\', '
+					.'\''. $old_content_row['content_path'] . '\', '
+					.'\''. $old_content_row['title'] . '\', '
+					.$old_content_row['use_customized_head'] . ', '
+					.$old_content_row['allow_test_export'] . ', '
+					. '1)';
+				
+				if (mysql_query($sql, $db)){
+					$folder_id = mysql_insert_id();
+					$this->reconstruct($v, '', $folder_id, $table_prefix);
+				} else {
+					//throw error
+					echo mysql_error();
+				}
+			}
+		}
 	}
 }
-
 ?>
