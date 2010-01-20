a9b7db4f976111c8e08ac24cfee07c4c2d7d8c7c
greg gay <ggay@ocad.ca>
2010-01-20 20:21:27 +0000

    moved backup files to mods/_core/backups

---
+++ docs/mods/_core/backups/delete.php
@@ -0,0 +1,51 @@
+<?php
+/****************************************************************************/
+/* ATutor																	*/
+/****************************************************************************/
+/* Copyright (c) 2002-2008 by Greg Gay, Joel Kronenberg & Heidi Hazelton	*/
+/* Adaptive Technology Resource Centre / University of Toronto				*/
+/* http://atutor.ca															*/
+/*																			*/
+/* This program is free software. You can redistribute it and/or			*/
+/* modify it under the terms of the GNU General Public License				*/
+/* as published by the Free Software Foundation.							*/
+/****************************************************************************/
+// $Id: delete.php 8901 2009-11-11 19:10:19Z cindy $
+
+define('AT_INCLUDE_PATH', '../../../include/');
+require(AT_INCLUDE_PATH.'vitals.inc.php');
+
+authenticate(AT_PRIV_ADMIN); 
+
+if (isset($_POST['submit_yes'])) {
+	require(AT_INCLUDE_PATH.'../mods/_core/backups/classes/Backup.class.php');
+	$Backup = new Backup($db, $_SESSION['course_id']);
+	$Backup->delete($_POST['backup_id']);
+	$msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
+	header('Location: index.php');
+	exit;
+}
+
+else if (isset($_POST['submit_no'])) {
+	$msg->addFeedback('CANCELLED');
+	header('Location: index.php');
+	exit;
+}
+
+require(AT_INCLUDE_PATH.'header.inc.php');
+
+	$delete_backup = intval($_REQUEST['backup_id']);
+	$sql = "SELECT * from ".TABLE_PREFIX."backups WHERE backup_id = '$delete_backup'";
+	$result = mysql_query($sql, $db);
+
+
+while ($row = mysql_fetch_assoc($result)){
+	$title = $row['file_name'];
+}
+	$index['backup_id'] = $_GET['backup_id'];
+	$msg->addConfirm(array('DELETE', $title ), $index);
+	$msg->printConfirm();
+
+require (AT_INCLUDE_PATH.'footer.inc.php');
+
+?>
