ff03d4e4191656d9045e3680c6fc9941f56365af
greg gay <ggay@ocad.ca>
2010-01-21 21:08:55 +0000

    moved enrollment to mods/_core/enrolment

---
+++ docs/mods/_core/enrolment/admin/privileges.php
@@ -0,0 +1,23 @@
+<?php
+/****************************************************************/
+/* ATutor														*/
+/****************************************************************/
+/* Copyright (c) 2002-2008 by Greg Gay & Joel Kronenberg        */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca												*/
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.				*/
+/****************************************************************/
+// $Id: privileges.php 7208 2008-01-09 16:07:24Z greg $
+define('AT_INCLUDE_PATH', '../../../../include/');
+require(AT_INCLUDE_PATH.'vitals.inc.php');
+
+admin_authenticate(AT_ADMIN_PRIV_ENROLLMENT);
+
+$course_id = intval($_REQUEST['course_id']);
+
+require(AT_INCLUDE_PATH.'../mods/_core/enrolment/html/privileges.inc.php');
+exit;
+?>
--- docs/mods/_core/enrolment/module.php
+++ docs/mods/_core/enrolment/module.php
@@ -8,47 +8,47 @@ define('AT_ADMIN_PRIV_ENROLLMENT', $this->getAdminPrivilege());
 $this->_stacks['users_online'] = array('title_var'=>'users_online', 'file'=>AT_INCLUDE_PATH.'html/dropdowns/users_online.inc.php');
 
 if (admin_authenticate(AT_ADMIN_PRIV_ENROLLMENT, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-	$this->_pages[AT_NAV_ADMIN] = array('admin/enrollment/index.php');
-	$this->_pages['admin/enrollment/index.php']['parent'] = AT_NAV_ADMIN;
+	$this->_pages[AT_NAV_ADMIN] = array('mods/_core/enrolment/admin/index.php');
+	$this->_pages['mods/_core/enrolment/admin/index.php']['parent'] = AT_NAV_ADMIN;
 
-	$this->_pages['admin/enrollment/index.php']['title_var'] = 'enrollment';
-	$this->_pages['admin/enrollment/index.php']['parent']    = AT_NAV_ADMIN;
-	$this->_pages['admin/enrollment/index.php']['guide']     = 'admin/?p=enrollment.php';
+	$this->_pages['mods/_core/enrolment/admin/index.php']['title_var'] = 'enrollment';
+	$this->_pages['mods/_core/enrolment/admin/index.php']['parent']    = AT_NAV_ADMIN;
+	$this->_pages['mods/_core/enrolment/admin/index.php']['guide']     = 'admin/?p=enrollment.php';
 
-	$this->_pages['admin/enrollment/enroll_edit.php']['title_var']    = 'enrollment';
-	$this->_pages['admin/enrollment/enroll_edit.php']['parent']   = 'admin/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/admin/enroll_edit.php']['title_var']    = 'enrollment';
+	$this->_pages['mods/_core/enrolment/admin/enroll_edit.php']['parent']   = 'mods/_core/enrolment/admin/index.php';
 
-	$this->_pages['admin/enrollment/privileges.php']['title_var'] = 'privileges';
-	$this->_pages['admin/enrollment/privileges.php']['parent']    = 'admin/enrollment/index.php';
-	$this->_pages['admin/enrollment/privileges.php']['guide']     = 'admin/?p=enrollment_privileges.php';
+	$this->_pages['mods/_core/enrolment/admin/privileges.php']['title_var'] = 'privileges';
+	$this->_pages['mods/_core/enrolment/admin/privileges.php']['parent']    = 'mods/_core/enrolment/admin/index.php';
+	$this->_pages['mods/_core/enrolment/admin/privileges.php']['guide']     = 'admin/?p=enrollment_privileges.php';
 
 	// linked from users.php
 	$this->_pages['admin/user_enrollment.php']['title_var'] = 'enrollment';
 	$this->_pages['admin/user_enrollment.php']['parent']    = 'admin/users.php';
 }
 
-$this->_pages['tools/enrollment/index.php']['title_var'] = 'enrollment';
-$this->_pages['tools/enrollment/index.php']['parent']    = 'tools/index.php';
-$this->_pages['tools/enrollment/index.php']['guide']     = 'instructor/?p=enrollment.php';
-$this->_pages['tools/enrollment/index.php']['children'] = array('tools/enrollment/export_course_list.php', 'tools/enrollment/import_course_list.php', 'tools/enrollment/create_course_list.php');
+$this->_pages['mods/_core/enrolment/index.php']['title_var'] = 'enrollment';
+$this->_pages['mods/_core/enrolment/index.php']['parent']    = 'tools/index.php';
+$this->_pages['mods/_core/enrolment/index.php']['guide']     = 'instructor/?p=enrollment.php';
+$this->_pages['mods/_core/enrolment/index.php']['children'] = array('mods/_core/enrolment/export_course_list.php', 'mods/_core/enrolment/import_course_list.php', 'mods/_core/enrolment/create_course_list.php');
 
-	$this->_pages['tools/enrollment/export_course_list.php']['title_var'] = 'list_export_course_list';
-	$this->_pages['tools/enrollment/export_course_list.php']['parent']    = 'tools/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/export_course_list.php']['title_var'] = 'list_export_course_list';
+	$this->_pages['mods/_core/enrolment/export_course_list.php']['parent']    = 'mods/_core/enrolment/index.php';
 
-	$this->_pages['tools/enrollment/import_course_list.php']['title_var'] = 'list_import_course_list';
-	$this->_pages['tools/enrollment/import_course_list.php']['parent']    = 'tools/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/import_course_list.php']['title_var'] = 'list_import_course_list';
+	$this->_pages['mods/_core/enrolment/import_course_list.php']['parent']    = 'mods/_core/enrolment/index.php';
 
-	$this->_pages['tools/enrollment/create_course_list.php']['title_var'] = 'list_create_course_list';
-	$this->_pages['tools/enrollment/create_course_list.php']['parent']    = 'tools/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/create_course_list.php']['title_var'] = 'list_create_course_list';
+	$this->_pages['mods/_core/enrolment/create_course_list.php']['parent']    = 'mods/_core/enrolment/index.php';
 
-	$this->_pages['tools/enrollment/verify_list.php']['title_var']  = 'course_list';
-	$this->_pages['tools/enrollment/verify_list.php']['parent'] = 'tools/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/verify_list.php']['title_var']  = 'course_list';
+	$this->_pages['mods/_core/enrolment/verify_list.php']['parent'] = 'mods/_core/enrolment/index.php';
 
-	$this->_pages['tools/enrollment/privileges.php']['title_var']  = 'privileges';
-	$this->_pages['tools/enrollment/privileges.php']['parent'] = 'tools/enrollment/index.php';
-	$this->_pages['tools/enrollment/privileges.php']['guide']     = 'instructor/?p=enrollment_privileges.php';
+	$this->_pages['mods/_core/enrolment/privileges.php']['title_var']  = 'privileges';
+	$this->_pages['mods/_core/enrolment/privileges.php']['parent'] = 'mods/_core/enrolment/index.php';
+	$this->_pages['mods/_core/enrolment/privileges.php']['guide']     = 'instructor/?p=enrollment_privileges.php';
 	
-	$this->_pages['tools/enrollment/enroll_edit.php']['title_var']    = 'enrollment';
-	$this->_pages['tools/enrollment/enroll_edit.php']['parent']   = 'tools/enrollment/index.php';
+	$this->_pages['mods/_core/enrolment/enroll_edit.php']['title_var']    = 'enrollment';
+	$this->_pages['mods/_core/enrolment/enroll_edit.php']['parent']   = 'mods/_core/enrolment/index.php';
 
 ?>
