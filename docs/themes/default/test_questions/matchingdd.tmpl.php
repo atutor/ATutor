c70d9f7f75f163eb3a78f50671a29f8e7ad76a8d
greg gay <ggay@ocad.ca>
2010-01-21 16:44:01 +0000

    fix include path

--- docs/themes/default/test_questions/matchingdd.tmpl.php
+++ docs/themes/default/test_questions/matchingdd.tmpl.php
@@ -6,4 +6,4 @@
 <?php for ($i=0; $i < $this->num_choices; $i++): ?>
 	<input type="hidden" name="answers[<?php echo $this->row['question_id']; ?>][<?php echo $i; ?>]" id="<?php echo $this->row['question_id']; ?>q<?php echo $i; ?>" value="<?php echo $this->response[$i]; ?>"/>
 <?php endfor; ?>
-<iframe id="qframe<?php echo $this->row['question_id']; ?>" src="<?php echo $this->base_href; ?>tools/tests/dd.php?qid=<?php echo $this->row['question_id'].SEP; ?>response=<?php echo $tmp_response; ?>" height="200" width="100%" frameborder="0"></iframe>
+<iframe id="qframe<?php echo $this->row['question_id']; ?>" src="<?php echo $this->base_href; ?>mods/_standard/tests/dd.php?qid=<?php echo $this->row['question_id'].SEP; ?>response=<?php echo $tmp_response; ?>" height="200" width="100%" frameborder="0"></iframe>
