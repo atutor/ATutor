b3b6fd1e7faa83bd671f865170882a6d75a76696
greg gay <ggay@ocad.ca>
2010-01-21 15:28:42 +0000

    replace duplicate view_results with student version

--- docs/mods/_standard/tests/view_results.php
+++ docs/mods/_standard/tests/view_results.php
@@ -10,78 +10,12 @@
 /* modify it under the terms of the GNU General Public License  */
 /* as published by the Free Software Foundation.				*/
 /****************************************************************/
-// $Id: view_results.php 8979 2009-11-30 20:11:07Z cindy $
+// $Id: view_results.php 9112 2010-01-20 15:06:52Z cindy $
 define('AT_INCLUDE_PATH', '../../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
-authenticate(AT_PRIV_TESTS);
-require(AT_INCLUDE_PATH.'../mods/_standard/tests/lib/test_result_functions.inc.php');
+require(AT_INCLUDE_PATH.'../mods/_standard/tests/lib/test_result_functions.inc.php'); // for print_result and print_score
 require(AT_INCLUDE_PATH.'../mods/_standard/tests/classes/testQuestions.class.php');
 $_letters = array(_AT('A'), _AT('B'), _AT('C'), _AT('D'), _AT('E'), _AT('F'), _AT('G'), _AT('H'), _AT('I'), _AT('J'));
-
-$tid = intval($_GET['tid']);
-if ($tid == 0){
-	$tid = intval($_POST['tid']);
-}
-
-$_pages['mods/_standard/tests/view_results.php']['title_var']  = 'view_results';
-$_pages['mods/_standard/tests/view_results.php']['parent'] = 'mods/_standard/tests/results.php?tid='.$tid;
-
-$_pages['mods/_standard/tests/results.php?tid='.$tid]['title_var'] = 'submissions';
-$_pages['mods/_standard/tests/results.php?tid='.$tid]['parent'] = 'mods/_standard/tests/index.php';
-
-$sql	= "SELECT * FROM ".TABLE_PREFIX."tests WHERE test_id=$tid AND course_id=$_SESSION[course_id]";
-$result	= mysql_query($sql, $db);
-
-if (!($row = mysql_fetch_array($result))){
-	require(AT_INCLUDE_PATH.'header.inc.php');
-	$msg->printErrors('ITEM_NOT_FOUND');
-	require (AT_INCLUDE_PATH.'footer.inc.php');
-	exit;
-}
-$test_title = $row['title'];
-$out_of		= $row['out_of'];
-
-if ($_POST['cancel']) {
-	$msg->addFeedback('CANCELLED');
-	header('Location: results.php?tid='.$tid);
-	exit;
-} else if ($_POST['back']) {
-	header('Location: results.php?tid='.$tid);
-	exit;
-} else if ($_POST['submit']) {
-	$tid = intval($_POST['tid']);
-	$rid = intval($_POST['rid']);
-	
-	$final_score = 0;
-	if (is_array($_POST['scores'])) {
-		foreach ($_POST['scores'] as $qid => $score) {
-			$qid          = intval($qid);
-			if ($score == '')
-			{
-				if ($row['result_release']==AT_RELEASE_MARKED)
-					$set_empty_final_score = true;
-			}
-			else
-			{
-				$score		  = floatval($score);
-				$final_score += $score;
-			}
-
-			$sql	= "UPDATE ".TABLE_PREFIX."tests_answers SET score='$score' WHERE result_id=$rid AND question_id=$qid";
-			$result	= mysql_query($sql, $db);
-		}
-	}
-
-	if ($set_empty_final_score)
-		$sql	= "UPDATE ".TABLE_PREFIX."tests_results SET final_score=NULL, date_taken=date_taken, end_time=end_time WHERE result_id=$rid AND status=1";
-	else
-		$sql	= "UPDATE ".TABLE_PREFIX."tests_results SET final_score='$final_score', date_taken=date_taken, end_time=end_time WHERE result_id=$rid AND status=1";
-	$result	= mysql_query($sql, $db);
-
-	$msg->addFeedback('RESULTS_UPDATED');
-	header('Location: results.php?tid='.$tid);
-	exit;
-}
 
 if (defined('AT_FORCE_GET_FILE') && AT_FORCE_GET_FILE) {
 	$content_base_href = 'get.php/';
@@ -89,52 +23,173 @@ if (defined('AT_FORCE_GET_FILE') && AT_FORCE_GET_FILE) {
 	$course_base_href = 'content/' . $_SESSION['course_id'] . '/';
 }
 
+if (isset($_REQUEST['goto_content']))
+{
+	header('Location: '.url_rewrite('content.php?cid='.$cid, AT_PRETTY_URL_IS_HEADER));
+	exit;
+}
+if (isset($_REQUEST['back']))
+{
+	header('Location: '.url_rewrite('../mods/_standard/tests/my_tests.php', AT_PRETTY_URL_IS_HEADER));
+	exit;
+}
+
 require(AT_INCLUDE_PATH.'header.inc.php');
 
 $tid = intval($_GET['tid']);
 $rid = intval($_GET['rid']);
+if (isset($_REQUEST['cid'])) $cid = $_REQUEST['cid'];
 
-$sql	= "SELECT TQ.*, TQA.* FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQ.course_id=$_SESSION[course_id] AND TQA.test_id=$tid ORDER BY TQA.ordering";
+$sql	= "SELECT title, random, passfeedback, failfeedback, passscore, passpercent FROM ".TABLE_PREFIX."tests WHERE test_id=$tid AND course_id=$_SESSION[course_id]";
 $result	= mysql_query($sql, $db);
+$row	= mysql_fetch_array($result);
+$test_title	= $row['title'];
+$passfeedback	= $row['passfeedback'];
+$failfeedback	= $row['failfeedback'];
+$passscore	= $row['passscore'];
+$passpercent	= $row['passpercent'];
+$is_random  = $row['random'];
+
+$mark_right = ' <img src="'.$_base_path.'images/checkmark.gif" alt="'._AT('correct_answer').'" title="'._AT('correct_answer').'" />';
+$mark_wrong = ' <img src="'.$_base_path.'images/x.gif" alt="'._AT('wrong_answer').'" title="'._AT('wrong_answer').'" />';
+
+$sql	= "SELECT * FROM ".TABLE_PREFIX."tests_results WHERE result_id=$rid";
+$result	= mysql_query($sql, $db); 
+if (!$row = mysql_fetch_assoc($result)){
+	$msg->printErrors('ITEM_NOT_FOUND');
+	require(AT_INCLUDE_PATH.'footer.inc.php');
+	exit;
+}
+$final_score= $row['final_score'];
+
+//make sure they're allowed to see results now
+$sql	= "SELECT result_release, out_of FROM ".TABLE_PREFIX."tests WHERE test_id=$tid AND course_id=$_SESSION[course_id]";
+$result	= mysql_query($sql, $db); 
+$row = mysql_fetch_assoc($result);
+
+if ( ($row['result_release']==AT_RELEASE_NEVER) || ($row['result_release']==AT_RELEASE_MARKED && $final_score=='') ) {
+	$msg->printErrors('RESULTS_NOT_RELEASED');
+	require(AT_INCLUDE_PATH.'footer.inc.php');
+	exit;
+}
+
+$out_of = $row['out_of'];
+
+/* Retrieve randomly choosed questions */
+$sql	= "SELECT question_id FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid";
+$result	= mysql_query($sql, $db); 
+$row = mysql_fetch_array($result);
+$random_id_string = $row[question_id];
+$row = mysql_fetch_array($result);	
+while ($row['question_id'] != '') {
+	$random_id_string = $random_id_string.','.$row['question_id'];
+	$row = mysql_fetch_array($result);
+}
+if (!$random_id_string) {
+	$random_id_string = 0;
+}
+
+if ($is_random) {
+	$sql	= "SELECT TQ.*, TQA.* FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string) ORDER BY TQ.question_id";
+} else {
+	$sql	= "SELECT TQ.*, TQA.* FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string) ORDER BY TQA.ordering, TQ.question_id";
+}
+$result	= mysql_query($sql, $db); 
 
 if (mysql_num_rows($result) == 0) {
 	echo '<p>'._AT('no_questions').'</p>';
 	require(AT_INCLUDE_PATH.'footer.inc.php');
 	exit;
 }
-?>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-<input type="hidden" name="tid" value="<?php echo $tid; ?>">
-<input type="hidden" name="rid" value="<?php echo $rid; ?>">
+// calculate test/my total score to display pass/fail feedback
+$sql_test_total = "SELECT sum(TQA.weight) test_total_score FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string)";
+$result_test_total	= mysql_query($sql_test_total, $db);
+$row_test_total = mysql_fetch_array($result_test_total);
+$test_total_score = $row_test_total["test_total_score"];
+
+while ($row = mysql_fetch_assoc($result)) {
+	$sql_this_score = "SELECT * FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid AND question_id=$row[question_id] AND member_id=$_SESSION[member_id]";
+	$result_this_score	= mysql_query($sql_this_score, $db); 
+	$this_score = mysql_fetch_assoc($result_this_score);
+
+	$my_score+=$this_score['score'];
+	$this_total += $row['weight'];
+}
+?>
+<form method="get" action="<?php echo $_REQUEST['PHP_SELF']; ?>">
+<?php if (isset($_REQUEST['cid'])) {?> <input type="hidden" name="cid" value="<?php echo $cid; ?>" /> <?php }?>
 
 <div class="input-form">
-	<fieldset class="group_form"><legend class="group_form"><?php echo AT_print($test_title, 'tests.title'); ?></legend>
+	<div class="row">
+		<h2><?php echo AT_print($test_title, 'tests.title'); ?></h2>
+	</div>
 
-	<?php while ($row = mysql_fetch_assoc($result)) {
-		/* get the results for this question */
-		$sql		= "SELECT C.* FROM ".TABLE_PREFIX."tests_answers C WHERE C.result_id=$rid AND C.question_id=$row[question_id]";
-		$result_a	= mysql_query($sql, $db);
-		if ($answer_row = mysql_fetch_assoc($result_a)) {
-			$obj = TestQuestions::getQuestion($row['type']);
-			$obj->displayResult($row, $answer_row, TRUE);
+	<div class="row">
+		<h3 align="center">
+			<?php 
+				// don't display any feedback if test is created as "no pass score"
+				if (($passscore == 0 && $passpercent == 0) || ($passpercent <> 0 && $this_total == 0))
+					echo '';
+				// display pass feedback for passed students
+				elseif (($passscore<>0 && $my_score>=$passscore) ||
+				    ($passpercent<>0 && ($my_score/$this_total*100)>=$passpercent))
+					echo '<font color="green">' . $passfeedback . '</font>';
+				// otherwise, display fail feedback
+				else
+					echo '<font color="red">' . $failfeedback . '</font>'; 
+			?>
+		</h3>
+	</div>
 
-			if ($row['feedback']) {
-				echo '<div class="row"><p><strong>'._AT('feedback').':</strong> ';
-				echo AT_print(nl2br($row['feedback']), 'tests_questions.feedback').'</p></div>';
-			}
+	<?php if ($row['instructions'] != ''): ?>
+		<div style="background-color: #f3f3f3; padding: 5px 10px; margin: 0px; border-top: 1px solid">
+			<strong><?php echo _AT('instructions'); ?></strong>
+		</div>
+		<div class="row" style="padding-bottom: 20px"><?php echo $row['instructions']; ?></div>
+	<?php endif; ?>
+
+	<?php
+	// reset the result cursor to beginning
+	mysql_data_seek ($result, 0);
+	
+	while ($row = mysql_fetch_assoc($result)) {
+		$sql		= "SELECT * FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid AND question_id=$row[question_id] AND member_id=$_SESSION[member_id]";
+		$result_a	= mysql_query($sql, $db); 
+		$answer_row = mysql_fetch_assoc($result_a);
+
+		$obj = TestQuestions::getQuestion($row['type']);
+		$obj->displayResult($row, $answer_row);
+
+		if ($row['feedback']) {
+			echo '<div class="row"><p><strong>'._AT('feedback').':</strong> ';
+			echo AT_print(nl2br($row['feedback']), 'tests_questions.feedback').'</p></div>';
 		}
 	}
 	?>
 
-	<div class="row buttons">
-	<?php if ($out_of): ?>
-		<input type="submit" value="<?php echo _AT('save'); ?>" name="submit" accesskey="s" /> <input type="submit" value="<?php echo _AT('cancel'); ?>" name="cancel" />
+	<?php if ($this_total): ?>
+		<div style="background-color: #f3f3f3; padding: 5px 10px; margin: 0px; border-top: 1px solid">
+			<strong>
+				<span style="float: right"><?php echo $my_score .' / '.$this_total; ?> <?php echo _AT('points'); ?></span>
+				<?php echo _AT('final_score'); ?>:
+			</strong>
+		</div>
 	<?php else: ?>
-		<input type="submit" value="<?php echo _AT('back'); ?>" name="back" />
+		<div style="background-color: #f3f3f3; padding: 5px 10px; margin: 0px; border-top: 1px solid">
+			<strong>
+				<?php echo _AT('done'); ?>!
+			</strong>
+		</div>
 	<?php endif; ?>
+
+	<div class="row buttons">
+		<?php if (isset($cid)) {?>
+		<input type="submit" value="<?php echo _AT('goto_content'); ?>" name="goto_content" />
+		<?php } else {?>
+		<input type="submit" value="<?php echo _AT('back'); ?>" name="back" />
+		<?php }?>
 	</div>
-	</fieldset>
 </div>
 </form>
 
