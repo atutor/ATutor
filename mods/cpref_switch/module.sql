ca32bc3628b32d69fe34ff7ee251e9e0a5bace9e
laurel <laurel.williams@utoronto.ca>
2010-01-21 16:42:50 +0000

    removed module.sql. Will use existing text

--- mods/cpref_switch/module.sql
+++
@@ -1,4 +0,0 @@
-# sql file for cpref_switch module
-
-INSERT INTO `language_text` VALUES ('en', '_module','cpref_switch','Content preferences switcher',NOW(),'');
-INSERT INTO `language_text` VALUES ('en', '_module','cpref_switch_text','A sample switcher text for detailed homepage.',NOW(),'');
