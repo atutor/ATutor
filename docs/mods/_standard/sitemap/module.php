1cbe8757ef9510b81a96438f54a4cf3291f6e912
greg gay <ggay@ocad.ca>
2010-02-04 18:05:10 +0000

    moved sitemap to mods/_standard/sitemap

--- docs/mods/_standard/sitemap/module.php
+++ docs/mods/_standard/sitemap/module.php
@@ -3,11 +3,11 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__FILE__ . ' is not a Module'); }
 
 // if this module is to be made available to students on the Home or Main Navigation
-$_student_tool = 'sitemap.php';
+$_student_tool = 'mods/_standard/sitemap/sitemap.php';
 
-$this->_pages['sitemap.php']['title_var'] = 'sitemap';
-$this->_pages['sitemap.php']['parent']    = 'index.php';
-$this->_pages['sitemap.php']['img']       = 'images/home-site_map.png';
-$this->_pages['sitemap.php']['text']      = _AT('sitemap_text');
+$this->_pages['mods/_standard/sitemap/sitemap.php']['title_var'] = 'sitemap';
+$this->_pages['mods/_standard/sitemap/sitemap.php']['parent']    = 'index.php';
+$this->_pages['mods/_standard/sitemap/sitemap.php']['img']       = 'images/home-site_map.png';
+$this->_pages['mods/_standard/sitemap/sitemap.php']['text']      = _AT('sitemap_text');
 
 ?>
