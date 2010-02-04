3b69f9461f8a70b19830b0882acb4ae681768e8c
greg gay <ggay@ocad.ca>
2010-02-04 19:14:01 +0000

    moved main content index into mods/_core/content

--- docs/mods/_core/content/module.php
+++ docs/mods/_core/content/module.php
@@ -17,27 +17,27 @@ $this->_stacks['search'] = array('title_var'=>'search', 'file'=>AT_INCLUDE_PATH.
 
 $this->_pages['search.php']['title_var']      = 'search';
 
-$this->_pages['tools/content/index.php']['title_var'] = 'content';
-$this->_pages['tools/content/index.php']['parent']    = 'tools/index.php';
-$this->_pages['tools/content/index.php']['guide']     = 'instructor/?p=content.php';
-$this->_pages['tools/content/index.php']['children']  = array('mods/_core/editor/add_content.php', 'mods/_core/editor/arrange_content.php', 'mods/_core/imscp/index.php');
+$this->_pages['mods/_core/content/index.php']['title_var'] = 'content';
+$this->_pages['mods/_core/content/index.php']['parent']    = 'tools/index.php';
+$this->_pages['mods/_core/content/index.php']['guide']     = 'instructor/?p=content.php';
+$this->_pages['mods/_core/content/index.php']['children']  = array('mods/_core/editor/add_content.php', 'mods/_core/editor/arrange_content.php', 'mods/_core/imscp/index.php');
 
 $this->_pages['mods/_core/editor/add_content.php']['title_var']    = 'add_content';
-$this->_pages['mods/_core/editor/add_content.php']['parent']   = 'tools/content/index.php';
+$this->_pages['mods/_core/editor/add_content.php']['parent']   = 'mods/_core/content/index.php';
 $this->_pages['mods/_core/editor/add_content.php']['guide']     = 'instructor/?p=creating_editing_content.php';
 
 $this->_pages['mods/_core/editor/arrange_content.php']['title_var']    = 'arrange_content';
-$this->_pages['mods/_core/editor/arrange_content.php']['parent']   = 'tools/content/index.php';
+$this->_pages['mods/_core/editor/arrange_content.php']['parent']   = 'mods/_core/content/index.php';
 $this->_pages['mods/_core/editor/arrange_content.php']['guide']     = 'instructor/?p=arrange_content.php';
 
 $this->_pages['mods/_core/editor/edit_content.php']['title_var'] = 'edit_content';
-$this->_pages['mods/_core/editor/edit_content.php']['parent']    = 'tools/content/index.php';
+$this->_pages['mods/_core/editor/edit_content.php']['parent']    = 'mods/_core/content/index.php';
 $this->_pages['mods/_core/editor/edit_content.php']['guide']     = 'instructor/?p=creating_editing_content.php';
 
 
 //instructors
 $this->_pages['mods/_core/imscp/index.php']['title_var'] = 'content_packaging';
-$this->_pages['mods/_core/imscp/index.php']['parent']    = 'tools/content/index.php';
+$this->_pages['mods/_core/imscp/index.php']['parent']    = 'mods/_core/content/index.php';
 $this->_pages['mods/_core/imscp/index.php']['guide']     = 'instructor/?p=content_packages.php';
 
 //students
@@ -51,10 +51,10 @@ if (!isset($_GET['cid']) && !isset($_POST['cid']))
 	$this->_pages['mods/_core/editor/edit_content_folder.php']['title_var'] = 'add_content_folder';
 else
 	$this->_pages['mods/_core/editor/edit_content_folder.php']['title_var'] = 'edit_content_folder';
-$this->_pages['mods/_core/editor/edit_content_folder.php']['parent']    = 'tools/content/index.php';
+$this->_pages['mods/_core/editor/edit_content_folder.php']['parent']    = 'mods/_core/content/index.php';
 $this->_pages['mods/_core/editor/edit_content_folder.php']['guide']     = 'instructor/?p=creating_editing_content_folder.php';
 
 $this->_pages['mods/_core/editor/delete_content.php']['title_var'] = 'delete_content';
-$this->_pages['mods/_core/editor/delete_content.php']['parent']    = 'tools/content/index.php';
+$this->_pages['mods/_core/editor/delete_content.php']['parent']    = 'mods/_core/content/index.php';
 
 ?>
