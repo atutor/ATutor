585bae53d01265d5b1d5507b387d121c1f29a332
greg gay <ggay@ocad.ca>
2010-02-09 16:52:00 +0000

    move admin categories to mods/_core/cats_categories

--- docs/mods/_core/cats_categories/module.php
+++ docs/mods/_core/cats_categories/module.php
@@ -6,25 +6,25 @@ define('AT_ADMIN_PRIV_CATEGORIES', $this->getAdminPrivilege());
 
 if (admin_authenticate(AT_ADMIN_PRIV_CATEGORIES, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
 	if (admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-		$this->_pages['admin/courses.php']['children'] = array('admin/course_categories.php');
-		$this->_pages['admin/course_categories.php']['parent']    = 'admin/courses.php';
+		$this->_pages['mods/_core/courses/admin/courses.php']['children'] = array('mods/_core/cats_categories/admin/course_categories.php');
+		$this->_pages['mods/_core/cats_categories/admin/course_categories.php']['parent']    = 'mods/_core/courses/admin/courses.php';
 	} else {
-		$this->_pages[AT_NAV_ADMIN] = array('admin/course_categories.php');
-		$this->_pages['admin/course_categories.php']['parent'] = AT_NAV_ADMIN;
+		$this->_pages[AT_NAV_ADMIN] = array('mods/_core/cats_categories/admin/course_categories.php');
+		$this->_pages['mods/_core/cats_categories/admin/course_categories.php']['parent'] = AT_NAV_ADMIN;
 	}
 
-$this->_pages['admin/course_categories.php']['title_var'] = 'cats_categories';
-$this->_pages['admin/course_categories.php']['guide']     = 'admin/?p=categories.php';
-$this->_pages['admin/course_categories.php']['children']  = array('admin/create_category.php');
+$this->_pages['mods/_core/cats_categories/admin/course_categories.php']['title_var'] = 'cats_categories';
+$this->_pages['mods/_core/cats_categories/admin/course_categories.php']['guide']     = 'admin/?p=categories.php';
+$this->_pages['mods/_core/cats_categories/admin/course_categories.php']['children']  = array('mods/_core/cats_categories/admin/create_category.php');
 
-	$this->_pages['admin/create_category.php']['title_var'] = 'create_category';
-	$this->_pages['admin/create_category.php']['parent']    = 'admin/course_categories.php';
+	$this->_pages['mods/_core/cats_categories/admin/create_category.php']['title_var'] = 'create_category';
+	$this->_pages['mods/_core/cats_categories/admin/create_category.php']['parent']    = 'mods/_core/cats_categories/admin/course_categories.php';
 
-	$this->_pages['admin/edit_category.php']['title_var'] = 'edit_category';
-	$this->_pages['admin/edit_category.php']['parent']    = 'admin/course_categories.php';
+	$this->_pages['mods/_core/cats_categories/admin/edit_category.php']['title_var'] = 'edit_category';
+	$this->_pages['mods/_core/cats_categories/admin/edit_category.php']['parent']    = 'mods/_core/cats_categories/admin/course_categories.php';
 
-	$this->_pages['admin/delete_category.php']['title_var'] = 'delete_category';
-	$this->_pages['admin/delete_category.php']['parent']    = 'admin/course_categories.php';
+	$this->_pages['mods/_core/cats_categories/admin/delete_category.php']['title_var'] = 'delete_category';
+	$this->_pages['mods/_core/cats_categories/admin/delete_category.php']['parent']    = 'mods/_core/cats_categories/admin/course_categories.php';
 
 }
 ?>
