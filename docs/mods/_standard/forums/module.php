90a9d3a80dec8a0839ccb25cc1ab21ce9c59b746
greg gay <ggay@ocad.ca>
2010-02-09 16:56:45 +0000

    adjust path to admin's shared forums tool, now in mods/_core/courses

--- docs/mods/_standard/forums/module.php
+++ docs/mods/_standard/forums/module.php
@@ -45,25 +45,25 @@ $this->_pages['mods/_standard/forums/forum/list.php']['children']        = array
 // for admin
 if (admin_authenticate(AT_ADMIN_PRIV_FORUMS, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
 	if (admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-		$this->_pages['admin/courses.php']['children'] = array('admin/forums.php');
-		$this->_pages['admin/forums.php']['parent']    = 'admin/courses.php';
+		$this->_pages['mods/_core/courses/admin/courses.php']['children'] = array('mods/_standard/forums/admin/forums.php');
+		$this->_pages['mods/_standard/forums/admin/forums.php']['parent']    = 'mods/_core/courses/admin/courses.php';
 	} else {
-		$this->_pages[AT_NAV_ADMIN] = array('admin/forums.php');
-		$this->_pages['admin/forums.php']['parent'] = AT_NAV_ADMIN;
+		$this->_pages[AT_NAV_ADMIN] = array('mods/_standard/forums/admin/forums.php');
+		$this->_pages['mods/_standard/forums/admin/forums.php']['parent'] = AT_NAV_ADMIN;
 	}
 
-	$this->_pages['admin/forums.php']['title_var'] = 'forums';
-	$this->_pages['admin/forums.php']['guide']     = 'admin/?p=forums.php';
-	$this->_pages['admin/forums.php']['children']  = array('admin/forum_add.php');
+	$this->_pages['mods/_standard/forums/admin/forums.php']['title_var'] = 'forums';
+	$this->_pages['mods/_standard/forums/admin/forums.php']['guide']     = 'mods/_standard/forums/admin/?p=forums.php';
+	$this->_pages['mods/_standard/forums/admin/forums.php']['children']  = array('mods/_standard/forums/admin/forum_add.php');
 
-		$this->_pages['admin/forum_add.php']['title_var'] = 'create_forum';
-		$this->_pages['admin/forum_add.php']['parent']    = 'admin/forums.php';
+		$this->_pages['mods/_standard/forums/admin/forum_add.php']['title_var'] = 'create_forum';
+		$this->_pages['mods/_standard/forums/admin/forum_add.php']['parent']    = 'mods/_standard/forums/admin/forums.php';
 
-		$this->_pages['admin/forum_edit.php']['title_var'] = 'edit_forum';
-		$this->_pages['admin/forum_edit.php']['parent']    = 'admin/forums.php';
+		$this->_pages['mods/_standard/forums/admin/forum_edit.php']['title_var'] = 'edit_forum';
+		$this->_pages['mods/_standard/forums/admin/forum_edit.php']['parent']    = 'mods/_standard/forums/admin/forums.php';
 
-		$this->_pages['admin/forum_delete.php']['title_var'] = 'delete_forum';
-		$this->_pages['admin/forum_delete.php']['parent']    = 'admin/forums.php';
+		$this->_pages['mods/_standard/forums/admin/forum_delete.php']['title_var'] = 'delete_forum';
+		$this->_pages['mods/_standard/forums/admin/forum_delete.php']['parent']    = 'mods/_standard/forums/admin/forums.php';
 }
 
 function forums_get_group_url($group_id) {
