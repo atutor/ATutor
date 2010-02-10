506bc178f872affe3df56eb263b49ff59db05ff5
greg gay <ggay@ocad.ca>
2010-02-10 20:59:08 +0000

    moved course tools to mods/_standard/course_tools

--- docs/mods/_standard/course_tools/module.php
+++ docs/mods/_standard/course_tools/module.php
@@ -4,13 +4,13 @@ if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module')
 
 define('AT_PRIV_STYLES', $this->getPrivilege());
 
-$this->_pages['tools/modules.php']['title_var'] = 'course_tools';
-$this->_pages['tools/modules.php']['parent']    = 'tools/index.php';
-$this->_pages['tools/modules.php']['children']  = array('tools/side_menu.php');
-$this->_pages['tools/modules.php']['guide']     = 'instructor/?p=student_tools.php';
+$this->_pages['mods/_standard/course_tools/modules.php']['title_var'] = 'course_tools';
+$this->_pages['mods/_standard/course_tools/modules.php']['parent']    = 'tools/index.php';
+$this->_pages['mods/_standard/course_tools/modules.php']['children']  = array('mods/_standard/course_tools/side_menu.php');
+$this->_pages['mods/_standard/course_tools/modules.php']['guide']     = 'instructor/?p=student_tools.php';
 
-	$this->_pages['tools/side_menu.php']['title_var'] = 'side_menu';
-	$this->_pages['tools/side_menu.php']['parent']    = 'tools/modules.php';
-	$this->_pages['tools/side_menu.php']['guide']     = 'instructor/?p=side_menu.php';
+	$this->_pages['mods/_standard/course_tools/side_menu.php']['title_var'] = 'side_menu';
+	$this->_pages['mods/_standard/course_tools/side_menu.php']['parent']    = 'mods/_standard/course_tools/modules.php';
+	$this->_pages['mods/_standard/course_tools/side_menu.php']['guide']     = 'instructor/?p=side_menu.php';
 
 ?>
