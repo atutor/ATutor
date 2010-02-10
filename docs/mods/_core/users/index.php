20817c40028576a193c3cd2ce5e023edbcba8200
greg gay <ggay@ocad.ca>
2010-02-10 19:07:28 +0000

    add redirect from index.php to users.php

---
+++ docs/mods/_core/users/index.php
@@ -0,0 +1,19 @@
+<?php
+/****************************************************************/
+/* ATutor							*/
+/****************************************************************/
+/* Copyright (c) 2002-2008				        */
+/* Adaptive Technology Resource Centre / University of Toronto  */
+/* http://atutor.ca					*/
+/*                                                              */
+/* This program is free software. You can redistribute it and/or*/
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation.		*/
+/****************************************************************/
+// $Id
+$_user_location = 'admin';
+
+header('Location: users.php');
+exit;
+
+?>
