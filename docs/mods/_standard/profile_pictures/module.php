4a267b291a526bda71f2edd8c670a9d1762d41e3
greg gay <ggay@ocad.ca>
2010-02-11 18:59:49 +0000

    moved profile picture to mods/_standard/profile_picture

--- docs/mods/_standard/profile_pictures/module.php
+++ docs/mods/_standard/profile_pictures/module.php
@@ -2,13 +2,13 @@
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__FILE__ . ' is not a Module'); }
 
-$this->_pages['users/profile_picture.php']['title_var'] = 'picture';
-$this->_pages['users/profile_picture.php']['parent']   = 'users/profile.php';
+$this->_pages['mods/_standard/profile_picture/profile_picture.php']['title_var'] = 'picture';
+$this->_pages['mods/_standard/profile_picture/profile_picture.php']['parent']   = 'mods/_standard/profile_picture/profile.php';
 
-$this->_pages['users/profile.php']['children']  = array('users/profile_picture.php');
+$this->_pages['mods/_standard/profile_picture/profile.php']['children']  = array('mods/_standard/profile_picture/profile_picture.php');
 
 if (admin_authenticate(AT_ADMIN_PRIV_USERS, TRUE)) {
-	$this->_pages['admin/profile_picture.php']['title_var'] = 'picture';
-	$this->_pages['admin/profile_picture.php']['parent']   = 'admin/users.php';
+	$this->_pages['mods/_standard/profile_picture/admin/profile_picture.php']['title_var'] = 'picture';
+	$this->_pages['mods/_standard/profile_picture/admin/profile_picture.php']['parent']   = 'admin/users.php';
 }
 ?>
