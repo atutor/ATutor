7c3b949753211cb67e2dd1b5659e91e68538d095
greg gay <ggay@ocad.ca>
2010-02-16 17:33:04 +0000

    moved groups to mod/_core/groups

---
+++ docs/mods/_core/groups/edit_group.php
@@ -0,0 +1,141 @@
+<?php
+/************************************************************************/
+/* ATutor																*/
+/************************************************************************/
+/* Copyright (c) 2002-2008 by Greg Gay, Joel Kronenberg & Heidi Hazelton*/
+/* Adaptive Technology Resource Centre / University of Toronto			*/
+/* http://atutor.ca														*/
+/*																		*/
+/* This program is free software. You can redistribute it and/or		*/
+/* modify it under the terms of the GNU General Public License			*/
+/* as published by the Free Software Foundation.						*/
+/************************************************************************/
+// $Id: edit_group.php 7482 2008-05-06 17:44:49Z greg $
+
+define('AT_INCLUDE_PATH', '../../../include/');
+require(AT_INCLUDE_PATH.'vitals.inc.php');
+authenticate(AT_PRIV_GROUPS);
+
+if (isset($_POST['cancel'])) {
+	$msg->addFeedback('CANCELLED');
+	header('Location: index.php');
+	exit;
+} else if (isset($_POST['submit'])) {
+	$modules = '';
+	if (isset($_POST['modules'])) {
+		$modules = implode('|', $_POST['modules']);
+	}
+
+	$_POST['title']   = trim($_POST['title']);
+
+	if (!$_POST['title']) {
+		$msg->addError(array('EMPTY_FIELDS', _AT('title')));
+	}
+
+	if (!$msg->containsErrors()) {
+		$_POST['title']       = $addslashes($_POST['title']);
+		$_POST['description'] = $addslashes($_POST['description']);
+
+		$id = intval($_POST['id']);
+		$type_id = intval($_POST['type_id']);
+
+		$sql = "SELECT type_id FROM ".TABLE_PREFIX."groups_types WHERE type_id=$type_id AND course_id=$_SESSION[course_id]";
+		$result = mysql_query($sql, $db);
+		if ($row = mysql_fetch_assoc($result)) {
+			$sql = "UPDATE ".TABLE_PREFIX."groups SET title='$_POST[title]', description='$_POST[description]', modules='$modules' WHERE group_id=$id AND type_id=$type_id";
+			$result = mysql_query($sql, $db);
+
+			// delete the modules that were un-checked
+			$old_modules = explode('|', $_POST['old_modules']);
+			$modules = explode('|', $modules);
+
+			foreach ($old_modules as $mod) {
+				if (!in_array($mod, $modules)) {
+					$module =& $moduleFactory->getModule($mod);
+					$module->deleteGroup($id);
+				}
+			}
+			foreach ($modules as $mod) {
+				if (!in_array($mod, $old_modules)) {
+					$module =& $moduleFactory->getModule($mod);
+					$module->createGroup($id);
+				}
+			}
+		}
+
+		$msg->addFeedback('GROUP_EDITED_SUCCESSFULLY');
+
+		header('Location: index.php');
+		exit;
+	}
+	$_GET['id'] = abs($_POST['id']);
+}
+
+require(AT_INCLUDE_PATH.'header.inc.php');
+
+	$_GET['id'] = intval($_GET['id']);
+
+	$sql = "SELECT * FROM ".TABLE_PREFIX."groups WHERE group_id=$_GET[id]";
+	$result = mysql_query($sql,$db);
+	if (!($row = mysql_fetch_assoc($result))) {
+		$msg->printErrors('GROUP_NOT_FOUND');
+		require (AT_INCLUDE_PATH.'footer.inc.php');
+		exit;
+	}
+	$sql = "SELECT title FROM ".TABLE_PREFIX."groups_types WHERE type_id=$row[type_id] AND course_id=$_SESSION[course_id]";
+	$result = mysql_query($sql,$db);
+	if (!($type_row = mysql_fetch_assoc($result))) {
+		$msg->printErrors('GROUP_NOT_FOUND');
+		require (AT_INCLUDE_PATH.'footer.inc.php');
+		exit;
+	}
+$old_modules = $row['modules'];
+$row['modules'] = explode('|', $row['modules']);
+?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="form">
+<input type="hidden" name="id" value="<?php echo $row['group_id']; ?>" />
+<input type="hidden" name="type_id" value="<?php echo $row['type_id']; ?>" />
+<input type="hidden" name="old_modules" value="<?php echo $old_modules; ?>" />
+<div class="input-form">
+	<fieldset class="group_form"><legend class="group_form"><?php echo _AT('edit'); ?></legend>
+	<div class="row">
+		<h3><?php echo $type_row['title']; ?></h3>
+	</div>
+
+	<div class="row">
+		<div class="required" title="<?php echo _AT('required_field'); ?>">*</div><label for="title"><?php echo _AT('title'); ?></label><br />
+		<input type="text" name="title" id="title" value="<?php echo htmlspecialchars($row['title']); ?>" size="20" maxlength="40" />
+	</div>
+
+	<div class="row">
+		<label for="description"><?php echo _AT('description'); ?>:</label><br />
+		<textarea name="description" id="description" cols="10" rows="2"><?php echo htmlspecialchars($row['description']); ?></textarea>
+	</div>
+
+	<div class="row">
+		<?php echo _AT('tools'); ?><br />
+			<?php
+			$modules = $moduleFactory->getModules(AT_MODULE_STATUS_ENABLED, 0, TRUE);
+			$keys = array_keys($modules);
+			$i=0;
+			?>
+			<?php foreach($keys as $module_name): ?>
+				<?php $module =& $modules[$module_name]; ?>
+				<?php if ($module->getGroupTool() && (in_array($module->getGroupTool(),$_pages[AT_NAV_HOME]) || in_array($module->getGroupTool(),$_pages[AT_NAV_COURSE])) ): ?>
+					<input type="checkbox" value="<?php echo $module_name; ?>" name="modules[]" id="m<?php echo ++$i; ?>" <?php 
+						if (in_array($module_name, $row['modules'])) { echo 'checked="checked"'; } 
+					?> /><label for="m<?php echo $i; ?>"><?php echo $module->getName(); ?></label><br />
+				<?php endif; ?>
+			<?php endforeach; ?>
+	</div>
+
+	<div class="row buttons">
+		<input type="submit" name="submit" value="<?php echo _AT('save'); ?>" accesskey="s" />
+		<input type="submit" name="cancel" value="<?php echo _AT('cancel'); ?>" />
+	</div>
+	</fieldset>
+</div>
+</form>
+
+<?php require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
---
+++ docs/mods/_core/groups/index.php
@@ -0,0 +1,113 @@
+<?php
+/************************************************************************/
+/* ATutor																*/
+/************************************************************************/
+/* Copyright (c) 2002-2008 by Greg Gay, Joel Kronenberg & Heidi Hazelton*/
+/* Adaptive Technology Resource Centre / University of Toronto			*/
+/* http://atutor.ca														*/
+/*																		*/
+/* This program is free software. You can redistribute it and/or		*/
+/* modify it under the terms of the GNU General Public License			*/
+/* as published by the Free Software Foundation.						*/
+/************************************************************************/
+// $Id: index.php 8901 2009-11-11 19:10:19Z cindy $
+
+define('AT_INCLUDE_PATH', '../../../include/');
+require(AT_INCLUDE_PATH.'vitals.inc.php');
+authenticate(AT_PRIV_GROUPS);
+
+if (isset($_GET['edit'], $_GET['id'])) {
+	$parts = explode('_', $_GET['id'], 2);
+	if (isset($parts[1]) && $parts[1]) {
+		header('Location: edit_group.php?id='.$parts[1]);
+		exit;
+	} else if ($parts[0]) {
+		header('Location: edit_type.php?id='.$parts[0]);
+		exit;
+	}
+} else if (isset($_GET['delete'], $_GET['id'])) {
+	$parts = explode('_', $_GET['id'], 2);
+	if (isset($parts[1]) && $parts[1]) {
+		header('Location: delete_group.php?id='.$parts[1]);
+		exit;
+	} else if ($parts[0]) {
+		header('Location: delete_type.php?id='.$parts[0]);
+		exit;
+	}
+} else if (isset($_GET['members'])) {
+	$parts = explode('_', $_GET['id'], 2);
+	if (isset($parts[1]) && $parts[1]) {
+		header('Location: members.php?id='.$parts[0].SEP.'gid='.$parts[1]);
+		exit;
+	} else if ($parts[0]) {
+		header('Location: members.php?id='.$parts[0]);
+		exit;
+	} else {
+		$msg->addError('NO_ITEM_SELECTED');
+	}
+} else if (isset($_GET['members']) || isset($_GET['delete']) || isset($_GET['edit'])) {
+	$msg->addError('NO_ITEM_SELECTED');
+}
+
+require(AT_INCLUDE_PATH.'header.inc.php');
+
+$sql = "SELECT type_id, title FROM ".TABLE_PREFIX."groups_types WHERE course_id=$_SESSION[course_id] ORDER BY title";
+$result = mysql_query($sql, $db);
+?>
+<div class="input-form">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" name="form">
+<fieldset class="group_form" margin:auto;"><legend class="group_form"><?php echo _AT('groups'); ?></legend>
+<table class="data" summary="" rules="cols" style="width: 80%">
+<tfoot>
+<tr>
+	<td>
+		<input type="submit" name="edit"    value="<?php echo _AT('edit'); ?>" />
+		<input type="submit" name="members" value="<?php echo _AT('members'); ?>" />
+		<input type="submit" name="delete"  value="<?php echo _AT('delete'); ?>" />
+	</td>
+</tr>
+</tfoot>
+<tbody>
+<?php if ($row = mysql_fetch_assoc($result)): ?>
+	<?php do { ?>
+
+		<?php 
+			$sql = "SELECT group_id, title FROM ".TABLE_PREFIX."groups WHERE type_id=$row[type_id] ORDER BY title";
+			$group_result = mysql_query($sql, $db);
+			$num_groups = mysql_num_rows($group_result);
+		?>
+		<tr onmousedown="document.form['g<?php echo $row['type_id']; ?>'].checked = true; rowselect(this);" id="r_<?php echo $row['type_id']; ?>">
+			<th>
+				<input type="radio" id="g<?php echo $row['type_id']; ?>" name="id" value="<?php echo $row['type_id']; ?>" />
+				<label for="g<?php echo $row['type_id']; ?>"><?php echo $row['title']; ?></label> (<?php echo $num_groups.' '._AT('groups'); ?>)</td>
+			</th>
+		</tr>
+		<?php if ($num_groups) : ?>
+			<?php while ($group_row = mysql_fetch_assoc($group_result)): ?>
+				<?php
+					$sql = "SELECT COUNT(*) AS cnt FROM ".TABLE_PREFIX."groups_members WHERE group_id=$group_row[group_id]";
+					$group_cnt_result = mysql_query($sql, $db);
+					$group_cnt = mysql_fetch_assoc($group_cnt_result);
+				?>
+				<tr onmousedown="document.form['g<?php echo $row['type_id'].'_'.$group_row['group_id']; ?>'].checked = true; rowselect(this);" id="r_<?php echo $row['type_id'].'_'.$group_row['group_id']; ?>">
+					<td class="indent"><input type="radio" id="g<?php echo $row['type_id'].'_'.$group_row['group_id']; ?>" name="id" value="<?php echo $row['type_id'].'_'.$group_row['group_id']; ?>" /> <label for="g<?php echo $row['type_id'].'_'.$group_row['group_id']; ?>"><?php echo $group_row['title']; ?></label> (<?php echo $group_cnt['cnt'].' '._AT('members'); ?>)</td>
+				</tr>
+			<?php endwhile; ?>
+		<?php else: ?>
+			<tr>
+				<td class="indent"><em><?php echo _AT('none_found'); ?></em></td>
+			</tr>
+		<?php endif; ?>
+
+	<?php } while ($row = mysql_fetch_assoc($result)); ?>
+<?php else: ?>
+	<tr>
+		<td><em><?php echo _AT('none_found'); ?></em></td>
+	</tr>
+<?php endif; ?>
+</tbody>
+</table>
+</fieldset>
+</form><br />
+</div>
+<?php require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
