93ec31dca497e2fff73714bba497050f748a5284
harris wong <hwong@ocad.ca>
2010-03-18 15:44:17 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=4161

--- docs/documentation/general/pa_albums.php
+++ docs/documentation/general/pa_albums.php
@@ -4,11 +4,11 @@
 
 <p>Album is a collection of photos, clicking on each of the photo thumbnail will bring you to the individual photo page.  </p>
 
-
 <h3>Uploading a photo</h3>
 <p>To upload a photo, click on the "Open Upload Manager".  A new section will appear and now click "Browse" to upload any photos in the format of gif/jpg/png.  After selecting a photo from the File Upload, ATutor will automatically resize the image and append its details at the top of the pending list.  Details include file name, file size, a thumbnail of the image, and a delete button.  The delete button allows the user to remove the pending photos anytime during the upload process.  At the right bottom of this section, you will see a "Memory Usage: x.xx/ 8 MB" message, which tells you how much memory you have used.  Once the memory limit is reached, an error message will display in the pending list.  Finally, click "Upload" to finish the upload process.  The photos should now be listed in the album.</p>
 
-
+<h3>Search</h3>
+<p>The search tab are shown on the top left on each Album page(Except Profile Album).  Users can type in any text to search for the relevant albums and photos.  The search will return matches found in the photo album's description, location; and photo's description, alternative text. </p>
 
 <h3>Edit Photos</h3>
 <p>Each photo allows the user to have a description and an alternative text.  Description(a.k.a. caption on some other site) will be shown at the bottom of the photo, describing what this photo is.  Alternative text is for the replacement for the image, whenever the image can not be load/seen.  This should summerize the essence of the image rather than describing every detail.  This should be used to comply the accessibility rule.  Beneath each of the photo thumbnail, user have the the option to select that photo as the album cover, or to remove it by checking the "delete" button.  When the user is done editing, click "Save" at the very end of the page.</p>
