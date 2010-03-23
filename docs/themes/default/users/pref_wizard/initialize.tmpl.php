49b043c6a03f49c291b38f50cf8f671cfeff9cc1
laurel <laurel.williams@utoronto.ca>
2010-03-23 14:09:38 +0000

    4150: check for existance of pref_wiz array before using it in in_array function.

--- docs/themes/default/users/pref_wizard/initialize.tmpl.php
+++ docs/themes/default/users/pref_wizard/initialize.tmpl.php
@@ -1,35 +1,37 @@
+<?php $prefs_set = isset($this->pref_wiz);?>
+
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo DISPLAY ?>" id="display" <?php if (in_array(DISPLAY, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo DISPLAY ?>" id="display" <?php if ($prefs_set && in_array(DISPLAY, $this->pref_wiz)) echo checked ?> />
         <label for="display">I would like to make the text on the screen easier to see.</label>
     </div>
 
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo NAVIGATION ?>" id="navigation" <?php if (in_array(NAVIGATION, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo NAVIGATION ?>" id="navigation" <?php if ($prefs_set && in_array(NAVIGATION, $this->pref_wiz)) echo checked ?> />
         <label for="navigation">I would like to enhance the navigation of the content.</label>
     </div>
 
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_TEXT ?>" id="altToText" <?php if (in_array(ALT_TO_TEXT, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_TEXT ?>" id="altToText" <?php if ($prefs_set && in_array(ALT_TO_TEXT, $this->pref_wiz)) echo checked ?> />
         <label for="altToText">I would like alternatives to textual content.</label>
     </div>
 
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_AUDIO ?>" id="altToAudio" <?php if (in_array(ALT_TO_AUDIO, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_AUDIO ?>" id="altToAudio" <?php if ($prefs_set && in_array(ALT_TO_AUDIO, $this->pref_wiz)) echo checked ?> />
         <label for="altToAudio">I would like alternatives to audio content.</label>
     </div>
     
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_VISUAL ?>" id="altToVisual" <?php if (in_array(ALT_TO_VISUAL, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo ALT_TO_VISUAL ?>" id="altToVisual" <?php if ($prefs_set && in_array(ALT_TO_VISUAL, $this->pref_wiz)) echo checked ?> />
         <label for="altToVisual">I would like alternatives to visual content.</label>
     </div>
     
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo SUPPORT ?>" id="support" <?php if (in_array(SUPPORT, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo SUPPORT ?>" id="support" <?php if ($prefs_set && in_array(SUPPORT, $this->pref_wiz)) echo checked ?> />
         <label for="support">I would like access to learner support tools.</label>
     </div>
     
     <div>
-        <input type="checkbox" name="pref_wiz[]" value="<?php echo ATUTOR ?>" id="atutor_pref" <?php if (in_array(ATUTOR, $_POST['pref_wiz'])) echo checked ?> />
+        <input type="checkbox" name="pref_wiz[]" value="<?php echo ATUTOR ?>" id="atutor_pref" <?php if ($prefs_set && in_array(ATUTOR, $_POST['pref_wiz'])) echo checked ?> />
         <label for="atutor_pref">I would like to change or review my ATutor preferences.</label>
     </div>
     
