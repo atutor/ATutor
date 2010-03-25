2e1eb18ab3dc0a5fd4aec268000498720ae99c67
greg gay <ggay@ocad.ca>
2010-03-25 17:44:53 +0000

    update file storage module news with new icon and updated path through bounce.php

--- docs/mods/_standard/file_storage/module_news.php
+++ docs/mods/_standard/file_storage/module_news.php
@@ -16,14 +16,14 @@
  * @return list of news, [timestamp]=>
  */
 function file_storage_news() {
-	global $db, $enrolled_courses;
+	global $db, $enrolled_courses, $system_courses;
 	$news = array();
 
 	if ($enrolled_courses == ''){
 		return $news;
 	} 
 
-	$sql = "SELECT date, file_id, file_name, description FROM ".TABLE_PREFIX."files WHERE owner_id IN $enrolled_courses ORDER BY date DESC";
+	$sql = "SELECT date, file_id, file_name, owner_id, description FROM ".TABLE_PREFIX."files WHERE owner_id IN $enrolled_courses ORDER BY date DESC";
 	$result = mysql_query($sql, $db);
 	if($result){
 		while($row = mysql_fetch_assoc($result)){
@@ -32,7 +32,12 @@ function file_storage_news() {
 			} else {
 				$filetext = $row['file_name'];
 			}
-			$news[] = array('time'=>$row['date'], 'object'=>$row, 'thumb'=>'images/file_types/images.gif', 'link'=>'<a href="'.url_rewrite('mods/_standard/file_storage/index.php?download=1'.htmlentities(SEP).'files[]='. $row['file_id'], AT_PRETTY_URL_IS_HEADER).'"'.
+			$news[] = array('time'=>$row['date'], 
+			      'object'=>$row, 
+			      'course'=>$system_courses[$row['owner_id']]['title'],
+			      'alt'=>_AT('download'),
+			      'thumb'=>'images/application_get.png', 
+			      'link'=>'<a href="bounce.php?course='.$row['owner_id'].'&p='.urlencode('mods/_standard/file_storage/index.php?download=1'.SEP.'files[]='. $row['file_id']).'"'.
 		          (strlen($filetext) > SUBLINK_TEXT_LEN ? ' title="'.$filetext.'"' : '') .'>'. 
 		          validate_length($filetext, SUBLINK_TEXT_LEN, VALIDATE_LENGTH_FOR_DISPLAY) .'</a>');
 		}
