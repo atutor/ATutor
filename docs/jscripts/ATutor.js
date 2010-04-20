d5208a521543afc84a691b2741c84601fcd1d437
laurel <laurel.williams@utoronto.ca>
2010-04-20 20:15:33 +0000

    4181: increased size of pref_wiz window to accommodate reset button.

--- docs/jscripts/ATutor.js
+++ docs/jscripts/ATutor.js
@@ -430,7 +430,7 @@ ATutor.users.preferences = ATutor.users.preferences || {};
     		if (query_string !== "") {
     			query_string = "?" + query_string;
     		}
-    		window.open(base_path + 'users/pref_wizard/index.php' + query_string,'newWin1','menubar=0,scrollbars=1,resizable=1,width=640,height=520');
+    		window.open(base_path + 'users/pref_wizard/index.php' + query_string,'newWin1','menubar=0,scrollbars=1,resizable=1,width=640,height=580');
     		return false;
     	});
     };
