18ac277d7cf258e2a7f00d5e669dfd872c02e03e
greg gay <ggay@ocad.ca>
2010-05-01 18:08:32 +0000

    combined folder and page shortcuts arrays and moved them into headerinc

--- docs/themes/default/editor/edit_content_folder.tmpl.php
+++ docs/themes/default/editor/edit_content_folder.tmpl.php
@@ -17,17 +17,17 @@ $onload = 'document.form.title.focus();';
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; if ($this->cid > 0) echo '?cid='.$this->cid; else if ($this->pid > 0) echo '?pid='.$this->pid;?>" method="post" name="form"> 
 <div class="input-form" style="width:95%;margin-left:1.5em;">
-<?php
+<!-- <?php
 if ($this->shortcuts): 
 ?>
-<fieldset id="shortcuts" style="margin-top:1em;float:right;clear:right;"><legend><?php echo _AT('shortcuts'); ?></legend>
+ <fieldset id="shortcuts" style="margin-top:1em;float:right;clear:right;"><legend><?php echo _AT('shortcuts'); ?></legend>
 	<ul>
 		<?php foreach ($this->shortcuts as $link): ?>
 			<li><a href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a></li>
 		<?php endforeach; ?>
 	</ul>
 </fieldset>
-<?php endif; ?>
+<?php endif; ?> -->
 	<div class="row">
 		<div style="font-weight:bold;"><span class="required" title="<?php echo _AT('required_field'); ?>">*</span><label for="ftitle"><?php echo _AT('content_folder_title');  ?></label></div>
 		<input type="text" name="title" id="ftitle" size="70" class="formfield" value="<?php echo ContentManager::cleanOutput($this->ftitle); ?>" />
