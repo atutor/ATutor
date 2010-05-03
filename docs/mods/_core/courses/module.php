77d563f8c407fbc33df24520437d4a5dd279c9e5
greg gay <ggay@ocad.ca>
2010-05-03 19:20:53 +0000

    moved auto enroll out of sysprefs to courses

--- docs/mods/_core/courses/module.php
+++ docs/mods/_core/courses/module.php
@@ -40,7 +40,7 @@ if (admin_authenticate(AT_ADMIN_PRIV_COURSES, TRUE) || admin_authenticate(AT_ADM
             $this->_pages['mods/_core/courses/admin/auto_enroll.php']['parent']    = 'mods/_core/courses/admin/courses.php';
             $this->_pages['mods/_core/courses/admin/auto_enroll.php']['guide']     = 'admin/?p=auto_enroll.php';
             $this->_pages['mods/_core/courses/admin/auto_enroll.php']['children']  = array_merge(array('mods/_core/courses/admin/auto_enroll_edit.php'));
-            $this->_pages['admin/config_edit.php']['children']  = array_merge((array) $this->_pages['admin/config_edit.php']['children'], array('mods/_core/courses/admin/auto_enroll.php'));
+            $this->_pages['admin/config_edit.php']['children']  = array_merge((array) $this->_pages['admin/config_edit.php']['children']);
 
 
 
