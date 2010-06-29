64f598f7dd376926343580c71e5dd6d398b8297d
cindy li <cli@ocad.ca>
2010-06-29 18:24:15 +0000

    point the module index pages to the new locations

--- docs/install/db/atutor_upgrade_1.6.4_to_2.0.sql
+++ docs/install/db/atutor_upgrade_1.6.4_to_2.0.sql
@@ -36,10 +36,6 @@ CREATE TABLE `oauth_client_tokens` (
 
 # END Adding feature of oauth client
 
-# point tile.php to new location
-UPDATE `courses` SET main_links=replace(main_links, 'tile.php', 'mods/_standard/tile_search/tile.php'), home_links=replace(home_links, 'tile.php', 'mods/_standard/tile_search/tile.php');
-
-
 # -------------- Photo Album Module Setup ----------------
 INSERT INTO `modules` VALUES ('_standard/photos',	 2, 16777216, 0, 0, 0);
 # Photo Album Table
@@ -125,6 +121,21 @@ INSERT INTO `themes` VALUES ('ATutor 1.6', '2.0', 'default16', NOW(), 'This is t
 # Add new field themes.type to seperate "Desktop" and "Mobile" themes
 ALTER TABLE `themes` ADD `type` varchar(20) NOT NULL default 'Desktop' AFTER `dir_name`;
 
-# point the index page of modules "glossary", "file_storage" to the new location
-UPDATE `courses` SET main_links=replace(main_links, '|glossary/index.php', '|mods/_core/glossary/index.php');
-UPDATE `courses` SET main_links=replace(main_links, '|file_storage/index.php', '|mods/_standard/file_storage/index.php');
+# point the module index pages to the new locations
+UPDATE `courses` SET main_links=replace(main_links, 'glossary/index.php', 'mods/_core/glossary/index.php'), home_links=replace(home_links, 'glossary/index.php', 'mods/_core/glossary/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'file_storage/index.php', 'mods/_standard/file_storage/index.php'), home_links=replace(home_links, 'file_storage/index.php', 'mods/_standard/file_storage/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'tile.php', 'mods/_standard/tile_search/tile.php'), home_links=replace(home_links, 'tile.php', 'mods/_standard/tile_search/tile.php');
+UPDATE `courses` SET main_links=replace(main_links, 'forum/list.php', 'mods/_standard/forums/forum/list.php'), home_links=replace(home_links, 'forum/list.php', 'mods/_standard/forums/forum/list.php');
+UPDATE `courses` SET main_links=replace(main_links, 'chat/index.php', 'mods/_standard/chat/index.php'), home_links=replace(home_links, 'chat/index.php', 'mods/_standard/chat/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'faq/index.php', 'mods/_standard/faq/index.php'), home_links=replace(home_links, 'faq/index.php', 'mods/_standard/faq/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'links/index.php', 'mods/_standard/links/index.php'), home_links=replace(home_links, 'links/index.php', 'mods/_standard/links/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'tools/my_tests.php', 'mods/_standard/tests/my_tests.php'), home_links=replace(home_links, 'tools/my_tests.php', 'mods/_standard/tests/my_tests.php');
+UPDATE `courses` SET main_links=replace(main_links, 'sitemap.php', 'mods/_standard/sitemap/sitemap.php'), home_links=replace(home_links, 'sitemap.php', 'mods/_standard/sitemap/sitemap.php');
+UPDATE `courses` SET main_links=replace(main_links, 'export.php', 'mods/_core/imscp/export.php'), home_links=replace(home_links, 'export.php', 'mods/_core/imscp/export.php');
+UPDATE `courses` SET main_links=replace(main_links, 'my_stats.php', 'mods/_standard/tracker/my_stats.php'), home_links=replace(home_links, 'my_stats.php', 'mods/_standard/tracker/my_stats.php');
+UPDATE `courses` SET main_links=replace(main_links, 'polls/index.php', 'mods/_standard/polls/index.php'), home_links=replace(home_links, 'polls/index.php', 'mods/_standard/polls/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'directory.php', 'mods/_standard/directory/directory.php'), home_links=replace(home_links, 'directory.php', 'mods/_standard/directory/directory.php');
+UPDATE `courses` SET main_links=replace(main_links, 'groups.php', 'mods/_core/groups/groups.php'), home_links=replace(home_links, 'groups.php', 'mods/_core/groups/groups.php');
+UPDATE `courses` SET main_links=replace(main_links, 'reading_list/index.php', 'mods/_standard/reading_list/index.php'), home_links=replace(home_links, 'reading_list/index.php', 'mods/_standard/reading_list/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'blogs/index.php', 'mods/_standard/blogs/index.php'), home_links=replace(home_links, 'blogs/index.php', 'mods/_standard/blogs/index.php');
+UPDATE `courses` SET main_links=replace(main_links, 'google_search/index.php', 'mods/_standard/google_search/index.php'), home_links=replace(home_links, 'google_search/index.php', 'mods/_standard/google_search/index.php');
