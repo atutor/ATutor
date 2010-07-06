b27868db7cb14931629ad69d2ed27c24fe0413a2
greg gay <ggay@ocad.ca>
2010-07-06 16:51:14 +0000

    update pa handbook text

--- docs/documentation/general/pa_photo.php
+++ docs/documentation/general/pa_photo.php
@@ -2,7 +2,7 @@
 
 <h2>Photo</h2>
 
-<p>This page display a photo and its description.  User can click "Prev" to go to a previous photo within the album, or similarly, "Next" for the next photo.  Owner of the photo can also edit the description by simply clicking on the description itself, or via the "Edit Photo" link.  </p>
+<p>This page display a photo and its description.  You can click the "Previous" and "Next" arrows to move through the photos in the album you are viewing.  Owners of a photo can also edit the description by simply clicking on the description itself, or via the "Edit Photo" link.  </p>
 
 <p>Also view <strong>Comments</strong> for details on posting comments. </p>
 
