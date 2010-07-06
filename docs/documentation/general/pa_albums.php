b32e0c05e52bb1943564e9a398d446bd6fa069ee
greg gay <ggay@ocad.ca>
2010-07-06 16:48:30 +0000

    update pa handbook text

--- docs/documentation/general/pa_albums.php
+++ docs/documentation/general/pa_albums.php
@@ -2,19 +2,19 @@
 
 <h2>Albums</h2>
 
-<p>Album is a collection of photos, clicking on each of the photo thumbnail will bring you to the individual photo page.  </p>
+<p>An Album is a collection of photos. Clicking on an album thumbnail image will display the photos in that album.  </p>
 
 <h3>Uploading a photo</h3>
-<p>To upload a photo, click on the "Open Upload Manager".  A new section will appear and now click "Browse" to upload any photos in the format of gif/jpg/png.  After selecting a photo from the File Upload, ATutor will automatically resize the image and append its details at the top of the pending list.  Details include file name, file size, a thumbnail of the image, and a delete button.  The delete button allows the user to remove the pending photos anytime during the upload process.  At the right bottom of this section, you will see a "Memory Usage: x.xx/ 8 MB" message, which tells you how much memory you have used.  Once the memory limit is reached, an error message will display in the pending list.  Finally, click "Upload" to finish the upload process.  The photos should now be listed in the album.</p>
+<p>To upload a photo, click on the "Open Upload Manager" button.  A new section will appear below where you click "Browse" to upload any photos in the format of gif/jpg/png from your local hard drive.  After selecting a photo from the File Upload, ATutor will automatically resize the image and append its details at the top of the pending list that appears after selecting an image.  Details include file name, file size, a thumbnail of the image, and a delete button.  The delete button allows you to remove the pending photos anytime during the upload process.  At the right bottom of this section, you will see a "Memory Usage: x.xx/ 8 MB" message, which tells you how much memory you have used.  Once the memory limit is reached, an error message will display in the pending list.  Finally, click "Upload" to finish the upload process.  The photos should now be listed in the album.</p>
 
 <h3>Search</h3>
-<p>The search tab are shown on the top left on each Album page(Except Profile Album).  Users can type in any text to search for the relevant albums and photos.  The search will return matches found in the photo album's description, location; and photo's description, alternative text. </p>
+<p>The search tab is displayed at the top left of the Photo Gallery home page.  Type in any text to search for relevant albums and photos.  The search will return matches found in the photo album's description or location; and photo's description or alternative text. Be sure when adding photos to your albums, to fill in this information so people can find you photos. </p>
 
 <h3>Edit Photos</h3>
-<p>Each photo allows the user to have a description and an alternative text.  Description(a.k.a. caption on some other site) will be shown at the bottom of the photo, describing what this photo is.  Alternative text is for the replacement for the image, whenever the image can not be load/seen.  This should summerize the essence of the image rather than describing every detail.  This should be used to comply the accessibility rule.  Beneath each of the photo thumbnail, user have the the option to select that photo as the album cover, or to remove it by checking the "delete" button.  When the user is done editing, click "Save" at the very end of the page.</p>
+<p>Each photo can have a description and alternative text.  Description (a.k.a. caption on some other sites) will be shown at the bottom of the photo, describing what this photo is.  Alternative text acts as a replacement for the image, whenever the image does not load, or can not be seen for any reason.  This information should summerize the essence of the image rather than describing ever detail.  Alternative text should be used to comply the accessibility rules.  Below each of the photo thumbnails, their is an option to select that photo as the album cover, or to remove it by checking the "delete" button.  When you are done editing, click "Save".</p>
 
 <h3>Organize Photos</h3>
-<p>Photos can be rearranged freely within each album by simply dragging the photo with your mouse, or to use [Ctrl] + [Up/Down/Left/Right] arrow keys on your keyboard.  The rearranged order is saved automatically.  </p>
+<p>Photos can be rearranged within each album by simply dragging the photo with your mouse, or using [Ctrl] + [Up/Down/Left/Right] arrow keys on your keyboard.  The rearranged order is saved automatically. Click on "Organize Photos" to rearrange them. </p>
 
 <p>Also view <strong>Comments</strong> for details on posting comments. </p>
 
