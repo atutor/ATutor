15f42a7f6b18d9e4f1318d0605f82682c5360904
greg gay <ggay@ocad.ca>
2010-07-06 16:36:07 +0000

    update pa handbook text

--- docs/documentation/general/pa_index.php
+++ docs/documentation/general/pa_index.php
@@ -2,25 +2,25 @@
 
 <h2>Photo Gallery</h2>
 
-<p>A photo gallery designed with accessibility in mind, allowing members to share course related photos and personal photos.</p>
+<p>A photo gallery is designed with accessibility in mind, allowing members to share course related photos and personal photos.</p>
 
 <h3>Basic Photo Album Features</h3>
 
 <dl>
 <dt>Photo Gallery</dt>
-<dd>This is the place that keep all your personal albums.  Both Private Album and Shared Album will be listed here.</dd>
+<dd>This is where your personal albums can be found.  Both Private Albums and Shared Albums will be listed here.</dd>
 
 <dt>Profile Album</dt>
-<dd>This is an album where you keep your profile pictures.  In the future, this will be linked to your profile page. </dd>
+<dd>This is an album where you keep your profile pictures. Photos collected here can be linked into your ATutpr profile page, or into your ATutor Social profile. </dd>
 
 <dt>Course Albums</dt>
-<dd>This is the place that keep all the course albums.  Anyone enrolled in the course is allowed to upload photos; students are allowed to edit/delete the photos they have uploaded.  Instructors have the privilege to edit/delete any photos within his/her course albums. </dd>
+<dd>This is where you keep all the course albums.  Anyone enrolled in the course is allowed to upload photos to a course album; students are allowed to edit and delete the photos they have uploaded.  Instructors have the privilege to edit and delete any photos within their course albums. </dd>
 
 <dt>Shared Albums</dt>
-<dd>This will show all albums with the "Shared" permission in the ATutor installation.  All users have the permission to browse and comment, but are not allowed to upload or edit photos.  </dd>
+<dd>Here all albums will be listed that have the "Shared" permission set.  All users have permission to browse and comment on photos in shared albums, but are not allowed to upload or edit photos.  </dd>
 
 <dt>Create Album</dt>
-<dd>To create an album.  You will have to specify the album name and its permission; location and description are optional.  If you are an instructor, you will have an extra option of "Album Type", which allows you to create "My Album" or "Course Album".  Under "Album Permission", "Private" means this album is not accessible by any other users except yourself, "Shared" means this album will be displayed under the "Shared Albums" tab, and will be accessible by ALL users.</dd>
+<dd>To create an album, specify the album name and its permissions. Location (geographically), and description are optional.  If you are an instructor, you will have an extra option of "Album Type", which allows you to create "My Album" or "Course Album".  Under "Album Permission", "Private" means this album is not accessible by any other users except yourself, "Shared" means this album will be displayed under the "Shared Albums" tab, and will be accessible by ALL users.</dd>
 </dl>
 
 
