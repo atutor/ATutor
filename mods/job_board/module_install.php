eefd6ca6968c44d47c1d714c1a79b541ea080c0a
harris wong <hwong@ocad.ca>
2010-07-13 14:19:08 +0000

    - taken out the module directory code

--- mods/job_board/module_install.php
+++ mods/job_board/module_install.php
@@ -50,13 +50,15 @@ $_cron_interval    = 0;
  * the following code is used for creating a module-specific directory.
  * it generates appropriate error messages to aid in its creation.
  */
-//$directory = AT_CONTENT_DIR .'job_board';
+/*
+$directory = AT_CONTENT_DIR .'job_board';
 // check if the directory is writeable
 if (!is_dir($directory) && !@mkdir($directory)) {
 	$msg->addError(array('MODULE_INSTALL', '<li>'.$directory.' does not exist. Please create it.</li>'));
 } else if (!is_writable($directory) && @chmod($directory, 0666)) {
 	$msg->addError(array('MODULE_INSTALL', '<li>'.$directory.' is not writeable. On Unix issue the command <kbd>chmod a+rw</kbd>.</li>'));
 }
+*/
 
 /******
  * the following code checks if there are any errors (generated previously)
