23a7f917e26703ef29c428991c04db61f0cf83fa
greg gay <ggay@ocad.ca>
2010-07-22 16:13:49 +0000

    language adjustments

--- mods/assignment_dropbox/module.sql
+++ mods/assignment_dropbox/module.sql
@@ -5,4 +5,4 @@ INSERT INTO `language_text` VALUES ('en', '_module','assignment_dropbox_text','S
 INSERT INTO `language_text` VALUES ('en', '_module','flag_text','<small>%s indicates files have been handed in.</small>',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_module','delete_text','Note: "Delete" button is only available before the assignment due date.',NOW(),'');
 
-UPDATE `language_text` SET TEXT='Assignments are submitted using the Assignment Dropbox or group File Storage tool. Assign to all students, or create a group and enable File Storage for that group, to add an assignment drop-box.' WHERE term='AT_INFOS_ASSIGNMENT_FS_SUBMISSIONS';
+UPDATE `language_text` SET TEXT='Assignments can be submitted using the Assignment Dropbox or through My Files in the  File Storage tool. Assign an assignment to all students, or create a group and enable File Storage for that group, to add an assignment drop-box.' WHERE term='AT_INFOS_ASSIGNMENT_FS_SUBMISSIONS';
