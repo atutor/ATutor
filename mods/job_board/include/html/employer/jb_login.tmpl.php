f3484d866d647823b16ec3b6c37c606f09d7a08b
greg gay <ggay@ocad.ca>
2010-07-26 16:53:22 +0000

    style adjustements

--- mods/job_board/include/html/employer/jb_login.tmpl.php
+++ mods/job_board/include/html/employer/jb_login.tmpl.php
@@ -12,7 +12,7 @@ function encrypt_password() {
 <div class="jb_login">
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="form">
 	<input type="hidden" name="form_password_hidden" value="" />
-		<fieldset><legend><?php echo _AT('jb_login') ;?></legend>
+		<fieldset class="group_form"><legend class="group_form"><?php echo _AT('jb_login') ;?></legend>
 			<p><?php echo _AT('jb_login_text') ;?></p>
 			<label for="login"><?php echo _AT('jb_login_name'); ?></label><br />
 			<input type="text" name="form_login" size="50" style="max-width: 80%; width: 80%;" id="login" /><br />
