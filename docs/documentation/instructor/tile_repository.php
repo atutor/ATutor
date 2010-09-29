e1a7cb1f58eef4be8de9d78c5fbbc1a2158104de
greg gay <ggay@ocad.ca>
2010-09-29 13:42:25 +0000

    update tile handbook page to be new AContent handbook page

--- docs/documentation/instructor/tile_repository.php
+++ docs/documentation/instructor/tile_repository.php
@@ -1,8 +1,8 @@
 <?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate$'; ?>
 
-<h2>Transformable Repository (EDIT THIS WHEN TRANSFORMABLE IS FINISHED)</h2>
-	<p>Content created in ATutor can be exported to the <em> Transformable Repository</em> using the <em>Export</em> tool on the Import/Export Content screen. Content can also be imported from the TILE repository by entering a search term into the <em>TILE Repository Search</em>, then using <em>Import</em> next to a listing in the search results.  Use the <em>Preview</em> link next to a search result to open the TILE content browser, or use <em>Download</em> to retrieve the content package from TILE repository. Once downloaded, the retrieved package can be imported into ATutor using the Import/Export Content import tool. </p>
+<h2>AContent Repository</h2>
+	<p>Content created in ATutor can be exported to the <em> AContent Repository</em> using the <em>Export</em> tool on the Import/Export Content screen. Content can also be imported from the AContent repository by entering a search term into the <em>AContent Search</em>, then using <em>Import</em> feature next to a listing in the search results.  Click on a content's title to <em>Preview</em> the content in AContent, or use <em>Download</em> to retrieve the content package from the AContent repository. Once downloaded, the retrieved package can be imported into ATutor using the Import/Export Content import tool, or imported into another system that supports the IMS Content Packaging standard. AContent can also be added to a course as a course tool, that students can use to search the repository for information.</p>
 	
-	<p>Visit the <a href="http://www.inclusivelearning.ca/" target="new">TILE web site</a> for more information about using the repository, and to set up an account on the system.</p>
+	<p>Visit the <a href="http://www.atutot.ca/acontent/" target="new">AContent web site</a> for more information about using the repository, and to set up an account on the system.</p>
 
 <?php require('../common/body_footer.inc.php'); ?>
