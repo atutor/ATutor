712b396d737277de8df1bb05ce1cbb438fa1f6e5
cindy li <cli@ocad.ca>
2010-10-04 18:55:00 +0000

    fix the bug that characters "(" and ")" in mp3 file name is not recognized.

--- docs/mods/_standard/flowplayer/module_format_content.php
+++ docs/mods/_standard/flowplayer/module_format_content.php
@@ -34,7 +34,7 @@ preg_match_all("#\[media[0-9a-z\|]*\]http://([\w\./-]+)\.mp3\[/media\]#i",$_inpu
 $media_replace[] ="<a class=\"flowplayerholder\" style=\"display:block;width:##WIDTH##px;height:##HEIGHT##px;\" href=\"http://##MEDIA1##.mp3\"></a>";
 
 // .mp3 - uses Flowplayer 3.0 from flowplayer.org (playing file from AT_content_dir)
-preg_match_all("#\[media[0-9a-z\|]*\]([\w\./-]+)\.mp3\[/media\]#i",$_input,$media_matches[],PREG_SET_ORDER);
+preg_match_all("#\[media[0-9a-z\|]*\](.+[^\s\"]+)\.mp3\[/media\]#i",$_input,$media_matches[],PREG_SET_ORDER);
 $media_replace[] ="<a class=\"flowplayerholder\" style=\"display:block;width:##WIDTH##px;height:##HEIGHT##px;\" href=\"".AT_BASE_HREF."get.php/".$_content_base_href."##MEDIA1##.mp3\"></a>";
 
 
