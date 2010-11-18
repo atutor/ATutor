61f21340f2dc9221edfe820bdf57b31f484a58a6
cindy li <cli@ocad.ca>
2010-11-18 18:56:07 +0000

    Removed the 1.1 infusion library. Will be replaced by infusion 1.3

--- docs/jscripts/infusion/components/inlineEdit/js/jquery.tinymce.js
+++
@@ -1,1 +0,0 @@
-(function($){if(typeof (tinyMCE)!=="undefined"){tinyMCE.init({mode:"none",theme:"simple"})}$.fn.tinymce=function(){this.each(function(){tinyMCE.execCommand("mceAddControl",false,this.id)});return this}})(jQuery);
--- docs/jscripts/infusion/components/reorderer/js/LayoutReorderer.js
+++
@@ -1,1 +0,0 @@
-fluid_1_1=fluid_1_1||{};(function($,fluid){fluid.reorderLayout=function(container,userOptions){var assembleOptions={layoutHandler:"fluid.moduleLayoutHandler",selectors:{columns:".flc-reorderer-column",modules:".flc-reorderer-module"}};var options=$.extend(true,assembleOptions,userOptions);return fluid.reorderer(container,options)}})(jQuery,fluid_1_1);
