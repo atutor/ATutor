31d81429949cb4ebdbd57b2b9f5743e7c8708aa1
greg gay <ggay@ocad.ca>
2010-11-30 18:24:38 +0000

    added vimeo to schema and upgrade for 2.0.2

--- docs/install/db/atutor_upgrade_2.0.1_to_2.0.2.sql
+++ docs/install/db/atutor_upgrade_2.0.1_to_2.0.2.sql
@@ -1,2 +1,5 @@
 # set the type of the mobile theme
 UPDATE `themes` SET `type` = 'Mobile' WHERE `dir_name` = 'mobile';
+
+# add the Vimeo module 
+INSERT INTO `AT_modules` (`dir_name`, `status`, `privilege`, `admin_privilege`, `cron_interval`, `cron_last_run`) VALUES('_standard/vimeo', 2, 0, 1, 0, 0);
