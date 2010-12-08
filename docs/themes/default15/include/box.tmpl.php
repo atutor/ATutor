26cc287a3f72928fda694c7618f635632559f943
greg gay <ggay@ocad.ca>
2010-12-08 20:21:18 +0000

    0004646 added side block expand/collapse toggle.

--- docs/themes/default15/include/box.tmpl.php
+++ docs/themes/default15/include/box.tmpl.php
@@ -1,7 +1,7 @@
 <?php if (!defined('AT_INCLUDE_PATH')) { exit; } ?>
 
 <br />
-<h4 class="box"><?php echo $this->title; ?></h4>
+<h4 class="box"><?php echo $this->title; ?><input class="fl-force-right" src="" alt="" title="" type="image" /></h4>
 <div class="box">
 		<?php echo $this->dropdown_contents; ?>
 </div>
