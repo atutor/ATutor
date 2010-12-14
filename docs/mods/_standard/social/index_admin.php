dcc54b6c48e62e23ac45b0296210c9d966c28348
greg gay <ggay@ocad.ca>
2010-12-14 18:49:31 +0000

    fix alignment for admins social/shidigh screen

--- docs/mods/_standard/social/index_admin.php
+++ docs/mods/_standard/social/index_admin.php
@@ -32,18 +32,18 @@ if($_POST['save']){
 require (AT_INCLUDE_PATH.'header.inc.php');
 ?>
 
-<div style="width:95%;margin:auto;">
-<div class="headingbox"><h3><?php echo _AT('admin_social'); ?></h3></a></div>
-<div class="contentbox">
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<p><?php echo _AT('shindig_blurb'); ?></p>
-		<dl id="public-profile">
-			<dt><label for="shindig"><?php echo _AT('shindig_url'); ?></dt>
-			<dl><input type="text" id="shindig" name="shindig_url" size="60" value="<?php echo $_config['shindig_url']; ?>" /></dd>
-		</dl>
-	<input type="submit" name="save" value="<?php echo _AT('save'); ?>">
-</form>
-</div>
+<div style="width:95%;margin-left:auto;margin-right:auto;">
+	<div class="headingbox"><h3><?php echo _AT('admin_social'); ?></h3></a></div>
+	<div class="contentbox">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+		<p><?php echo _AT('shindig_blurb'); ?></p>
+
+				<label for="shindig"><?php echo _AT('shindig_url'); ?></label>
+				<input type="text" id="shindig" name="shindig_url" size="60" value="<?php echo $_config['shindig_url']; ?>" />
+
+		<input type="submit" name="save" value="<?php echo _AT('save'); ?>" />
+	</form>
+	</div>
 </div>
 <?php 
 
