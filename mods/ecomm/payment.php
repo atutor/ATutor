8a03b52d19360705b5da75a918b550d4f4293fbb
greg gay <ggay@ocad.ca>
2011-02-07 19:46:09 +0000

    removed incorrectly used number_format();

--- mods/ecomm/payment.php
+++ mods/ecomm/payment.php
@@ -26,7 +26,7 @@ $course_id = intval($_GET['course_id']);
 $sql = "SELECT course_fee FROM ".TABLE_PREFIX."ec_course_fees WHERE course_id=$course_id";
 $result = mysql_query($sql, $db);
 if ($this_course_fee = mysql_fetch_assoc($result)) {
-	$this_course_fee = number_format($this_course_fee['course_fee'], 2);
+	$this_course_fee = $this_course_fee['course_fee'];
 } else {
 	header('location: index.php');
 	exit;
@@ -38,12 +38,14 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 ///Check if a partial payment has already been made so the balance can be calculated
 $sql4 = "SELECT SUM(amount) AS total_amount FROM ".TABLE_PREFIX."payments WHERE course_id='$course_id' AND approved=1 AND member_id=$member_id";
 $result4 = mysql_query($sql4, $db);
-if ($row4 = mysql_fetch_assoc($result4)) {
-	$amount_paid = number_format($row4['total_amount'],2);
-} else {
-	$amount_paid = 0.00;
+while ($row4 = mysql_fetch_assoc($result4)) {
+	if($row4['total_amount'] > 0){
+		$amount_paid = $row4['total_amount'];
+	} else {
+		$amount_paid = 0.00;
+	}
 }
-$balance_course_fee = number_format($this_course_fee - $amount_paid, 2);
+$balance_course_fee = $this_course_fee - $amount_paid;
 $this_course_fee = $balance_course_fee;
 
 $sql = "INSERT INTO ".TABLE_PREFIX."payments VALUES (NULL, NULL, 0, '', '{$_SESSION['member_id']}', '$course_id', '$balance_course_fee')";
@@ -68,7 +70,7 @@ $payment_id = mysql_insert_id($db);
 			<dd><?php echo $_config['ec_currency_symbol'].$amount_paid;?></dd>
 
 			<dt><?php echo _AT('ec_balance_due');?></dt>
-			<dd><?php echo $_config['ec_currency_symbol'].$balance_course_fee;?></dd>
+			<dd><?php echo $_config['ec_currency_symbol'].number_format($balance_course_fee, 2).' '.$_config['ec_currency'];;?></dd>
 		</dl>
 			
 		<h4><?php echo _AT('ec_requirements'); ?></h4>
