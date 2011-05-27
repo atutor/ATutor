c5ba9acc08c1ac5d823881495c7564ba2e807af3
greg gay <ggay@ocad.ca>
2011-05-27 18:52:21 +0000

    added the basic LTI tables, the module now integrated as standard.

--- docs/install/db/atutor_upgrade_2.0.2_to_2.0.3.sql
+++ docs/install/db/atutor_upgrade_2.0.2_to_2.0.3.sql
@@ -29,3 +29,61 @@ ALTER TABLE `pa_albums` MODIFY `description` TEXT;
 ALTER TABLE `pa_album_comments` MODIFY `comment` TEXT;
 
 ALTER TABLE `pa_photo_comments` MODIFY `comment` TEXT;
+
+# add the BasicLTI module 
+
+# -------------- External Tools/BasicLTI  Starts -----------------
+CREATE TABLE `basiclti_tools` (
+	`id` mediumint(10) NOT NULL AUTO_INCREMENT,
+	`toolid` varchar(32) NOT NULL,
+	`course_id` mediumint(10) NOT NULL DEFAULT '0',
+	`title` varchar(255) NOT NULL,
+	`description` varchar(1024),
+	`timecreated` TIMESTAMP,
+	`timemodified` TIMESTAMP,
+	`toolurl` varchar(1023) NOT NULL,
+	`resourcekey` varchar(1023) NOT NULL,
+	`password` varchar(1023) NOT NULL,
+	`preferheight` mediumint(4) NOT NULL DEFAULT '0',
+	`allowpreferheight` mediumint(1) NOT NULL DEFAULT '0',
+	`sendname` mediumint(1) NOT NULL DEFAULT '0',
+	`sendemailaddr` mediumint(1) NOT NULL DEFAULT '0',
+	`acceptgrades` mediumint(1) NOT NULL DEFAULT '0',
+	`allowroster` mediumint(1) NOT NULL DEFAULT '0',
+	`allowsetting` mediumint(1) NOT NULL DEFAULT '0',
+	`allowcustomparameters` mediumint(1) NOT NULL DEFAULT '0',
+	`customparameters` text,
+	`organizationid` varchar(64),
+	`organizationurl` varchar(255),
+	`organizationdescr` varchar(255),
+	`launchinpopup` mediumint(1) NOT NULL DEFAULT '0',
+	`debuglaunch` mediumint(1) NOT NULL DEFAULT '0',
+	PRIMARY KEY ( `id`, `toolid` )
+);
+
+CREATE TABLE `basiclti_content` (
+	`id` mediumint(10) NOT NULL AUTO_INCREMENT,
+	`content_id` mediumint(10) NOT NULL DEFAULT '0',
+	`course_id` mediumint(10) NOT NULL DEFAULT '0',
+	`toolid` varchar(32) NOT NULL DEFAULT '',
+	`preferheight` mediumint(4) NOT NULL DEFAULT '0',
+	`sendname` mediumint(1) NOT NULL DEFAULT '0',
+	`sendemailaddr` mediumint(1) NOT NULL DEFAULT '0',
+	`gradebook_test_id` mediumint(10) NOT NULL DEFAULT '0',
+	`allowroster` mediumint(1) NOT NULL DEFAULT '0',
+	`allowsetting` mediumint(1) NOT NULL DEFAULT '0',
+	`customparameters` text,
+	`launchinpopup` mediumint(1) NOT NULL DEFAULT '0',
+	`debuglaunch` mediumint(1) NOT NULL DEFAULT '0',
+	`placementsecret` varchar(1023),
+	`timeplacementsecret` mediumint(10) NOT NULL DEFAULT '0',
+	`oldplacementsecret` varchar(1023),
+	`setting` text(8192),
+	`xmlimport` text(16384),
+	PRIMARY KEY ( `id`, `course_id`, `content_id` )
+);
+# Add BasicLTI to modules
+INSERT INTO `modules` VALUES('_standard/basiclti', 2, 67108864, 16384, 0, 0);
+
+# -------------- External Tools/BasicLTI  Ends -----------------
+
