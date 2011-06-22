546366d96ccfdd1d6551d6b5a8d0ddc5cfc8999a
greg gay <ggay@ocad.ca>
2011-06-22 20:36:53 +0000

    first checkin of wp_config.php

---
+++ mods/wordpress/wp_config.php
@@ -0,0 +1,13 @@
+<?php
+// WordPress database access information.
+// Set the values below to those needed to access your Wordpress database
+// Wordpress needs to be installed in the same server as ATutor
+
+define('WP_DB_USER',	'');           # enter your mysql database user name
+define('WP_DB_PWD',	'');           # enter your mysql database password
+define('WP_DB_NAME',	'wordpress');         #default "wordpress"
+define('WP_DB_PREFIX',	'wp_');            #default "wp_"
+define('WP_DB_HOST',	'localhost');      #default "localhost"
+define('WP_DB_PORT',	'3600');           #default "3600"
+
+?>
