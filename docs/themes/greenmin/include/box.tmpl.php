de4b8a3ffed2f7aa8b437058ee7fd591b7e9f054
greg gay <ggay@ocad.ca>
2011-08-02 17:25:05 +0000

    #4726   replace side menu h4 with h2 so assessibility validation passes the properly nested headings requirement

--- docs/themes/greenmin/include/box.tmpl.php
+++ docs/themes/greenmin/include/box.tmpl.php
@@ -6,7 +6,7 @@ $compact_title = str_replace(' ', '', $this->title);
 ?>
 
 <br />
-<h4 class="box"><span><?php echo $this->title ?></span><input class="fl-force-right" src="" alt="" title="" type="image" /></h4>
+<h2 class="box"><span><?php echo $this->title ?></span><input class="fl-force-right" src="" alt="" title="" type="image" /></h2>
 <div class="box" id="menu_<?php echo $compact_title ?>">
 	<?php echo $this->dropdown_contents; ?>
 </div>
