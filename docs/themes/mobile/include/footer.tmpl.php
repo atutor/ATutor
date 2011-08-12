12535380518e3196a08b3cebd7fbade58eff0911
alison benjamin <radiocontrolled@gmail.com>
2011-08-12 13:32:52 +0000

    ARIA roles updated

--- docs/themes/mobile/include/footer.tmpl.php
+++ docs/themes/mobile/include/footer.tmpl.php
@@ -10,18 +10,18 @@
 
 <div id="footer" class="fl-navbar fl-table">
 <div id="footer-links"> <!-- top help/search/login links -->
-	<ul class="footer-links-tabs fl-tabs flc-themer">  
+	<ul class="footer-links-tabs fl-tabs flc-themer" role="menu">  
 		<?php if (isset($_SESSION['member_id']) && $_SESSION['member_id'] > 0): ?>
 			<?php if(!$this->just_social): ?>					
 				<?php if ($_SESSION['is_super_admin']): ?>
-				<li>	<a  href="<?php echo $this->base_path; ?>bounce.php?admin"><?php echo _AT('back'); ?></a> </li>
+				<li role="menuitem">	<a  href="<?php echo $this->base_path; ?>bounce.php?admin"><?php echo _AT('back'); ?></a> </li>
 				<?php endif; ?>
 	
 				<?php if ($this->course_id > -1): ?>
 					<?php if (get_num_new_messages()): ?>
-				<li>		<a  href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?> (<?php echo get_num_new_messages(); ?>)</a> </li>
+				<li role="menuitem">		<a  href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?> (<?php echo get_num_new_messages(); ?>)</a> </li>
 					<?php else: ?>
-				<li>		<a href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?></a></li>
+				<li role="menuitem">		<a href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?></a></li>
 					<?php endif; ?>
 				<?php endif; ?>
 			<?php endif; ?>
@@ -30,10 +30,10 @@
 		<?php if(!$this->just_social): ?>
 			<li><a href="<?php echo $this->base_path; ?>search.php"><?php echo _AT('search'); ?></a> </li>
 		<?php endif; ?> -->
-		<li><a href="<?php echo $this->base_path; ?>help/index.php"><?php echo _AT('help'); ?></a></li>
+		<li role="menuitem"><a href="<?php echo $this->base_path; ?>help/index.php"><?php echo _AT('help'); ?></a></li>
 
 		<?php if (isset($_SESSION['valid_user']) && $_SESSION['valid_user']): ?>					 
-			<li><a href="<?php echo $this->base_path; ?>logout.php"><?php echo _AT('logout'); ?></a></li>
+			<li role="menuitem"><a href="<?php echo $this->base_path; ?>logout.php"><?php echo _AT('logout'); ?></a></li>
 		<?php else: ?>
 			<!-- <li><a href="<?php echo $this->base_path; ?>login.php?course=<?php echo $this->course_id; ?>"><?php echo _AT('login'); ?></a></li> -->
 		<?php endif; ?>
@@ -59,30 +59,30 @@
 
 <div id="footer" class="fl-navbar fl-table">
 <div id="footer-links"> <!-- top help/search/login links -->
-	<ul class="footer-links-tabs fl-tabs flc-themer">  
+	<ul class="footer-links-tabs fl-tabs flc-themer" role="menu">  
 		<?php if (isset($_SESSION['member_id']) && $_SESSION['member_id'] > 0): ?>
 			<?php if(!$this->just_social): ?>					
 				<?php if ($_SESSION['is_super_admin']): ?>
-				<li>	<a  href="<?php echo $this->base_path; ?>bounce.php?admin"><?php echo _AT('back'); ?></a> </li>
+				<li role="menuitem">	<a  href="<?php echo $this->base_path; ?>bounce.php?admin"><?php echo _AT('back'); ?></a> </li>
 				<?php endif; ?>
 	
 				<?php if ($this->course_id > -1): ?>
 					<?php if (get_num_new_messages()): ?>
-				<li>		<a  href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?> (<?php echo get_num_new_messages(); ?>)</a> </li>
+				<li role="menuitem">		<a  href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?> (<?php echo get_num_new_messages(); ?>)</a> </li>
 					<?php else: ?>
-				<li>		<a href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?></a></li>
+				<li role="menuitem">		<a href="<?php echo $this->base_path; ?>inbox/index.php"><?php echo _AT('inbox'); ?></a></li>
 					<?php endif; ?>
 				<?php endif; ?>
 			<?php endif; ?>
 		<?php endif; ?>
 		
 		<?php if(!$this->just_social): ?>
-			<li><a href="<?php echo $this->base_path; ?>search.php"><?php echo _AT('search'); ?></a> </li>
+			<li role="menuitem"><a href="<?php echo $this->base_path; ?>search.php"><?php echo _AT('search'); ?></a> </li>
 		<?php endif; ?> 
-		<li><a href="<?php echo $this->base_path; ?>help/index.php"><?php echo _AT('help'); ?></a></li>
+		<li role="menuitem"><a href="<?php echo $this->base_path; ?>help/index.php"><?php echo _AT('help'); ?></a></li>
 
 		<?php if (isset($_SESSION['valid_user']) && $_SESSION['valid_user']): ?>					 
-			<li><a href="<?php echo $this->base_path; ?>logout.php"><?php echo _AT('logout'); ?></a></li>
+			<li role="menuitem"><a href="<?php echo $this->base_path; ?>logout.php"><?php echo _AT('logout'); ?></a></li>
 		<?php else: ?>
 			<!-- <li><a href="<?php echo $this->base_path; ?>login.php?course=<?php echo $this->course_id; ?>"><?php echo _AT('login'); ?></a></li> -->
 		<?php endif; ?>
