5cc619e03e4d117de0141fb4feba451421db320c
Greg Gay <greg@atutorspaces.com>
2012-01-05 20:37:40 -0500

    adjust feedback messages

--- make_multi.sh
+++ make_multi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This shell script is used to create ATutor subsites. Enter a unique sub site 
 # ID number as the first parameter, and a unique single word name for the site 
 # as the second paramater( e.g. ./make_multi.sh 234 mysite). This will create a 
@@ -15,30 +15,60 @@
 # multisite installations
 ###########
 
+######
+# Make sure there's a subsite ID and Alias passwed to this script
+if [ -z $1 ]
+	then 	
+	echo "No ATutor site ID was provided"
+	exit 1
+fi
+
+
+if [ -z $2 ]
+	then 	
+	echo "No ATutor site alias was provided"
+	exit 1
+fi
+####
 # Determine where the make_multi.sh script is being run from
 # Sub sites will be created next to the base ATutor installation
-thispwd=`pwd -P`
-base_dir=`dirname $thispwd`
+#thispwd=`pwd -P`
+#base_dir=`dirname $thispwd`
+thispwd=$(readlink -f "$0")
+thispwd=`dirname $thispwd`
+base_dir=`dirname "$thispwd"`
 
 # Detect the hostname, to be used in creating subsite directories
 base_http=`hostname -s`.`hostname -d`
 
 # Gather the first command link paramater (ID) and assign it to the
 # sub_dir variable
+if [ -z $1 ]
+	then 	
+	echo "No ATutor site ID was provided"
+	exit 1
+fi
 
+
+if [ -z $2 ]
+	then 	
+	echo "No ATutor site alias was provided"
+	exit 1
+fi
 sub_dir=$1
+
+# Gather the second command line parameter (alias) and assign it
+# to the domain_dir_alias variable
+domain_dir_alias=$2
+
 
 # Determine if the path of the sub site exists yet
 domain_dir=$base_dir/$sub_dir.$base_http
 if [ -d $domain_dir ]
 	then 	
-	echo "The directory $domain_dir already exists"
+	echo "The site http://$domain_dir_alias.$base_http already exists"
 	exit 1
-fi
-# Gather the second command line parameter (alias) and assign it
-# to the domain_dir_alias variable
-domain_dir_alias=$2
-
+fi 
 # Determine if the alias of the sub site exists yet
 subsite_alias=$base_dir/$domain_dir_alias.$base_http
 if [ -h $subsite_alias ]
@@ -48,8 +78,14 @@ if [ -h $subsite_alias ]
 fi
 
 # Create the sub site's directory and make it writable
-echo 'Creating ' $domain_dir
+#echo 'Creating ' $domain_dir
 mkdir $domain_dir
+#if [ `mkdir $domain_dir` ]
+#then
+#echo "created $domain_dir"
+#else
+#echo "did not create $domain_dir"
+#fi
 chmod a+rwx $domain_dir
 
 # Create the sub site's content directory and make it writable
@@ -57,7 +93,7 @@ mkdir $domain_dir/content
 chmod a+rwx $domain_dir/content
 
 # Copy the ATutor installation script from the base site to the sub site
-cp -r install $domain_dir/install
+cp -r $thispwd/install $domain_dir/install
 
 # Copy the config.inc.php file from the base site to the sub site
 # to be used to automate setup of the database
@@ -94,6 +130,9 @@ chmod a+w $domain_dir/mods
 # Create a symbolic link between the ID based directory for the sub site, and 
 # and the alias directory
 # echo 'Creating link ' $base_dir.$domain_dir_alias.$base_http
+# echo 'Creating ' $subsite_alias
+echo "Creating http://$2.$base_http<br />"
 ln -s $domain_dir $subsite_alias
 
-#echo "DONE, send the link http://$domain_dir_alias.$base_http to the alias directory to the new sub site's administrator"
+echo "DONE, Your ATutor installation is ready for setup at <a href=\"http://$2.$base_http\">http://$2.$base_http</a>"
+
