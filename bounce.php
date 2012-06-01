f3ed5579a30e8a003b15ed1bd300b6bab4eac647
Greg Gay <info@atutor.ca>
2012-06-01 16:12:35 -0400

    unset all prefs when admin returns from course to admin area

--- bounce.php
+++ bounce.php
@@ -114,6 +114,7 @@ if (isset($_GET['admin']) && isset($_SESSION['is_super_admin'])) {
 		$_SESSION['course_id']  = -1;
 		$_SESSION['privileges'] = intval($row['privileges']);
 		$_SESSION['lang'] = $row['language'];
+		unset($_SESSION['prefs']);
 		assign_session_prefs(unserialize(stripslashes($_config['pref_defaults'])), 1);
 		unset($_SESSION['member_id']);
 		unset($_SESSION['is_super_admin']);
