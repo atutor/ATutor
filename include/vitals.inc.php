9c8da4d941315e67462f13f91253739e452ec604
Cindy Qi Li <cli@ocad.ca>
2012-07-25 12:28:23 -0400

    5033: Fixed the issue that except theme and display prefs, other preferences are still not reset.

--- include/vitals.inc.php
+++ include/vitals.inc.php
@@ -237,14 +237,8 @@ if (($_POST['theme'] || $_POST['mobile_theme']) && $_POST['submit']) {
 	$_SESSION['prefs']['PREF_THEME'] = $addslashes($_POST['theme']);
 	$_SESSION['prefs']['PREF_MOBILE_THEME'] = $addslashes($_POST['mobile_theme']);
 } else if ($_POST['set_default']) {
-	$_SESSION['prefs']['PREF_THEME'] = 'default';
-	$_SESSION['prefs']['PREF_MOBILE_THEME'] = 'mobile';
-	// Remove the preference vars that are set on user preference page
-	unset($_SESSION["prefs"]["PREF_BG_COLOUR"]);
-	unset($_SESSION["prefs"]["PREF_FG_COLOUR"]);
-	unset($_SESSION["prefs"]["PREF_HL_COLOUR"]);
-	unset($_SESSION["prefs"]["PREF_FONT_FACE"]);
-	unset($_SESSION["prefs"]["PREF_FONT_TIMES"]);
+	$_SESSION['prefs'] = array();
+	assign_session_prefs(unserialize(stripslashes($_config["pref_defaults"])), 1);
 }
 
 // Reset PREF_THEME when:
