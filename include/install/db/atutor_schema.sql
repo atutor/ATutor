527e9d6aed0327b97c152be341f9011625b3d1c5
Greg <greg@atutorspaces.com>
2012-09-08 17:35:55 -0400

    add some module language to the static language text file

--- include/install/db/atutor_schema.sql
+++ include/install/db/atutor_schema.sql
@@ -1020,7 +1020,7 @@ CREATE TABLE `myown_patches_files` (
 ) ENGINE = MyISAM ;
 
 # insert the default theme
-INSERT INTO `themes` VALUES ('ATutor', '2.1', 'default', 'Desktop', NOW(), 'This is the default ATutor theme and cannot be deleted as other themes inherit from it. Please do not alter this theme directly as it would complicate upgrading. Instead, create a new theme derived from this one.', 2, 0);
+INSERT INTO `themes` VALUES ('ATutor', '2.1', 'default', 'Desktop', NOW(), 'This is the default ATutor theme and cannot be deleted as other themes inherit from it. Please do not alter this theme directly as it would complicate upgrading. Instead, create a new theme derived from this one.', 1, 0);
 INSERT INTO `themes` VALUES ('Fluid', '2.1', 'fluid', 'Desktop', NOW(), 'Theme that implements the Fluid reorderer used to drag-and-drop the menu from side-to-side.', 1, 0);
 INSERT INTO `themes` VALUES ('ATutor Classic', '2.1', 'default_classic', 'Desktop', NOW(), 'This is the ATutor Classic theme which makes use of the custom Header and logo images. To customize those images you must edit the <code>theme.cfg.php</code> in this themes directory.', 1,0);
 INSERT INTO `themes` VALUES ('Blumin', '2.1', 'blumin', 'Desktop', NOW(), 'This is the plone look-alike theme.', 1, 0); 
@@ -1032,7 +1032,7 @@ INSERT INTO `themes` VALUES ('ATutor 1.6', '2.1', 'default16', 'Desktop', NOW(),
 INSERT INTO `themes` VALUES ('IDI Theme', '2.1', 'idi', 'Desktop', NOW(), 'The theme created for the IDI course server.', 1, 0);
 INSERT INTO `themes` VALUES ('Mobile', '2.1', 'mobile', 'Mobile', NOW(), 'This is the default theme for mobile devices.', 3, 0);
 INSERT INTO `themes` VALUES('Simple', '2.1', 'simplified_desktop', 'Desktop', NOW(), 'An adapted version of the iPad theme, designed to make a desktop look like an iPad.', 1, 0);
-INSERT INTO `themes` VALUES('ATutorSpaces', '2.1', 'atspaces', 'Desktop', NOW(), 'This is the default theme for the ATutorSpaces.com hosting service.', 1, 0);
+INSERT INTO `themes` VALUES('ATutorSpaces', '2.1', 'atspaces', 'Desktop', NOW(), 'This is the default theme for the ATutorSpaces.com hosting service.', 2, 0);
 
 # --------------------------------------------------------
 # Table structure for table `users_online`
@@ -1114,9 +1114,9 @@ INSERT INTO `config` (`name`, `value`) VALUES('atlas', 'http://maps.google.ca/')
 INSERT INTO `config` (`name`, `value`) VALUES('calculator', 'http://www.calculateforfree.com/');
 INSERT INTO `config` (`name`, `value`) VALUES('note_taking', 'http://www.aypwip.org/webnote/');
 INSERT INTO `config` (`name`, `value`) VALUES('abacas', 'http://www.mandarintools.com/abacus.html');
-INSERT INTO `config` (`name`, `value`) VALUES('transformable_uri', 'http://localhost/transformable/');
-INSERT INTO `config` (`name`, `value`) VALUES('transformable_web_service_id', '90c3cd6f656739969847f3a99ac0f3c7');
-INSERT INTO `config` (`name`, `value`) VALUES('transformable_oauth_expire', '93600');
+#INSERT INTO `config` (`name`, `value`) VALUES('transformable_uri', 'http://localhost/transformable/');
+#INSERT INTO `config` (`name`, `value`) VALUES('transformable_web_service_id', '90c3cd6f656739969847f3a99ac0f3c7');
+#INSERT INTO `config` (`name`, `value`) VALUES('transformable_oauth_expire', '93600');
 
 # End Access4All setup 
 
@@ -1616,3 +1616,102 @@ CREATE TABLE `basiclti_content` (
 ) ENGINE = MyISAM;
 # -------------- External Tools/BasicLTI  Ends -----------------
 
+# ---------------ATutorSpaces additional config options -------------------
+# This is for the service module, to disable create course
+# It should be commented out for most installations
+
+#########
+# Setup Services module
+# reassign create courses to the services module
+INSERT INTO `config` VALUES('disable_create','1');
+INSERT INTO `modules` (dir_name, status, privilege, admin_privilege, cron_interval, cron_last_run)
+SELECT '_core/services', 2, 0, max(admin_privilege)*2, 0, 0 FROM `AT_modules`; 
+
+# Add the ATutorSpaces theme, set it as the default theme, and 
+# set the usual Default theme just enabled.
+#INSERT INTO `themes` VALUES ('ATutorSpaces', '2.1', 'atspaces', 'Desktop', NOW(), 'This is the default theme for ATutorSpaces.', 2);
+#comment out the default theme in the schema above
+#INSERT INTO `themes` VALUES ('ATutor', '2.1', 'default', 'Desktop', NOW(), 'This is the default ATutor theme.', 1);
+
+# Public AContent setup
+INSERT INTO `config` (`name`,`value`) VALUES ('transformable_uri','http://acontent.atutorspaces.com/');
+INSERT INTO `config` (`name`,`value`) VALUES ('transformable_web_service_id','987f65dddffa43abd43b30426e6c7c1c');
+INSERT INTO `config` (`name`,`value`) VALUES ('transformable_oauth_expire','93600');
+
+
+########
+# Set the default Home URL to atutorspaces.com
+INSERT INTO `config` (`name`,`value`) VALUES ('home_url','http://www.atutorspaces.com');
+
+########
+# Setup Payments module
+# sql for ATutor Ecommerce module
+INSERT INTO `modules` (dir_name, status, privilege, admin_privilege, cron_interval, cron_last_run)
+SELECT 'payments', 2, max(privilege)*2, max(admin_privilege)*2, 0, 0 FROM `AT_modules`; 
+
+CREATE TABLE `payments` (
+`payment_id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+`approved` TINYINT NOT NULL DEFAULT '0',
+`transaction_id` CHAR( 100 ) NOT NULL ,
+`member_id` MEDIUMINT UNSIGNED NOT NULL ,
+`course_id` MEDIUMINT UNSIGNED NOT NULL ,
+`amount` DECIMAL( 7, 2 ) NOT NULL DEFAULT '0'
+) ENGINE=MYISAM ;
+
+
+CREATE TABLE `ec_course_fees` (
+  `course_id` smallint( 8 )  NOT  NULL ,
+  `course_fee` DECIMAL( 7, 2 ) NOT  NULL DEFAULT '0',
+  `auto_approve` tinyint( 1 )  default NULL ,
+  `auto_email` tinyint( 1 )  default NULL ,
+ PRIMARY  KEY ( `course_id` )
+) ENGINE=MyISAM;
+
+
+########
+# Setup BigBlueButton module
+# sql for bigbluebutton module v0.4
+INSERT INTO `modules` (`dir_name`,`status`,`privilege`,`admin_privilege`,`cron_interval`,`cron_last_run`) 
+SELECT 'bigbluebutton',2,max(privilege)*2,max(admin_privilege)*2,0,0 FROM `AT_modules`;
+
+CREATE TABLE `bigbluebutton` (
+  `meeting_id` tinyint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` tinyint(8) unsigned NOT NULL,
+  `course_name` varchar(155) NOT NULL,
+  `course_timing` varchar(15) NOT NULL,
+  `message` text NOT NULL,
+  `status` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`meeting_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+INSERT INTO `config` (`name`,`value`) VALUES ('bbb_url','http://bbb.atutorspaces.com');
+#INSERT INTO `config` (`name`,`value`) VALUES ('bbb_salt','3ACE8DC6-947B-11E1-83F7-9734BBE59B18');
+INSERT INTO `config` (`name`,`value`) VALUES ('bbb_max_recordings','3');
+
+########
+# Setup the OpenLearn Module 
+CREATE TABLE `ol_search_open_learn` (
+      `id` mediumint(8) unsigned NOT NULL,
+      `identifier` varchar (768),
+	 `datestamp` varchar (768),
+	 `catalog` varchar (768),
+	 `entry` varchar (768),
+	 `title` blob ,
+	 `description` blob ,
+	 `keywords` blob ,
+	 `website` varchar (768),
+	 `cc` varchar (768),
+	 `cp` varchar (768),
+      PRIMARY KEY ( `id` )
+) ENGINE = MyISAM;
+
+INSERT INTO `modules` (`dir_name`,`status`,`privilege`,`admin_privilege`,`cron_interval`,`cron_last_run`) 
+SELECT 'ol_search_open_learn',2,max(privilege)*2,max(admin_privilege)*2,0,0 FROM `AT_modules`;
+
+#configuration data for OpenLearn module
+INSERT INTO `config` VALUES ('ol_url','http://openlearn.open.ac.uk/local/oai/oai2.php?verb=ListRecords&metadataPrefix=oai_ilox');
+INSERT INTO `config` VALUES ('ol_last_updation','');
+INSERT INTO `config` VALUES ('ol_base_url','http://openlearn.open.ac.uk/local/oai/oai2.php?');
+INSERT INTO `config` VALUES ('ol_rss_url','http://openlearn.open.ac.uk/rss/file.php/stdfeed/');
+INSERT INTO `config` VALUES ('ol_doc_url','http://openlearn.open.ac.uk/blocks/formats/download_unit.php?id=');
