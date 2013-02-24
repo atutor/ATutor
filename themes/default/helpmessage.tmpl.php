f183d0ba70e59912c49ad9532dbc876dca2b5fb1
Greg Gay <ggay@ocad.ca>
2013-02-24 18:28:57 -0500

    added help message template

---
+++ themes/default/helpmessage.tmpl.php
@@ -0,0 +1,25 @@
+<?php  
+/*
+ * @author Jacek Materna
+ *
+ *  One Savant variable: $item which is the processed ouput message content according to lang spec.
+ */
+ 
+global $_base_href;
+
+?>
+
+<div id="help" class="divClass">
+    <a href="#" onclick="return false;" id="delete" class="deleteDiv">
+        <img src="<?php echo $_base_href; ?>mods/helpme/images/close.png" alt="<?php echo _AT('helpme_dismiss'); ?>" title="<?php echo _AT('helpme_dismiss'); ?>" role="link"/>
+    </a>
+    <?php if (is_array($this->item)) : ?>
+        <ul>
+        <?php foreach($this->item as $i) : ?>
+            <li><?php echo $i; ?></li>
+        <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+ <div class="helpme_count"><?php echo $this->helpme_count; ?>/<?php echo $this->helpme_total; ?></div>
+</div>
+
