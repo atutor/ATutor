914281a1c2a4ea3312403c39c5e7a5e673c2d9a7
gregrgay <info@atutor.ca>
2013-03-22 16:29:55 -0400

    5240 switch token to save first, and update language db to make that message adaptable to other than adpated content panel where it cuyrrently disaplys

--- mods/_core/editor/preview.php
+++ mods/_core/editor/preview.php
@@ -22,7 +22,7 @@ $cid = intval($_POST['cid']);
 if ($cid == 0) {
 	require(AT_INCLUDE_PATH.'header.inc.php');
 	$missing_fields[] = _AT('content_id');
-	$msg->printErrors(array('EMPTY_FIELDS', $missing_fields));
+	$msg->printErrors(array('SAVE_BEFORE_PROCEED', $missing_fields));
 	require (AT_INCLUDE_PATH.'footer.inc.php');
 	exit;
 }
