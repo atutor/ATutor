46809faf43e1c16dd4559f82e4ffbe21d69d4a4e
geetakshi <geetakshi.19@gmail.com>
2013-04-04 00:55:01 +0530

    ISSUE-5201: Changing mods/_core/modules/classes/ModuleUtility.class.php to use soft tabs with 4 spaces.

--- mods/_core/modules/classes/ModuleUtility.class.php
+++ mods/_core/modules/classes/ModuleUtility.class.php
@@ -1,14 +1,14 @@
 <?php
 /****************************************************************/
-/* ATutor														*/
+/* ATutor                                                       */
 /****************************************************************/
 /* Copyright (c) 2002-2010                                      */
 /* Inclusive Design Institute                                   */
-/* http://atutor.ca												*/
+/* http://atutor.ca                                             */
 /*                                                              */
 /* This program is free software. You can redistribute it and/or*/
 /* modify it under the terms of the GNU General Public License  */
-/* as published by the Free Software Foundation.				*/
+/* as published by the Free Software Foundation.                */
 /****************************************************************/
 // $Id$
 
@@ -21,12 +21,12 @@
 */
 class ModuleUtility {
 
-	private static $main_defaults;
-	private static $home_defaults;
+    private static $main_defaults;
+    private static $home_defaults;
 
-	function ModuleUtility() {
-		// constructor
-	}
+    function ModuleUtility() {
+        // constructor
+    }
 
     public static function up_set($post, $up) {
         $_new_modules  = array();
@@ -72,32 +72,32 @@ class ModuleUtility {
         return $defaults;
     }
 
-	public static function set_config_values($config_name, $name) {
+    public static function set_config_values($config_name, $name) {
         if (!($_config_defaults[$config_name] == $name)) {
             $result = queryDB("REPLACE INTO %sconfig VALUES('%s', '%s')", array(TABLE_PREFIX, $config_name, $name));
-		} else if ($_config_defaults[$config_name] == $name) {
+        } else if ($_config_defaults[$config_name] == $name) {
             $result = queryDB("DELETE FROM %sconfig WHERE name='%s'", array(TABLE_PREFIX, $config_name));
-		}
-	}
-	/**
-	*
-	* Set default course tools
-	* @access public
-	* @static
-	*
-	*/
-	public static function set_default_tools($post_up, $post_down, $post_main, $post_home, $post_submit) {
-		global $msg;
-		if (isset($post_up)) {
-			$up = key($post_up);
+        }
+    }
+    /**
+    *
+    * Set default course tools
+    * @access public
+    * @static
+    *
+    */
+    public static function set_default_tools($post_up, $post_down, $post_main, $post_home, $post_submit) {
+        global $msg;
+        if (isset($post_up)) {
+            $up = key($post_up);
             if(isset($post_home)) {
                 $post_home = self::up_set($post_home, $up);
             }
             if(isset($post_main)) {
                 $post_main = self::up_set($post_main, $up);
             }
-			$post_submit = TRUE;
-		} else if (isset($post_down)) {
+            $post_submit = TRUE;
+        } else if (isset($post_down)) {
             $down = key($post_down);
             if(isset($post_home)) {
                 $post_home = self::down_set($post_home, $down);
@@ -105,32 +105,32 @@ class ModuleUtility {
             if(isset($post_main)) {
                 $post_main = self::down_set($post_main, $down);
             }
-			$post_submit = TRUE;
-		}
-		if (isset($post_submit)) {
+            $post_submit = TRUE;
+        }
+        if (isset($post_submit)) {
             $main_defaults = self::submit_set($post_main, $main_defaults);
             $home_defaults = self::submit_set($post_home, $home_defaults);
 
-			self::set_config_values('main_defaults', $main_defaults);
-			self::set_config_values('home_defaults', $home_defaults);
-		}
-	}
+            self::set_config_values('main_defaults', $main_defaults);
+            self::set_config_values('home_defaults', $home_defaults);
+        }
+    }
 
-	/**
-	* Return set config values
-	* @access public
-	* @return array
-	* @static
-	*/
-	public static function get_main_defaults()
-	{
-		return $main_defaults;
-	}
+    /**
+    * Return set config values
+    * @access public
+    * @return array
+    * @static
+    */
+    public static function get_main_defaults()
+    {
+        return $main_defaults;
+    }
 
-	public static function get_home_defaults()
-	{
-		return $home_defaults;
-	}
+    public static function get_home_defaults()
+    {
+        return $home_defaults;
+    }
 
 }
 
