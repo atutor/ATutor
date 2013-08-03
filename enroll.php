a585d14aca33b33dd6022c5c307169cbc9285c6c
Greg Gay <ggay@ocad.ca>
2013-08-03 17:08:07 -0400

    replaced mysql with queryDB

--- enroll.php
+++ enroll.php
@@ -2,7 +2,7 @@
 /***********************************************************************/
 /* ATutor															   */
 /***********************************************************************/
-/* Copyright (c) 2002-2010                                             */
+/* Copyright (c) 2002-2013                                             */
 /* Inclusive Design Institute	                                       */
 /* http://atutor.ca													   */
 /*																	   */
@@ -33,26 +33,25 @@ if ($course == 0) {
 }
 
 $sql	= "SELECT access, member_id FROM ".TABLE_PREFIX."courses WHERE course_id=$course";
-$result = mysql_query($sql, $db);
-$course_info = mysql_fetch_array($result);
-
+$course_info = queryDB($sql, array(TABLE_PREFIX, $course), TRUE);
+   
 if ($_POST['submit']) {
 	$_SESSION['enroll'] = AT_ENROLL_YES;
 	$_POST['form_course_id'] = intval($_POST['form_course_id']);
 
-	if ($course_info[0] == 'private') {
-		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $_POST[form_course_id], 'n', 0, '"._AT('student')."', 0)";
-		$result = mysql_query($sql, $db);
+	if ($course_info['access'] == 'private') {
 
+		$sql	= "INSERT INTO %scourse_enrollment VALUES (%d, %d, 'n', 0, '"._AT('student')."', 0)";
+		$result = queryDB($sql, array(TABLE_PREFIX,$_SESSION['member_id'], $_POST['form_course_id']));
+		
 		// send the email thing. if needed
-
-		$sql	= "SELECT notify, member_id FROM ".TABLE_PREFIX."courses WHERE course_id=$_POST[form_course_id] AND notify=1";
-		$result = mysql_query($sql, $db);
-		if ($row = mysql_fetch_array($result)) {
+		$sql	= "SELECT notify, member_id FROM %scourses WHERE course_id=%d AND notify=1";
+		$row = queryDB($sql, array(TABLE_PREFIX, $_POST['form_course_id']), TRUE);
+		
+		if (isset($row['member_id']) && $row['member_id'] !='') {
 			// notify is enabled. get the email
-			$sql	= "SELECT email, first_name, last_name FROM ".TABLE_PREFIX."members WHERE member_id=$row[member_id]";
-			$result = mysql_query($sql, $db);
-			$row	= mysql_fetch_assoc($result);
+			$sql	= "SELECT email, first_name, last_name FROM %smembers WHERE member_id=%d";
+			$row = queryDB($sql, array(TABLE_PREFIX, $row['member_id']), TRUE);
 
 			$to_email = $row['email'];
 
@@ -79,25 +78,28 @@ if ($_POST['submit']) {
 			}
 		}
 	} else {
+
 		// public or protected
-		$sql	= "INSERT INTO ".TABLE_PREFIX."course_enrollment VALUES ($_SESSION[member_id], $_POST[form_course_id], 'y', 0, '"._AT('student')."', 0)";
-		$result = mysql_query($sql, $db);
+		$sql	= "INSERT INTO %scourse_enrollment VALUES ('%d', '%d', 'y', 0, '"._AT('student')."', 0)";
+		$result = queryDB($sql, array(TABLE_PREFIX, $_SESSION['member_id'], $_POST['form_course_id']));
+
+
 	}
+
 }
 
 if ($_SESSION['valid_user'] === true) {
 
-	$sql	= "SELECT * FROM ".TABLE_PREFIX."course_enrollment WHERE member_id=$_SESSION[member_id] AND course_id=$course";
-	$result = mysql_query($sql, $db);
-	$row	= mysql_fetch_array($result);
+	$sql	= "SELECT * FROM %scourse_enrollment WHERE member_id=%d AND course_id=%d";
+	$row = queryDB($sql, array(TABLE_PREFIX,$_SESSION['member_id'], $course ), TRUE);
 
-	if (($course_info[0] == 'public') || ($course_info[0] == 'protected')) {
-		if ($row != '') {
+	if (($course_info['access'] == 'public') || ($course_info['access'] == 'protected')) {
+		if ($row['member_id'] != '') {
 		
 			$feedback = array('NOW_ENROLLED', $system_courses[$course]['title']);
 			$msg->addFeedback($feedback);
 			header("Location:index.php");
-		} else if ($course_info[1] != $_SESSION['member_id']) {
+		} else if ($course_info['member_id'] != $_SESSION['member_id']) {
 
 			require(AT_INCLUDE_PATH.'header.inc.php'); ?>
 
@@ -135,7 +137,7 @@ if ($_SESSION['valid_user'] === true) {
 
 		} else if ($_POST['submit']) {
 			$msg->printFeedbacks('APPROVAL_PENDING');
-		} else if ($course_info[1] != $_SESSION['member_id'] ){
+		} else if ($course_info['member_id'] != $_SESSION['member_id'] ){
 			 // request has already been made
 			 $msg->printErrors('ALREADY_ENROLED');
 		} else {
