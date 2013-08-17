a12d3b1d87535c86f1711a1422fc1de6a8d24fd3
atutor <info@atutor.ca>
2013-08-17 10:40:32 -0400

    replace mysql with queryDB()

--- mods/_core/courses/admin/auto_enroll_delete.php
+++ mods/_core/courses/admin/auto_enroll_delete.php
@@ -24,15 +24,15 @@ if (isset($_POST['submit_no'])) {
 	/* delete has been confirmed, delete this category */
 	$auto_enroll_id	= intval($_POST['auto_enroll_id']);
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."auto_enroll WHERE auto_enroll_id=$auto_enroll_id";
-	$result = mysql_query($sql, $db);
+	$sql = "DELETE FROM %sauto_enroll WHERE auto_enroll_id=%d";
+	$rows_auto_enroll = queryDB($sql, array(TABLE_PREFIX,$auto_enroll_id));
 
-	write_to_log(AT_ADMIN_LOG_DELETE, 'auto_enroll', mysql_affected_rows($db), $sql);
-
-	$sql = "DELETE FROM ".TABLE_PREFIX."auto_enroll_courses WHERE auto_enroll_id=$auto_enroll_id";
-	$result = mysql_query($sql, $db);
-
-	write_to_log(AT_ADMIN_LOG_DELETE, 'auto_enroll_courses', mysql_affected_rows($db), $sql);
+	write_to_log(AT_ADMIN_LOG_DELETE, 'auto_enroll', $rows_auto_enroll, $sqlout);
+	
+	$sql = "DELETE FROM %sauto_enroll_courses WHERE auto_enroll_id=%d";
+	$rows_auto_enroll_courses = queryDB($sql, array(TABLE_PREFIX, $auto_enroll_id));
+	
+	write_to_log(AT_ADMIN_LOG_DELETE, 'auto_enroll_courses', $rows_auto_enroll_courses, $sqlout);
 
 	$msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
 	header('Location: auto_enroll.php');
@@ -43,18 +43,18 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 
 	$_GET['auto_enroll_id'] = intval($_GET['auto_enroll_id']); 
 
-	$sql = "SELECT * FROM ".TABLE_PREFIX."auto_enroll WHERE auto_enroll_id=$_GET[auto_enroll_id]";
-	$result = mysql_query($sql,$db) or die(mysql_error());
-
-	if (mysql_num_rows($result) == 0) {
-		$msg->printErrors('ITEM_NOT_FOUND');
+	$sql = "SELECT * FROM %sauto_enroll WHERE auto_enroll_id=%d";
+	$rows_auto_enroll = queryDB($sql, array(TABLE_PREFIX, $_GET['auto_enroll_id']), TRUE);
+	
+	if (count($rows_auto_enroll) == 0) {
+			$msg->printErrors('ITEM_NOT_FOUND');
 	} else {
-		$row = mysql_fetch_assoc($result);
+	    $row = $rows_auto_enroll;
+		//$row = mysql_fetch_assoc($result);
 		
 		$hidden_vars['name']= $row['name'];
 		$hidden_vars['auto_enroll_id']	= $row['auto_enroll_id'];
 
-//		$confirm = array('DELETE_AUTO_ENROLL', AT_print($row['name'], 'auto_enroll.name'));
 		$confirm = array('DELETE_AUTO_ENROLL', $row['name']);
 		$msg->addConfirm($confirm, $hidden_vars);
 		
