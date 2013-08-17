9f067efd5d283151d2e854f9fd0e94f52d07460a
atutor <info@atutor.ca>
2013-08-17 15:32:51 -0400

    replace mysql with queryDB()

--- mods/_core/courses/admin/auto_enroll.php
+++ mods/_core/courses/admin/auto_enroll.php
@@ -52,23 +52,22 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 </tfoot>
 <tbody>
 <?php
-$sql	= "SELECT * FROM ".TABLE_PREFIX."auto_enroll ae ORDER BY name";
-$result = mysql_query($sql, $db) or die(mysql_error());
 
-if ($row = mysql_fetch_assoc($result)): ?>
-	<?php
-	do {
+$sql	= "SELECT * FROM ".TABLE_PREFIX."auto_enroll ae ORDER BY name";
+$rows_ae = queryDB($sql, array(TABLE_PREFIX));
+
+if(count($rows_ae) > 0){
+    foreach($rows_ae as $row){
 		$courses = "";
-		$sql_courses = "SELECT c.title FROM ". TABLE_PREFIX."auto_enroll_courses aec, " . 
-		                         TABLE_PREFIX ."courses c 
-		          WHERE aec.auto_enroll_id = ". $row["auto_enroll_id"] . "
+		$sql_courses = "SELECT c.title FROM %sauto_enroll_courses aec, %scourses c 
+		          WHERE aec.auto_enroll_id = %d
 		            AND aec.course_id = c.course_id
 		          ORDER BY c.title";
-
-		$result_courses = mysql_query($sql_courses, $db) or die(mysql_error());
-
-		while ($row_courses = mysql_fetch_assoc($result_courses))
+		$rows_courses = queryDB($sql_courses, array(TABLE_PREFIX, TABLE_PREFIX, $row['auto_enroll_id']));
+		
+        foreach($rows_courses as $row_courses){
 			$courses .= $row_courses["title"] . "<br>";
+			}
 	?>
 		<tr onmousedown="document.form['m<?php echo $row['auto_enroll_id']; ?>'].checked = true; rowselect(this);" id="r_<?php echo $row['auto_enroll_id']; ?>">
 			<td width="10"><input type="radio" name="auto_enroll_id" value="<?php echo $row['auto_enroll_id']; ?>" id="m<?php echo $row['auto_enroll_id']; ?>" /></td>
@@ -76,12 +75,13 @@ if ($row = mysql_fetch_assoc($result)): ?>
 			<td><?php echo $courses; ?></td>
 			<td nowrap><?php echo $_base_href. "registration.php?en_id=". $row['associate_string']; ?></td>
 		</tr>
-	<?php } while ($row = mysql_fetch_assoc($result)); ?>
-<?php else: ?>
+	<?php } ?>
+
+<?php }else{ ?>
 	<tr>
 		<td colspan="4"><?php echo _AT('none_found'); ?></td>
 	</tr>
-<?php endif; ?>
+<?php } ?>
 </tbody>
 </table>
 
