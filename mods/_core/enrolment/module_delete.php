4a6a374634d26065f741a549e5dfbb4534e01168
Greg Gay <info@atutor.ca>
2013-08-23 10:17:56 -0400

    correct $ to % typo in

--- mods/_core/enrolment/module_delete.php
+++ mods/_core/enrolment/module_delete.php
@@ -2,7 +2,7 @@
 
 function enrolment_delete($course) {
 	global $db;
-	$sql	= "DELETE FROM %scourse_enrollment WHERE course_id=$d";
+	$sql	= "DELETE FROM %scourse_enrollment WHERE course_id=%d";
 	$result = queryDB($sql, array(TABLE_PREFIX, $course));
 
 	$sql	= "DELETE FROM %sauto_enroll_courses WHERE course_id=%d";
