37288f77e7ab740bf860609657a4dfcd2d973d22
Greg Gay <info@atutor.ca>
2013-08-23 10:22:24 -0400

    replace mysql with queryDB()

--- mods/_core/glossary/module_delete.php
+++ mods/_core/glossary/module_delete.php
@@ -3,9 +3,8 @@
 function glossary_delete($course) {
 	global $db;
 
-	$sql	= "DELETE FROM ".TABLE_PREFIX."glossary WHERE course_id=$course";
-	$result = mysql_query($sql, $db);
-
+	$sql	= "DELETE FROM %sglossary WHERE course_id=%d";
+	$result = queryDB($sql, array(TABLE_PREFIX, $course));
 }
 
 ?>
