eeb931005100283e02e2ad07d3347daa44746580
Greg Gay <info@atutor.ca>
2013-09-05 15:22:42 -0400

    update DB queries to use queryDB()

--- mods/_core/courses/module_extend_date.php
+++ mods/_core/courses/module_extend_date.php
@@ -12,55 +12,65 @@
         
         global $db;
         $course = array();      
+               $sql = "SELECT course_id FROM %scourse_enrollment WHERE member_id = %d";
+        $rows_enrolled = queryDB($sql, array(TABLE_PREFIX, $_SESSION['member_id']));
+        $enrolled_count = count( $rows_enrolled);
+        foreach($rows_enrolled as $row){
+            $c++;
+            if($c == $enrolled_count){
+            $enrolled .= $row['course_id'];
+            }else{
+            $enrolled .= $row['course_id'].",";
+            }
+        }
+        debug_to_log($enrolled);
         
         // get the course details along with the relevant dates
         $sql = "SELECT M.first_name, M.last_name, C.title, C.release_date, C.end_date
-                FROM " . TABLE_PREFIX . "courses C , " . TABLE_PREFIX . "members M , ".
-                TABLE_PREFIX . "course_enrollment E WHERE C.course_id = '".
-                $course_id . "' AND M.member_id = '" . $member_id."' 
-                AND E.member_id = M.member_id";   
+                FROM %scourses C , %smembers M , %scourse_enrollment E WHERE C.course_id IN (%s) AND M.member_id = '%d' 
+                AND E.member_id = M.member_id AND E.course_id = C.course_id";   
         
         
-        $result     = mysql_query($sql,$db) or die(mysql_error());
-        $row_count  = mysql_num_rows($result);     
+        $rows_courses = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, TABLE_PREFIX, $enrolled, $member_id));
+        $row_count  = count($rows_courses);     
         
         if ($row_count > 0) {
             $index = 0; 
-            $row   = mysql_fetch_assoc($result);
-                            
-            if(strpos( $row['release_date'].'', '0000-00-00' ) === false) {
-                $unix_ts = strtotime($row['release_date']);
-                $time    = date('h:i A',$unix_ts);
-                // release_date
-                $course[$index] = array(
-                            "id"        => rand(10000,15000) . '',
-                            "title"     => _AT('calendar_course_start') . $row['title']/*. 
+            foreach($rows_courses as $row){           
+                if(strpos( $row['release_date'].'', '0000-00-00' ) === false) {
+                    $unix_ts = strtotime($row['release_date']);
+                    $time    = date('h:i A',$unix_ts);
+                    // release_date
+                    $course[$index] = array(
+                                "id"        => rand(10000,15000) . '',
+                                "title"     => _AT('calendar_course_start') . $row['title']/*. 
+                                               _AT('calendar_course_token')*/,
+                                "start"     => $row['release_date'],
+                                "end"       => $row['release_date'],
+                                "allDay"    => false,
+                                "color"     => 'green',
+                                "textColor" => 'white',
+                                "editable"  => false                        
+                            );                          
+                    $index++;
+                }
+                //end date
+                if (strpos( $row['end_date'].'', '0000-00-00' ) === false) {
+                    $unix_ts = strtotime($row['end_date']);
+                    $time    = date('h:i A',$unix_ts);
+                    $course[$index] = array(
+                            "id"        => rand(10000,15000).'',
+                            "title"     => _AT('calendar_course_end') . $row['title']/*.
                                            _AT('calendar_course_token')*/,
-                            "start"     => $row['release_date'],
-                            "end"       => $row['release_date'],
+                            "start"     => $row['end_date'],
+                            "end"       => $row['end_date'],
                             "allDay"    => false,
-                            "color"     => 'green',
+                            "color"     => 'maroon',
                             "textColor" => 'white',
-                            "editable"  => false                        
-                        );                          
-                $index++;
-            }
-            //end date
-            if (strpos( $row['end_date'].'', '0000-00-00' ) === false) {
-                $unix_ts = strtotime($row['end_date']);
-                $time    = date('h:i A',$unix_ts);
-                $course[$index] = array(
-                        "id"        => rand(10000,15000).'',
-                        "title"     => _AT('calendar_course_end') . $row['title']/*.
-                                       _AT('calendar_course_token')*/,
-                        "start"     => $row['end_date'],
-                        "end"       => $row['end_date'],
-                        "allDay"    => false,
-                        "color"     => 'maroon',
-                        "textColor" => 'white',
-                        "editable"  => false 
-                    );
-                $index++;
+                            "editable"  => false 
+                        );
+                    $index++;
+                }
             }
         }        
         return $course;
