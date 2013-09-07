5e09e065ffd4c11715d505d09c01a041cf2f28d1
atutor <info@atutor.ca>
2013-09-07 10:15:35 -0400

    replace mysql with queryDB()

--- mods/_standard/basiclti/tool/admin_delete.php
+++ mods/_standard/basiclti/tool/admin_delete.php
@@ -8,12 +8,12 @@ admin_authenticate(AT_ADMIN_BASICLTI);
 
 $tool = intval($_REQUEST['id']);
 
-$sql = "SELECT title FROM ".TABLE_PREFIX."basiclti_tools WHERE id = ".$tool.";";
-$result = mysql_query($sql, $db) or die(mysql_error());
-$row = mysql_fetch_assoc($result);
+$sql = "SELECT title FROM %sbasiclti_tools WHERE id = %d";
+$row = queryDB($sql, array(TABLE_PREFIX, $tool), TRUE);
+
 
 if ( strlen($row["title"]) < 1) {
-        $msg->addFeedback('UNABLE_TO_FIND_TOOL');
+        $msg->addError('UNABLE_TO_FIND_TOOL');
         header('Location: ../index_admin.php');
         exit;
 }
@@ -23,9 +23,11 @@ if (isset($_POST['submit_no'])) {
         header('Location: ../index_admin.php');
         exit;
 } else if (isset($_POST['submit_yes'])) {
-	$sql = "DELETE FROM ".TABLE_PREFIX."basiclti_tools WHERE id = ".$tool.";";
-	$result = mysql_query($sql, $db) or die(mysql_error());
-        write_to_log(AT_ADMIN_LOG_DELETE, 'basiclti_delete', mysql_affected_rows($db), $sql);
+
+		$sql = "DELETE FROM %sbasiclti_tools WHERE id =%d";
+	    $result = queryDB($sql, array(TABLE_PREFIX, $tool));
+	    global $sqlout;
+        write_to_log(AT_ADMIN_LOG_DELETE, 'basiclti_delete', $result, $sqlout);
         $msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
         header('Location: ../index_admin.php');
         exit;
@@ -39,13 +41,6 @@ if (!isset($_POST['step'])) {
         $msg->addConfirm(array('DELETE_TOOL_1', $row['title']), $hidden_vars);
         $msg->printConfirm();
 } 
-/*
 
-else if ($_POST['step'] == 1) {
-        $hidden_vars['step']   = 2;
-        $hidden_vars['id'] = $tool;
-        $msg->addConfirm(array('DELETE_TOOL_2', $row['title']), $hidden_vars);
-        $msg->printConfirm();
-}*/
 
 require(AT_INCLUDE_PATH.'footer.inc.php'); 
