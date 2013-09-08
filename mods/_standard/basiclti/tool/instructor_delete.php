2f0c0e05c9770fa7eda6a89eaadb91711f6d8777
atutor <info@atutor.ca>
2013-09-08 09:37:25 -0400

    replace mysql with queryDB()

--- mods/_standard/basiclti/tool/instructor_delete.php
+++ mods/_standard/basiclti/tool/instructor_delete.php
@@ -10,13 +10,11 @@ if ( !is_int($_SESSION['course_id']) || $_SESSION['course_id'] < 1 ) {
 
 $tool = intval($_REQUEST['id']);
 
-$sql = "SELECT title FROM ".TABLE_PREFIX."basiclti_tools WHERE id = ".$tool.
-       " AND course_id = ". $_SESSION['course_id'];
-$result = mysql_query($sql, $db) or die(mysql_error());
-$row = mysql_fetch_assoc($result);
+$sql = "SELECT title FROM %sbasiclti_tools WHERE id = %d AND course_id = %d";
+$row = queryDB($sql, array(TABLE_PREFIX, $tool, $_SESSION['course_id']), TRUE);
 
 if ( strlen($row["title"]) < 1) {
-        $msg->addFeedback('UNABLE_TO_FIND_TOOL');
+        $msg->addError('UNABLE_TO_FIND_TOOL');
         header('Location: ../index_instructor.php');
         exit;
 }
@@ -25,11 +23,12 @@ if (isset($_POST['submit_no'])) {
         $msg->addFeedback('CANCELLED');
         header('Location: ../index_instructor.php');
         exit;
-} else if (isset($_POST['step']) && ($_POST['step'] == 2) && isset($_POST['submit_yes'])) {
-	$sql = "DELETE FROM ".TABLE_PREFIX."basiclti_tools WHERE id = ".$tool.
-               " AND course_id = ". $_SESSION['course_id'];
-	$result = mysql_query($sql, $db) or die(mysql_error());
-        write_to_log(AT_ADMIN_LOG_DELETE, 'basiclti_delete', mysql_affected_rows($db), $sql);
+} else if (isset($_POST['step']) && ($_POST['step'] == 1) && isset($_POST['submit_yes'])) {
+
+	$sql = "DELETE FROM %sbasiclti_tools WHERE id = %d AND course_id = %d";
+	$result = queryDB($sql, array(TABLE_PREFIX, $tool, $_SESSION['course_id']));
+	    global $sqlout;
+        write_to_log(AT_ADMIN_LOG_DELETE, 'basiclti_delete', $result, $sqlout);
         $msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
         header('Location: ../index_instructor.php');
         exit;
@@ -42,11 +41,6 @@ if (!isset($_POST['step'])) {
         $hidden_vars['id'] = $tool;
         $msg->addConfirm(array('DELETE_TOOL_1', $row['title']), $hidden_vars);
         $msg->printConfirm();
-} else if ($_POST['step'] == 1) {
-        $hidden_vars['step']   = 2;
-        $hidden_vars['id'] = $tool;
-        $msg->addConfirm(array('DELETE_TOOL_2', $row['title']), $hidden_vars);
-        $msg->printConfirm();
-}
+} 
 
 require(AT_INCLUDE_PATH.'footer.inc.php'); 
