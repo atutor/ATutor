5d2dbbe115897efb51356067b6cb07dbf15d3882
Greg Gay <info@atutor.ca>
2013-09-10 15:45:46 -0400

    replace mysql with queryDB()

--- mods/_standard/blogs/module_news.php
+++ mods/_standard/blogs/module_news.php
@@ -23,20 +23,25 @@ function blogs_news() {
         return $news;
     } 
 
-    $sql = "SELECT G.group_id, G.title, G.modules, T.course_id FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types  T USING (type_id) WHERE T.course_id IN $enrolled_courses ORDER BY G.title";
-
-
-    $result = mysql_query($sql, $db);
-    if ($result){
-        if (mysql_num_rows($result) > 0) {
-            while ($row = mysql_fetch_assoc($result)) {
-                if (strpos($row['modules'], '_standard/blogs') !== FALSE) {
-                    // retrieve the last posted date/time from this blog
-                    $sql = "SELECT MAX(date) AS date FROM ".TABLE_PREFIX."blog_posts WHERE owner_type=".BLOGS_GROUP." AND owner_id={$row['group_id']}";
-                    $date_result = mysql_query($sql, $db);
-                    $row2 = mysql_fetch_assoc($date_result);					
-                    $last_updated = ' - ' . _AT('last_updated', AT_date(_AT('forum_date_format'), $row2['date'], AT_DATE_MYSQL_DATETIME));
+    $sql = "SELECT G.group_id, G.title, G.modules, T.course_id FROM %sgroups G INNER JOIN %sgroups_types  T USING (type_id) WHERE T.course_id IN %s ORDER BY G.title";
+    $rows_enrolled = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, $enrolled_courses));
+    if (count($rows_enrolled) > 0){
+        foreach($rows_enrolled as $row){
+            if (strpos($row['modules'], '_standard/blogs') !== FALSE) {
+                // check for group membership before showing news.
+                $sql = "SELECT member_id FROM %sgroups_members WHERE member_id=%d AND group_id= %d";
+                $row_group_member = queryDB($sql, array(TABLE_PREFIX, $_SESSION['member_id'], $row['group_id']), TRUE);
                 
+                // check for course instructor, show blog news if so
+                $sql = "SELECT member_id from %scourses WHERE member_id =%d";
+                $row_instructor = queryDB($sql, array(TABLE_PREFIX, $_SESSION['member_id']));
+                
+                if(count($row_group_member) > 0 || count($row_instructor ) > 0){                                
+                    // retrieve the last posted date/time from this blog
+                    $sql = "SELECT MAX(date) AS date FROM %sblog_posts WHERE owner_type=%d AND owner_id=%d";
+                    $row2 = queryDB($sql, array(TABLE_PREFIX, BLOGS_GROUP, $row['group_id']), TRUE);				
+                    $last_updated = ' - ' . _AT('last_updated', AT_date(_AT('forum_date_format'), $row2['date'], AT_DATE_MYSQL_DATETIME));
+            
                     $link_title = $row['title'];
                     $news[] = array('time'=>$row2['date'], 
                     'object'=>$row, 
@@ -46,7 +51,7 @@ function blogs_news() {
                     'link'=>'<a href="bounce.php?course='.$row['course_id'].SEP.'p='.urlencode('mods/_standard/blogs/view.php?ot='.BLOGS_GROUP.SEP.'oid='.$row['group_id']).'"'.
                       (strlen($link_title) > SUBLINK_TEXT_LEN ? ' title="'.AT_print($link_title, 'blog_posts.title').'"' : '') .'>'. 
                       AT_print(validate_length($link_title, SUBLINK_TEXT_LEN, VALIDATE_LENGTH_FOR_DISPLAY), 'blog_posts.title') .'</a>');
-                }
+               }
             }
         }
     }
