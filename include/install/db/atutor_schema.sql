6727452c72e760c7da1487c8d6bcfddb69440ac3
Greg Gay <info@atutor.ca>
2013-09-11 13:59:12 -0400

    add calendar sql to db schema

--- include/install/db/atutor_schema.sql
+++ include/install/db/atutor_schema.sql
@@ -1632,3 +1632,36 @@ INSERT INTO `config` (`name`,`value`) VALUES ('transformable_oauth_expire','9360
 ########
 # Set the default Home URL to atutorspaces.com
 INSERT INTO `config` (`name`,`value`) VALUES ('home_url','http://www.atutorspaces.com');
+
+########
+# sql  for calendar module
+
+CREATE TABLE `calendar_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256),
+  `start` datetime,
+  `end` datetime,
+  `allDay` varchar(20),
+  `userid` int(8),
+  PRIMARY KEY (`id`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_google_sync` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(256),
+  `userid` int(8),
+  `calids` text,
+  PRIMARY KEY (`id`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_bookmark` (
+  `memberid` int(11),
+  `ownerid` int(8),
+  `courseid` int(8),
+  `calname` varchar(256)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_notification` (
+  `memberid` int(11),
+  `status` int(8)
+) ENGINE = MyISAM;
