a24da7743fc2f358fc522ca952ed93318325086b
Greg Gay <info@atutor.ca>
2013-09-11 11:57:28 -0400

    replace mysql with queryDB()

--- mods/_standard/blogs/delete_comment.php
+++ mods/_standard/blogs/delete_comment.php
@@ -27,9 +27,10 @@ if (!($owner_status = blogs_authenticate($owner_type, $owner_id)) || !query_bit(
 $id = abs($_REQUEST['id']);
 $delete_id = abs($_REQUEST['delete_id']);
 
-$sql = "SELECT post_id FROM ".TABLE_PREFIX."blog_posts WHERE owner_type=$owner_type AND owner_id=$owner_id AND post_id=$id";
-$result = mysql_query($sql, $db);
-if (!$row = mysql_fetch_assoc($result)) {
+$sql = "SELECT post_id FROM %sblog_posts WHERE owner_type=%d AND owner_id=%d AND post_id=%d";
+$row_posts = queryDB($sql, array(TABLE_PREFIX, $owner_type, $owner_id, $id));
+
+if(count($row_posts) == 0){
 	$msg->addError('ACCESS_DENIED');
 	header('Location: index.php');
 	exit;
@@ -41,11 +42,11 @@ if (isset($_POST['submit_no'])) {
 	exit;
 } else if (isset($_POST['submit_yes'])) {
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."blog_posts_comments WHERE comment_id=$delete_id AND post_id=$id";
-	$result = mysql_query($sql, $db);
-	if (mysql_affected_rows($db) == 1) {
-		$sql = "UPDATE ".TABLE_PREFIX."blog_posts SET num_comments=num_comments-1, date=date WHERE owner_type=$owner_type AND owner_id=$owner_id AND post_id=$id";
-		$result = mysql_query($sql, $db);
+	$sql = "DELETE FROM %sblog_posts_comments WHERE comment_id=%d AND post_id=%d";
+	$result = queryDB($sql, array(TABLE_PREFIX, $delete_id, $id));	
+	if($result == 1){
+		$sql = "UPDATE %sblog_posts SET num_comments=num_comments-1, date=date WHERE owner_type=%d AND owner_id=%d AND post_id=%d";
+		$result = queryDB($sql, array(TABLE_PREFIX, $owner_type, $owner_id, $id));
 	}
 
 	$msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
@@ -57,9 +58,8 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 
 $hidden_vars = array('id' => $id, 'ot' => $owner_type, 'oid' => $owner_id, 'delete_id' => $delete_id);
 //get the comment to print into the confirm box
-$sql = 'SELECT comment FROM '.TABLE_PREFIX.'blog_posts_comments WHERE comment_id='.$delete_id;
-$result = mysql_query($sql, $db);
-$row = mysql_fetch_assoc($result);
+$sql = 'SELECT comment FROM %sblog_posts_comments WHERE comment_id=%d';
+$row = queryDB($sql, array(TABLE_PREFIX, $delete_id), TRUE);
 
 $msg->addConfirm(array('DELETE', AT_print($row['comment'], 'blog_posts_comments.comment')), $hidden_vars);
 $msg->printConfirm();
