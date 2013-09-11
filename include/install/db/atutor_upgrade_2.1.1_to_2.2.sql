217ae8d75bd5dd7adb1f4f30518e4f367679838b
Greg Gay <info@atutor.ca>
2013-09-11 13:58:30 -0400

    create v2.2 sql upgrade script

---
+++ include/install/db/atutor_upgrade_2.1.1_to_2.2.sql
@@ -0,0 +1,34 @@
+# DB Upgrade for ATutor 2.2
+
+########
+# sql  for calendar module
+
+CREATE TABLE `calendar_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256),
+  `start` datetime,
+  `end` datetime,
+  `allDay` varchar(20),
+  `userid` int(8),
+  PRIMARY KEY (`id`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_google_sync` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(256),
+  `userid` int(8),
+  `calids` text,
+  PRIMARY KEY (`id`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_bookmark` (
+  `memberid` int(11),
+  `ownerid` int(8),
+  `courseid` int(8),
+  `calname` varchar(256)
+) ENGINE = MyISAM;
+
+CREATE TABLE `calendar_notification` (
+  `memberid` int(11),
+  `status` int(8)
+) ENGINE = MyISAM;
