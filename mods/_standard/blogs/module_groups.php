9f08c3615f3899ea4b17d9c8d39241c71588616f
Greg Gay <info@atutor.ca>
2013-09-11 11:29:53 -0400

    replace mysql with queryDB()

--- mods/_standard/blogs/module_groups.php
+++ mods/_standard/blogs/module_groups.php
@@ -9,21 +9,19 @@ function blogs_create_group($group_id) {
 
 // delete group
 function blogs_delete_group($group_id) {
-	global $db;
-
 	// deleting a group involves deleting all the blog entries and comments for that group
 
 	// for each entry, delete the comments
-	$sql = "SELECT post_id FROM ".TABLE_PREFIX."blog_posts WHERE owner_type=".BLOGS_GROUP." AND owner_id=$group_id";
-	$result = mysql_query($sql, $db);
-
-	while ($row = mysql_fetch_assoc($result)) {
-		$sql = "DELETE FROM ".TABLE_PREFIX."blog_posts_comments WHERE post_id=$row[post_id]";
-		mysql_query($sql, $db);
+	$sql = "SELECT post_id FROM %sblog_posts WHERE owner_type=%d AND owner_id=%d";
+	$rows_posts = queryDB($sql, array(TABLE_PREFIX, BLOGS_GROUP, $group_id));
+	
+	foreach($rows_posts as $row){
+		$sql = "DELETE FROM %sblog_posts_comments WHERE post_id=%d";
+		queryDB($sql, array(TABLE_PREFIX, $row['post_id']));
 	}
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."blog_posts WHERE owner_type=".BLOG_GROUP." AND owner_id=$group_id";
-	mysql_query($sql, $db);
+	$sql = "DELETE FROM %sblog_posts WHERE owner_type=%d AND owner_id=%d";
+	queryDB($sql, array(TABLE_PREFIX, BLOG_GROUP, $group_id));
 }
 
 ?>
