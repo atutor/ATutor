26e26860c23a9c2d61065afcb912260fc76f412c
Greg Gay <info@atutor.ca>
2013-09-13 14:39:37 -0400

    replace mysql with queryDB()

--- mods/_standard/calendar/google_connect_disconnect.php
+++ mods/_standard/calendar/google_connect_disconnect.php
@@ -21,38 +21,40 @@
      * If the request is for dissociation then user's entry in the 
      * database is removed and session token is invalidated. 
      */
-              $_user_location	= 'public';
+    $_user_location	= 'public';
     require_once 'includes/classes/googlecalendar.class.php';
     define('AT_INCLUDE_PATH', '../../../include/');
     require(AT_INCLUDE_PATH.'vitals.inc.php');
 
     $gcalobj = new GoogleCalendar();
-    global $db;
     
     if (isset($_GET['logout'])) {
         //request is for dissociation
         //removed entry from database
-        $query = "DELETE FROM " . TABLE_PREFIX . "calendar_google_sync WHERE userid='".
-               $_SESSION['member_id'] . "'";
-        mysql_query($query, $db);
+
+        $query = "DELETE FROM %scalendar_google_sync WHERE userid=%d";
+        queryDB($query, array(TABLE_PREFIX, $_SESSION['member_id']));
+        
         //invalidate session token
         $gcalobj->logout();
     } else {
         //request is for association
         if (!isset($_GET['token'])) {
             //redirect to login page/consent page
-            $query = "DELETE FROM " . TABLE_PREFIX . "calendar_google_sync WHERE userid='".
-                   $_SESSION['member_id']."'";
-            mysql_query($query, $db);
+
+            $query = "DELETE FROM %scalendar_google_sync WHERE userid=%d";
+            queryDB($query, array(TABLE_PREFIX, $_SESSION['member_id']));
+            
             unset($_SESSION['sessionToken']);
             $authSubUrl = $gcalobj->getAuthSubUrl();
             header('Location:' . $authSubUrl);
         } else {
             //insert session token in the database for future use and close pop-up window
             $client = $gcalobj->getAuthSubHttpClient();
-            $query    = "INSERT INTO " . TABLE_PREFIX . "calendar_google_sync (token,userid,calids) VALUES ('".
-                      $_SESSION['sessionToken'] . "','" . $_SESSION['member_id'] . "','')";
-            mysql_query($query, $db);
+
+            $query    = "INSERT INTO %scalendar_google_sync (token,userid,calids) VALUES ('%s', %d,'')";
+            queryDB($query, array(TABLE_PREFIX, $_SESSION['sessionToken'],  $_SESSION['member_id']));
+            
             echo '<script>window.opener.location.reload(true);window.close();</script>';
         }
     }
