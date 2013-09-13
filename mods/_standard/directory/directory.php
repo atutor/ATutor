a0d8521393872393f9947473816cf8268b52c750
atutor <info@atutor.ca>
2013-09-13 19:24:11 -0400

    replace mysql with queryDB()

--- mods/_standard/directory/directory.php
+++ mods/_standard/directory/directory.php
@@ -42,8 +42,10 @@ if (isset($_GET['online_status']) && ($_GET['online_status'] != '')) {
 $group = abs($_GET['group']);
 
 $sql_groups = implode(',', $_SESSION['groups']);
-$sql = "SELECT G.title, G.group_id, T.title AS type_title FROM ".TABLE_PREFIX."groups G INNER JOIN ".TABLE_PREFIX."groups_types T USING (type_id) WHERE T.course_id=$_SESSION[course_id] AND G.group_id IN ($sql_groups) ORDER BY T.title";
-$result_groups = mysql_query($sql, $db);
+if($sql_groups != 0){
+    $sql = "SELECT G.title, G.group_id, T.title AS type_title FROM %sgroups G INNER JOIN %sgroups_types T USING (type_id) WHERE T.course_id=%d AND G.group_id IN (%s) ORDER BY T.title";
+    $rows_groups = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, $_SESSION['course_id'], $sql_groups));
+}
 
 if ($_GET['order'] == 'asc') {
 	$order = 'desc';
@@ -54,28 +56,29 @@ if ($_GET['order'] == 'asc') {
 $group_members = '';
 if ($group) {
 	$group_members = array();
-	$sql = "SELECT member_id FROM ".TABLE_PREFIX."groups_members WHERE group_id=$group";
-	$result = mysql_query($sql, $db);
-	while ($row = mysql_fetch_assoc($result)) {
+
+	$sql = "SELECT member_id FROM %sgroups_members WHERE group_id=%d";
+	$rows_groups_members = queryDB($sql, array(TABLE_PREFIX, $group));
+	
+	foreach($rows_groups_members as $row){
 		$group_members[] = $row['member_id'];
 	}
 	$group_members = ' AND C.member_id IN (' . implode(',', $group_members) . ')';
 }
 
 /* look through enrolled students list */
-$sql_members = "SELECT C.member_id, C.approved, C.privileges, M.login, M.first_name, M.second_name, M.last_name FROM ".TABLE_PREFIX."course_enrollment C, ".TABLE_PREFIX."members M	WHERE C.course_id=$_SESSION[course_id] AND C.member_id=M.member_id AND (C.approved='y' OR C.approved='a')	$group_members ORDER BY M.login $order";
+$sql_members = "SELECT C.member_id, C.approved, C.privileges, M.login, M.first_name, M.second_name, M.last_name FROM %scourse_enrollment C, %smembers M	WHERE C.course_id=%d AND C.member_id=M.member_id AND (C.approved='y' OR C.approved='a')	$group_members ORDER BY M.login $order";
+$rows_members = queryDB($sql_members, array(TABLE_PREFIX, TABLE_PREFIX, $_SESSION['course_id']));
 
-$result_members = mysql_query($sql_members, $db);
-
-while ($row_members = mysql_fetch_assoc($result_members)) {
+foreach($rows_members as $row_members){
 	$all_[$row_members['member_id']] = $row_members;
 	$all_[$row_members['member_id']]['online'] = FALSE;
 }
 
-$sql_online = "SELECT member_id FROM ".TABLE_PREFIX."users_online WHERE course_id = $_SESSION[course_id] AND expiry>".time();
-$result_online = mysql_query($sql_online, $db);
+$sql_online = "SELECT member_id FROM %susers_online WHERE course_id = %d AND expiry>".time();
+$rows_online = queryDB($sql_online, array(TABLE_PREFIX, $_SESSION['course_id']));
 
-while ($row_online = mysql_fetch_assoc($result_online)) {
+foreach($rows_online as $row_online){
 	if ($all_[$row_online['member_id']] != '') {
 		$all_[$row_online['member_id']]['online'] = TRUE;
 		$online[$row_online['member_id']] = $all_[$row_online['member_id']];
@@ -96,7 +99,8 @@ if ($all) {
 
 require(AT_INCLUDE_PATH.'header.inc.php');
 
-$savant->assign('result_groups', $result_groups);
+$savant->assign('rows_groups', $rows_groups);
+$savant->assign('group', $group);
 $savant->assign('final', $final);
 $savant->assign('base_href', $_base_href);
 $savant->assign('on', $on);
