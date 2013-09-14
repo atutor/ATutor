054e2a4ea0ce1a9b2d6c199313039fc7a6b5f411
atutor <info@atutor.ca>
2013-09-14 09:57:18 -0400

    replace mysql with queryDB()

--- mods/_standard/faq/delete_question.php
+++ mods/_standard/faq/delete_question.php
@@ -25,11 +25,14 @@ if (isset($_POST['submit_no'])) {
 	$_POST['topic_id'] = intval($_POST['topic_id']);
 
 	// check that this topic_id belongs to this course:
-	$sql    = "SELECT topic_id FROM ".TABLE_PREFIX."faq_topics WHERE topic_id=$_POST[topic_id] AND course_id=$_SESSION[course_id]";
-	$result = mysql_query($sql, $db);
-	if ($row = mysql_fetch_assoc($result)) {
-		$sql = "DELETE FROM ".TABLE_PREFIX."faq_entries WHERE entry_id=$_POST[id] AND topic_id=$_POST[topic_id]";
-		$result = mysql_query($sql, $db);
+
+	$sql    = "SELECT topic_id FROM %sfaq_topics WHERE topic_id=%d AND course_id=%d";
+	$row_topics = queryDB($sql, array(TABLE_PREFIX, $_POST['topic_id'], $_SESSION['course_id']), TRUE);
+	
+	if(count($row_topics) > 0){
+
+		$sql = "DELETE FROM %sfaq_entries WHERE entry_id=%d AND topic_id=%d";
+		$result = queryDB($sql, array(TABLE_PREFIX, $_POST['id'], $_POST['topic_id']));
 	}
 
 	$msg->addFeedback('QUESTION_DELETED');
@@ -42,10 +45,11 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 
 $_GET['id'] = intval($_GET['id']); 
 
-$sql = "SELECT question, topic_id FROM ".TABLE_PREFIX."faq_entries WHERE entry_id=$_GET[id]";
+$sql = "SELECT question, topic_id FROM %sfaq_entries WHERE entry_id=%d";
+$row = queryDB($sql, array(TABLE_PREFIX, $_GET['id']), TRUE);
 
-$result = mysql_query($sql,$db);
-if ($row = mysql_fetch_assoc($result)) {
+if(count($row) > 0){
+
 	$hidden_vars['topic_id'] = $row['topic_id'];
 	$hidden_vars['id'] = $_GET['id'];
 
