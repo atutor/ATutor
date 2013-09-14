5c6e0ae1ce8e046cb98a2fd9470646e73e111e90
atutor <info@atutor.ca>
2013-09-14 10:51:28 -0400

    replace mysql with queryDB()

--- mods/_standard/faq/index.php
+++ mods/_standard/faq/index.php
@@ -16,34 +16,36 @@ require (AT_INCLUDE_PATH.'vitals.inc.php');
 require (AT_INCLUDE_PATH.'header.inc.php');
 
 $counter = 1;
-$sql	 = "SELECT name, topic_id FROM ".TABLE_PREFIX."faq_topics WHERE course_id=$_SESSION[course_id] ORDER BY name";
-$result  = mysql_query($sql, $db);
+
+$sql	 = "SELECT name, topic_id FROM %sfaq_topics WHERE course_id=%d ORDER BY name";
+$rows_topics  = queryDB($sql, array(TABLE_PREFIX, $_SESSION['course_id']));
 ?>
 
-<?php if ($row = mysql_fetch_assoc($result)) : ?>
+<?php 
+if(count($rows_topics) > 0):  ?>
 	<ul style="list-style: none;">
-		<?php do { ?>
+		<?php 
+		foreach($rows_topics as $row){ ?>
 			<li style="font-weight: bold; margin-bottom: 10px;">
 				<?php echo AT_print($row['name'], 'faqs.topic'); ?>
 				<?php 
-					$entry_sql = "SELECT * FROM ".TABLE_PREFIX."faq_entries WHERE topic_id=$row[topic_id] ORDER BY question";
-					$entry_result = mysql_query($entry_sql, $db);
-				if ($entry_row = mysql_fetch_assoc($entry_result)) {?>
+					$entry_sql = "SELECT * FROM %sfaq_entries WHERE topic_id=%d ORDER BY question";
+					$rows_entries = queryDB($entry_sql, array(TABLE_PREFIX, $row['topic_id']));
+                if($rows_entries > 0){ ?>
 				<ol start="<?php echo $counter; ?>">
-
-					<?php do { ?>
+					<?php  foreach($rows_entries as $entry_row){ ?>
 						<li style="font-weight: normal">
 							<h3><?php echo AT_print($entry_row['question'], 'faqs.question'); ?></h3>
 							<p><?php echo AT_print($entry_row['answer'], 'faqs.answer');?></p>
 						</li>
 						<?php $counter++; ?>
-					<?php } while ($entry_row = mysql_fetch_assoc($entry_result)) ?>
+					<?php }  ?>
 				</ol>
 				<?php } else { ?>
 					<p style="padding-left: 20px; padding-top:3px; font-weight:normal;"><?php echo _AT('no_questions');  ?></p>
 				<?php } ?>
 			</li>
-		<?php } while($row = mysql_fetch_assoc($result)); ?>
+		<?php }  ?>
 	</ul>
 <?php else: ?>
 	<?php echo _AT('none_found'); ?>
