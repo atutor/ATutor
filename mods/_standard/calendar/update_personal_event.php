f7f136e475f10029eaa3ef612f5ccf84a1b13e55
Greg Gay <info@atutor.ca>
2013-09-16 13:09:25 -0400

    replace mysql with queryDB()

--- mods/_standard/calendar/update_personal_event.php
+++ mods/_standard/calendar/update_personal_event.php
@@ -17,9 +17,19 @@
      */
     
     //Retrieve all the parameters from request
-             $_user_location = 'public';
+    $_user_location = 'public';
+
     define('AT_INCLUDE_PATH', '../../../include/');
     require (AT_INCLUDE_PATH.'vitals.inc.php');
+        
+    if (!$_SESSION['valid_user']) {
+        require(AT_INCLUDE_PATH.'header.inc.php');
+        $info = array('INVALID_USER', $_SESSION['course_id']);
+        $msg->printInfos($info);
+        require(AT_INCLUDE_PATH.'footer.inc.php');
+        exit;
+    }
+    
     $iddd      = $_GET["id"];
     $newstartd = $_GET["start"];
     $newend    = $_GET["end"];
@@ -27,34 +37,29 @@
     $alld      = $_GET["allday"];
     $command   = $_GET["cmd"];
     
-    //Connect to database
-    global $db;
     //Use SQL Query according to the situation
     if( strcmp($command,"drag") == 0 ) {
         //Event is dragged, so update dates
-        $query = "UPDATE `".TABLE_PREFIX."calendar_events` SET start = '".$newstartd.
-        "', end = '".$newend."' WHERE id=".$iddd;
-        mysql_query( $query, $db );
+        $query = "UPDATE `%scalendar_events` SET start = '%s', end = '%s' WHERE id=%d";
+        queryDB( $query, array(TABLE_PREFIX, $newstartd, $newend, $iddd));
     }
     else if( strcmp($command,"create") == 0 ) {
         //New event is created, first insert the record in the table and then return id to javascript
-        $query = "INSERT INTO `".TABLE_PREFIX."calendar_events` (title,start,end,allDay,userid) values".
-        " ('".$newtitle."','".$newstartd."','".$newend."','".$alld."','".$_SESSION['member_id']."')" ;
-        mysql_query( $query, $db );
-        $query = "SELECT MAX(id) from `".TABLE_PREFIX."calendar_events`";
-        $resultno = mysql_query( $query, $db );
-        $idno=mysql_fetch_row($resultno);  
-        echo $idno[0];    
+        $query = "INSERT INTO `%scalendar_events` (title,start,end,allDay,userid) values ('%s','%s','%s',%d,%d)" ;
+        queryDB( $query, array(TABLE_PREFIX, $newtitle, $newstartd, $newend, $alld, $_SESSION['member_id']));
+        
+        $query = "SELECT MAX(id) from `%scalendar_events`";
+        $idno = queryDB($query, array(TABLE_PREFIX), TRUE); 
+        echo $idno['id'];    
     }
     else if( strcmp($command,"delete") == 0 ) {
         //Delete the event
-        $query = "DELETE FROM `".TABLE_PREFIX."calendar_events` WHERE id=".$iddd;
-        mysql_query( $query, $db );
+        $query = "DELETE FROM `%scalendar_events` WHERE id=%d";
+        queryDB($query, array(TABLE_PREFIX, $iddd));
     }
     else if( strcmp($command,"update") == 0 ) {
         //User clicked on the event. The dates and title may be changed. So update the database record.
-        $query = "UPDATE `".TABLE_PREFIX."calendar_events` SET title = '".$newtitle."' , start = '".
-        $newstartd."', end = '".$newend."' WHERE id=".$iddd;
-        mysql_query( $query, $db );
+        $query = "UPDATE `%scalendar_events` SET title = '%s' , start = '%s', end = '%s' WHERE id=%d";
+        queryDB( $query, array(TABLE_PREFIX, $newtitle, $newstartd, $newend, $iddd) );
     }
 ?>
