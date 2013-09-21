8eb73a7c75cd788fc181a7de80c7669c5c4bd65b
atutor <info@atutor.ca>
2013-09-21 14:14:04 -0400

    replace mysql with queryDB()

--- mods/_standard/forums/module_groups.php
+++ mods/_standard/forums/module_groups.php
@@ -2,30 +2,29 @@
 
 // create group
 function forums_create_group($group_id) {
-	global $db;
 
-	$sql	= "INSERT INTO ".TABLE_PREFIX."forums VALUES (NULL,'', '', 0, 0, NOW(), 0)";
-	$result = mysql_query($sql,$db);
+	$sql	= "INSERT INTO %sforums VALUES (NULL,'', '', 0, 0, NOW(), 0)";
+	$result = queryDB($sql, array(TABLE_PREFIX));
 
-	$sql	= "INSERT INTO ".TABLE_PREFIX."forums_groups VALUES (LAST_INSERT_ID(),  $group_id)";
-	$result = mysql_query($sql,$db);
+	$sql	= "INSERT INTO %sforums_groups VALUES (LAST_INSERT_ID(), %d)";
+	$result = queryDB($sql,array(TABLE_PREFIX, $group_id));
 }
 
 
 // delete group
 function forums_delete_group($group_id) {
-	global $db;
 
 	require_once(AT_INCLUDE_PATH.'../mods/_standard/forums/lib/forums.inc.php');
-	
-	$sql = "SELECT forum_id FROM ".TABLE_PREFIX."forums_groups WHERE group_id=$group_id";
-	$result = mysql_query($sql, $db);
-	while ($row = mysql_fetch_assoc($result)) {
+
+	$sql = "SELECT forum_id FROM %sforums_groups WHERE group_id=%d";
+	$rows_gforums = queryDB($sql, array(TABLE_PREFIX, $group_id));
+		
+	foreach($rows_gforums as $row){
 		delete_forum($row['forum_id']);
 	}
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."forums_groups WHERE group_id=$group_id";
-	$result = mysql_query($sql, $db);
+	$sql = "DELETE FROM %sforums_groups WHERE group_id=%d";
+	$result = queryDB($sql, array(TABLE_PREFIX, $group_id));
 }
 
 ?>
