f9ebaa2bf467a36704b780fecfd57c81fbe3ff5c
atutor <info@atutor.ca>
2013-09-27 19:58:25 -0400

    create course home admin toolbar

--- mods/_standard/announcements/module.php
+++ mods/_standard/announcements/module.php
@@ -3,7 +3,8 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 if (!isset($this) || (isset($this) && (strtolower(get_class($this)) != 'module'))) { exit(__FILE__ . ' is not a Module'); }
 
 define('AT_PRIV_ANNOUNCEMENTS', $this->getPrivilege());
-
+//debug(AT_PRIV_ANNOUNCEMENTS);
+//exit;
 $this->_pages['mods/_standard/announcements/index.php']['title_var'] = 'announcements';
 $this->_pages['mods/_standard/announcements/index.php']['guide']     = 'instructor/?p=announcements.php';
 $this->_pages['mods/_standard/announcements/index.php']['parent']    = 'tools/index.php';
@@ -17,5 +18,10 @@ $this->_pages['mods/_standard/announcements/index.php']['children']  = array('mo
 
 	$this->_pages['mods/_standard/announcements/delete_news.php']['title_var']  = 'delete_announcement';
 	$this->_pages['mods/_standard/announcements/delete_news.php']['parent'] = 'mods/_standard/announcements/index.php';
-
+	
+if($_SESSION['is_admin'] > 0 || authenticate(AT_PRIV_ANNOUNCEMENTS, TRUE) && $_SESSION['hide_admin'] == 0){	
+	$this->_pages['mods/_standard/announceents/add_news.php']['title_var']  = 'add_announcement';
+	$this->_pages['mods/_standard/announcements/add_news.php']['parent'] = 'index.php';
+	$this->_pages['index.php']['children']  = array('mods/_standard/announcements/add_news.php');
+}
 ?>
