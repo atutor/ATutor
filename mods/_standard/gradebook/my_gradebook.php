31decdc97860b20ec27c5681f1ad61eac7da2926
atutor <info@atutor.ca>
2013-09-29 12:09:15 -0400

    replace mysql with queryDB()

--- mods/_standard/gradebook/my_gradebook.php
+++ mods/_standard/gradebook/my_gradebook.php
@@ -41,25 +41,25 @@ require(AT_INCLUDE_PATH.'header.inc.php');
 <?php
 
 $sql = "(SELECT g.gradebook_test_id, g.id, type, t.title, t.end_date due_date, grade_scale_id, t.result_release ".
-				" FROM ".TABLE_PREFIX."gradebook_tests g, ".TABLE_PREFIX."tests t".
+				" FROM %sgradebook_tests g, %stests t".
 				" WHERE g.type='ATutor Test'".
 				" AND g.id = t.test_id".
-				" AND t.course_id=".$_SESSION["course_id"].
+				" AND t.course_id=%d".
 				" ORDER BY t.title) ".
 				" UNION (SELECT g.gradebook_test_id, g.id, g.type, a.title, a.date_due due_date, grade_scale_id, '' result_release ".
-				" FROM ".TABLE_PREFIX."gradebook_tests g, ".TABLE_PREFIX."assignments a".
+				" FROM %sgradebook_tests g, %sassignments a".
 				" WHERE g.type='ATutor Assignment'".
 				" AND g.id = a.assignment_id".
-				" AND a.course_id=".$_SESSION["course_id"].
+				" AND a.course_id=%d".
 				" ORDER BY title)".
 				" UNION (SELECT gradebook_test_id, id, type, title, due_date, grade_scale_id, '' result_release ".
-				" FROM ".TABLE_PREFIX."gradebook_tests".
-				" WHERE course_id=".$_SESSION["course_id"].
+				" FROM %sgradebook_tests".
+				" WHERE course_id=%d".
 				" ORDER BY title)";
-$result = mysql_query($sql, $db) or die(mysql_error());
+$rows_tests = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, $_SESSION["course_id"], TABLE_PREFIX, TABLE_PREFIX, $_SESSION["course_id"],TABLE_PREFIX,$_SESSION["course_id"]));
 
-if (mysql_num_rows($result) == 0)
-{
+if(count($rows_tests) == 0){
+
 ?>
 	<tr>
 		<td colspan="6"><?php echo _AT('none_found'); ?></td>
@@ -68,14 +68,14 @@ if (mysql_num_rows($result) == 0)
 }
 else
 {
-	while ($row = mysql_fetch_assoc($result))
-	{
-		$sql_grade = "SELECT grade FROM ".TABLE_PREFIX."gradebook_detail WHERE gradebook_test_id=".$row["gradebook_test_id"]." AND member_id=".$_SESSION["member_id"];
-		$result_grade = mysql_query($sql_grade, $db) or die(mysql_error());
-		$row_grade = mysql_fetch_assoc($result_grade);
-		
-		if (mysql_num_rows($result_grade) == 0)
+    foreach($rows_tests as $row){
+
+		$sql_grade = "SELECT grade FROM %sgradebook_detail WHERE gradebook_test_id=%d AND member_id=%d";
+		$row_grade = queryDB($sql_grade, array(TABLE_PREFIX, $row["gradebook_test_id"], $_SESSION["member_id"]), TRUE);
+        
+        if(count($row_grade) == 0){
 			$grade = "";
+		}
 		else
 		{
 			$grade = $row_grade["grade"];
@@ -86,9 +86,8 @@ else
 			// get "completed date" and "time spent"
 			if ($grade <> "")
 			{
-				$sql_tr = "SELECT R.result_id, R.date_taken, (UNIX_TIMESTAMP(R.end_time) - UNIX_TIMESTAMP(R.date_taken)) AS diff FROM ".TABLE_PREFIX."tests_results R WHERE R.status=1 AND R.test_id=".$row["id"]." AND R.member_id='".$_SESSION[member_id]."'";
-				$result_tr = mysql_query($sql_tr, $db) or die(mysql_error());
-				$row_tr = mysql_fetch_assoc($result_tr);
+				$sql_tr = "SELECT R.result_id, R.date_taken, (UNIX_TIMESTAMP(R.end_time) - UNIX_TIMESTAMP(R.date_taken)) AS diff FROM %stests_results R WHERE R.status=1 AND R.test_id=%d AND R.member_id=%d";
+				$row_tr = queryDB($sql_tr, array(TABLE_PREFIX, $row["id"], $_SESSION['member_id']), TRUE);
 			}
 ?>
 		<tr>
