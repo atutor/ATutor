8a943583d4cc85e0329850509633acc366f1f624
atutor <info@atutor.ca>
2013-09-29 18:11:34 -0400

    replace mysql with queryDB()

--- mods/_standard/patcher/patch_delete.php
+++ mods/_standard/patcher/patch_delete.php
@@ -24,39 +24,38 @@ if (isset($_POST['submit_no'])) {
 	/* delete has been confirmed, delete this category */
 	$myown_patch_id	= intval($_POST['myown_patch_id']);
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."myown_patches WHERE myown_patch_id=$myown_patch_id";
-	$result = mysql_query($sql, $db) or die(mysql_error());
+	$sql = "DELETE FROM %smyown_patches WHERE myown_patch_id=%d";
+	$result = queryDB($sql,array(TABLE_PREFIX, $myown_patch_id));
+    global $sqlout;
+	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches', $result, $sqlout);
 
-	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches', mysql_affected_rows($db), $sql);
+	$sql = "DELETE FROM %smyown_patches_dependent WHERE myown_patch_id=%d";
+	$result = queryDB($sql,array(TABLE_PREFIX, $myown_patch_id));
+    global $sqlout;
+	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches_dependent', $result, $sqlout);
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."myown_patches_dependent WHERE myown_patch_id=$myown_patch_id";
-	$result = mysql_query($sql, $db) or die(mysql_error());
-
-	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches_dependent', mysql_affected_rows($db), $sql);
-
-	$sql = "DELETE FROM ".TABLE_PREFIX."myown_patches_files WHERE myown_patch_id=$myown_patch_id";
-	$result = mysql_query($sql, $db) or die(mysql_error());
-
-	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches_files', mysql_affected_rows($db), $sql);
-
+	$sql = "DELETE FROM %smyown_patches_files WHERE myown_patch_id=%d";
+	$result = queryDB($sql,array(TABLE_PREFIX, $myown_patch_id));
+    global $sqlout;
+	write_to_log(AT_ADMIN_LOG_DELETE, 'myown_patches_files', $result, $sqlout);
 	$msg->addFeedback('ACTION_COMPLETED_SUCCESSFULLY');
+		
+	
 	header('Location: myown_patches.php');
 	exit;
 }
 
-//require('../../include/header.inc.php');
 require(AT_INCLUDE_PATH.'header.inc.php');
 
 $_GET['myown_patch_id'] = intval($_GET['myown_patch_id']); 
 
-$sql = "SELECT myown_patch_id, atutor_patch_id FROM ".TABLE_PREFIX."myown_patches m WHERE m.myown_patch_id=$_GET[myown_patch_id]";
-$result = mysql_query($sql,$db) or die(mysql_error());
+$sql = "SELECT myown_patch_id, atutor_patch_id FROM %smyown_patches m WHERE m.myown_patch_id=%d";
+$row = queryDB($sql, array(TABLE_PREFIX, $_GET['myown_patch_id']), TRUE);
 
-if (mysql_num_rows($result) == 0) {
+if(count($row) == 0){
+
 	$msg->printErrors('ITEM_NOT_FOUND');
 } else {
-	$row = mysql_fetch_assoc($result);
-	
 	$hidden_vars['atutor_patch_id']= $row['atutor_patch_id'];
 	$hidden_vars['myown_patch_id']	= $row['myown_patch_id'];
 
