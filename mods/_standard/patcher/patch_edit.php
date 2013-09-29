badcfc7bf47a67ec45f97c8ac5290b74cc63a3e3
atutor <info@atutor.ca>
2013-09-29 15:03:00 -0400

    replace mysql with queryDB()

--- mods/_standard/patcher/patch_edit.php
+++ mods/_standard/patcher/patch_edit.php
@@ -27,15 +27,15 @@ $myown_patch_id = $_REQUEST["myown_patch_id"];
 // URL called by form action
 $url = dirname($_SERVER['PHP_SELF']) . "/patch_creator.php?myown_patch_id=" . $myown_patch_id;
 
-$sql_patches = "SELECT * from ".TABLE_PREFIX."myown_patches m where myown_patch_id=". $myown_patch_id;
-$result_patches = mysql_query($sql_patches, $db) or die(mysql_error());
-$row_patches = mysql_fetch_assoc($result_patches);
+$sql_patches = "SELECT * from %smyown_patches m where myown_patch_id=%d";
+$row_patches = queryDB($sql_patches, array(TABLE_PREFIX, $myown_patch_id), TRUE);
 
-$sql_patch_dependent = "SELECT * from ".TABLE_PREFIX."myown_patches_dependent m where myown_patch_id=". $myown_patch_id;
-$result_patch_dependent = mysql_query($sql_patch_dependent, $db) or die(mysql_error());
+$sql_patch_dependent = "SELECT * from %smyown_patches_dependent m where myown_patch_id=%d";
+$rows_patch_dependent = queryDB($sql_patch_dependent, array(TABLE_PREFIX, $myown_patch_id));
 
-$sql_patch_files = "SELECT * from ".TABLE_PREFIX."myown_patches_files m where myown_patch_id=". $myown_patch_id . " order by myown_patches_files_id";
-$result_patch_files = mysql_query($sql_patch_files, $db) or die(mysql_error());
+$sql_patch_files = "SELECT * from %smyown_patches_files m where myown_patch_id=%d order by myown_patches_files_id";
+$rows_patch_files = queryDB($sql_patch_files, array(TABLE_PREFIX, $myown_patch_id));
+
 
 require ('patch_edit_interface.tmpl.php');
 
