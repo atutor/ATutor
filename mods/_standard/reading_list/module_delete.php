b5fa6b86559fea485be317c7659dfd901a5a1376
atutor <info@atutor.ca>
2013-10-09 18:17:49 -0400

    replace mysql with queryDB()

--- mods/_standard/reading_list/module_delete.php
+++ mods/_standard/reading_list/module_delete.php
@@ -1,13 +1,11 @@
 <?php
 
-function reading_list_delete($course) {
-	global $db;
+function reading_list_delete($course) {	
+	$sql = "DELETE FROM %sreading_list WHERE course_id=%d";
+	queryDB($sql, array(TABLE_PREFIX, $course));
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."reading_list WHERE course_id=$course";
-	mysql_query($sql, $db);
-
-	$sql = "DELETE FROM ".TABLE_PREFIX."external_resources WHERE course_id=$course";
-	mysql_query($sql, $db);
+	$sql = "DELETE FROM %sexternal_resources WHERE course_id=%d";
+	queryDB($sql, array(TABLE_PREFIX, $course));
 }
 
 ?>
