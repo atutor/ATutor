5a08cc861c6dea97e914df2aa681660b7f00ef2d
atutor <info@atutor.ca>
2013-10-21 16:38:45 -0400

    replace mysql with queryDB()

--- mods/_standard/statistics/module_delete.php
+++ mods/_standard/statistics/module_delete.php
@@ -1,11 +1,9 @@
 <?php
 
 function statistics_delete($course) {
-	global $db;
 
-	$sql = "DELETE FROM ".TABLE_PREFIX."course_stats WHERE course_id=$course";
-	$result = mysql_query($sql, $db);
-
+	$sql = "DELETE FROM %scourse_stats WHERE course_id=%d";
+	$result = queryDB($sql, array(TABLE_PREFIX, $course));
 }
 
 ?>
