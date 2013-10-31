be39294ae9e024b2dbb5d45376a0de4db0c1a51a
atutor <info@atutor.ca>
2013-10-30 20:21:34 -0400

    replace mysql with queryDB()

--- mods/_standard/tests/view_results.php
+++ mods/_standard/tests/view_results.php
@@ -41,9 +41,9 @@ $tid = intval($_GET['tid']);
 $rid = intval($_GET['rid']);
 if (isset($_REQUEST['cid'])) $cid = $_REQUEST['cid'];
 
-$sql    = "SELECT title, random, passfeedback, failfeedback, passscore, passpercent FROM ".TABLE_PREFIX."tests WHERE test_id=$tid AND course_id=$_SESSION[course_id]";
-$result    = mysql_query($sql, $db);
-$row    = mysql_fetch_array($result);
+$sql    = "SELECT title, random, passfeedback, failfeedback, passscore, passpercent FROM %stests WHERE test_id=%d AND course_id=%d";
+$row    = queryDB($sql, array(TABLE_PREFIX, $tid, $_SESSION['course_id']), TRUE);
+
 $test_title    = $row['title'];
 $passfeedback    = $row['passfeedback'];
 $failfeedback    = $row['failfeedback'];
@@ -54,9 +54,10 @@ $is_random  = $row['random'];
 $mark_right = ' <img src="'.$_base_path.'images/checkmark.gif" alt="'._AT('correct_answer').'" title="'._AT('correct_answer').'" />';
 $mark_wrong = ' <img src="'.$_base_path.'images/x.gif" alt="'._AT('wrong_answer').'" title="'._AT('wrong_answer').'" />';
 
-$sql    = "SELECT * FROM ".TABLE_PREFIX."tests_results WHERE result_id=$rid";
-$result    = mysql_query($sql, $db); 
-if (!$row = mysql_fetch_assoc($result)){
+$sql    = "SELECT * FROM %stests_results WHERE result_id=%d";
+$row    = queryDB($sql, array(TABLE_PREFIX, $rid), TRUE); 
+
+if (count($row) == 0){
     $msg->printErrors('ITEM_NOT_FOUND');
     require(AT_INCLUDE_PATH.'footer.inc.php');
     exit;
@@ -64,9 +65,8 @@ if (!$row = mysql_fetch_assoc($result)){
 $final_score= $row['final_score'];
 
 //make sure they're allowed to see results now
-$sql    = "SELECT result_release, out_of FROM ".TABLE_PREFIX."tests WHERE test_id=$tid AND course_id=$_SESSION[course_id]";
-$result    = mysql_query($sql, $db); 
-$row = mysql_fetch_assoc($result);
+$sql    = "SELECT result_release, out_of FROM %stests WHERE test_id=%d AND course_id=%d";
+$row    = queryDB($sql, array(TABLE_PREFIX, $tid, $_SESSION['course_id']), TRUE); 
 
 if ( ($row['result_release']==AT_RELEASE_NEVER) || ($row['result_release']==AT_RELEASE_MARKED && $final_score=='') ) {
     $msg->printErrors('RESULTS_NOT_RELEASED');
@@ -77,43 +77,39 @@ if ( ($row['result_release']==AT_RELEASE_NEVER) || ($row['result_release']==AT_R
 $out_of = $row['out_of'];
 
 /* Retrieve randomly choosed questions */
-$sql    = "SELECT question_id FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid";
-$result    = mysql_query($sql, $db); 
-$row = mysql_fetch_array($result);
-$random_id_string = $row[question_id];
-$row = mysql_fetch_array($result);    
-while ($row['question_id'] != '') {
-    $random_id_string = $random_id_string.','.$row['question_id'];
-    $row = mysql_fetch_array($result);
+$sql    = "SELECT question_id FROM %stests_answers WHERE result_id=%d";
+$rows_answers = queryDB($sql, array(TABLE_PREFIX, $rid)); 
+
+$count = 0;
+foreach($rows_answers as $row){
+    $random_id_string .= $rows_answers[$count]['question_id'].",";
+    $count++;
 }
+
+$random_id_string = rtrim($random_id_string, ",");
+
 if (!$random_id_string) {
     $random_id_string = 0;
 }
-
 if ($is_random) {
-    $sql    = "SELECT TQ.*, TQA.* FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string) ORDER BY TQ.question_id";
+    $sql    = "SELECT TQ.*, TQA.* FROM %stests_questions TQ INNER JOIN %stests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=%d AND TQ.question_id IN (%s) ORDER BY TQ.question_id";
 } else {
-    $sql    = "SELECT TQ.*, TQA.* FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string) ORDER BY TQA.ordering, TQ.question_id";
+    $sql    = "SELECT TQ.*, TQA.* FROM %stests_questions TQ INNER JOIN %stests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=%d AND TQ.question_id IN (%s) ORDER BY TQA.ordering, TQ.question_id";
 }
-$result    = mysql_query($sql, $db); 
 
-if (mysql_num_rows($result) == 0) {
+$rows_questions  = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, $tid, $random_id_string));
+
+if(count($rows_questions) == 0){
     echo '<p>'._AT('no_questions').'</p>';
     require(AT_INCLUDE_PATH.'footer.inc.php');
     exit;
 }
 
-// calculate test/my total score to display pass/fail feedback
-$sql_test_total = "SELECT sum(TQA.weight) test_total_score FROM ".TABLE_PREFIX."tests_questions TQ INNER JOIN ".TABLE_PREFIX."tests_questions_assoc TQA USING (question_id) WHERE TQA.test_id=$tid AND TQ.question_id IN ($random_id_string)";
-$result_test_total    = mysql_query($sql_test_total, $db);
-$row_test_total = mysql_fetch_array($result_test_total);
-$test_total_score = $row_test_total["test_total_score"];
+foreach($rows_questions as $row){
 
-while ($row = mysql_fetch_assoc($result)) {
-    $sql_this_score = "SELECT * FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid AND question_id=$row[question_id] AND member_id=$_SESSION[member_id]";
-    $result_this_score    = mysql_query($sql_this_score, $db); 
-    $this_score = mysql_fetch_assoc($result_this_score);
-
+    $sql_this_score = "SELECT * FROM %stests_answers WHERE result_id=%d AND question_id=%d AND member_id=%d";
+    $this_score    = queryDB($sql_this_score, array(TABLE_PREFIX, $rid, $row['question_id'], $_SESSION['member_id']), TRUE); 
+    
     $my_score+=$this_score['score'];
     $this_total += $row['weight'];
 }
@@ -140,10 +136,10 @@ while ($row = mysql_fetch_assoc($result)) {
                 // display pass feedback for passed students
                 elseif (($passscore<>0 && $my_score>=$passscore) ||
                     ($passpercent<>0 && ($my_score/$this_total*100)>=$passpercent))
-                    echo '<font color="green">' . $passfeedback . '</font>';
+                    echo '<span style="color:green">' . $passfeedback . '</span>';
                 // otherwise, display fail feedback
                 else
-                    echo '<font color="red">' . $failfeedback . '</font>'; 
+                    echo '<span style="color:red">' . $failfeedback . '</span>'; 
             ?>
         </h3>
     </div>
@@ -156,13 +152,10 @@ while ($row = mysql_fetch_assoc($result)) {
     <?php endif; ?>
 
     <?php
-    // reset the result cursor to beginning
-    mysql_data_seek ($result, 0);
-    
-    while ($row = mysql_fetch_assoc($result)) {
-        $sql        = "SELECT * FROM ".TABLE_PREFIX."tests_answers WHERE result_id=$rid AND question_id=$row[question_id] AND member_id=$_SESSION[member_id]";
-        $result_a    = mysql_query($sql, $db); 
-        $answer_row = mysql_fetch_assoc($result_a);
+    foreach($rows_questions as $row){
+
+        $sql        = "SELECT * FROM %stests_answers WHERE result_id=%d AND question_id=%d AND member_id=%d";
+        $answer_row    = queryDB($sql, array(TABLE_PREFIX, $rid, $row['question_id'], $_SESSION['member_id']), TRUE); 
 
         $obj = TestQuestions::getQuestion($row['type']);
         $obj->displayResult($row, $answer_row);
