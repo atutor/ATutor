c87ae71f24b37f165a82a3851a4c074236bcb9b3
atutor <info@atutor.ca>
2013-11-09 13:46:01 -0500

    replace mysql with queryDB()

--- mods/_standard/tests/module_groups.php
+++ mods/_standard/tests/module_groups.php
@@ -6,10 +6,8 @@ function tests_create_group($group_id) { }
 
 // delete group
 function tests_delete_group($group_id) {
-	global $db;
-
-	$sql	= "DELETE FROM ".TABLE_PREFIX."tests_groups WHERE group_id=$row[group_id]";
-	$result2 = mysql_query($sql, $db);
+	$sql	= "DELETE FROM %stests_groups WHERE group_id=%d";
+	$result2 = queryDB($sql, array(TABLE_PREFIX, $row['group_id']));
 }
 
 ?>
