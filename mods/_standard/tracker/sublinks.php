9562c66050f1117c9b69ca9fb7fc4570e32164dd
atutor <info@atutor.ca>
2013-11-13 18:14:09 -0500

    replace mysql with queryDB()

--- mods/_standard/tracker/sublinks.php
+++ mods/_standard/tracker/sublinks.php
@@ -2,15 +2,15 @@
 
 if (!defined('AT_INCLUDE_PATH')) { exit; }
 
-global $db, $contentManager;
+global $contentManager;
 
 $record_limit = 3;		// number of sublinks to display at module home "detail view"
 
-$sql = "SELECT content_id FROM ".TABLE_PREFIX."member_track WHERE course_id=$_SESSION[course_id] AND member_id=$_SESSION[member_id] ORDER BY last_accessed DESC limit ".$record_limit;
-$result = mysql_query($sql, $db);
+$sql = "SELECT content_id FROM %smember_track WHERE course_id=%d AND member_id=%d ORDER BY last_accessed DESC limit %d";
+$rows_tracks = queryDB($sql, array(TABLE_PREFIX, $_SESSION['course_id'], $_SESSION['member_id'], $record_limit));
 
-if (mysql_num_rows($result) > 0) {
-	while ($row = mysql_fetch_assoc($result)) {
+if(count($rows_tracks) > 0){
+	foreach($rows_tracks as $row){
 		$link_title = $contentManager->_menu_info[$row['content_id']]['title'];
 		
 		$list[] = '<a href="'.url_rewrite('content.php?cid='.$row['content_id'], AT_PRETTY_URL_IS_HEADER).'"'.
