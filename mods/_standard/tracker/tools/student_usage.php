5ebf0c055d67fa45f3dcde586998e8abf6fb7e58
atutor <info@atutor.ca>
2013-11-13 19:17:47 -0500

    replace mysql with queryDB()

--- mods/_standard/tracker/tools/student_usage.php
+++ mods/_standard/tracker/tools/student_usage.php
@@ -19,15 +19,16 @@ authenticate(AT_PRIV_CONTENT);
 require(AT_INCLUDE_PATH.'header.inc.php');
 
 $sql	= "SELECT M.member_id, M.login, CONCAT(M.first_name, ' ', M.second_name, ' ', M.last_name) AS full_name
-			FROM ".TABLE_PREFIX."members M, ".TABLE_PREFIX."course_enrollment C 
-			WHERE M.member_id=C.member_id AND C.course_id=$_SESSION[course_id]";
-$result = mysql_query($sql, $db);
+			FROM %smembers M, %scourse_enrollment C 
+			WHERE M.member_id=C.member_id AND C.course_id=%d";
+$rows_members = queryDB($sql, array(TABLE_PREFIX, TABLE_PREFIX, $_SESSION['course_id']));
 
 $_GET['id'] = intval($_GET['id']);
 
-$sql = "SELECT counter, content_id, SEC_TO_TIME(duration) AS total FROM ".TABLE_PREFIX."member_track WHERE member_id=$_GET[id] AND course_id=$_SESSION[course_id] ORDER BY counter DESC";
-$result_list = mysql_query($sql, $db);
-$savant->assign('result_list', $result_list);
-$savant->assign('result', $result);
+$sql = "SELECT counter, content_id, SEC_TO_TIME(duration) AS total FROM %smember_track WHERE member_id=%d AND course_id=%d ORDER BY counter DESC";
+$rows_list = queryDB($sql, array(TABLE_PREFIX, $_GET['id'], $_SESSION['course_id']));
+
+$savant->assign('rows_list', $rows_list);
+$savant->assign('rows_members', $rows_members);
 $savant->display('instructor/content/tracker/student_usage.tmpl.php');
 require(AT_INCLUDE_PATH.'footer.inc.php'); ?>
--- themes/default/instructor/content/tracker/student_usage.tmpl.php
+++ themes/default/instructor/content/tracker/student_usage.tmpl.php
@@ -5,7 +5,7 @@
 		<label for="id"><?php echo _AT('select_member'); ?></label><br />
 		<select name="id" id="id">
 			<?php
-				while ($row = mysql_fetch_assoc($this->result)) {
+			    foreach($this->rows_members as $row){
 					$sender = get_display_name($row['member_id']);
 					echo '<option value="'.$row['member_id'].'"';
 					if ($row['member_id'] == $_GET['id']) {
@@ -34,14 +34,19 @@
 	</tr>
 	</thead>
 	<tbody>
-	<?php if ($row = mysql_fetch_assoc($this->result_list)): ?>
-		<?php do { ?>
+	<?php 
+	if(count($this->rows_list) > 0):
+	//if ($row = mysql_fetch_assoc($this->result_list)): 
+	?>
+		<?php 
+		foreach($this->rows_list as $row){
+		?>
 			<tr>
 				<td><?php echo $contentManager->_menu_info[$row['content_id']]['title']; ?></td>
 				<td><?php echo $row['counter']; ?></td>
 				<td><?php echo $row['total']; ?></td>
 			</tr>
-		<?php } while ($row = mysql_fetch_assoc($this->result_list)); ?>
+		<?php } ?>
 	<?php else: ?>
 		<tr>
 			<td colspan="3"><?php echo _AT('none_found'); ?></td>
