bfa96ba2a1dbf4c44fc551c2bebe843ca45ff8f0
atutor <info@atutor.ca>
2013-12-03 19:12:22 -0500

    remove old comments

--- mods/_standard/forums/module_news.php
+++ mods/_standard/forums/module_news.php
@@ -24,10 +24,9 @@ function forums_news() {
 		return $news;
 	} 
 
-	//$sql = 'SELECT E.approved, E.last_cid, C.* FROM '.TABLE_PREFIX.'course_enrollment E, '.TABLE_PREFIX.'courses C WHERE C.course_id in '. $enrolled_courses . '  AND E.member_id=1 AND E.course_id=C.course_id ORDER BY C.title';
-	//$result = mysql_query($sql, $db);
 	$sql = 'SELECT E.approved, E.last_cid, C.* FROM '.TABLE_PREFIX.'course_enrollment E, '.TABLE_PREFIX.'courses C WHERE C.course_id in '. $enrolled_courses . '  AND E.member_id='.$_SESSION['member_id'].' AND E.course_id=C.course_id ORDER BY C.title';
 	$rows_en_courses = queryDB($sql, array());
+	
     if(count($rows_en_courses) > 0){
 	    foreach($rows_en_courses as $row){
 			$all_forums = get_forums($row['course_id']);
