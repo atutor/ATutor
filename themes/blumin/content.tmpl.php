9a52573beb146e26d82506478358421cdd577a77
atutor <info@atutor.ca>
2014-01-14 20:03:11 -0500

    update blumin theme for ATutor 2.2

--- themes/blumin/content.tmpl.php
+++ themes/blumin/content.tmpl.php
@@ -25,15 +25,6 @@ if (!defined('AT_INCLUDE_PATH')) { exit; }
 ?>
 </div>
 
-<?php if ($this->shortcuts): ?>
-<fieldset id="shortcuts"><legend><?php echo _AT('shortcuts'); ?></legend>
-	<ul>
-		<?php foreach ($this->shortcuts as $link): ?>
-			<li><a href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a></li>
-		<?php endforeach; ?>
-	</ul>
-</fieldset>
-<?php endif; ?>
 
 <?php 
 if ($_SESSION["prefs"]["PREF_SHOW_CONTENTS"] && $this->content_table <> "") 
