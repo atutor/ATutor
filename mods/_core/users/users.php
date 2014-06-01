4aec53d7feaa3bac5feee860070b65292fe25df7
ATutorSpace Root User <jamon@atutorspaces.com>
2014-06-01 12:12:07 -0400

    replaced % with %% in user query for compatibility with queryDB()

--- mods/_core/users/users.php
+++ mods/_core/users/users.php
@@ -111,7 +111,7 @@ if ($_GET['search']) {
 		$term = trim($term);
 		$term = str_replace(array('%','_'), array('\%', '\_'), $term);
 		if ($term) {
-			$term = '%'.$term.'%';
+			$term = '%%'.$term.'%%';
 			$sql .= "((M.first_name LIKE '$term') OR (M.second_name LIKE '$term') OR (M.last_name LIKE '$term') OR (M.email LIKE '$term') OR (M.login LIKE '$term')) $predicate";
 		}
 	}
@@ -134,7 +134,7 @@ if ($_GET['searchid']) {
 		$term = str_replace(array('%','_'), array('\%', '\_'), $term);
 		if ($term) {
 			if (strpos($term, '-') === FALSE) {
-				$term = '%'.$term.'%';
+				$term = '%%'.$term.'%%';
 				$sql .= "(L.public_field LIKE '$term') OR ";
 			} else {
 				// range search
