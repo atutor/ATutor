ef60062604231b766110b7ee92ab84884ba7499e
atutor <info@atutor.ca>
2015-04-02 18:55:16 -0400

    5576 removed html comments from around the MyFile option in File Storage worker select menu.

--- mods/_standard/file_storage/index.php
+++ mods/_standard/file_storage/index.php
@@ -580,7 +580,7 @@ if ($_SESSION['member_id'] && $_SESSION['enroll']){
 		<select name="ot" id="ot">
 			<option value="1" <?php if ($owner_type == WORKSPACE_COURSE) { echo 'selected="selected"'; } ?>><?php echo _AT('course_files'); ?></option>
 			<?php if ($_SESSION['member_id'] && $_SESSION['enroll']): ?>
-				<!-- <option value="2" <?php if ($owner_type == WORKSPACE_PERSONAL) { echo 'selected="selected"'; } ?>><?php echo _AT('my_files'); ?></option> -->
+				<option value="2" <?php if ($owner_type == WORKSPACE_PERSONAL) { echo 'selected="selected"'; } ?>><?php echo _AT('my_files'); ?></option>
 			<?php endif; ?>
 			<?php if (isset($file_storage_groups)): ?>
 				<optgroup label="<?php echo _AT('groups'); ?>">
