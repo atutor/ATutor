5e80d864af0451d734f533a4b8ad6c1c9f13f9f2
Greg Gay <gregrgay@gmail.com>
2016-01-06 14:59:45 -0500

    added update for 2.1 theme, to 2.2.1

--- include/install/db/atutor_upgrade_2.2_to_2.2.1.sql
+++ include/install/db/atutor_upgrade_2.2_to_2.2.1.sql
@@ -10,6 +10,7 @@ UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'ATutor 1.5' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'Mobile' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'ATutor 1.6' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'ATutor 2.0' LIMIT 1 ;
+UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'ATutor 2.1' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'IDI Theme' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'Simple' LIMIT 1 ;
 UPDATE `themes` SET `version` = '2.2.1' WHERE `title` = 'ATutorSpaces' LIMIT 1 ;
