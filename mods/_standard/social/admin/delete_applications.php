60010c4c408e63cf6f54805870e6dd01adf665eb
Greg Gay <gregrgay@gmail.com>
2016-06-16 18:01:11 -0400

    5691-29 added admin authenticate for AT_ADMIN_PRIV_SOCIAL

--- mods/_standard/social/admin/delete_applications.php
+++ mods/_standard/social/admin/delete_applications.php
@@ -15,6 +15,8 @@ $_user_location	= 'admin';
 
 define('AT_INCLUDE_PATH', '../../../../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
+admin_authenticate(AT_ADMIN_PRIV_SOCIAL);
+
 require(AT_SOCIAL_INCLUDE.'constants.inc.php');
 require(AT_SOCIAL_INCLUDE.'friends.inc.php');
 include(AT_SOCIAL_INCLUDE.'classes/Application.class.php');
