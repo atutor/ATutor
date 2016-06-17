88141119daec4c49ca60a057e865351bb3d46c5f
Greg Gay <gregrgay@gmail.com>
2016-06-17 17:56:29 -0400

    5683 added authenticate() to manage page.

--- tools/index.php
+++ tools/index.php
@@ -14,6 +14,7 @@
 
 define('AT_INCLUDE_PATH', '../include/');
 require(AT_INCLUDE_PATH.'vitals.inc.php');
+authenticate();
 tool_origin('off');
 require(AT_INCLUDE_PATH.'header.inc.php');
 $module_list = $moduleFactory->getModules(AT_MODULE_STATUS_ENABLED, 0, TRUE);
